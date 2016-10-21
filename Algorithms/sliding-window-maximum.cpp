c8d52b0c2a45f16dea9334b409b9b00f6da76306
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-10-21 16:02:30 -0500

    239. Sliding Window Maximum

--- Algorithms/sliding-window-maximum.cpp
+++ Algorithms/sliding-window-maximum.cpp
@@ -2,33 +2,61 @@
 // https://leetcode.com/problems/sliding-window-maximum/
 #include <iostream>
 #include <vector>
-#include <dequeue>
+#include <climits>
+#include <deque>
 #include <algorithm>
 using namespace std;
+// BEGIN: https://discuss.leetcode.com/topic/19055/java-o-n-solution-using-deque-with-explanation
 class Solution {
 public:
 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 		vector<int> result;
-		dequeue<int> dq;
+		deque<int> dq;
 		const int n = nums.size();
-		if (n == 0) return result;
-		if (n == 1) return result = nums;
-		if (n == 2) return result = {max(nums[0], nums[1])};
 		for (int i = 0; i < n; i++) {
-			if (dq.size() < 2) {
-				dq.push_back(nums[i]);
-				continue;
-			}
-			int maxVal = INT_MIN;
-			for (const auto &j : dq) maxVal = max(maxVal, dq.front());
+			while (!dq.empty() && i - dq.front() > k - 1) dq.pop_front();
+			while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+			dq.push_back(i);
+			if (i >= k - 1) result.push_back(nums[dq.front()]);
 		}
 		return result;
 	}
 };
+// END: https://discuss.leetcode.com/topic/19055/java-o-n-solution-using-deque-with-explanation
+// class Solution {
+// public:
+// 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+// 		vector<int> result;
+// 		const int n = nums.size();
+// 		if (n == 0) return result;
+// 		deque<int> dq;
+// 		int maxVal = INT_MIN;
+// 		for (int i = 0; i < n; i++) {
+// 			maxVal = INT_MIN;
+// 			if ((int)dq.size() + 1 < k) {
+// 				dq.push_back(nums[i]);
+// 				maxVal = max(maxVal, nums[i]);
+// 				continue;
+// 			}
+// 			dq.push_back(nums[i]);
+// 			for (const auto &j : dq) maxVal = max(maxVal, j);
+// 			result.push_back(maxVal);
+// 			dq.pop_front();
+// 		}
+// 		if (n < k) result.push_back(maxVal);
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
-	vector<int> nums = {1,3,-1,-3,5,3,6,7};
+	vector<int> nums;
+	for (const auto &i : solution.maxSlidingWindow(nums, 0)) cout << i << '\t';
+	cout << "\tPassed\n";
+	nums = {1,3,-1,-3,5,3,6,7};
 	for (const auto &i : solution.maxSlidingWindow(nums, 3)) cout << i << '\t';
+	cout << "\tPassed\n";
+	nums = {1,-1};
+	for (const auto &i : solution.maxSlidingWindow(nums, 1)) cout << i << '\t';
 	cout << "\tPassed\n";
 	cout << "\nPassed All\n";
 	return 0;
