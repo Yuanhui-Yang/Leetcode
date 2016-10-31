dcbb52e39138ea60f6958f1d4d09b61d4cf5737a
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-10-30 21:45:48 -0500

    441. Arranging Coins

---
+++ Algorithms/arranging-coins.cpp
@@ -0,0 +1,18 @@
+// 441. Arranging Coins
+// https://leetcode.com/contest/11/problems/arranging-coins/
+#include <iostream>
+#include <cmath>
+using namespace std;
+class Solution {
+public:
+	int arrangeCoins(int n) {
+		return (int)(sqrt(2.0 * n + 0.25) - 0.5);
+	}
+};
+int main(void) {
+	Solution solution;
+	cout << solution.arrangeCoins(5) << "\tPassed\n";
+	cout << solution.arrangeCoins(8) << "\tPassed\n";
+	cout << "\nPassed All\n";
+	return 0;
+}
