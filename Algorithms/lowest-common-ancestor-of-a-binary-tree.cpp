16332fd9e1e629d02ece1f89c6e8a9741f2cd2b8
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-11-30 03:15:35 -0600

    236. Lowest Common Ancestor of a Binary Tree

---
+++ Algorithms/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,40 @@
+// 236. Lowest Common Ancestor of a Binary Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+#include <iostream>
+#include <cassert>
+#include <queue>
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root || root == p || root == q) return root;
+		if (!p) return q;
+		if (!q) return p;
+		if (p == q) return p;
+		TreeNode *left = this->lowestCommonAncestor(root->left, p, q);
+		TreeNode *right = this->lowestCommonAncestor(root->right, p, q);
+		return left && right ? root : left ? left : right;
+	}
+};
+int main(void) {
+	Solution solution;
+	TreeNode *root = new TreeNode(3);
+	root->left = new TreeNode(5);
+	root->right = new TreeNode(1);
+	root->left->left = new TreeNode(6);
+	root->left->right = new TreeNode(2);
+	root->left->right->left = new TreeNode(7);
+	root->left->right->right = new TreeNode(4);
+	root->right->left = new TreeNode(0);
+	root->right->right = new TreeNode(8);
+	assert(root == solution.lowestCommonAncestor(root, root->left, root->right));
+	assert(root->left == solution.lowestCommonAncestor(root, root->left, root->left->right->right));
+	cout << "\nPassed All\n";
+	return 0;
+}
