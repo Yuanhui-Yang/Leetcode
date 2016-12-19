a6fde4a189bbc0862de50f7597fffb0ad3ec71ba
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-12-19 01:45:01 -0600

    324. Wiggle Sort II

--- Algorithms/wiggle-sort-ii.cpp
+++ Algorithms/wiggle-sort-ii.cpp
@@ -35,6 +35,47 @@ public:
 		nums = result;
 	}
 };
+
+// class Solution {
+// public:
+// 	void wiggleSort(vector<int>& nums) {
+// 		const int n = nums.size();
+// 		if (n <= 2) {
+// 			sort(begin(nums), end(nums));
+// 			return;
+// 		}
+// 		int median_index = partition(0, n - 1, n / 2, nums);
+// 		int median = nums[median_index];
+// 		vector<int> result(n, median);
+// 		for (int i = 0, j = 1; i < n; i++) {
+// 			if (j < n && nums[i] > result[j]) {
+// 				result[j] = nums[i];
+// 				j += 2;
+// 			}
+// 		}
+// 		for (int i = 0, j = (n & 1) ? (n - 1) : (n - 2); i < n; i++) {
+// 			if (j >= 0 && nums[i] < result[j]) {
+// 				result[j] = nums[i];
+// 				j -= 2;
+// 			}
+// 		}
+// 		nums = result;
+// 	}
+// private:
+// 	int partition(const int left, const int right, const int index, vector<int>& nums) {
+// 		int i = left;
+// 		for (int j = left, pivot = nums[right]; j < right; j++) {
+// 			if (nums[j] < pivot) {
+// 				swap(nums[i], nums[j]);
+// 				i++;
+// 			}
+// 		}
+// 		swap(nums[i], nums[right]);
+// 		if (i < index) return partition(i + 1, right, index, nums);
+// 		if (i > index) return partition(left, i - 1, index, nums);
+// 		return i;
+// 	}
+// };
 // END: http://en.cppreference.com/w/cpp/algorithm/nth_element
 // END: https://discuss.leetcode.com/topic/32920/o-n-time-o-1-space-solution-with-detail-explanations
 // END: https://discuss.leetcode.com/topic/32929/o-n-o-1-after-median-virtual-indexing
