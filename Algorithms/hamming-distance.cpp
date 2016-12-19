8fcbcac29c60c3f306830e3fd8be7dc3bca10c34
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-12-18 19:41:22 -0600

    461. Hamming Distance

--- Algorithms/hamming-distance.cpp
+++ Algorithms/hamming-distance.cpp
@@ -4,15 +4,24 @@
 #include <cassert>
 #include <bitset>
 using namespace std;
+
 class Solution {
 public:
 	int hammingDistance(int x, int y) {
-		bitset<32> a(x), b(y);
-		int result = 0;
-		for (int i = 0; i < 32; i++) result += a[i] != b[i];
-		return result;
+		bitset<32> result(x ^ y);
+		return result.count();
 	}
 };
+
+// class Solution {
+// public:
+// 	int hammingDistance(int x, int y) {
+// 		bitset<32> a(x), b(y);
+// 		int result = 0;
+// 		for (int i = 0; i < 32; i++) result += a[i] != b[i];
+// 		return result;
+// 	}
+// };
 int main(void) {
 	Solution solution;
 	assert(2 == solution.hammingDistance(1, 4));
