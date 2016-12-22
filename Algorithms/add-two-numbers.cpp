a847f9c378324c125a8ad40cbad972a56a5bdbaf
Yuanhui Yang <yuanhui.yang@outlook.com>
2016-12-21 23:25:16 -0600

    2. Add Two Numbers

--- Algorithms/add-two-numbers.cpp
+++ Algorithms/add-two-numbers.cpp
@@ -1,86 +1,92 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// 2. Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/
+#include <iostream>
+#include <cassert>
+#include <vector>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int l1_len = length(l1);
-        int l2_len = length(l2);
-        int ret_len = max(l1_len, l2_len);
-        ListNode* ret_head = initialize_list(ret_len);
-        plus_list(ret_head, l1, l2);
-        update_list(ret_head);
-        return ret_head;
-    }
-private:
-    int length(ListNode* l){
-        int len = 0;
-        ListNode* itr = l;
-        while(itr){
-            ++len;
-            itr = itr->next;
-        }
-        return len;
-    }
-private:
-    ListNode* initialize_list(int len){
-    	ListNode* head = NULL;
-        if(len < 1){
-            exit(0);
-        }
-    	else if(len >= 1){
-            int l = 0;
-            head = new ListNode(0);
-            ListNode* itr = head;
-            while(l < len - 1){
-                itr->next = new ListNode(0);
-                itr = itr->next;
-                ++l;
-            }
-        }
-    return head;
-    }
-private:
-    void plus_list(ListNode* ret_head, ListNode* l1, ListNode* l2){
-        ListNode* l1_itr = l1;
-        ListNode* l2_itr = l2;
-        ListNode* ret_head_itr = ret_head;
-        while(ret_head_itr){
-            if(l1_itr && l2_itr){
-                ret_head_itr->val = l1_itr->val + l2_itr->val;
-                l1_itr = l1_itr->next;
-                l2_itr = l2_itr->next;   
-            }
-            else if(l1_itr && !l2_itr){
-                ret_head_itr->val = l1_itr->val;
-                l1_itr = l1_itr->next;   
-            }
-            else if(!l1_itr && l2_itr){
-                ret_head_itr->val = l2_itr->val;
-                l2_itr = l2_itr->next;   
-            }
-            ret_head_itr = ret_head_itr->next;
-        }
-    }
-private:
-    void update_list(ListNode* ret_head){
-        ListNode* itr = ret_head;
-        while(itr){
-            if(itr->val >= 10 && itr->next){
-                itr->val -= 10;
-                itr->next->val += 1;   
-            }
-            else if(itr->val >= 10 && !itr->next){
-                itr->next = new ListNode(0);
-                itr->val -= 10;
-                itr->next->val += 1;                
-            }
-            itr = itr->next;
-        }
-    }
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode *result = NULL;
+		ListNode *current = NULL;
+		int carry = 0;
+		while (l1 || l2 || carry) {
+			if (l1 && l2) {
+				int val = carry + l1->val + l2->val;
+				carry = val / 10;
+				val %= 10;
+				ListNode *new_ListNode = new ListNode(val);
+				if (current == NULL) {
+					result = new_ListNode;
+					current = new_ListNode;
+					l1 = l1->next;
+					l2 = l2->next;
+					continue;
+				}
+				if (current != NULL) {
+					current->next = new_ListNode;
+					current = current->next;
+					l1 = l1->next;
+					l2 = l2->next;
+					continue;
+				}
+				l1 = l1->next;
+				l2 = l2->next;
+				continue;
+			}
+			if (!l1 && !l2 && carry) {
+				ListNode *new_ListNode = new ListNode(carry);
+				carry = 0;
+				current->next = new_ListNode;
+				return result;
+			}
+			if (l1 || l2) {
+				ListNode *l3 = l1 ? l1 : l2;
+				int val = carry + l3->val;
+				carry = val / 10;
+				val %= 10;
+				ListNode *new_ListNode = new ListNode(val);
+				if (current == NULL) {
+					result = new_ListNode;
+					current = new_ListNode;
+					l1 = l1 ? l1->next : l1;
+					l2 = l2 ? l2->next : l2;
+					continue;
+				}
+				if (current != NULL) {
+					current->next = new_ListNode;
+					current = current->next;
+					l1 = l1 ? l1->next : l1;
+					l2 = l2 ? l2->next : l2;
+					continue;
+				}
+				l1 = l1 ? l1->next : l1;
+				l2 = l2 ? l2->next : l2;
+				continue;
+			}
+		}
+		return result;
+	}
 };
+int main(void) {
+	Solution solution;
+	ListNode *l1 = NULL;
+	l1 = new ListNode(2);
+	l1->next = new ListNode(4);
+	l1->next->next = new ListNode(3);
+	ListNode *l2 = NULL;
+	l2 = new ListNode(5);
+	l2->next = new ListNode(6);
+	l2->next->next = new ListNode(4);
+	ListNode *l3 = solution.addTwoNumbers(l1, l2);
+	vector<int> result;
+	for (ListNode *i = l3; i; i = i->next) result.push_back(i->val);
+	assert(vector<int>({7, 0, 8}) == result);
+	cout << "\nPassed All\n";
+	return 0;
+}
