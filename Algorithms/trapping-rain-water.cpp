79e79b3df418c09665578e8483b349b600d1ecf3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-01-10 17:05:45 -0600

    42. Trapping Rain Water

--- Algorithms/trapping-rain-water.cpp
+++ Algorithms/trapping-rain-water.cpp
@@ -1,46 +1,88 @@
 // 42. Trapping Rain Water
 // https://leetcode.com/problems/trapping-rain-water/
-// https://leetcode.com/discuss/16171/sharing-my-simple-c-code-o-n-time-o-1-space
 #include <iostream>
+#include <cassert>
 #include <vector>
+#include <queue>
+#include <utility>
+#include <functional>
 using namespace std;
 class Solution {
 public:
-	int trap(const vector<int>& height) {
-		int result(0);
-		int left(0);
-		int right(height.size() - 1);
-		int maxleft(0);
-		int minright(0);
-		while(left <= right) {
-			if (height[left] <= height[right]) {
-				if (height[left] < maxleft) {
-					result += maxleft - height[left];
-					++left;
+	int trap(vector<int>& height) {
+		if (height.size() < 3) return 0;
+		priority_queue<pair<int, size_t>, vector<pair<int, size_t>>, greater<pair<int, size_t>>> min_heap;
+		size_t i = 0, j = height.size() - 1;
+		min_heap.push({height[i], i});
+		min_heap.push({height[j], j});
+		int result = 0;
+		while (i + 1 < j) {
+			pair<int, size_t> min_heap_top = min_heap.top();
+			min_heap.pop();
+			if (min_heap_top.second == i) {
+				i++;
+				if (min_heap_top.first >= height[i]) {
+					result += min_heap_top.first - height[i];
+					min_heap.push({min_heap_top.first, i});
+					continue;
 				}
-				else {
-					maxleft = height[left];
-					++left;
-				}
+				min_heap.push({height[i], i});
+				continue;
 			}
-			else {
-				if (height[right] < minright) {
-					result += minright - height[right];
-					--right;
-				}
-				else {
-					minright = height[right];
-					--right;
-				}
+			j--;
+			if (min_heap_top.first >= height[j]) {
+				result += min_heap_top.first - height[j];
+				min_heap.push({min_heap_top.first, j});
+				continue;
 			}
+			min_heap.push({height[j], j});
+			continue;			
 		}
 		return result;
 	}
 };
+// BEGIN: https://leetcode.com/discuss/16171/sharing-my-simple-c-code-o-n-time-o-1-space
+// class Solution {
+// public:
+// 	int trap(vector<int>& height) {
+// 		int result(0);
+// 		int left(0);
+// 		int right(height.size() - 1);
+// 		int maxleft(0);
+// 		int minright(0);
+// 		while(left <= right) {
+// 			if (height[left] <= height[right]) {
+// 				if (height[left] < maxleft) {
+// 					result += maxleft - height[left];
+// 					++left;
+// 				}
+// 				else {
+// 					maxleft = height[left];
+// 					++left;
+// 				}
+// 			}
+// 			else {
+// 				if (height[right] < minright) {
+// 					result += minright - height[right];
+// 					--right;
+// 				}
+// 				else {
+// 					minright = height[right];
+// 					--right;
+// 				}
+// 			}
+// 		}
+// 		return result;
+// 	}
+// };
+// END: https://leetcode.com/discuss/16171/sharing-my-simple-c-code-o-n-time-o-1-space
 int main(void) {
 	Solution solution;
-	vector<int> height({0,1,0,2,1,0,1,3,2,1,2,1});
-	cout << solution.trap(height);
+	vector<int> height;
+	int result = 0;
+	height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+	result = solution.trap(height);
+	assert(6 == result);
 	cout << "\nPassed\n";
 	return 0;
 }
