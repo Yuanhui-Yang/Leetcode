041f0b94f8ba2f4f17ff53726f872a6e56428aac
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-01-16 07:38:21 -0600

    411. Minimum Unique Word Abbreviation

---
+++ Algorithms/minimum-unique-word-abbreviation.cpp
@@ -0,0 +1,21 @@
+// 411. Minimum Unique Word Abbreviation
+// https://leetcode.com/problems/minimum-unique-word-abbreviation/
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	string minAbbreviation(string target, vector<string>& dictionary) {
+
+	}
+};
+int main(void) {
+	Solution solution;
+	string result;
+	result = solution.minAbbreviation();
+	assert( == result);
+	cout << "\nPassed All\n";
+	return 0;
+}
