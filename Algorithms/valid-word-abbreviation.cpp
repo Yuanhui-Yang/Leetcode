5da9cf333677e3e54f0e645b45b37b6ad79e75fe
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-01-19 22:45:20 -0600

    408. Valid Word Abbreviation

--- Algorithms/valid-word-abbreviation.cpp
+++ Algorithms/valid-word-abbreviation.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <string>
 using namespace std;
+
 class Solution {
 public:
 	bool validWordAbbreviation(string word, string abbr) {
@@ -11,36 +12,69 @@ public:
 			return false;
 		}
 		size_t i = 0, j = 0;
-		for (i = 0, j = 0; i < abbr.size(); i++) {
-			if (j >= word.size()) {
+		while (i < abbr.size() && j < word.size()) {
+			if (abbr[i] == '0') {
 				return false;
 			}
 			if (abbr[i] >= 'a' && abbr[i] <= 'z') {
 				if (abbr[i] == word[j]) {
+					i++;
 					j++;
 					continue;
 				}
-				if (abbr[i] != word[j]) {
-					return false;
-				}
-				continue;
+				return false;
 			}
-			if (abbr[i] >= '0' && abbr[i] <= '9') {
-				if (abbr[i] == '0') {
-					return false;
-				}
-				size_t k = i;
+			if (abbr[i] >= '1' && abbr[i] <= '9') {
+				size_t step = 0;
 				while (i < abbr.size() && abbr[i] >= '0' && abbr[i] <= '9') {
+					step = abbr[i] - '0' + 10 * step;
 					i++;
 				}
-				size_t step = stoul(abbr.substr(k, i--));
 				j += step;
-				continue;
 			}
 		}
-		return j == word.size();
+		return i == abbr.size() && j == word.size();
 	}
 };
+
+// class Solution {
+// public:
+// 	bool validWordAbbreviation(string word, string abbr) {
+// 		if (abbr.size() > word.size()) {
+// 			return false;
+// 		}
+// 		size_t i = 0, j = 0;
+// 		for (i = 0, j = 0; i < abbr.size(); i++) {
+// 			if (j >= word.size()) {
+// 				return false;
+// 			}
+// 			if (abbr[i] >= 'a' && abbr[i] <= 'z') {
+// 				if (abbr[i] == word[j]) {
+// 					j++;
+// 					continue;
+// 				}
+// 				if (abbr[i] != word[j]) {
+// 					return false;
+// 				}
+// 				continue;
+// 			}
+// 			if (abbr[i] >= '0' && abbr[i] <= '9') {
+// 				if (abbr[i] == '0') {
+// 					return false;
+// 				}
+// 				size_t k = i;
+// 				while (i < abbr.size() && abbr[i] >= '0' && abbr[i] <= '9') {
+// 					i++;
+// 				}
+// 				size_t step = stoul(abbr.substr(k, i--));
+// 				j += step;
+// 				continue;
+// 			}
+// 		}
+// 		return j == word.size();
+// 	}
+// };
+
 int main(void) {
 	Solution solution;
 	bool result;
