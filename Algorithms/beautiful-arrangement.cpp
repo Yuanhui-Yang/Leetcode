ee21de07d9d29fca94f9d66c6ac729f2de3f2876
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-02-19 12:24:38 -0600

    526. Beautiful Arrangement

--- Algorithms/beautiful-arrangement.cpp
+++ Algorithms/beautiful-arrangement.cpp
@@ -3,5 +3,29 @@
 #include <iostream>
 #include <cassert>
 #include <cctype>
-#include <>
 using namespace std;
+
+class Solution {
+public:
+	int countArrangement(int N) {
+		int result = 0;
+		for (int i = 1; i <= N; i++) {
+
+		}
+		return result;
+	}
+};
+int main(void) {
+	Solution solution;
+	int N = 0;
+	int result = 0;
+	int answer = 0;
+
+	N = 2;
+	result = solution.countArrangement(N);
+	answer = 2;
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
