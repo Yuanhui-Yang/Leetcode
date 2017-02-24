16e2944621fee44563d717265f0ed1617e8a8214
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-02-24 14:48:24 -0600

    517. Super Washing Machines

--- Algorithms/super-washing-machines.cpp
+++ Algorithms/super-washing-machines.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 class Solution {
 public:
@@ -11,6 +13,26 @@ public:
 	}
 };
 int main(void) {
+	Solution solution;
+	vector<int> machines;
+	int result = 0;
+	int answer = 0;
+
+	machines = {1, 0, 5};
+	answer = 3;
+	result = solution.findMinMoves(machines);
+	assert(answer == result);
+
+	machines = {0, 3, 0};
+	answer = 2;
+	result = solution.findMinMoves(machines);
+	assert(answer == result);
+
+	machines = {0, 2, 0};
+	answer = -1;
+	result = solution.findMinMoves(machines);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
