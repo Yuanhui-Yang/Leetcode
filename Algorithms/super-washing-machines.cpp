14f2898bdd839281b0aa1fcad2e34ad5629de978
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-02-24 14:18:57 -0600

    517. Super Washing Machines

---
+++ Algorithms/super-washing-machines.cpp
@@ -0,0 +1,16 @@
+// 517. Super Washing Machines
+// https://leetcode.com/problems/super-washing-machines/
+#include <iostream>
+#include <cassert>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int findMinMoves(vector<int>& machines) {
+
+	}
+};
+int main(void) {
+	cout << "\nPassed All\n";
+	return 0;
+}
