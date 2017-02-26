26ea9fedcd66becfe3a54c0b9997a5b059e11bc4
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-02-25 20:08:29 -0600

    316. Remove Duplicate Letters

--- Algorithms/remove-duplicate-letters.cpp
+++ Algorithms/remove-duplicate-letters.cpp
@@ -7,11 +7,50 @@
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 class Solution {
 public:
 	string removeDuplicateLetters(string s) {
-
+		if (s.empty()) {
+			return "";
+		}
+		map<char, set<size_t>> tree_map;
+		for (size_t i = 0; i < s.size(); i++) {
+			tree_map[s.at(i)].insert(i);
+		}
+		string result;
+		while (!tree_map.empty()) {
+			for (map<char, set<size_t>>::iterator it = begin(tree_map);
+				it != end(tree_map);
+				it++) {
+				if (validate(it, tree_map)) {
+					result.push_back(it->first);
+					tree_map.erase(it);
+					break;
+				}
+			}
+		}
+		return result;
+	}
+private:
+	bool validate(map<char, set<size_t>>::iterator it, map<char, set<size_t>>& tree_map) {
+			for (map<char, set<size_t>>::iterator jt = begin(tree_map);
+				jt != end(tree_map);
+				jt++) {
+				if (jt != it && jt->second.upper_bound(*begin(it->second)) == end(jt->second)) {
+					return false;
+				}
+			}
+			for (map<char, set<size_t>>::iterator jt = begin(tree_map);
+				jt != end(tree_map);
+				jt++) {
+				if (jt != it) {
+					jt->second.erase(begin(jt->second), jt->second.upper_bound(*begin(it->second)));
+				}
+			}
+			return true;
 	}
 };
 int main(void) {
@@ -20,12 +59,17 @@ int main(void) {
 	string result;
 	string answer;
 
-	result = "bcabc";
+	s = "bbcaac";
+	answer = "bac";
+	result = solution.removeDuplicateLetters(s);
+	assert(answer == result);
+
+	s = "bcabc";
 	answer = "abc";
 	result = solution.removeDuplicateLetters(s);
 	assert(answer == result);
 
-	result = "cbacdcbc";
+	s = "cbacdcbc";
 	answer = "acdb";
 	result = solution.removeDuplicateLetters(s);
 	assert(answer == result);
