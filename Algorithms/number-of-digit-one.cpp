4ab7959225e1abcc1b5a6e02ce04b69794814291
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-02-25 22:05:01 -0600

    233. Number of Digit One

---
+++ Algorithms/number-of-digit-one.cpp
@@ -0,0 +1,24 @@
+// 233. Number of Digit One
+// https://leetcode.com/problems/number-of-digit-one/
+#include <iostream>
+#include <cassert>
+using namespace std;
+class Solution {
+public:
+	int countDigitOne(int n) {
+
+	}
+};
+int main(void) {
+	Solution solution;
+	int n = 0;
+	int result = 0;
+	int answer = 0;
+
+	n = 13;
+	answer = 6;
+	
+
+	cout << "\nPassed All\n";
+	return 0;
+}
