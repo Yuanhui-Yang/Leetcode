c6fec409a00802c41fb2eb6b8915099944db8569
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-02-26 19:18:43 -0600

    432. All O`one Data Structure

---
+++ Algorithms/all-oone-data-structure.cpp
@@ -0,0 +1,47 @@
+// 432. All O`one Data Structure
+// https://leetcode.com/problems/all-oone-data-structure/
+#include <iostream>
+#include <cassert>
+using namespace std;
+class AllOne {
+public:
+	/** Initialize your data structure here. */
+	AllOne() {
+
+	}
+
+	/** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+	void inc(string key) {
+
+	}
+
+	/** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+	void dec(string key) {
+
+	}
+
+	/** Returns one of the keys with maximal value. */
+	string getMaxKey() {
+
+	}
+
+	/** Returns one of the keys with Minimal value. */
+	string getMinKey() {
+
+	}
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * string param_3 = obj.getMaxKey();
+ * string param_4 = obj.getMinKey();
+ */
+
+int main(void) {
+	
+	cout << "\nPassed All\n";
+	return 0;
+}
