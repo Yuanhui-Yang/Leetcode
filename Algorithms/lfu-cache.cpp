5bdae679d55a743f981055627fe66e149e917fd3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-09 17:07:28 -0600

    460. LFU Cache

--- Algorithms/lfu-cache.cpp
+++ Algorithms/lfu-cache.cpp
@@ -1,6 +1,6 @@
 // 460. LFU Cache
 // https://leetcode.com/problems/lfu-cache/
-// Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+// Design and implement a data structure for Least Frequently Used (LFU) cache [https://en.wikipedia.org/wiki/Least_frequently_used]. It should support the following operations: get and put.
 
 // get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
 // put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
