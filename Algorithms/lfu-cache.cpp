27c31d86cc0eb32ff251a586af28fb30a6a223f7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-09 17:05:42 -0600

    460. LFU Cache

---
+++ Algorithms/lfu-cache.cpp
@@ -0,0 +1,87 @@
+// 460. LFU Cache
+// https://leetcode.com/problems/lfu-cache/
+// Design and implement a data structure for Least Frequently Used (LFU) cache. It should support the following operations: get and put.
+
+// get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+// put(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it should invalidate the least frequently used item before inserting a new item. For the purpose of this problem, when there is a tie (i.e., two or more keys that have the same frequency), the least recently used key would be evicted.
+
+// Follow up:
+// Could you do both operations in O(1) time complexity?
+
+// Example:
+
+// LFUCache cache = new LFUCache( 2 /* capacity */ );
+
+// cache.put(1, 1);
+// cache.put(2, 2);
+// cache.get(1);       // returns 1
+// cache.put(3, 3);    // evicts key 2
+// cache.get(2);       // returns -1 (not found)
+// cache.get(3);       // returns 3.
+// cache.put(4, 4);    // evicts key 1.
+// cache.get(1);       // returns -1 (not found)
+// cache.get(3);       // returns 3
+// cache.get(4);       // returns 4
+
+#include <iostream> // std::cout; std::cin
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // memset
+#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::string::npos
+#include <list>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+class LFUCache {
+public:
+	LFUCache(int capacity) {
+
+	}
+
+	int get(int key) {
+
+	}
+
+	void put(int key, int value) {
+
+	}
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+int main(void) {
+	LFUCache cache(2 /* capacity */ );
+	cache.put(1, 1);
+	cache.put(2, 2);
+	cache.get(1);       // returns 1
+	cache.put(3, 3);    // evicts key 2
+	cache.get(2);       // returns -1 (not found)
+	cache.get(3);       // returns 3.
+	cache.put(4, 4);    // evicts key 1.
+	cache.get(1);       // returns -1 (not found)
+	cache.get(3);       // returns 3
+	cache.get(4);       // returns 4
+	cout << "\nPassed All\n";
+	return 0;
+}
