52c70517b7e18757b5ac94fe17f40cbe65033cdb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-09 08:38:00 -0600

    531. Lonely Pixel I

---
+++ Algorithms/lonely-pixel-i.cpp
@@ -0,0 +1,31 @@
+// 531. Lonely Pixel I
+// https://leetcode.com/problems/lonely-pixel-i/
+#include <iostream>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+using namespace std;
+class Solution {
+public:
+	int findLonelyPixel(vector<vector<char>>& picture) {
+
+	}
+};
+int main(void) {
+	
+	cout << "\nPassed All\n";
+	return 0;
+}
