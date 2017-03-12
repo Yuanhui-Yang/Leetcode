ad815dedcacfff088967781cb38bd0ff8b7d0cbf
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-12 18:33:53 -0500

    527. Word Abbreviation

--- Algorithms/word-abbreviation.cpp
+++ Algorithms/word-abbreviation.cpp
@@ -37,6 +37,7 @@
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
+#include <utility> // pair; make_pair; swap
 #include <iterator>
 #include <functional> // std::less<int>; std::greater<int>
 using namespace std;
@@ -44,7 +45,59 @@ using namespace std;
 class Solution {
 public:
 	vector<string> wordsAbbreviation(vector<string>& dict) {
-
+		if (dict.empty()) {
+			return {};
+		}
+		unordered_map<string, set<pair<string, size_t>>> h;
+		const size_t n = dict.size();
+		vector<string> result(n);
+		for (size_t i = 0; i < n; i++) {
+			string s = dict.at(i);
+			if (s.size() <= 3) {
+				result.at(i) = s;
+				continue;
+			}
+			string k = s.front() + to_string(s.size() - 2) + s.back();
+			h[k].insert(make_pair(s, i));
+		}
+		for (const auto &i : h) {
+			const string &k = i.first;
+			const set<pair<string, size_t>> &v = i.second;
+			if (v.size() == 1) {
+				result.at(begin(v)->second) = k;
+				continue;
+			}
+			const size_t n = begin(v)->first.size();
+			const size_t j = abbrPrefix(v);
+			for (auto &l : v) {
+				const string s1 = l.first;
+				const string s2 = l.first.substr(0, j + 1) + to_string(n - j - 2) + l.first.back();
+				result.at(l.second) = s2.size() < s1.size() ? s2 : s1;
+			}
+		}
+		return result;
+	}
+private:
+	size_t abbrPrefix(const set<pair<string, size_t>> &v) {
+		size_t result = 0;
+		const size_t n = begin(v)->first.size();
+		string p;
+		for (const auto &i : v) {
+			if (p.empty()) {
+				p = i.first;
+				continue;
+			}
+			size_t j = 0;
+			while (j < n and p.at(j) == i.first.at(j)) {
+				j++;
+			}
+			result = max(result, j);
+			if (result + 2 >= n) {
+				return n;
+			}
+			p = i.first;
+		}
+		return result;
 	}
 };
 
@@ -52,7 +105,19 @@ int main(void) {
 	Solution solution;
 	vector<string> dict, result, answer;
 
-	dict = 
+	dict = {"abcdefg", "abccefg", "abcckkg"};
+	answer = {"abcd2g", "abccefg", "abcckkg"};
+	result = solution.wordsAbbreviation(dict);
+	for (const auto &x : result) {
+		cout << x << '\t';
+	}
+	cout << '\n';
+	assert(answer == result);
+
+	dict = {"like", "god", "internal", "me", "internet", "interval", "intension", "face", "intrusion"};
+	answer = {"l2e", "god", "internal", "me", "i6t", "interval", "inte4n", "f2e", "intr4n"};
+	result = solution.wordsAbbreviation(dict);
+	assert(answer == result);
 
 	cout << "\nPassed All\n";
 	return 0;
