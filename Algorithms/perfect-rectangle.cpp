103e60d54870a30e5e3eadd5816b15d1f1326d62
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-13 01:54:17 -0500

    391. Perfect Rectangle

--- Algorithms/perfect-rectangle.cpp
+++ Algorithms/perfect-rectangle.cpp
@@ -1,2 +1,115 @@
 // 391. Perfect Rectangle
 // https://leetcode.com/problems/perfect-rectangle/
+
+// Given N axis-aligned rectangles where N > 0, determine if they all together form an exact cover of a rectangular region.
+
+// Each rectangle is represented as a bottom-left point and a top-right point. For example, a unit square is represented as [1,1,2,2]. (coordinate of bottom-left point is (1, 1) and top-right point is (2, 2)).
+
+
+// Example 1:
+
+// rectangles = [
+//   [1,1,3,3],
+//   [3,1,4,2],
+//   [3,2,4,4],
+//   [1,3,2,4],
+//   [2,3,3,4]
+// ]
+
+// Return true. All 5 rectangles together form an exact cover of a rectangular region.
+
+// Example 2:
+
+// rectangles = [
+//   [1,1,2,3],
+//   [1,3,2,4],
+//   [3,1,4,2],
+//   [3,2,4,4]
+// ]
+
+// Return false. Because there is a gap between the two rectangular regions.
+
+// Example 3:
+
+// rectangles = [
+//   [1,1,3,3],
+//   [3,1,4,2],
+//   [1,3,2,4],
+//   [3,2,4,4]
+// ]
+
+// Return false. Because there is a gap in the top center.
+
+// Example 4:
+
+// rectangles = [
+//   [1,1,3,3],
+//   [3,1,4,2],
+//   [1,3,2,4],
+//   [2,2,4,4]
+// ]
+
+// Return false. Because two of the rectangles overlap with each other.
+
+#include <iostream> // std::cout; std::cin
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // memset
+#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::string::npos
+#include <list>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // pair; make_pair; swap
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+class Solution {
+public:
+	bool isRectangleCover(vector<vector<int>>& rectangles) {
+		
+	}
+};
+
+int main(void) {
+	Solution solution;
+	vector<vector<int>> rectangles;
+	bool result, answer;
+
+	rectangles = {{1, 1, 3, 3}, {3, 1, 4, 2}, {3, 2, 4, 4}, {1, 3, 2, 4}, {2, 3, 3, 4}};
+	answer = true;
+	result = solution.isRectangleCover(rectangles);
+	assert(answer == result);
+
+	rectangles = {{1, 1, 2, 3}, {1, 3, 2, 4}, {3, 1, 4, 2}, {3, 2, 4, 4}};
+	answer = false;
+	result = solution.isRectangleCover(rectangles);
+	assert(answer == result);
+
+	rectangles = {{1, 1, 3, 3}, {3, 1, 4, 2}, {1, 3, 2, 4}, {3, 2, 4, 4}};
+	answer = false;
+	result = solution.isRectangleCover(rectangles);
+	assert(answer == result);
+
+	rectangles = {{1, 1, 3, 3}, {3, 1, 4, 2}, {1, 3, 2, 4}, {2, 2, 4, 4}};
+	answer = false;
+	result = solution.isRectangleCover(rectangles);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
