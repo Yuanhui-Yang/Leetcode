e4ded717883f9fe74397c360fce4b1d35d10f746
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-21 04:42:39 -0500

    420. Strong Password Checker

--- Algorithms/strong-password-checker.cpp
+++ Algorithms/strong-password-checker.cpp
@@ -1,14 +1,16 @@
 // 420. Strong Password Checker
 // https://leetcode.com/problems/strong-password-checker/
 
-// A password is considered strong if below conditions are all met:
+/*
+A password is considered strong if below conditions are all met:
 
-// It has at least 6 characters and at most 20 characters.
-// It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
-// It must NOT contain three repeating characters in a row ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
-// Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM change required to make s a strong password. If s is already strong, return 0.
+It has at least 6 characters and at most 20 characters.
+It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
+It must NOT contain three repeating characters in a row ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
+Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM change required to make s a strong password. If s is already strong, return 0.
 
-// Insertion, deletion or replace of any one character are all considered as one change.
+Insertion, deletion or replace of any one character are all considered as one change.
+*/
 
 #include <iostream> // std::cout; std::cin
 #include <cstdlib> // rand
@@ -17,22 +19,22 @@
 #include <cmath> // pow; sqrt; round; fabs; abs; log
 #include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
 #include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // memset
-#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
 #include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
 #include <numeric> // std::accumulate; std::iota
-#include <string> // std::string::npos
-#include <list>
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
 #include <bitset>
 #include <vector>
 #include <deque>
 #include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue>
-#include <set>
-#include <map>
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
 #include <unordered_set>
 #include <unordered_map>
-#include <utility> // pair; make_pair; swap
+#include <utility> // std::pair; std::make_pair
 #include <iterator>
 #include <functional> // std::less<int>; std::greater<int>
 using namespace std;
