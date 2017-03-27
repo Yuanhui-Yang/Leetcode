0b0815c5d61a433dbba95ab2b11c9f1b4d44914a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-27 07:37:00 -0500

    545. Boundary of Binary Tree

--- Algorithms/boundary-of-binary-tree.cpp
+++ Algorithms/boundary-of-binary-tree.cpp
@@ -80,16 +80,50 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+void garbageCollection(TreeNode* root) {
+	if (root) {
+		garbageCollection(root->left);
+		garbageCollection(root->right);
+		delete root;
+	}
+}
+
 class Solution {
 public:
 	vector<int> boundaryOfBinaryTree(TreeNode* root) {
-
+		return {};
 	}
 };
 
 int main(void) {
 	Solution solution;
-	
+	TreeNode *root = NULL;
+	vector<int> result, answer;
+
+	root = new TreeNode(1);
+	root->right = new TreeNode(2);
+	root->right->left = new TreeNode(3);
+	root->right->right = new TreeNode(4);
+	answer = {1, 3, 4, 2};
+	result = solution.boundaryOfBinaryTree(root);
+	garbageCollection(root);
+	assert(answer == result);
+
+	root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+	root->right->left = new TreeNode(6);
+	root->left->right->left = new TreeNode(7);
+	root->left->right->right = new TreeNode(8);
+	root->right->left->left = new TreeNode(9);
+	root->right->left->right = new TreeNode(10);
+	answer = {1, 2, 4, 7, 8, 9, 10, 6, 3};
+	result = solution.boundaryOfBinaryTree(root);
+	garbageCollection(root);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
