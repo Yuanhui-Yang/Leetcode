b14dbeba42ef61b75ed9a04f3ddfd7db3d6086eb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-28 10:52:26 -0500

    1. Two Sum

--- Algorithms/two-sum.cpp
+++ Algorithms/two-sum.cpp
@@ -1,36 +1,70 @@
 // 1. Two Sum
 // https://leetcode.com/problems/two-sum/
-#include <iostream>
-#include <cassert>
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
-#include <algorithm>
-#include <iterator>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
 #include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
-// BEGIN: https://leetcode.com/articles/two-sum/
+
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
-		const int nums_size = nums.size();
-		if (nums_size < 2) return {-1, -1};
-		unordered_map<int, int> hash_map;
-		for (int i = 0; i < nums_size; i++) {
-			if (hash_map.count(target - nums[i])) {
-				return {hash_map[target - nums[i]], i};
+		unordered_map<int, int> h;
+		for (int i = 0, n = nums.size(); i < n; i++) {
+			if (!h.empty() and h.count(target - nums.at(i))) {
+				return {h.at(target - nums.at(i)), i};
 			}
-			hash_map[nums[i]] = i;
+			h[nums.at(i)] = i;
 		}
-		return {-1, -1};
+		return {};
 	}
 };
-// END: https://leetcode.com/articles/two-sum/
+
 int main(void) {
 	Solution solution;
-	vector<int> nums;
+	vector<int> nums, result, answer;
+	int target = 0;
+
 	nums = {2, 7, 11, 15};
-	assert(vector<int>({0, 1}) == solution.twoSum(nums, 9));
-	nums = {3, 2, 4};
-	assert(vector<int>({1, 2}) == solution.twoSum(nums, 6));
+	target = 9;
+	answer = {0, 1};
+	result = solution.twoSum(nums, target);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
