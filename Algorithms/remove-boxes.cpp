2f699380a796271a2f31696ab8969f3b04c2fbbc
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-03-30 16:33:09 -0500

    546. Remove Boxes

--- Algorithms/remove-boxes.cpp
+++ Algorithms/remove-boxes.cpp
@@ -164,10 +164,10 @@ int main(void) {
 	vector<int> boxes;
 	int result = 0, answer = 0;
 
-	// boxes = {3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7};
-	// answer = 136;
-	// result = solution.removeBoxes(boxes);
-	// assert(answer == result);
+	boxes = {3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7};
+	answer = 136;
+	result = solution.removeBoxes(boxes);
+	assert(answer == result);
 
 	boxes = {1, 3, 2, 2, 2, 3, 4, 3, 1};
 	answer = 23;
