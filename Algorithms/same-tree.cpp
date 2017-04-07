7acd2c23eac67395855ead30b4702f45abc56192
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-07 15:55:48 -0400

    100. Same Tree

--- Algorithms/same-tree.cpp
+++ Algorithms/same-tree.cpp
@@ -41,19 +41,16 @@ struct TreeNode {
 class Solution {
 public:
 	bool isSameTree(TreeNode* p, TreeNode* q) {
-		if (!p and !q) {
-			return true;
+		if (!p and q) {
+			return false;
 		}
 		if (p and !q) {
 			return false;
 		}
-		if (!p and q) {
-			return false;
+		if (!p and !q) {
+			return true;
 		}
-		if (p->val != q->val) {
-			return false;
-		}
-		return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+		return p->val == q->val and isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
 	}
 };
 
