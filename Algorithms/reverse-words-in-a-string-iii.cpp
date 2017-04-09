71fe296111329cc6114143ea78acdc99525f0348
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-09 12:19:45 -0400

    557. Reverse Words in a String III

---
+++ Algorithms/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,67 @@
+// 557. Reverse Words in a String III
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+/*
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Note: In the string, each word is separated by single space and there will not be any extra space in the string.
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+class Solution {
+public:
+	string reverseWords(string s) {
+		for (int i = 0, n = s.size(); i < n; i++) {
+			if (s.at(i) != ' ') {
+				int j = i;
+				while (i < n and s.at(i) != ' ') {
+					i++;
+				}
+				reverse(next(begin(s), j), next(begin(s), i));
+			}
+		}
+		return s;
+	}
+};
+
+int main(void) {
+	Solution solution;
+	string s, result, answer;
+
+	s = "Let's take LeetCode contest";
+	answer = "s'teL ekat edoCteeL tsetnoc";
+	result = solution.reverseWords(s);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
