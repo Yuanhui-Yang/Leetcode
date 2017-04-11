c5ea440fa079732253901984403f8ca8f9141b7c
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-10 23:08:37 -0400

    7. Reverse Integer

---
+++ Algorithms/reverse-integer.cpp
@@ -0,0 +1,102 @@
+// 7. Reverse Integer
+// https://leetcode.com/problems/reverse-integer/
+
+/*
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+click to show spoilers.
+
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+Note:
+The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+class Solution {
+public:
+	int reverse(int x) {
+		if (x == 0) {
+			return 0;
+		}
+		if (x < 0) {
+			string s = to_string(-(long long)x), t = to_string(-(long long)INT_MIN);
+			std::reverse(begin(s), end(s));
+			if (s.size() == t.size() and s > t) {
+				return 0;
+			}
+			return -stoll(s);
+		}
+		string s = to_string(x), t = to_string(INT_MAX);
+		std::reverse(begin(s), end(s));
+		if (s.size() == t.size() and s > t) {
+			return 0;
+		}
+		return stoll(s);
+	}
+};
+
+int main(void) {
+	Solution solution;
+	int x, result, answer;
+
+	x = -2147483648;
+	answer = 0;
+	result = solution.reverse(x);
+	assert(answer == result);
+
+	x = -1563847412;
+	answer = 0;
+	result = solution.reverse(x);
+	assert(answer == result);
+
+	x = 123;
+	answer = 321;
+	result = solution.reverse(x);
+	assert(answer == result);
+
+	x = -123;
+	answer = -321;
+	result = solution.reverse(x);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
