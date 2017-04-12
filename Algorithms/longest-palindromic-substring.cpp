63760e66b52649b385d159d5a63a4cb710d722d6
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-11 22:44:20 -0400

    5. Longest Palindromic Substring

--- Algorithms/longest-palindromic-substring.cpp
+++ Algorithms/longest-palindromic-substring.cpp
@@ -1,34 +1,109 @@
 // 5. Longest Palindromic Substring
 // https://leetcode.com/problems/longest-palindromic-substring/
-// https://github.com/soulmachine/leetcode
-#include <iostream>
-#include <string>
-#include <algorithm>
+
+/*
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 class Solution {
 public:
-	string longestPalindrome(const string& s) {
-		const int n = s.size();
-		bool f[n][n];
-		fill_n(&f[0][0], n * n, false);
-		int maxLen = 1;
-		int start = 0;
-		for (int i = 0; i < n; ++i) {
-			f[i][i] = true;
-			for (int j = 0; j < i; ++j) {
-				f[j][i] = (s[j] == s[i]) && (i - j < 2 || f[j + 1][i - 1]);
-				if (f[j][i] && i - j + 1 > maxLen) {
-					start = j;
-					maxLen = i - j + 1;
-				}
-			}
+	string longestPalindrome(string s) {
+		if (s.empty()) {
+			return "";
 		}
-		return s.substr(start, maxLen);
+		this->s = s;
+		this->OPT = vector<vector<string>>(s.size(), vector<string>(s.size()));
+		return this->longestPalindrome(0, s.size() - 1);
 	}
+private:
+	string longestPalindrome(int i, int j) {
+		if (i > j) {
+			return "";
+		}
+		if (!OPT.at(i).at(j).empty()) {
+			return OPT.at(i).at(j);
+		}
+		if (i == j) {
+			return OPT[i][j] = s.substr(i, 1);
+		}
+		string x = longestPalindrome(i + 1, j - 1);
+		if (s.at(i) == s.at(j) and size_t(j - i) == 1 + x.size()) {
+			return OPT[i][j] = s.substr(i, j + 1 - i);
+		}
+		string y = longestPalindrome(i, j - 1);
+		string z = longestPalindrome(i + 1, j);
+		return OPT[i][j] = y.size() > z.size() ? y : z;
+	}
+	string s;
+	vector<vector<string>> OPT;
 };
-int main(int argc, char** argv) {
+
+int main(void) {
 	Solution solution;
-	solution.longestPalindrome("LeetCode");
-	cout << "Passed\n";
+	string s, result;
+	unordered_set<string> answer;
+
+	s = "lphbehiapswjudnbcossedgioawodnwdruaaxhbkwrxyzaxygmnjgwysafuqbmtzwdkihbwkrjefrsgjbrycembzzlwhxneiijgzidhngbmxwkhphoctpilgooitqbpjxhwrekiqupmlcvawaiposqttsdgzcsjqrmlgyvkkipfigttahljdhtksrozehkzgshekeaxezrswvtinyouomqybqsrtegwwqhqivgnyehpzrhgzckpnnpvajqevbzeksvbezoqygjtdouecnhpjibmsgmcqcgxwzlzztdneahixxhwwuehfsiqghgeunpxgvavqbqrelnvhnnyqnjqfysfltclzeoaletjfzskzvcdwhlbmwbdkxnyqappjzwlowslwcbbmcxoiqkjaqqwvkybimebapkorhfdzntodhpbhgmsspgkbetmgkqlolsltpulgsmyapgjeswazvhxedqsypejwuzlvegtusjcsoenrcmypexkjxyduohlvkhwbrtzjnarusbouwamazzejhnetfqbidalfomecehfmzqkhndpkxinzkpxvhwargbwvaeqbzdhxzmmeeozxxtzpylohvdwoqocvutcelgdsnmubyeeeufdaoznxpvdiwnkjliqtgcmvhilndcdelpnilszzerdcuokyhcxjuedjielvngarsgxkemvhlzuprywlypxeezaxoqfges";
+	answer = {"pnnp"};
+	result = solution.longestPalindrome(s);
+	assert(answer.count(result));
+
+	s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy";
+	answer = {"fklkf"};
+	result = solution.longestPalindrome(s);
+	assert(answer.count(result));
+
+	s = "babad";
+	answer = {"bab", "aba"};
+	result = solution.longestPalindrome(s);
+	assert(answer.count(result));
+
+	s = "cbbd";
+	answer = {"bb"};
+	result = solution.longestPalindrome(s);
+	assert(answer.count(result));
+
+	cout << "\nPassed All\n";
 	return 0;
 }
