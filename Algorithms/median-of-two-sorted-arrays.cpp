f67e4cfa03d03ecabee667bb4640cf723a1c2dca
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-22 23:18:02 -0400

    4. Median of Two Sorted Arrays

--- Algorithms/median-of-two-sorted-arrays.cpp
+++ Algorithms/median-of-two-sorted-arrays.cpp
@@ -46,22 +46,24 @@ The median is (2 + 3)/2 = 2.5
 #include <functional> // std::less<int>; std::greater<int>
 using namespace std;
 
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.empty() and nums2.empty()) {
-            return 0.0;
-        }
-        vector<int> result;
-        result.insert(end(result), begin(nums1), end(nums1));
-        result.insert(end(result), begin(nums2), end(nums2));
-        inplace_merge(begin(result), next(begin(result), nums1.size()), end(result));
-        if (result.size() % 2) {
-            return result.at(result.size() / 2);
-        }
-        return 0.5 * result.at(result.size() / 2 - 1) + 0.5 * result.at(result.size() / 2);
-    }
-};
+// BEGIN: Time Complexity O(n) Space Complexity O(n)
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         if (nums1.empty() and nums2.empty()) {
+//             return 0.0;
+//         }
+//         vector<int> result;
+//         result.insert(end(result), begin(nums1), end(nums1));
+//         result.insert(end(result), begin(nums2), end(nums2));
+//         inplace_merge(begin(result), next(begin(result), nums1.size()), end(result));
+//         if (result.size() % 2) {
+//             return result.at(result.size() / 2);
+//         }
+//         return 0.5 * result.at(result.size() / 2 - 1) + 0.5 * result.at(result.size() / 2);
+//     }
+// };
+// END: Time Complexity O(n) Space Complexity O(n)
 
 int main(void) {
     Solution solution;
