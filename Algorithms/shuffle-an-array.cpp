2f3014637cc48da308cf8e4d4dc2622657d422ef
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-25 00:21:30 -0500

    384. Shuffle an Array

--- Algorithms/shuffle-an-array.cpp
+++ Algorithms/shuffle-an-array.cpp
@@ -1,62 +1,77 @@
 // 384. Shuffle an Array
 // https://leetcode.com/problems/shuffle-an-array/
-// http://www.cplusplus.com/reference/algorithm/shuffle/
-// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-// https://discuss.leetcode.com/topic/54022/c-solution-with-fisher-yates-algorithm
-#include <iostream>
+
+/*
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <utility>
-#include <algorithm>
-#include <random>
-#include <chrono>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
 #include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 class Solution {
 public:
 	Solution(vector<int> nums) {
-		srand(time(NULL));
 		this->nums = nums;
 	}
 
 	/** Resets the array to its original configuration and return it. */
 	vector<int> reset() {
-		return this->nums;
+		return nums;
 	}
 
 	/** Returns a random shuffling of the array. */
 	vector<int> shuffle() {
-		vector<int> result(this->nums);
-		for (int i = this->nums.size() - 1; i >= 1; --i) {
+		if (nums.size() < 2) {
+			return nums;
+		}
+		vector<int> result;
+		for (int i = 0, n = nums.size(); i < n; i++) {
 			int j = rand() % (i + 1);
-			swap(result[i], result[j]);
+			result.push_back(nums.at(i));
+			swap(result.at(j), result.back());
 		}
 		return result;
 	}
-	// vector<int> shuffle() {
-	// 	vector<int> result(this->nums);
-	// 	for (int i = this->nums.size() - 1; i >= 1; --i) {
-	// 		int j = rand() % (i + 1);
-	// 		swap(result[i], result[j]);
-	// 	}
-	// 	return result;
-	// }
-	// vector<int> shuffle() {
-	// 	vector<int> result(this->nums);
-	// 	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-	// 	std::shuffle(begin(result), end(result), default_random_engine(seed));
-	// 	return result;
-	// }
-	// vector<int> shuffle() {
-	// 	vector<int> result;
-	// 	for (size_t i = 0; i < this->nums.size(); ++i) {
-	// 		size_t j = rand() % (1 + result.size());
-	// 		result.push_back(this->nums[i]);
-	// 		swap(result[j], result.back());
-	// 	}
-	// 	return result;
-	// }
 private:
 	vector<int> nums;
 };
@@ -68,22 +83,15 @@ private:
  * vector<int> param_2 = obj.shuffle();
  */
 
- int main(void) {
-	// Init an array with set 1, 2, and 3.
-	vector<int> nums = {1, 2, 3};
+int main(void) {
+	vector<int> nums, result_reset, result_shuffle;
+
+	nums = {1, 2, 3};
 	Solution solution(nums);
+	result_reset = solution.reset();
+	assert(result_reset == nums);
+	result_shuffle = solution.shuffle();
 
-	// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
-	for (const auto &i : solution.shuffle()) cout << i << '\t';
-	cout << "\tPassed\n";
-
-	// Resets the array back to its original configuration [1,2,3].
-	for (const auto &i : solution.reset()) cout << i << '\t';
-	cout << "\tPassed\n";
-
-	// Returns the random shuffling of array [1,2,3].
-	for (const auto &i : solution.shuffle()) cout << i << '\t';
-	cout << "\tPassed\n";
-
- 	return 0;
- }
+	cout << "\nPassed All\n";
+	return 0;
+}
