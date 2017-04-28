9353dbdb5ab10fe2e757925a4605a156ddf62e5f
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-27 21:51:54 -0500

    535. Encode and Decode TinyURL

--- Algorithms/encode-and-decode-tinyurl.cpp
+++ Algorithms/encode-and-decode-tinyurl.cpp
@@ -1,33 +1,36 @@
 // 535. Encode and Decode TinyURL
 // https://leetcode.com/problems/encode-and-decode-tinyurl/
 
-// Note: This is a companion problem to the System Design problem: Design TinyURL.
-// TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+/*
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
 
-// Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+*/
 
 #include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
 #include <cstdlib> // rand
 #include <cassert> // assert
 #include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
 #include <cmath> // pow; sqrt; round; fabs; abs; log
 #include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
 #include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // memset
-#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
 #include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
 #include <numeric> // std::accumulate; std::iota
-#include <string> // std::string::npos
-#include <list>
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
 #include <bitset>
 #include <vector>
 #include <deque>
 #include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue>
-#include <set>
-#include <map>
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
 #include <unordered_set>
 #include <unordered_map>
+#include <utility> // std::pair; std::make_pair
 #include <iterator>
 #include <functional> // std::less<int>; std::greater<int>
 using namespace std;
@@ -37,61 +40,51 @@ public:
 
 	// Encodes a URL to a shortened URL.
 	string encode(string longUrl) {
-		if (h.empty() or !h.count(longUrl)) {
-			size_t n = v.size();
-			string s(6, '0');
-			for (int i = 5; i >= 0 and n > 0; i--, n /= 62) {
-				size_t x = n % 62;
-				if (x < 10) {
-					s.at(i) = '0' + x;
-					continue;
-				}
-				if (x < 36) {
-					x -= 10;
-					s.at(i) = 'a' + x;
-					continue;
-				}
-				if (x < 62) {
-					x -= 36;
-					s.at(i) = 'A' + x;
-					continue;
-				}
+		if (!h1.empty() and h1.count(longUrl)) {
+			const list<pair<string, size_t>>::iterator p = h1.at(longUrl);
+			string shortUrl(shortUrlSize, table.front());
+			for (size_t shortUrlKey = p->second, i = shortUrlSize - 1; shortUrlKey and i != string::npos; shortUrlKey /= table.size(), i--) {
+				const size_t j = shortUrlKey % table.size();
+				shortUrl.at(i) = table.at(j);
 			}
-			v.push_back(longUrl);
-			return h[longUrl] = s;
+			return shortUrl;
 		}
-		return h.at(longUrl);
+		string shortUrl(shortUrlSize, table.front());
+		size_t shortUrlKey = 0;
+		do {
+			shortUrlKey = 0;
+			for (size_t i = shortUrlSize - 1; i != string::npos; i--) {
+				const size_t j = rand() % table.size();
+				shortUrlKey = shortUrlKey * table.size() + j;
+				const char ch = table.at(j);
+				shortUrl.at(i) = ch;
+			}
+		} while (!h2.empty() and h2.count(shortUrlKey));
+		l.push_back(make_pair(longUrl, shortUrlKey));
+		h1[longUrl] = prev(end(l));
+		h2[shortUrlKey] = prev(end(l));
+		return shortUrl;
 	}
 
 	// Decodes a shortened URL to its original URL.
 	string decode(string shortUrl) {
-		if (shortUrl.size() != 6) {
+		size_t shortUrlKey = 0;
+		for (size_t i = shortUrlSize - 1; i != string::npos; i--) {
+			const char ch = shortUrl.at(i);
+			const size_t id = '0' <= ch and ch <= '9' ? ch - '0' : 'a' <= ch and ch <= 'z' ? 10 + ch - 'a' : 36 + ch - 'A';
+			shortUrlKey = shortUrlKey * table.size() + id;
+		}
+		if (h2.empty() or !h2.count(shortUrlKey)) {
 			return "";
 		}
-		size_t n = 0;
-		for (int i = 0; i < 6; i++) {
-			char c = shortUrl.at(i);
-			if ('0' <= c and c <= '9') {
-				n = 62 * n + (c - '0');
-				continue;
-			}
-			if ('a' <= c and c <= 'z') {
-				n = 62 * n + 10 + (c - 'a');
-				continue;
-			}
-			if ('A' <= c and c <= 'Z') {
-				n = 62 * n + 36 + (c - 'A');
-				continue;
-			}
-		}
-		if (n >= v.size()) {
-			return "";
-		}
-		return v.at(n);
+		return h2.at(shortUrlKey)->first;
 	}
 private:
-	unordered_map<string, string> h;
-	vector<string> v;
+	size_t shortUrlSize = 6;
+	string table = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	list<pair<string, size_t>> l;
+	unordered_map<string, list<pair<string, size_t>>::iterator> h1;
+	unordered_map<size_t, list<pair<string, size_t>>::iterator> h2;
 };
 
 // Your Solution object will be instantiated and called as such:
@@ -100,14 +93,12 @@ private:
 
 int main(void) {
 	Solution solution;
-	string url, shortUrl, longUrl;
+	string shortUrl, longUrl, result;
 
-	url = "https://leetcode.com/problems/design-tinyurl";
-	shortUrl = solution.encode(url);
-	longUrl = solution.decode(shortUrl);
-	// cout << "\nshortUrl = " << shortUrl << '\n';
-	// cout << "\nlongUrl = " << longUrl << '\n';
-	assert(url == longUrl);
+	longUrl = "https://leetcode.com/problems/design-tinyurl";
+	shortUrl = solution.encode(longUrl);
+	result = solution.decode(shortUrl);
+	assert(longUrl == result);
 
 	cout << "\nPassed All\n";
 	return 0;
