23a7726ff7e0ce725af4b5cdeb5f4d64771e3185
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-29 07:09:51 -0500

    144. Binary Tree Preorder Traversal

--- Algorithms/binary-tree-preorder-traversal.cpp
+++ Algorithms/binary-tree-preorder-traversal.cpp
@@ -1,11 +1,49 @@
 // 144. Binary Tree Preorder Traversal
 // https://leetcode.com/problems/binary-tree-preorder-traversal/
-#include <iostream>
-#include <cassert>
+
+/*
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
-#include <stack>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
 #include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 struct TreeNode {
 	int val;
 	TreeNode *left;
@@ -13,91 +51,90 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// BEGIN: http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
-class Solution {
-public:
-	vector<int> preorderTraversal(TreeNode* root) {
-		vector<int> result;
-		while (root) {
-			if (!root->left) {
-				result.push_back(root->val);
-				root = root->right;
-				continue;
-			}
-			TreeNode *predecessor = root->left;
-			while (predecessor->right && predecessor->right != root) predecessor = predecessor->right;
-			if (!predecessor->right) {
-				result.push_back(root->val);
-				predecessor->right = root;
-				root = root->left;
-				continue;
-			}
-			predecessor->right = NULL;
-			root = root->right;
-		}
-		return result;
+void gc(TreeNode*& root) {
+	if (root) {
+		gc(root->left);
+		gc(root->right);
+		delete root;
+		root = NULL;
 	}
-};
-// END: http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
+}
 
-// BEGIN: http://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/
+// BEGIN: Time Complexity O(n) Space Complexity O(1)
 // class Solution {
 // public:
 // 	vector<int> preorderTraversal(TreeNode* root) {
-// 		if (!root) return {};
 // 		vector<int> result;
-// 		vector<TreeNode*> stack;
-// 		stack.push_back(root);
-// 		while (!stack.empty()) {
-// 			TreeNode *stack_top = stack.back();
-// 			stack.pop_back();
-// 			result.push_back(stack_top->val);
-// 			if (stack_top->right) stack.push_back(stack_top->right);
-// 			if (stack_top->left) stack.push_back(stack_top->left);
+// 		while (root) {
+// 			result.push_back(root->val);
+// 			if (root->left) {
+				
+// 			}
 // 		}
 // 		return result;
 // 	}
 // };
-// END: http://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/
+// END: Time Complexity O(n) Space Complexity O(1)
 
+// BEGIN: https://discuss.leetcode.com/topic/30632/preorder-inorder-and-postorder-iteratively-summarization
+// BEGIN: Time Complexity O(n) Space Complexity O(n)
 // class Solution {
 // public:
 // 	vector<int> preorderTraversal(TreeNode* root) {
 // 		vector<int> result;
-// 		this->preorderTraversal(root, result);
+// 		list<TreeNode*> list;
+// 		while (!list.empty() or root) {
+// 			if (root) {
+// 				result.push_back(root->val);
+// 				list.push_back(root);
+// 				root = root->left;
+// 			}
+// 			else {
+// 				root = list.back()->right;
+// 				list.pop_back();
+// 			}
+// 		}
 // 		return result;
 // 	}
-// private:
-// 	void preorderTraversal(TreeNode* root, vector<int>& result) {
-// 		if (!root) return;
-// 		result.push_back(root->val);
-// 		this->preorderTraversal(root->left, result);
-// 		this->preorderTraversal(root->right, result);
-// 	}
 // };
+// END: Time Complexity O(n) Space Complexity O(n)
+// END: https://discuss.leetcode.com/topic/30632/preorder-inorder-and-postorder-iteratively-summarization
 
+// BEGIN: Time Complexity O(n) Space Complexity O(n)
 // class Solution {
 // public:
 // 	vector<int> preorderTraversal(TreeNode* root) {
-// 		if (!root) return {};
-// 		vector<int> result = {root->val};
-// 		vector<int> leftSubtree = this->preorderTraversal(root->left);
-// 		vector<int> rightSubtree = this->preorderTraversal(root->right);
-// 		result.insert(end(result), begin(leftSubtree), end(leftSubtree));
-// 		result.insert(end(result), begin(rightSubtree), end(rightSubtree));
+// 		vector<int> result;
+// 		if (root) {
+// 			vector<int> left = preorderTraversal(root->left), right = preorderTraversal(root->right);
+// 			result.push_back(root->val);
+// 			result.insert(end(result), begin(left), end(left));
+// 			result.insert(end(result), begin(right), end(right));
+// 		}
 // 		return result;
 // 	}
 // };
+// END: Time Complexity O(n) Space Complexity O(n)
 
 int main(void) {
 	Solution solution;
 	TreeNode *root = NULL;
+	vector<int> result, answer;
+
+	root = NULL;
+	result = solution.preorderTraversal(root);
+	gc(root);
+	answer = {};
+	assert(answer == result);
+
 	root = new TreeNode(1);
 	root->right = new TreeNode(2);
 	root->right->left = new TreeNode(3);
-	vector<int> result;
 	result = solution.preorderTraversal(root);
-	assert(vector<int>({1, 2, 3}) == result);
+	gc(root);
+	answer = {1, 2, 3};
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
