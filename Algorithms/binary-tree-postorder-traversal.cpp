cdb2af7d5527a3a93e8f35a0a812ca59332f37ff
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-04-29 07:10:44 -0500

    145. Binary Tree Postorder Traversal

--- Algorithms/binary-tree-postorder-traversal.cpp
+++ Algorithms/binary-tree-postorder-traversal.cpp
@@ -1,94 +1,59 @@
 // 145. Binary Tree Postorder Traversal
 // https://leetcode.com/problems/binary-tree-postorder-traversal/
-// http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
-#include <iostream>
+
+/*
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
-#include <stack>
-#include <algorithm>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
 #include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 struct TreeNode {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
 class Solution {
 public:
 	vector<int> postorderTraversal(TreeNode* root) {
-		vector<int> result;
-		TreeNode *dump = new TreeNode(0);
-		dump->left = root;
-		while (dump) {
-			if (!dump->left) {
-				dump = dump->right;
-				continue;
-			}
-			TreeNode *predecessor = dump->left;
-			while (predecessor->right && predecessor->right != dump) predecessor = predecessor->right;
-			if (!predecessor->right) {
-				predecessor->right = dump;
-				dump = dump->left;
-				continue;
-			}
-			this->reverseOut(dump->left, predecessor, result);
-			predecessor->right = NULL;
-			dump = dump->right;
-		}
-		return result;
-	}
-private:
-	void reverse(TreeNode* begin, TreeNode* end) {
-		TreeNode* i = begin->right;
-		while (begin != end) {
-			TreeNode *j = i->right;
-			i->right = begin;
-			begin = i;
-			i = j;
-		}
-	}
-	void reverseOut(TreeNode* begin, TreeNode* end, vector<int>& result) {
-		this->reverse(begin, end);
-		for (TreeNode *it = end; it != begin; it = it->right) result.push_back(it->val);
-		result.push_back(begin->val);
-		this->reverse(end, begin);
+
 	}
 };
-// class Solution {
-// public:
-// 	vector<int> postorderTraversal(TreeNode* root) {
-// 		vector<int> result;
-// 		this->postorderTraversal(root, result);
-// 		return result;
-// 	}
-// private:
-// 	void postorderTraversal(TreeNode* root, vector<int>& result) {
-// 		if (!root) return;
-// 		this->postorderTraversal(root->left, result);
-// 		this->postorderTraversal(root->right, result);
-// 		result.push_back(root->val);
-// 	}
-// };
-// class Solution {
-// public:
-// 	vector<int> postorderTraversal(TreeNode* root) {
-// 		if (!root) return {};
-// 		vector<int> leftSubtree =  this->postorderTraversal(root->left);
-// 		vector<int> rightSubtree =  this->postorderTraversal(root->right);
-// 		vector<int> result;
-// 		result.insert(end(result), begin(leftSubtree), end(leftSubtree));
-// 		result.insert(end(result), begin(rightSubtree), end(rightSubtree));
-// 		result.push_back(root->val);
-// 		return result;
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(1);
-	root->right = new TreeNode(2);
-	root->right->left =  new TreeNode(3);
-	for (const auto &i : solution.postorderTraversal(root)) cout << i << '\t';
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
