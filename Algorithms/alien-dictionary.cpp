434d41ecf82bcbc6f5a5819d4bdd1b97f9dc9ece
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-03 01:32:36 -0500

    269. Alien Dictionary

--- Algorithms/alien-dictionary.cpp
+++ Algorithms/alien-dictionary.cpp
@@ -1,61 +1,169 @@
 // 269. Alien Dictionary
 // https://leetcode.com/problems/alien-dictionary/
-#include <iostream>
-#include <string>
+
+/*
+There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+
+Example 1:
+Given the following words in dictionary,
+
+[
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+The correct order is: "wertf".
+
+Example 2:
+Given the following words in dictionary,
+
+[
+  "z",
+  "x"
+]
+The correct order is: "zx".
+
+Example 3:
+Given the following words in dictionary,
+
+[
+  "z",
+  "x",
+  "z"
+]
+The order is invalid, so return "".
+
+Note:
+You may assume all letters are in lowercase.
+You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+If the order is invalid, return an empty string.
+There may be multiple valid order of letters, return any one of them is fine.
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <iterator>
-#include <unordered_map>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
 #include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/28308/java-ac-solution-using-bfs
+
+// BEGIN: http://www.cnblogs.com/TenosDoIt/p/3644225.html
+// BEGIN: http://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
+// BEGIN: Time Complexity O(V + E) Space Complexity O(V + E)
 class Solution {
 public:
 	string alienOrder(vector<string>& words) {
-		string result;
-		unordered_map<char, unordered_set<char>> graph;
-		unordered_map<char, int> indegrees;
-		for (const auto &i : words) for (const auto &j : i) indegrees[j] = 0;
-		for (size_t i = 1; i < words.size(); i++) {
-			size_t j = 0, k = min(words[i - 1].size(), words[i].size());
-			while (j < k && words[i - 1][j] == words[i][j]) j++;
-			if (j == k) {
-				if (words[i - 1].size() > words[i].size()) return "";
+		if (words.empty()) {
+			return "";
+		}
+		unordered_map<char, unordered_set<char>> g;
+		unordered_set<char> s;
+		for (size_t i  = 0, j = 0, n = 0; i + 1 < words.size(); i++) {
+			string& si = words.at(i), &sj = words.at(i + 1);
+			s.insert(begin(si), end(si));
+			n = min(si.size(), sj.size());
+			j = 0;
+			while (j < n and si.at(j) == sj.at(j)) {
+				j++;
+			}
+			if (j == n) {
 				continue;
 			}
-			if (graph.count(words[i - 1][j]) && graph[words[i - 1][j]].count(words[i][j])) continue;
-			graph[words[i - 1][j]].insert(words[i][j]);
-			indegrees[words[i][j]]++;
+			if (!g.empty() and g.count(sj.at(j)) and g.at(sj.at(j)).count(si.at(j))) {
+				return "";
+			}
+			g[si.at(j)].insert(sj.at(j));
 		}
-		queue<char> zeros;
-		for (const auto &i : indegrees) if (i.second == 0) zeros.push(i.first);
-		while (!zeros.empty()) {
-			char front = zeros.front();
-			result.push_back(front);
-			zeros.pop();
-			for (const auto &i : graph[front]) if (--indegrees[i] == 0) zeros.push(i);
+		s.insert(begin(words.back()), end(words.back()));
+		int h[26];
+		memset(h, 0, sizeof(h));
+		for (const auto &i : g) {
+			for (const auto &j : i.second) {
+				int id = j - 'a';
+				h[id]++;
+			}
 		}
-		return indegrees.size() == result.size() ? result : "";
+		string result, current, next;
+		for (int i = 0; i < 26; i++) {
+			if (!h[i]) {
+				char c = i + 'a';
+				if (s.count(c)) {
+					current.push_back(c);
+				}
+			}
+		}
+		while (!current.empty()) {
+			next.clear();
+			for (const auto &i : current) {
+				result.push_back(i);
+				if (!g.empty() and g.count(i)) {
+					for (const auto &j : g.at(i)) {
+						int id = j - 'a';
+						if (--h[id] == 0) {
+							next.push_back(j);
+						}
+					}
+					g.erase(i);
+				}
+			}
+			current = next;
+		}
+		return g.empty() ? result : "";
 	}
 };
-// END: https://discuss.leetcode.com/topic/28308/java-ac-solution-using-bfs
+// END: Time Complexity O(V + E) Space Complexity O(V + E)
+// END: http://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
+// END: http://www.cnblogs.com/TenosDoIt/p/3644225.html
+
 int main(void) {
 	Solution solution;
-	vector<string> words = {"wrt","wrf","er","ett","rftt"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
-	words = {"z", "z"};
-	cout << solution.alienOrder(words) << "\tPassed\n";	
-	words = {"zy","zx"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
-	words = {"wrtkj","wrt"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
-	words = {"za","zb","ca","cb"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
-	words = {"ri","xz","qxf","jhsguaw","dztqrbwbm","dhdqfb","jdv","fcgfsilnb","ooby"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
-	words = {"bsusz","rhn","gfbrwec","kuw","qvpxbexnhx","gnp","laxutz","qzxccww"};
-	cout << solution.alienOrder(words) << "\tPassed\n";
+	vector<string> words;
+	string result;
+	unordered_set<string> answer;
+
+	words = {"bsusz", "rhn", "gfbrwec"," kuw", "qvpxbexnhx", "gnp", "laxutz", "qzxccww"};
+	answer = {""};
+	result = solution.alienOrder(words);
+	assert(answer.count(result));
+
+	words = {"wrt", "wrf", "er", "ett", "rftt"};
+	answer = {"wertf"};
+	result = solution.alienOrder(words);
+	assert(answer.count(result));
+
+	words = {"z", "x"};
+	answer = {"zx"};
+	result = solution.alienOrder(words);
+	assert(answer.count(result));
+
+	words = {"z", "x", "z"};
+	answer = {""};
+	result = solution.alienOrder(words);
+	assert(answer.count(result));
+
 	cout << "\nPassed All\n";
 	return 0;
 }
