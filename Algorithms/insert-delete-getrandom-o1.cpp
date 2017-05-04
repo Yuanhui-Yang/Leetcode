b35afe2c92708e3a77bd03372005afd6ef263b9b
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-03 19:41:13 -0500

    380. Insert Delete GetRandom O(1)

--- Algorithms/insert-delete-getrandom-o1.cpp
+++ Algorithms/insert-delete-getrandom-o1.cpp
@@ -1,43 +1,105 @@
 // 380. Insert Delete GetRandom O(1)
 // https://leetcode.com/problems/insert-delete-getrandom-o1/
-#include <iostream>
-#include <cstdlib>
-#include <unordered_map>
+
+/*
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+Example:
+
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
 #include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 class RandomizedSet {
 public:
 	/** Initialize your data structure here. */
 	RandomizedSet() {
-		this->hashmap.clear();
-		this->array.clear();
+		this->v.clear();
+		this->h.clear();
 	}
 
 	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
 	bool insert(int val) {
-		if (this->hashmap.count(val)) return false;
-		this->hashmap[val] = this->array.size();
-		this->array.push_back(val);
+		if (!h.empty() and h.count(val)) {
+			return false;
+		}
+		h[val] = v.size();
+		v.push_back(val);
 		return true;
 	}
 
 	/** Removes a value from the set. Returns true if the set contained the specified element. */
 	bool remove(int val) {
-		if (!this->hashmap.count(val)) return false;
-		this->array[this->hashmap[val]] = this->array.back();
-		this->hashmap[this->array.back()] = this->hashmap[val];
-		this->array.pop_back();
-		this->hashmap.erase(val);
+		if (h.empty() or !h.count(val)) {
+			return false;
+		}
+		h.at(v.back()) = h.at(val);
+		v.at(h.at(val)) = v.back();
+		h.erase(val);
+		v.pop_back();
 		return true;
 	}
 
 	/** Get a random element from the set. */
 	int getRandom() {
-		return this->array[rand() % this->array.size()];
+		size_t i = rand() % v.size();
+		return v.at(i);
 	}
 private:
-	unordered_map<int, int> hashmap;
-	vector<int> array;
+	vector<int> v;
+	unordered_map<int, size_t> h;
 };
 
 /**
@@ -48,15 +110,15 @@ private:
  * int param_3 = obj.getRandom();
  */
 
-int main(void) {
+ int main(void) {
 	RandomizedSet randomSet;
-	cout << boolalpha << randomSet.insert(1) <<  "\tPassed\n";
-	cout << boolalpha << randomSet.remove(2) <<  "\tPassed\n";
-	cout << boolalpha << randomSet.insert(2) <<  "\tPassed\n";
-	cout << boolalpha << randomSet.getRandom() <<  "\tPassed\n";
-	cout << boolalpha << randomSet.remove(1) <<  "\tPassed\n";
-	cout << boolalpha << randomSet.insert(2) <<  "\tPassed\n";
-	cout << boolalpha << randomSet.getRandom() <<  "\tPassed\n";
+	assert(true == randomSet.insert(1));
+	assert(false == randomSet.remove(2));
+	assert(true == randomSet.insert(2));
+	randomSet.getRandom();
+	assert(true == randomSet.remove(1));
+	assert(false == randomSet.insert(2));
+	randomSet.getRandom();
 	cout <<  "\nPassed All\n";
 	return 0;
 }
