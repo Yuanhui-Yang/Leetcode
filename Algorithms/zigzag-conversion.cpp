0533aa930eb83fb9ee45ebb38d749b35e242f9c2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-06 22:10:17 -0500

    6. ZigZag Conversion

---
+++ Algorithms/zigzag-conversion.cpp
@@ -0,0 +1,80 @@
+// 6. ZigZag Conversion
+// https://leetcode.com/problems/zigzag-conversion/
+
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+class Solution {
+public:
+	string convert(string s, int numRows) {
+		if (s.empty() or numRows <= 0) {
+			return "";
+		}
+		if (s.size() == 1) {
+			return s;
+		}
+		string result(s);
+		for (size_t i = 0, n = s.size(), j = 2 * numRows - 1, k = n % j; i < n; i++) {
+			size_t x = i / j, y = i % j;
+			size_t r = y < numRows ? y : (2 * numRows - y - 2);
+			size_t c = y < numRows ? 1 : 2;
+			if (r <= k) {
+				
+			}
+		}
+		return result;
+	}
+};
+
+int main(void) {
+	Solution solution;
+	string s, result, answer;
+	int numRows = 0;
+
+	s = "PAYPALISHIRING";
+	numRows = 3;
+	answer = "PAHNAPLSIIGYIR";
+	result = solution.convert(s, numRows);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
