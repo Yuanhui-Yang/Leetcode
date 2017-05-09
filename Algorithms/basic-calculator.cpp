633d24cfc5938763738576cc3d8b6375989c84c2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-09 05:29:16 -0500

    224. Basic Calculator

--- Algorithms/basic-calculator.cpp
+++ Algorithms/basic-calculator.cpp
@@ -14,3 +14,80 @@ Some examples:
 "(1+(4+5+2)-3)+(6+8)" = 23
 Note: Do not use the eval built-in library function.
 */
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
+using namespace std;
+
+/*
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+You may assume that the given expression is always valid.
+
+Some examples:
+"1 + 1" = 2
+" 2-1 + 2 " = 3
+"(1+(4+5+2)-3)+(6+8)" = 23
+Note: Do not use the eval built-in library function.
+*/
+
+// BEGIN: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+// BEGIN: http://www.geeksforgeeks.org/expression-evaluation/
+class Solution {
+public:
+	int calculate(string s) {
+		
+	}
+};
+// END: http://www.geeksforgeeks.org/expression-evaluation/
+// END: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+
+int main(void) {
+	Solution solution;
+	string s;
+	int result = 0, answer = 0;
+
+	s = "1 + 1";
+	answer = 2;
+	result = solution.calculate(s);
+	assert(answer == result);
+
+	s = " 2-1 + 2 ";
+	answer = 3;
+	result = solution.calculate(s);
+	assert(answer == result);
+
+	s = "(1+(4+5+2)-3)+(6+8)";
+	answer = 23;
+	result = solution.calculate(s);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
+	return 0;
+}
