4aa449e373d3df77325dc892da5713602ff48c54
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-10 05:06:54 -0500

    449. Serialize and Deserialize BST

--- Algorithms/serialize-and-deserialize-bst.cpp
+++ Algorithms/serialize-and-deserialize-bst.cpp
@@ -1,9 +1,44 @@
 // 449. Serialize and Deserialize BST
 // https://leetcode.com/problems/serialize-and-deserialize-bst/
-#include <iostream>
-#include <cassert>
-#include <string>
+
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
+
 struct TreeNode {
 	int val;
 	TreeNode *left;
@@ -16,52 +51,72 @@ public:
 
 	// Encodes a tree to a single string.
 	string serialize(TreeNode* root) {
-		return root ? to_string(root->val) + ',' + serialize(root->left) + ',' + serialize(root->right) : "#";
-
+		return !root ? "#" : to_string(root->val) + '?' + serialize(root->left) + ':' + serialize(root->right);
 	}
 
 	// Decodes your encoded data to tree.
 	TreeNode* deserialize(string data) {
-		int i = 0;
-		return deserialize(i, data);
-	}
-private:
-	TreeNode* deserialize(int& i, const string& data) {
-		const int data_size = data.size();
-		if (i >= data_size) return NULL;
-		if (data[i] == '#') {
-			i += 2;
+		if (data == "#") {
 			return NULL;
 		}
-		const int j = i;
-		while (i < data_size && data[i] != ',') i++;
-		TreeNode *root = new TreeNode(stoi(data.substr(j, i++ - j)));
-		root->left = deserialize(i, data);
-		root->right = deserialize(i, data);
+		size_t i = 0, n = data.size();
+		while (i < n and data.at(i) != '?') {
+			i++;
+		}
+		if (i == n) {
+			return new TreeNode(stoi(data));
+		}
+		size_t j = i + 1, k = 1;
+		while (j < n and k > 0) {
+			if (data.at(j) == '?') {
+				k++;
+			}
+			else if (data.at(j) == ':') {
+				k--;
+			}
+			j++;
+		}
+		TreeNode *root = new TreeNode(stoi(data.substr(0, i)));
+		root->left = deserialize(data.substr(i + 1, j - i - 2));
+		root->right = deserialize(data.substr(j));
 		return root;
 	}
 };
+
+void gc(TreeNode*& root) {
+	if (root) {
+		gc(root->left);
+		gc(root->right);
+		delete root;
+		root = NULL;
+	}
+}
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	if (!p or !q) {
+		return p == q;
+	}
+	return p->val == q->val and isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+}
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.deserialize(codec.serialize(root));
 
-bool helper(TreeNode* root, TreeNode* result) {
-	if (!root && !result) return true;
-	if (!root && result) return false;
-	if (root && !result) return false;
-	if (root->val != result->val) return false;
-	return helper(root->left, result->left) && helper(root->right, result->right);
-}
- 
 int main(void) {
 	Codec codec;
-	TreeNode *root = NULL;
-	root = new TreeNode(2);
-	root->left = new TreeNode(1);
+	TreeNode *root = NULL, *result = NULL;
+
+	root = new TreeNode(1);
+	root->left = new TreeNode(0);
 	root->right = new TreeNode(3);
-	TreeNode *result = codec.deserialize(codec.serialize(root));
-	assert(helper(root, result));
+	root->right->left = new TreeNode(2);
+	root->right->right = new TreeNode(5);
+	result = codec.deserialize(codec.serialize(root));
+	assert(isSameTree(root, result));
+	gc(root);
+	gc(result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
