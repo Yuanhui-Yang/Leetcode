24a7f9a3222d8e17727d5cc0c4d94a1c79b202fb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-10 03:58:33 -0500

    439. Ternary Expression Parser

--- Algorithms/ternary-expression-parser.cpp
+++ Algorithms/ternary-expression-parser.cpp
@@ -1,51 +1,121 @@
 // 439. Ternary Expression Parser
 // https://leetcode.com/problems/ternary-expression-parser/
-#include <iostream>
-#include <string>
+
+/*
+Given a string representing arbitrarily nested ternary expressions, calculate the result of the expression. You can always assume that the given expression is valid and only consists of digits 0-9, ?, :, T and F (T and F represent True and False respectively).
+
+Note:
+
+The length of the given string is ≤ 10000.
+Each number will contain only one digit.
+The conditional expressions group right-to-left (as usual in most languages).
+The condition will always be either T or F. That is, the condition will never be a digit.
+The result of the expression will always evaluate to either a digit 0-9, T or F.
+Example 1:
+
+Input: "T?2:3"
+
+Output: "2"
+
+Explanation: If true, then result is 2; otherwise result is 3.
+Example 2:
+
+Input: "F?1:T?4:5"
+
+Output: "4"
+
+Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+             "(F ? 1 : (T ? 4 : 5))"                   "(F ? 1 : (T ? 4 : 5))"
+          -> "(F ? 1 : 4)"                 or       -> "(T ? 4 : 5)"
+          -> "4"                                    -> "4"
+Example 3:
+
+Input: "T?T?F:5:3"
+
+Output: "F"
+
+Explanation: The conditional expressions group right-to-left. Using parenthesis, it is read/evaluated as:
+
+             "(T ? (T ? F : 5) : 3)"                   "(T ? (T ? F : 5) : 3)"
+          -> "(T ? F : 3)"                 or       -> "(T ? F : 5)"
+          -> "F"                                    -> "F"
+*/
+
+#include <iostream> // std::cout; std::cin
+#include <fstream> // std::fstream::open; std::fstream::close; 
+#include <cstdlib> // rand
+#include <cassert> // assert
+#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
+#include <cmath> // pow; sqrt; round; fabs; abs; log
+#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
+#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
+#include <cstring> // std::memset
+#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
+#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
+#include <numeric> // std::accumulate; std::iota
+#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
+#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <stack> // std::stack::top; std::stack::pop; std::stack::push
+#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
+#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
+#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
+#include <unordered_set>
+#include <unordered_map>
+#include <utility> // std::pair; std::make_pair
+#include <iterator>
+#include <functional> // std::less<int>; std::greater<int>
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/64418/5ms-c-fast-solution
+
 class Solution {
 public:
 	string parseTernary(string expression) {
-		const int n = expression.size();
-		int i = 0, j = 0, k = 1;
-		while (i < n && expression[i] != '?') i++;
-		if (i == n) return expression;
-		j = ++i;
-		while (j < n) {
-			if (expression[j] == '?') k++;
-			if (expression[j] == ':') k--;
-			if (k == 0) break; 
+		size_t n = expression.size(), i = 0;
+		while (i < n and expression.at(i) != '?') {
+			i++;
+		}
+		if (i == n) {
+			return expression;
+		}
+		size_t j = i + 1, k = 1;
+		while (j < n and k > 0) {
+			if (expression.at(j) == '?') {
+				k++;
+			}
+			else if (expression.at(j) == ':') {
+				k--;
+			}
 			j++;
 		}
-		return expression[0] == 'T' ? this->parseTernary(expression.substr(i, j - i)) : this->parseTernary(expression.substr(j + 1));
+		if (expression.front() == 'T') {
+			return parseTernary(expression.substr(2, j - 3));
+		}
+		return parseTernary(expression.substr(j));
 	}
 };
-// class Solution {
-// public:
-// 	string parseTernary(string expression) {
-// 		return this->parseTernary(expression, 0, (int)expression.size());
-// 	}
-// private:
-// 	string parseTernary(const string& expression, int i, int j) {
-// 		int x = i, y = 0, z = 1;
-// 		while (x < j && expression[x] != '?') x++;
-// 		if (x == j) return expression.substr(i, j - i);
-// 		y = ++x;
-// 		while (y < j && z) {
-// 			if (expression[y] == '?') z++;
-// 			if (expression[y] == ':') z--;
-// 			y++;
-// 		}
-// 		return expression[i] == 'T' ? this->parseTernary(expression, x, y - 1) : this->parseTernary(expression, y, j); 
-// 	}
-// };
-// END: https://discuss.leetcode.com/topic/64418/5ms-c-fast-solution
+
 int main(void) {
 	Solution solution;
-	cout << solution.parseTernary("T?2:3") << "\tPassed\n";
-	cout << solution.parseTernary("F?1:T?4:5") << "\tPassed\n";
-	cout << solution.parseTernary("T?T?F:5:3") << "\tPassed\n";
+	string expression, result, answer;
+
+	expression = "T?2:3";
+	answer = "2";
+	result = solution.parseTernary(expression);
+	assert(answer == result);
+
+	expression = "F?1:T?4:5";
+	answer = "4";
+	result = solution.parseTernary(expression);
+	assert(answer == result);
+
+	expression = "T?T?F:5:3";
+	answer = "F";
+	result = solution.parseTernary(expression);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
