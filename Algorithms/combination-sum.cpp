9589f7fae9c5da6b2a81a59b249ee5d6fd31d030
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-31 01:58:02 -0500

    39. Combination Sum

--- Algorithms/combination-sum.cpp
+++ Algorithms/combination-sum.cpp
@@ -1,61 +1,74 @@
 // 39. Combination Sum
 // https://leetcode.com/problems/combination-sum/
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
+
+/*
+Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	vector<vector<int>> combinationSum(vector<int>& candidates, const int& target) {
-		sort(begin(candidates), end(candidates));
+	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+		if (candidates.empty()) {
+			return {};
+		}
 		vector<vector<int>> result;
-		vector<int> path; 
-		this->combinationSum(result, 0, path, target, candidates);
+		vector<int> v;
+		dfs(result, candidates, v, target, 0, candidates.size());
 		return result;
 	}
 private:
-	void combinationSum(vector<vector<int>>& result, const int& start, vector<int>& path, const int& target, const vector<int>& candidates) {
-		if (target < 0)
-			return;
-		else if (target == 0) {
-			if (path.empty())
-				return;
-			else {
-				result.push_back(path);
-				return;
-			}
-		}
-		else {
-			for (int i = start; i < candidates.size(); i++) {
-				path.push_back(candidates[i]);
-				this->combinationSum(result, i, path, target - candidates[i], candidates);
-				path.pop_back();
-			}
+	void dfs(vector<vector<int>>& result, const vector<int>& candidates, vector<int>& v, int target, size_t i, const size_t n) {
+		if (target == 0) {
+			result.push_back(v);
 			return;
 		}
-	}
+		if (target < 0) {
+			return;
+		}
+		for (size_t j = i; j < n; j++) {
+			int val = candidates[j];
+			v.push_back(val);
+			dfs(result, candidates, v, target - val, j, n);
+			v.pop_back();
+		}
+	} 
 };
+
 int main(void) {
 	Solution solution;
+	int target;
+	vector<int> candidates;
+	vector<vector<int>> result, answer;
 
-	// vector<int> candidates = {2,3,6,7};
-	// int target = 7;
-
-	vector<int> candidates = {4, 5, 2};
-	int target = 16;	
-	cout << '[';
-	for (const auto& i : solution.combinationSum(candidates, target)) {
-		cout << '\n';
-		cout << '\t';
-		cout << '[';
-		cout << '\t';
-		for (const auto& j : i)
+	target = 7;
+	candidates = {2, 3, 6, 7};
+	answer = {{7}, {2, 2, 3}};
+	sort(begin(answer), end(answer));
+	result = solution.combinationSum(candidates, target);
+	sort(begin(result), end(result));
+	for (const auto &i : result) {
+		for (const auto &j : i) {
 			cout << j << '\t';
-		cout << ']';
+		}
 		cout << '\n';
 	}
-	cout << ']';
-	cout << "\nPassed\n";
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
