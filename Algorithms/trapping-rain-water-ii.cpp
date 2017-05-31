fa403188ca08d360d8226f0bef2a1aa68f053213
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-31 00:25:50 -0500

    407. Trapping Rain Water II

--- Algorithms/trapping-rain-water-ii.cpp
+++ Algorithms/trapping-rain-water-ii.cpp
@@ -1,68 +1,87 @@
 // 407. Trapping Rain Water II
 // https://leetcode.com/problems/trapping-rain-water-ii/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <utility>
-#include <queue>
-#include <functional>
+
+/*
+Given an m x n matrix of positive integers representing the height of each unit cell in a 2D elevation map, compute the volume of water it is able to trap after raining.
+
+Note:
+Both m and n are less than 110. The height of each unit cell is greater than 0 and is less than 20,000.
+
+Example:
+
+Given the following 3x6 height map:
+[
+  [1,4,3,1,3,2],
+  [3,2,1,3,2,4],
+  [2,3,3,2,3,1]
+]
+
+Return 4.
+
+The above image represents the elevation map [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]] before the rain.
+
+
+After the rain, water are trapped between the blocks. The total volume of water trapped is 4.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int trapRainWater(vector<vector<int>>& heightMap) {
-		if (heightMap.size() < 3 || heightMap.front().size() < 3) return 0;
-		const int x_size = heightMap.size(), y_size = heightMap.front().size();
-		priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> min_heap;
-		for (int i = 1; i + 1 < x_size; i++) {
-			min_heap.push({heightMap[i][0], {i, 0}});
-			min_heap.push({heightMap[i][y_size - 1], {i, y_size - 1}});
+		if (heightMap.size() < 3 or heightMap.front().size() < 3) {
+			return 0;
 		}
-		for (int j = 1; j + 1 < y_size; j++) {
-			min_heap.push({heightMap[0][j], {0, j}});
-			min_heap.push({heightMap[x_size - 1][j], {x_size - 1, j}});
+		const int m = heightMap.size(), n = heightMap.front().size();
+		vector<vector<bool>> visited(m, vector<bool>(n, false));
+		priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+		for (int i = 0; i < m; i++) {
+			pair<int, pair<int, int>> a = {heightMap[i].front(), {i, 0}}, b = {heightMap[i].back(), {i, n - 1}};
+			visited[i].front() = true;
+			visited[i].back() = true;
+			pq.push(a);
+			pq.push(b);
 		}
-		vector<vector<bool>> visited(x_size, vector<bool>(heightMap.front().size(), false));
-		for (int i = 0; i < x_size; i++) {
-			visited[i][0] = true;
-			visited[i][y_size - 1] = true;
-		}
-		for (int j = 0; j < y_size; j++) {
-			visited[0][j] = true;
-			visited[x_size - 1][j] = true;
+		for (int j = 0; j < n; j++) {
+			pair<int, pair<int, int>> a = {heightMap.front()[j], {0, j}}, b = {heightMap.back()[j], {m - 1, j}};
+			visited.front()[j] = true;
+			visited.back()[j] = true;
+			pq.push(a);
+			pq.push(b);
 		}
 		int result = 0;
-		const vector<int> dx = {-1, 0, 1, 0};
-		const vector<int> dy = {0, -1, 0, 1};
-		while (!min_heap.empty()) {
-			pair<int, pair<int, int>> min_heap_top = min_heap.top();
-			min_heap.pop();
-			const int x = min_heap_top.second.first;
-			const int y = min_heap_top.second.second;
+		const int dx[4] = {-1, 0, 0, 1}, dy[4] = {0, -1, 1, 0};
+		while (!pq.empty()) {
+			pair<int, pair<int, int>> t = pq.top();
+			pq.pop();
+			int x = t.second.first, y = t.second.second, z = t.first;
 			for (int i = 0; i < 4; i++) {
-				const int x_next = x + dx[i];
-				const int y_next = y + dy[i];
-				if (x_next >= 0 && x_next < x_size && y_next >= 0 && y_next < y_size && !visited[x_next][y_next]) {
-					visited[x_next][y_next] = true;
-					if (min_heap_top.first >= heightMap[x_next][y_next]) {
-						result += min_heap_top.first - heightMap[x_next][y_next];
-						min_heap.push({min_heap_top.first, {x_next, y_next}});
-						continue;
-					}
-					min_heap.push({heightMap[x_next][y_next], {x_next, y_next}});
-					continue;
+				int nx = x + dx[i], ny = y + dy[i];
+				if (nx >= 0 and nx < m and ny >= 0 and ny < n and !visited[nx][ny]) {
+					int nz = heightMap[nx][ny];
+					pair<int, pair<int, int>> c = {max(nz, z), {nx, ny}};
+					visited[nx][ny] = true;
+					result += max(0, z - nz);
+					pq.push(c);
 				}
 			}
 		}
 		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
+
 	vector<vector<int>> heightMap;
-	int result = 0;
+	int result, answer;
+
 	heightMap = {{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}};
+	answer = 4;
 	result = solution.trapRainWater(heightMap);
-	assert(4 == result);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
