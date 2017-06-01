a8fcb50fa7583c7b848744a06d8d649ab6d34471
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-05-31 23:57:38 -0500

    20. Valid Parentheses

--- Algorithms/valid-parentheses.cpp
+++ Algorithms/valid-parentheses.cpp
@@ -1,72 +1,57 @@
 // 20. Valid Parentheses
 // https://leetcode.com/problems/valid-parentheses/
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <vector>
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	bool isValid(string s) {
-		vector<char> stack;
-		for (size_t i = 0; i < s.size(); i++) {
-			const char c = s[i];
-			if (stack.empty()) {
-				if (c == '(' || c == '[' || c == '{') {
-					stack.push_back(c);
-					continue;
-				}
-				return false;
+		stack<char> stk;
+		for (const auto &i : s) {
+			switch (i) {
+				case '(':
+				case '[':
+				case '{': stk.push(i); break;
+				case ')': if (stk.empty() or stk.top() != '(') return false; stk.pop(); break;
+				case ']': if (stk.empty() or stk.top() != '[') return false; stk.pop(); break;
+				case '}': if (stk.empty() or stk.top() != '{') return false; stk.pop(); break;
 			}
-			const char stack_back = stack.back();
-			if (stack_back == '(' || stack_back == '[' || stack_back == '{') {
-				if (c == '(' || c == '[' || c == '{') {
-					stack.push_back(c);
-					continue;
-				}
-				if (stack_back == '(') {
-					if (c == ')') {
-						stack.pop_back();
-						continue;
-					}
-					return false;
-				}
-				if (stack_back == '[') {
-					if (c == ']') {
-						stack.pop_back();
-						continue;
-					}
-					return false;
-				}
-				if (stack_back == '{') {
-					if (c == '}') {
-						stack.pop_back();
-						continue;
-					}
-					return false;
-				}
-				return false;
-			}
-			return false;
 		}
-		return stack.empty();
+		return stk.empty();
 	}
 };
+
 int main(void) {
 	Solution solution;
-	bool result;
+	string s;
+	bool answer, result;
 
-	result = solution.isValid("()");
-	assert(true == result);
+	s = "()";
+	answer = true;
+	result = solution.isValid(s);
+	assert(answer == result);
 
-	result = solution.isValid("()[]{}");
-	assert(true == result);
+	s = "()[]{}";
+	answer = true;
+	result = solution.isValid(s);
+	assert(answer == result);
 
-	result = solution.isValid("(]");
-	assert(false == result);
+	s = "(]";
+	answer = false;
+	result = solution.isValid(s);
+	assert(answer == result);
 
-	result = solution.isValid("([)]");
-	assert(false == result);
+	s = "([)]";
+	answer = false;
+	result = solution.isValid(s);
+	assert(answer == result);
 
 	cout << "\nPassed All\n";
 	return 0;
