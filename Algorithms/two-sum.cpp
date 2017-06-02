68989d830e3da24c658627bfda71a2b068c329d3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-02 12:48:55 -0500

    1. Two Sum

--- Algorithms/two-sum.cpp
+++ Algorithms/two-sum.cpp
@@ -13,48 +13,22 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
-#include <iostream> // std::cout; std::cin
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
+#include <bits/stdc++.h>
 using namespace std;
 
-// BEGIN: Time Complexity: Linear on nums.size(), Space Complexity: Linear on nums.size()
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		unordered_map<int, int> h;
-		for (int i = 0, n = nums.size(); i < n; i++) {
-			if (!h.empty() and h.count(target - nums.at(i))) {
-				return {h.at(target - nums.at(i)), i};
+		for (int i = 0, n = nums.size(); i < n; ++i) {
+			if (h.count(target - nums[i])) {
+				return {h[target - nums[i]], i};
 			}
-			h[nums.at(i)] = i;
+			h[nums[i]] = i;
 		}
 		return {};
 	}
 };
-// END: Time Linear on nums.size(), Space Complexity: Linear on nums.size()
 
 int main(void) {
 	Solution solution;
