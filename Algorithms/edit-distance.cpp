f362b1b1934e2f941bac6901f802bf77e51c6f49
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-02 19:12:16 -0500

    72. Edit Distance

--- Algorithms/edit-distance.cpp
+++ Algorithms/edit-distance.cpp
@@ -1,51 +1,55 @@
 // 72. Edit Distance
 // https://leetcode.com/problems/edit-distance/
-// https://github.com/soulmachine/leetcode
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
+
+/*
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	int minDistance(const string& word1, const string& word2) {
-		const int m = word1.size();
-		const int n = word2.size();
+	int minDistance(string word1, string word2) {
+		int m = word1.size(), n = word2.size();
 		vector<vector<int>> OPT(m + 1, vector<int>(n + 1, 0));
-		for (int i(0); i < m + 1; ++i) {
-			OPT[i][0] = i;
-		}
-		for (int j(0); j < n + 1; ++j) {
-			OPT[0][j] = j;
-		}
-		for (int i(1); i < m + 1; ++i) {
-			for (int j(1); j < n + 1; ++j) {
+		for (int i = 0; i <= m; ++i) {
+			for (int j = 0; j <= n; ++j) {
+				if (i == 0 or j == 0) {
+					OPT[i][j] = i + j;
+					continue;
+				}
 				if (word1[i - 1] == word2[j - 1]) {
 					OPT[i][j] = OPT[i - 1][j - 1];
+					continue;
 				}
-				else {
-					OPT[i][j] = min(OPT[i - 1][j - 1] + 1, OPT[i][j - 1] + 1);
-					OPT[i][j] = min(OPT[i][j], OPT[i - 1][j] + 1);
-				}
+				OPT[i][j] = 1 + OPT[i - 1][j - 1];
+				OPT[i][j] = min(OPT[i][j], 1 + OPT[i - 1][j]);
+				OPT[i][j] = min(OPT[i][j], 1 + OPT[i][j - 1]);
 			}
 		}
 		return OPT[m][n];
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<string> A({"", "sea", "a"});
-	vector<string> B({"a", "eat", "b"});
-	vector<int> C({1, 2, 1});
-	for (size_t i(0); i < C.size(); ++i) {
-		if (C[i] ^ solution.minDistance(A[i], B[i])) {
-			cout << "\nError\n";
-			return 0;
-		}
-		else {
-			cout << "\nPassed\n";
-		}
-	}
+
+	string word1, word2;
+	int answer, result;
+
+	word1 = "";
+	word2 = "";
+	answer = 0;
+	result = solution.minDistance(word1, word2);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
