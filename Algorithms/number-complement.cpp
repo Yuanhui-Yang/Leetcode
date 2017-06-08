c09afb37e9a50cb2c37369c2d7aa8e3a053e7759
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-08 18:50:34 -0500

    476. Number Complement

--- Algorithms/number-complement.cpp
+++ Algorithms/number-complement.cpp
@@ -1,30 +1,50 @@
 // 476. Number Complement
 // https://leetcode.com/problems/number-complement/
-#include <iostream>
-#include <cassert>
+
+/*
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int findComplement(int num) {
-		int result = 0;
-		for (size_t i = 0, j = 0; i < 8 * sizeof(int); i++) {
-			int idx = 1 << (8 * sizeof(int) - i - 1);
-			if (!j) {
-				if (idx & num) j = 1;
-				continue;
-			}
-			if (!(idx & num)) result += idx;
+		int mask = ~0;
+		while (mask & num) {
+			mask <<= 1;
 		}
-		return result;
+		return ~mask & ~num;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	int result = 0;
-	result = solution.findComplement(5);
-	assert(2 == result);
-	result = solution.findComplement(1);
-	assert(0 == result);
+	int num, answer, result;
+
+	num = 5;
+	answer = 2;
+	result = solution.findComplement(num);
+	assert(answer == result);
+
+	num = 1;
+	answer = 0;
+	result = solution.findComplement(num);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
