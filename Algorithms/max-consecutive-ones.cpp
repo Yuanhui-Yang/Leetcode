218512aab418b371e33da90d8993ed1005a1e669
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-09 18:02:17 -0500

    485. Max Consecutive Ones

--- Algorithms/max-consecutive-ones.cpp
+++ Algorithms/max-consecutive-ones.cpp
@@ -1,34 +1,49 @@
 // 485. Max Consecutive Ones
 // https://leetcode.com/problems/max-consecutive-ones/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
+
+/*
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int findMaxConsecutiveOnes(vector<int>& nums) {
-		int result = 0;
-		for (size_t i = 0, j = 0; i < nums.size(); i++) {
-			if (nums[i] == 1) {
-				j = i;
-				while (i < nums.size() && nums[i] == 1) {
-					i++;
-				}
-				result = max(result, int(i - j));
-				continue;
+		int result = 0, len = 0;
+		for (const auto &i : nums) {
+			if (i == 1) {
+				result = result < ++len ? len : result;
+			}
+			else {
+				len = 0;
 			}
 		}
 		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	int result = 0;
 	vector<int> nums;
+	int answer, result;
+
 	nums = {1, 1, 0, 1, 1, 1};
+	answer = 3;
 	result = solution.findMaxConsecutiveOnes(nums);
-	assert(3 == result);
-	cout << "\nPassed all\n";
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
