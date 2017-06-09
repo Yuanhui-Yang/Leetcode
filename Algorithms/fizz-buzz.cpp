6beca4ec32c81edfb09e409e2239f9431199f394
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-09 14:53:52 -0500

    412. Fizz Buzz

--- Algorithms/fizz-buzz.cpp
+++ Algorithms/fizz-buzz.cpp
@@ -1,26 +1,70 @@
 // 412. Fizz Buzz
 // https://leetcode.com/problems/fizz-buzz/
-#include <iostream>
-#include <string>
-#include <vector>
+
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	vector<string> fizzBuzz(int n) {
 		vector<string> result;
-		for (int i = 1; i <= n; i++) {
-			if (i % 3 == 0 && i % 5 == 0) result.push_back("FizzBuzz");
-			else if (i % 3 == 0 && i % 5 != 0) result.push_back("Fizz");
-			else if (i % 3 != 0 && i % 5 == 0) result.push_back("Buzz");
-			else result.push_back(to_string(i));
+		for (int i = 1; i <= n; ++i) {
+			if (i % 15 == 0) {
+				result.push_back("FizzBuzz");
+			}
+			else if (i % 3 == 0) {
+				result.push_back("Fizz");
+			}
+			else if (i % 5 == 0) {
+				result.push_back("Buzz");
+			}
+			else {
+				result.push_back(to_string(i));
+			}
 		}
 		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	for (const auto &i : solution.fizzBuzz(15)) cout << i << '\t';
-	cout << "\tPassed\n";
+	int n;
+	vector<string> answer, result;
+
+	n = 15;
+	answer = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+	result = solution.fizzBuzz(n);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
