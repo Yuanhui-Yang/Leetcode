4294b050e309091fdec010ccc44b2a5aa81d7421
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-10 12:57:54 -0500

    371. Sum of Two Integers

--- Algorithms/sum-of-two-integers.cpp
+++ Algorithms/sum-of-two-integers.cpp
@@ -1,26 +1,38 @@
 // 371. Sum of Two Integers
 // https://leetcode.com/problems/sum-of-two-integers/
-// https://discuss.leetcode.com/topic/49771/java-simple-easy-understand-solution-with-explanation/2
-#include <iostream>
+
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int getSum(int a, int b) {
-		if (!a) return b;
-		if (!b) return a;
 		while (b) {
-			int carry = a & b;
-			a ^= b;
-			b = carry << 1;
+			int c = a;
+			a = c ^ b;
+			b = (c & b) << 1;
 		}
 		return a;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	int a = 1;
-	int b = 2;
-	cout << solution.getSum(a, b) <<"\tPassed\n";
+	int a, b, answer, result;
+
+	a = 1;
+	b = 2;
+	answer = 3;
+	result = solution.getSum(a, b);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
