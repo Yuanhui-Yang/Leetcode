f205c704278dca7fd332fcb7e2800f4d3ac75e50
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-10 13:08:14 -0500

    258. Add Digits

--- Algorithms/add-digits.cpp
+++ Algorithms/add-digits.cpp
@@ -1,18 +1,36 @@
 // 258. Add Digits
 // https://leetcode.com/problems/add-digits/
-// https://en.wikipedia.org/wiki/Digital_root
-// https://en.wikipedia.org/wiki/Modulo_operation
-#include <iostream>
+
+/*
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int addDigits(int num) {
 		return 1 + (num - 1) % 9;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	cout << solution.addDigits(38) << "\tPassed\n";
+	int num, answer, result;
+
+	num = 38;
+	answer = 2;
+	result = solution.addDigits(num);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
