8fd4ee3e461d9710450b6f41ac19048f8f545d01
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-10 16:45:43 -0500

    530. Minimum Absolute Difference in BST

--- Algorithms/minimum-absolute-difference-in-bst.cpp
+++ Algorithms/minimum-absolute-difference-in-bst.cpp
@@ -1,58 +1,115 @@
 // 530. Minimum Absolute Difference in BST
 // https://leetcode.com/problems/minimum-absolute-difference-in-bst/
-#include <iostream>
-#include <cassert>
-#include <cstdlib>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <iterator>
+
+/*
+Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+
+Example:
+
+Input:
+
+   1
+    \
+     3
+    /
+   2
+
+Output:
+1
+
+Explanation:
+The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+Note: There are at least two nodes in this BST.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 struct TreeNode {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
+void gc(TreeNode* root) {
+	if (root) {
+		gc(root->left);
+		gc(root->right);
+		delete root;
+	}
+}
+
 class Solution {
 public:
 	int getMinimumDifference(TreeNode* root) {
-		vector<int> nums;
-		dfs(root, nums);
-		sort(begin(nums), end(nums));
-		int result = 0;
-		for (size_t i = 1; i < nums.size(); i++) {
-			if (i == 1) {
-				result = nums.at(1) - nums.at(0);
-				continue;
+		int result = INT_MAX;
+		TreeNode *p = NULL;
+		stack<TreeNode*> s;
+		while (!s.empty() or root) {
+			if (root) {
+				s.push(root);
+				root = root->left;
 			}
-			if (i > 1) {
-				result = min(result, nums.at(i) - nums.at(i - 1));
-				continue;
+			else {
+				TreeNode *t = s.top();
+				s.pop();
+				if (p) {
+					result = min(result, t->val - p->val);
+				}
+				p = t;
+				root = t->right;
 			}
 		}
 		return result;
 	}
-private:
-	void dfs(TreeNode* root, vector<int>& nums) {
-		if (root) {
-			nums.push_back(root->val);
-			dfs(root->left, nums);
-			dfs(root->right, nums);
-		}
-	} 
 };
+
+// class Solution {
+// public:
+// 	int getMinimumDifference(TreeNode* root) {
+// 		int result = INT_MAX;
+// 		TreeNode *p = NULL;
+// 		while (root) {
+// 			if (!root->left) {
+// 				if (p) {
+// 					result = min(result, root->val - p->val);
+// 				}
+// 				p = root;
+// 				root = root->right;
+// 				continue;
+// 			}
+// 			TreeNode *pred = root->left;
+// 			while (pred->right and pred->right != root) {
+// 				pred = pred->right;
+// 			}
+// 			if (!pred->right) {
+// 				pred->right = root;
+// 				root = root->left;
+// 				continue;
+// 			}
+// 			pred->right = NULL;
+// 			if (p) {
+// 				result = min(result, root->val - p->val);
+// 			}
+// 			p = root;
+// 			root = root->right;
+// 		}
+// 		return result;
+// 	}
+// };
+
 int main(void) {
 	Solution solution;
-	TreeNode *root = NULL;
-	int result = 0;
-	int answer = 0;
+	TreeNode *root;
+	int answer, result;
 
 	root = new TreeNode(1);
 	root->right = new TreeNode(3);
 	root->right->left = new TreeNode(2);
 	answer = 1;
 	result = solution.getMinimumDifference(root);
+	gc(root);
 	assert(answer == result);
 
 	cout << "\nPassed All\n";
