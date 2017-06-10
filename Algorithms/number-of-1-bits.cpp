0fcf0824880f49fe43efaba870bc36d585c3cc5d
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-10 03:22:54 -0500

    191. Number of 1 Bits

--- Algorithms/number-of-1-bits.cpp
+++ Algorithms/number-of-1-bits.cpp
@@ -1,19 +1,37 @@
 // 191. Number of 1 Bits
 // https://leetcode.com/problems/number-of-1-bits/
-#include <iostream>
-#include <cassert>
-#include <bitset>
+
+/*
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int hammingWeight(uint32_t n) {
-		bitset<32> val(n);
-		return val.count();
+		int result = 0;
+		while (n) {
+			++result;
+			n &= n - 1;
+		}
+		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	assert(3 == solution.hammingWeight(0b00000000000000000000000000001011));
+	uint32_t n;
+	int answer, result;
+
+	n = 11;
+	answer = 3;
+	result = solution.hammingWeight(n);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
