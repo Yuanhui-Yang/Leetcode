503a07d848dac551d1b200f4cffb64abf127ff1a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-10 15:59:37 -0500

    492. Construct the Rectangle

--- Algorithms/construct-the-rectangle.cpp
+++ Algorithms/construct-the-rectangle.cpp
@@ -1,28 +1,50 @@
-// 492. Construct the rectangle
+// 492. Construct the Rectangle
 // https://leetcode.com/problems/construct-the-rectangle/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <cmath>
+
+/*
+For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+
+1. The area of the rectangular web page you designed must equal to the given target area.
+
+2. The width W should not be larger than the length L, which means L >= W.
+
+3. The difference between length L and width W should be as small as possible.
+You need to output the length L and the width W of the web page you designed in sequence.
+Example:
+Input: 4
+Output: [2, 2]
+Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1]. 
+But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+Note:
+The given area won't exceed 10,000,000 and is a positive integer
+The web page's width and length you designed must be positive integers.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	vector<int> constructRectangle(int area) {
-		int W = sqrt(area);
-		while (W > 1 && area % W) {
-			W--;
+		int w = sqrt(area);
+		while (w > 1 and area % w) {
+			w--;
 		}
-		return {area / W, W};
+		return {area / w, w};
 	}
 };
+
 int main(void) {
 	Solution solution;
-	int area = 0;
-	vector<int> result;
+	int area;
+	vector<int> answer, result;
 
 	area = 4;
+	answer = {2, 2};
+	sort(begin(answer), end(answer));
 	result = solution.constructRectangle(area);
-	assert(vector<int>({2, 2}) == result);
+	sort(begin(result), end(result));
+	assert(answer == result);
 
 	cout << "\nPassed All\n";
 	return 0;
