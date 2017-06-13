7e4f2c22b07239b3b246849c890a822c0d22d20c
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-13 17:26:21 -0500

    151. Reverse Words in a String

--- Algorithms/reverse-words-in-a-string.cpp
+++ Algorithms/reverse-words-in-a-string.cpp
@@ -1,66 +1,74 @@
 // 151. Reverse Words in a String
 // https://leetcode.com/problems/reverse-words-in-a-string/
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <algorithm>
-#include <iterator>
+
+/*
+Given an input string, reverse the string word by word.
+
+For example,
+Given s = "the sky is blue",
+return "blue is sky the".
+
+Update (2015-02-12):
+For C programmers: Try to solve it in-place in O(1) space.
+
+click to show clarification.
+
+Clarification:
+What constitutes a word?
+A sequence of non-space characters constitutes a word.
+Could the input string contain leading or trailing spaces?
+Yes. However, your reversed string should not contain leading or trailing spaces.
+How about multiple spaces between two words?
+Reduce them to a single space in the reversed string.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	void reverseWords(string &s) {
-		if (s.empty()) {
-			return;
-		}
-		while (!s.empty() && s.back() == ' ') {
+		while (!s.empty() and s.back() == ' ') {
 			s.pop_back();
-		}
-		if (s.empty()) {
-			return;
 		}
 		reverse(begin(s), end(s));
-		while (!s.empty() && s.back() == ' ') {
+		while (!s.empty() and s.back() == ' ') {
 			s.pop_back();
 		}
-		if (s.empty()) {
-			return;
-		}
-		for (size_t i = 0, j = 0; i < s.size(); i++) {
-			if (s.at(i) != ' ') {
-				j = i;
-				while (i + 1 < s.size() && s.at(i + 1) != ' ') {
-					i++;
+		for (int n = s.size(), i = 0; i < n; ++i) {
+			if (s[i] == ' ') {
+				int j = i;
+				while (i + 1 < n and s[i + 1] == ' ') {
+					++i;
+				}
+				s.erase(next(begin(s), j), next(begin(s), i));
+				n -= i - j;
+				i -= i - j;
+			}
+			else {
+				int j = i;
+				while (i + 1 < n and s[i + 1] != ' ') {
+					++i;
 				}
 				reverse(next(begin(s), j), next(begin(s), i + 1));
-				continue;
-			}
-			if (s.at(i) == ' ') {
-				j = i;
-				while (i + 1 < s.size() && s.at(i + 1) == ' ') {
-					i++;
-				}
-				const size_t d = i - j;
-				s.erase(next(begin(s), j), next(begin(s), i));
-				i -= d;
-				continue;
 			}
 		}
 	}
 };
+
 int main(void) {
 	Solution solution;
-	string s;
-	string answer;
-
-	s = "   one.   +two three?   ~four   !five- ";
-	answer = "!five- ~four three? +two one.";
-	solution.reverseWords(s);
-	assert(answer == s);
+	string s, answer;
 
 	s = "the sky is blue";
 	answer = "blue is sky the";
 	solution.reverseWords(s);
-	assert(answer == s);
+	assert(s == answer);
+
+	s = " ";
+	answer = "";
+	solution.reverseWords(s);
+	assert(s == answer);
 
 	cout << "\nPassed All\n";
 	return 0;
