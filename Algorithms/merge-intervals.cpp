52eaaf52a21b80e809f1c8dc8e8b906e63b2df86
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-13 16:21:15 -0500

    56. Merge Intervals

--- Algorithms/merge-intervals.cpp
+++ Algorithms/merge-intervals.cpp
@@ -1,56 +1,62 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <algorithm>
+// 56. Merge Intervals
+// https://leetcode.com/problems/merge-intervals/
+
+/*
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 struct Interval {
 	int start;
 	int end;
 	Interval() : start(0), end(0) {}
-	Interval(const int& s, const int& e) : start(s), end(e) {}
+	Interval(int s, int e) : start(s), end(e) {}
 };
+
+struct Comp {
+	bool operator() (const struct Interval& a, const struct Interval& b) {
+		return a.start == b.start ? a.end < b.end : a.start < b.start;
+	}
+};
+
 class Solution {
 public:
 	vector<Interval> merge(vector<Interval>& intervals) {
-		if (intervals.size() <= 1)
-			return intervals;
-		sort(begin(intervals), end(intervals), this->comp);
-		this->merge(0, intervals);
-		return intervals;
-	}
-private:
-	static bool comp(const Interval& a, const Interval& b) {return a.start < b.start;}
-private:
-	void merge(const int& i, vector<Interval>& intervals) {
-		if (i + 1 == intervals.size())
-			return;
-		else {
-			if (intervals[i].end >= intervals[i + 1].start) {
-				intervals[i].end = max(intervals[i].end, intervals[i + 1].end);
-				intervals.erase(begin(intervals) + i + 1);
-				this->merge(i, intervals);
-				return;
+		sort(begin(intervals), end(intervals), Comp());
+		vector<Interval> result;
+		for (int n = intervals.size(), i = 0; i < n; ++i) {
+			int x = intervals[i].start, y = intervals[i].end;
+			while (i + 1 < n and intervals[i + 1].start <= y) {
+				y = max(y, intervals[i + 1].end);
+				++i;
 			}
-			else {
-				this->merge(i + 1, intervals);
-				return;
-			}
+			result.push_back(Interval(x, y));
 		}
+		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<Interval> a;
-	vector<Interval> b;
-	a.push_back(Interval(1, 3));
-	a.push_back(Interval(2, 6));
-	a.push_back(Interval(8, 10));
-	a.push_back(Interval(15, 18));
-	b.push_back(Interval(1, 6));
-	b.push_back(Interval(8, 10));
-	b.push_back(Interval(15, 18));
+	vector<Interval> intervals, answer, result;
+
+	intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+	answer = {{1, 6}, {8, 10}, {15, 18}};
+	sort(begin(answer), end(answer), Comp());
+	result = solution.merge(intervals);
+	sort(begin(result), end(result), Comp());
+	assert(answer.size() == result.size());
+	for (size_t n = answer.size(), i = 0; i < n; ++i) {
+		assert(answer[i].start == result[i].start);
+		assert(answer[i].end == result[i].end);
+	}
+
 	cout << "\nPassed All\n";
-	getchar();
 	return 0;
 }
