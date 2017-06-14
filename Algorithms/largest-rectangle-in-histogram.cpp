242843d8b689f57cccd8fb0775294600549eb979
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-14 04:16:58 -0500

    84. Largest Rectangle in Histogram

--- Algorithms/largest-rectangle-in-histogram.cpp
+++ Algorithms/largest-rectangle-in-histogram.cpp
@@ -1,33 +1,71 @@
 // 84. Largest Rectangle in Histogram
 // https://leetcode.com/problems/largest-rectangle-in-histogram/
-// http://fisherlei.blogspot.com/2012/12/leetcode-largest-rectangle-in-histogram.html
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
+
+/*
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+For example,
+Given heights = [2,1,5,6,2,3],
+return 10.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int largestRectangleArea(vector<int>& heights) {
-		heights.push_back(0);
 		stack<int> s;
+		heights.push_back(0);
 		int result = 0;
-		for (int i = 0; i < (int)heights.size(); ) {
-			if (s.empty() || heights[i] > heights[s.top()])
-				s.push(i++);
-			else {
-				int top = s.top();
+		for (int n = heights.size(), i = 0; i < n; ++i) {
+			while (!s.empty() and heights[s.top()] >= heights[i]) {
+				int j = s.top();
 				s.pop();
-				result = max(result, heights[top] * (s.empty() ? i : (i - s.top() - 1)));
+				result = max(result, heights[j] * (i - (s.empty() ? 0 : s.top() + 1)));
 			}
+			s.push(i);
 		}
 		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<int> heights = {2,1,5,6,2,3};
-	cout << solution.largestRectangleArea(heights) << "\tPassed\n";
+	vector<int> heights;
+	int anwer, result;
+
+	heights = {3, 6, 5, 7, 4, 8, 1, 0};
+	anwer = 20;
+	result = solution.largestRectangleArea(heights);
+	assert(anwer == result);
+
+	heights = {1, 2, 2};
+	anwer = 4;
+	result = solution.largestRectangleArea(heights);
+	assert(anwer == result);
+
+	heights = {1};
+	anwer = 1;
+	result = solution.largestRectangleArea(heights);
+	assert(anwer == result);
+
+	heights = {4, 2, 0, 3, 2, 5};
+	anwer = 6;
+	result = solution.largestRectangleArea(heights);
+	assert(anwer == result);
+
+	heights = {2, 1, 5, 6, 2, 3};
+	anwer = 10;
+	result = solution.largestRectangleArea(heights);
+	assert(anwer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
