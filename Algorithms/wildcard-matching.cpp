6f8d247b78dae505f4d3de84c267f9730bbc168a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-19 10:53:26 -0500

    44. Wildcard Matching

--- Algorithms/wildcard-matching.cpp
+++ Algorithms/wildcard-matching.cpp
@@ -1,43 +1,103 @@
 // 44. Wildcard Matching
 // https://leetcode.com/problems/wildcard-matching/
-// https://discuss.leetcode.com/topic/3040/linear-runtime-and-constant-space-solution
-#include <iostream>
-#include <vector>
-#include <string>
+
+/*
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	bool isMatch(const string& s, const string& p) {
-		return this->isMatch(s.c_str(), p.c_str());
-	}
-private:
-	bool isMatch(const char *s, const char *p) {
-		const char* star = NULL;
-		const char* ss = s;
-		while (*s) {
-			if (*p == '?' || *p == *s) { ++s; ++p; continue; }
-			if (*p == '*') { star = p++; ss = s; continue; }
-			if (star) { p = star + 1; s = ++ss; continue; }
-			return false;
+	bool isMatch(string s, string p) {
+		int m = s.size(), n = p.size();
+		vector<vector<bool>> matrix(m + 1, vector<bool>(n + 1, false));
+		matrix[0][0] = true;
+		for (int i = 0; i <= m; ++i) {
+			for (int j = 1; j <= n; ++j) {
+				if (p[j - 1] == '*') {
+					matrix[i][j] = matrix[i][j - 1] or (i > 0 and matrix[i - 1][j]);
+				}
+				else {
+					matrix[i][j] = i > 0 and matrix[i - 1][j - 1] and (p[j - 1] == s[i - 1] or p[j - 1] == '?');
+				}
+			}
 		}
-		while (*p == '*') ++p;
-		return *p == '\0';
+		return matrix[m][n];
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<string> a({ "", "aa", "aa", "aaa", "aa", "aa", "ab", "aab", "b", "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb" });
-	vector<string> b({ "*", "a", "aa", "aa", "*", "a*", "?*", "c*a*b", "*?*?", "abbabaaabbabbaababbabbbbbabbbabbbabaaaaababababbbabababaabbababaabbbbbbaaaabababbbaabbbbaabbbbababababbaabbaababaabbbababababbbbaaabbbbbabaaaabbababbbbaababaabbababbbbbababbbabaaaaaaaabbbbbaabaaababaaaabb"
-"**aa*****ba*a*bb**aa*ab****a*aaaaaa***a*aaaa**bbabb*b*b**aaaaaaaaa*a********ba*bbb***a*ba*bb*bb**a*b*bb" });
-	vector<bool> c({ true, false, true, false, true, true, true, false, false, false });
-	for (size_t i(0); i < a.size(); ++i) {
-		if (c[i] ^ solution.isMatch(a[i], b[i])) {
-			cout << "\nError\n";
-			getchar();
-			return 0;
-		}
-	}
-	cout << "\nPassed\n";
-	getchar();
+	string s, p;
+	bool answer, result;
+
+	s = "";
+	p = "*";
+	answer = true;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "a";
+	answer = false;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "aa";
+	answer = true;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "aaa";
+	answer = false;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "*";
+	answer = true;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "a*";
+	answer = true;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aa";
+	p = "?*";
+	answer = true;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	s = "aab";
+	p = "c*a*b";
+	answer = false;
+	result = solution.isMatch(s, p);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
