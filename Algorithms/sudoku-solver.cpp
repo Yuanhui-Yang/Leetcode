3b4e091d5325c83fbc32c1171a243a3b0a7d43ab
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-19 08:11:38 -0500

    37. Sudoku Solver

--- Algorithms/sudoku-solver.cpp
+++ Algorithms/sudoku-solver.cpp
@@ -27,13 +27,14 @@ public:
 		memset(c, 0, sizeof(c));
 		for (int i = 0; i < 9; ++i) {
 			for (int j = 0; j < 9; ++j) {
-				if (board[i][j] == '.') {
+				char ch = board[i][j];
+				if (ch == '.') {
 					continue;
 				}
-				int val = 1 << (board[i][j] - '1'), k = i / 3 * 3 + j / 3;
-				a[i] |= val;
-				b[j] |= val;
-				c[k] |= val;
+				int mask = 1 << (ch - '1'), k = i / 3 * 3 + j / 3;
+				a[i] |= mask;
+				b[j] |= mask;
+				c[k] |= mask;
 			}
 		}
 		dfs(board, a, b, c, 0, 0);
@@ -43,28 +44,28 @@ private:
 		if (i == 9) {
 			return true;
 		}
-		while (j < 9 and board[i][j] != '.') {
-			++j;
-		}
 		if (j == 9) {
 			return dfs(board, a, b, c, i + 1, 0);
 		}
-		int k = i / 3 * 3 + j / 3;
-		for (int l = 0; l < 9; ++l) {
-			int val = 1 << l;
-			if (!(a[i] & val) and !(b[j] & val) and !(c[k] & val)) {
-				board[i][j] = '1' + l;
-				a[i] |= val;
-				b[j] |= val;
-				c[k] |= val;
-				if (dfs(board, a, b, c, i, j + 1)) {
-					return true;
-				}
-				a[i] &= ~val;
-				b[j] &= ~val;
-				c[k] &= ~val;
-				board[i][j] = '.';
+		if (board[i][j] != '.') {
+			return dfs(board, a, b, c, i, j + 1);
+		}
+		for (int k = i / 3 * 3 + j / 3, l = 0; l < 9; ++l) {
+			int mask = 1 << l;
+			if ((a[i] & mask) or (b[j] & mask) or (c[k] & mask)) {
+				continue;
 			}
+			a[i] |= mask;
+			b[j] |= mask;
+			c[k] |= mask;
+			board[i][j] = '1' + l;
+			if (dfs(board, a, b, c, i, j + 1)) {
+				return true;
+			}
+			board[i][j] = '.';
+			a[i] &= ~mask;
+			b[j] &= ~mask;
+			c[k] &= ~mask;
 		}
 		return false;
 	}
@@ -72,21 +73,21 @@ private:
 
 int main(void) {
 	Solution solution;
-	vector<string> result, answer;
+	vector<string> matrix, answer;
 	vector<vector<char>> board;
 
-	result = {"..9748...", "7........", ".2.1.9...", "..7...24.", ".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.."};
+	matrix = {"..9748...", "7........", ".2.1.9...", "..7...24.", ".64.1.59.", ".98...3..", "...8.3.2.", "........6", "...2759.."};
+	answer = {"519748632", "783652419", "426139875", "357986241", "264317598", "198524367", "975863124", "832491756", "641275983"};
 	board.clear();
-	for (const auto &i : result) {
+	for (const auto &i : matrix) {
 		board.push_back(vector<char>(begin(i), end(i)));
 	}
-	answer = {"519748632", "783652419", "426139875", "357986241", "264317598", "198524367", "975863124", "832491756", "641275983"};
 	solution.solveSudoku(board);
-	result.clear();
+	matrix.clear();
 	for (const auto &i : board) {
-		result.push_back(string(begin(i), end(i)));
+		matrix.push_back(string(begin(i), end(i)));
 	}
-	assert(answer == result);
+	assert(answer == matrix);
 
 	cout << "\nPassed All\n";
 	return 0;
