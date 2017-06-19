d99f5c680e3cbaf4d7bbf3c379c44a559d343259
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-19 12:04:16 -0500

    45. Jump Game II

--- Algorithms/jump-game-ii.cpp
+++ Algorithms/jump-game-ii.cpp
@@ -1,50 +1,55 @@
 // 45. Jump Game II
 // https://leetcode.com/problems/jump-game-ii/
-#include <iostream>
-#include <vector>
+
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+For example:
+Given array A = [2,3,1,1,4]
+
+The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+Note:
+You can assume that you can always reach the last index.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	int jump(const vector<int>& nums) {
-		int result(0);
-		int i(0);
-		while (i < nums.size() - 1) {
-			if (i + nums[i] >= nums.size() - 1) {
-				++result;
-				break;
-			}
-			else {
-				int upperbound(-1);
-				int nexti(-1);
-				for (int j(1); j <= nums[i]; ++j) {
-					if (i + j + nums[i + j] > upperbound) {
-						upperbound = i + j + nums[i + j];
-						nexti = i + j;
+	int jump(vector<int>& nums) {
+		int result = 0, i = 0, n = nums.size();
+		while (i + 1 < n) {
+			int j = i + nums[i];
+			if (i + nums[i] + 1 < n) {
+				for (int k = 1; k <= nums[i]; ++k) {
+					if (i + k + nums[i + k] > j + nums[j]) {
+						j = i + k;
 					}
 				}
-				i = nexti;
-				++result;
 			}
+			i = j;
+			++result;
 		}
 		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<vector<int>> A;
-	vector<int> b;
-	A.push_back(vector<int>({ 2,3,1,1,4 }));
-	A.push_back(vector<int>({ 4,1,1,3,1,1,1 }));
-	b.push_back(2);
-	b.push_back(2);
-	for (size_t i(0); i < b.size(); ++i) {
-		if (b[i] ^ solution.jump(A[i])) {
-			cout << "\nError\n";
-			getchar();
-			return 0;
-		}
-	}
-	cout << "\nPassed\n";
-	getchar();
+	vector<int> nums;
+	int answer, result;
+
+	nums = {2, 3, 1, 1, 4};
+	answer = 2;
+	result = solution.jump(nums);
+	assert(answer == result);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
