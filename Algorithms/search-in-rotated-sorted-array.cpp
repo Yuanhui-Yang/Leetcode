20ce828eaf5c0ee221cdd11bbd4f80b35bf24528
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-18 22:22:18 -0500

    33. Search in Rotated Sorted Array

--- Algorithms/search-in-rotated-sorted-array.cpp
+++ Algorithms/search-in-rotated-sorted-array.cpp
@@ -1,62 +1,75 @@
 // 33. Search in Rotated Sorted Array
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
-// https://github.com/soulmachine/leetcode
-#include <iostream>
-#include <vector>
+
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	int search(const vector<int>& nums, const int& target) {
+	int search(vector<int>& nums, int target) {
 		if (nums.empty()) {
 			return -1;
 		}
-		if (nums.size() == 1) {
-			return nums.front() == target ? 0 : -1;
-		}
-		int left = 0;
-		int right = nums.size() - 1;
-		int mid = 0;
-		while (left <= right) {
-			mid = left + (right - left) / 2;
-			if (nums[mid] == target) {
-				return mid;
-			}
-			if (mid >= 1 && nums[mid - 1] == target) {
-				return mid - 1;
-			}
-			if (mid + 1 < nums.size() && nums[mid + 1] == target) {
-				return mid + 1;
-			}
-			if (nums[mid] > nums.front()) {
-				if (nums.front() <= target && target < nums[mid]) {
-					right = mid - 1;
+		int n = nums.size(), i = 0, j = n - 1;
+		if (target >= nums.front()) {
+			while (i + 1 < j) {
+				int mid = i + (j - i) / 2;
+				if (nums[mid] < nums.front()) {
+					j = mid;
+				}
+				else if (nums[mid] < target) {
+					i = mid;
 				}
 				else {
-					left = mid + 1;
-				}
-			}
-			else {
-				if (nums[mid] < target && target <= nums.back()) {
-					left = mid + 1;
-				}
-				else {
-					right = mid - 1;
+					j = mid;
 				}
 			}
 		}
-		return nums[mid] == target ? mid : -1;
+		else {
+			while (i + 1 < j) {
+				int mid = i + (j - i) / 2;
+				if (nums[mid] >= nums.front()) {
+					i = mid;
+				}
+				else if (nums[mid] < target) {
+					i = mid;
+				}
+				else {
+					j = mid;
+				}
+			}
+		}
+		if (nums[i] == target) {
+			return i;
+		}
+		if (nums[j] == target) {
+			return j;
+		}
+		return -1;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<int> nums({4, 5, 6, 7, 0, 1, 2});
-	for (int i = 0; i < nums.size(); ++i) {
-		if (i ^ solution.search(nums, nums[i])) {
-			cout << "\nError\n";
-			return 0;
-		}
-	}
-	cout << "\nPassed\n";
+	vector<int> nums;
+	int target, answer, result;
+
+	nums = {4, 5, 6, 7, 0, 1, 2};
+	target = 0;
+	answer = 4;
+	result = solution.search(nums, target);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
--- README.md
+++ README.md
@@ -53,6 +53,7 @@ int main(void) {
 | 30 | [Substring with Concatenation of All Words](https://leetcode.com/problems/substring-with-concatenation-of-all-words/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/substring-with-concatenation-of-all-words.cpp) | Hard |
 | 31 | [Next Permutation](https://leetcode.com/problems/next-permutation/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/next-permutation.cpp) | Medium |
 | 32 | [Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/longest-valid-parentheses.cpp) | Hard |
+| 33 | [Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/search-in-rotated-sorted-array.cpp) | Medium |
 | 34 | [Search for a Range](https://leetcode.com/problems/search-for-a-range/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/search-for-a-range.cpp) | Medium |
 | 35 | [Search Insert Position](https://leetcode.com/problems/search-insert-position/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/search-insert-position.cpp) | Medium |
 | 37 | [Sudoku Solver](https://leetcode.com/problems/sudoku-solver/) | [C++](https://github.com/yuanhui-yang/LeetCode/blob/master/Algorithms/sudoku-solver.cpp) | Easy |
