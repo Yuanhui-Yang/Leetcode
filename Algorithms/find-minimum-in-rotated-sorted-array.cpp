3b66cd9d59fc55428eece076d342e2464b7ffb4e
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-20 06:05:43 -0500

    153. Find Minimum in Rotated Sorted Array

--- Algorithms/find-minimum-in-rotated-sorted-array.cpp
+++ Algorithms/find-minimum-in-rotated-sorted-array.cpp
@@ -1,43 +1,61 @@
 // 153. Find Minimum in Rotated Sorted Array
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
-#include <iostream>
-#include <vector>
-#include <limits>
+
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int findMin(vector<int>& nums) {
-		const int n = nums.size();
-		if (n == 1) return nums[0];
-		int i = 0, j = n - 1;
-		if (nums[i] < nums[j]) return nums[i];
-		int mid = i + (j - i) / 2;
-		while (i < j) {
-			if (i + 1 == j) return min(nums[i], nums[j]);
-			if (nums[mid] > nums[0]) {
-				if (mid + 1 < n && nums[mid + 1] < nums[mid]) return nums[mid + 1];
-				i = mid + 1;
+		if (nums.empty()) {
+			return INT_MAX;
+		}
+		int n = nums.size(), i = 0, j = n - 1, result = nums.front();
+		while (i + 1 < j) {
+			int mid = i + (j - i) / 2;
+			if (nums[mid] >= nums.front()) {
+				i = mid;
 			}
 			else {
-				if (mid - 1 >= 0 &&nums[mid - 1] > nums[mid]) return nums[mid];
-				j = mid - 1;
+				result = min(result, nums[mid]);
+				j = mid;
 			}
-			mid = i + (j - i) / 2;
 		}
-		return nums[mid];
+		result = min(result, min(nums[i], nums[j]));
+		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-	cout << solution.findMin(nums) << '\n';
-	cout << "\nPassed\n";
+	vector<int> nums;
+	int answer, result;
+
+	nums = {4, 5, 6, 7, 0, 1, 2};
+	answer = 0;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
 	nums = {2, 1};
-	cout << solution.findMin(nums) << '\n';
-	cout << "\nPassed\n";
+	answer = 1;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
 	nums = {1, 2, 3};
-	cout << solution.findMin(nums) << '\n';
-	cout << "\nPassed\n";	
+	answer = 1;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
