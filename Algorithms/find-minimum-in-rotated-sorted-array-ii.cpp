6ab4683d599f7eac85f5b7b63fd9feec76a89c23
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-20 06:22:17 -0500

    154. Find Minimum in Rotated Sorted Array II

--- Algorithms/find-minimum-in-rotated-sorted-array-ii.cpp
+++ Algorithms/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,59 +1,73 @@
 // 154. Find Minimum in Rotated Sorted Array II
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
-#include <iostream>
-#include <vector>
+
+/*
+Follow up for "Find Minimum in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+The array may contain duplicates.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int findMin(vector<int>& nums) {
-		if (nums.size() == 1) return nums[0];
-		int left = 0;
-		int right = nums.size() - 1;
-		if (nums[left] < nums[right]) return nums[left];
-		int mid = left + (right - left) / 2;
-		while (left < right) {
-			if (nums[left] < nums[right]) return nums[left];
-			if (nums[left] < nums[mid]) {
-				while (mid + 1 <= right && nums[mid + 1] == nums[mid]) ++mid;
-				if (mid + 1 <= right && nums[mid] > nums[mid + 1]) return nums[mid + 1];
-				if (mid + 1 <= right && nums[mid] < nums[mid + 1]) {
-					left = mid + 1;
-					mid = left + (right - left) / 2;
-					continue;
-				}
-				++left;
-				mid = left + (right - left) / 2;
-				continue;				
-			}
-			if (nums[left] == nums[mid]) {
-				if (mid + 1 <= right && nums[mid] > nums[mid + 1]) return nums[mid + 1];
-				if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) return nums[mid];
-				++left;
-				mid = left + (right - left) / 2;
-				continue;
-			}
-			while (mid - 1 >= 0 && nums[mid - 1] == nums[mid]) --mid;
-			if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) return nums[mid];
-			if (mid - 1 >= 0 && nums[mid - 1] < nums[mid]) {
-				right = mid - 1;
-				mid = left + (right - left) / 2;
-				continue;				
-			}
-			--right;
-			mid = left + (right - left) / 2;
-			continue;		
+		if (nums.empty()) {
+			return -1;
 		}
-		return nums[mid];
+		int n = nums.size(), i = 0, j = n - 1, result = nums.front();
+		while (i + 1 < j and nums[i] == nums.front()) {
+			++i;
+		}
+		while (i + 1 < j) {
+			int mid = i + (j - i) / 2;
+			if (nums[mid] > nums.front()) {
+				i = mid;
+			}
+			else {
+				result = min(result, nums[mid]);
+				j = mid;
+			}
+		}
+		result = min(result, min(nums[i], nums[j]));
+		return result;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-	cout << solution.findMin(nums) << '\n';
-	cout << "\nPassed\n";
+	vector<int> nums;
+	int answer, result;
+
+	nums = {4, 5, 6, 7, 0, 1, 2};
+	answer = 0;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
 	nums = {10, 1, 10, 10, 10};
-	cout << solution.findMin(nums) << '\n';
-	cout << "\nPassed\n";
+	answer = 1;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
+	nums = {3, 3, 1};
+	answer = 1;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
+	nums = {3, 3, 1, 3};
+	answer = 1;
+	result = solution.findMin(nums);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
