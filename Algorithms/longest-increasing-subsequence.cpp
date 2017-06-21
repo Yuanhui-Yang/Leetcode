0efb567298660dc5849efdfdba60114094b73bf7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-21 09:47:20 -0500

    300. Longest Increasing Subsequence

--- Algorithms/longest-increasing-subsequence.cpp
+++ Algorithms/longest-increasing-subsequence.cpp
@@ -1,43 +1,48 @@
 // 300. Longest Increasing Subsequence
 // https://leetcode.com/problems/longest-increasing-subsequence/
-// https://discuss.leetcode.com/topic/28696/9-lines-c-code-with-o-nlogn-complexity
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
+
+/*
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+For example,
+Given [10, 9, 2, 5, 3, 7, 101, 18],
+The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+
+Your algorithm should run in O(n2) complexity.
+
+Follow up: Could you improve it to O(n log n) time complexity?
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int lengthOfLIS(vector<int>& nums) {
-		vector<int> result;
-		for (const auto& i : nums) {
-			vector<int>::iterator it = lower_bound(begin(result), end(result), i);
-			if (it == end(result)) result.push_back(i);
-			else *it = i;
+		vector<int> v;
+		for (const auto &i : nums) {
+			vector<int>::iterator it = lower_bound(begin(v), end(v), i);
+			if (it == end(v)) {
+				v.push_back(i);
+			}
+			else {
+				*it = i;
+			}
 		}
-		return result.size();
+		return v.size();
 	}
 };
-// class Solution {
-// public:
-// 	int lengthOfLIS(vector<int>& nums) {
-// 		const int n = nums.size();
-// 		vector<int> OPT(n + 1, 0);
-// 		int result = 0;
-// 		for (int i = n - 1; i >= 0; --i) {
-// 			int tmp = 0;
-// 			for (int j = 1; i + j < n; ++j)
-// 				if (nums[i + j] > nums[i])
-// 					tmp = max(tmp, OPT[i + j]);
-// 			result = max(result, OPT[i] = (1 + tmp));
-// 		}
-// 		return result;
-// 	}
-// };
+
 int main(void) {
 	Solution solution;
-	vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
-	cout << solution.lengthOfLIS(nums) << "\tPassed\n";
+	vector<int> nums;
+	int answer, result;
+
+	nums = {10, 9, 2, 5, 3, 7, 101, 18};
+	answer = 4;
+	result = solution.lengthOfLIS(nums);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
