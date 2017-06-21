7c3aba873b06a11394cfa2e79d9c22d1334cb56f
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-21 17:06:06 -0500

    398. Random Pick Index

--- Algorithms/random-pick-index.cpp
+++ Algorithms/random-pick-index.cpp
@@ -1,22 +1,42 @@
 // 398. Random Pick Index
 // https://leetcode.com/problems/random-pick-index/
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include <vector>
-#include <iterator>
-#include <algorithm>
+
+/*
+Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	Solution(vector<int> nums): nums(nums) {srand (time(NULL));}
+	Solution(vector<int> nums) {
+		this->nums.clear();
+		this->nums = nums;
+	}
 
 	int pick(int target) {
-		int result = 0;
-		for (int i = result, n = 1; i < (int)nums.size(); i++) {
-			if (nums[i] == target)
-				if (rand() % n++ == 0)
+		int result = -1;
+		for (int n = nums.size(), i = 0, cnt = 0; i < n; ++i) {
+			if (nums[i] == target) {
+				if (rand() % (++cnt) == 0) {
 					result = i;
+				}
+			}
 		}
 		return result;
 	}
@@ -31,10 +51,20 @@ private:
  */
 
 int main(void) {
-	vector<int> nums = {1, 2, 3, 3, 3};
+	vector<int> nums, results;
 	Solution solution(nums);
-	cout << solution.pick(3) << "\tPassed\n";
-	cout << solution.pick(1) << "\tPassed\n";
+	int result, target;
+
+	nums = {1, 2, 3, 3, 3};
+	solution = Solution(nums);
+	target = 3;
+	result = solution.pick(target);
+	results.push_back(result);
+	result = solution.pick(target);
+	results.push_back(result);
+	result = solution.pick(target);
+	results.push_back(result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
