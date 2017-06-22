360051d9ae9aa8821af240705c2197f53c0e41f4
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-22 18:24:12 -0500

    55. Jump Game

--- Algorithms/jump-game.cpp
+++ Algorithms/jump-game.cpp
@@ -1,52 +1,57 @@
 // 55. Jump Game
 // https://leetcode.com/problems/jump-game/
-#include <iostream>
-#include <vector>
+
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	bool canJump(const vector<int>& nums) {
-		int i(0);
-		if (i + nums[i] >= nums.size() - 1)
-			return true;
-		else {
-
-			while (i + nums[i] < nums.size() - 1) {
-				int nextI(-1);
-				int maxRight(-1);
-				for (int j(1);  j <= nums[i]; ++j) {
-					if (i + j + nums[i + j] > maxRight) {
-						maxRight = i + j + nums[i + j];
-						nextI = i + j;
-					}
+	bool canJump(vector<int>& nums) {
+		int n = nums.size(), i = 0;
+		while (i + 1 < n and i + nums[i] + 1 < n) {
+			int k = i;
+			for (int j = 1; j <= nums[i]; ++j) {
+				if (i + j + nums[i + j] > k + nums[k]) {
+					k = i + j;
 				}
-				if (i >= nextI)
-					return false;
-				else
-					i = nextI;
 			}
-			return true;
+			if (k == i) {
+				return false;
+			}
+			i = k;
 		}
+		return true;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<vector<int>> A;
-	vector<bool> b;
-	A.push_back(vector<int>({2,3,1,1,4}));
-	b.push_back(true);
-	A.push_back(vector<int>({3,2,1,0,4}));
-	b.push_back(false);
-	for (size_t i(0); i < b.size(); ++i) {
-		if (b[i] ^ solution.canJump(A[i])) {
-			cout << "\nError\n";
-			getchar();
-			return 0;
-		}
-		else
-			cout << "\nPassed\n";
-	}
+	vector<int> nums;
+	bool answer, result;
+
+	nums = {2, 3, 1, 1, 4};
+	answer = true;
+	result = solution.canJump(nums);
+	assert(answer == result);
+
+	nums = {3, 2, 1, 0, 4};
+	answer = false;
+	result = solution.canJump(nums);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
-	getchar();
 	return 0;
 }
