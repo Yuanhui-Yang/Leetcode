400986bf42be96ce28f6d94e2dde7560916cfd54
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-22 00:31:34 -0500

    208. Implement Trie (Prefix Tree)

--- Algorithms/implement-trie-prefix-tree.cpp
+++ Algorithms/implement-trie-prefix-tree.cpp
@@ -1,72 +1,95 @@
 // 208. Implement Trie (Prefix Tree)
 // https://leetcode.com/problems/implement-trie-prefix-tree/
-// http://www.cnblogs.com/grandyang/p/4491665.html
-// https://leetcode.com/articles/implement-trie-prefix-tree/
-// http://www.cplusplus.com/reference/cstring/memset/
-#include <iostream>
-#include <cstring>
-#include <string>
+
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-class TrieNode {
-public:
-	// Initialize your data structure here.
-	TrieNode(): isEnd(false) {
-		memset(links, 0, sizeof(links));
-	}
-	TrieNode *links[26];
-	bool isEnd;
-};
 
 class Trie {
 public:
-	Trie(): root(new TrieNode()) {}
+	/** Initialize your data structure here. */
+	Trie() {
+	root = new Node();
+	}
 
-	// Inserts a word into the trie.
+	/** Inserts a word into the trie. */
 	void insert(string word) {
-		TrieNode *it = this->root;
-		for (const auto &c : word) {
-			if (!it->links[c - 'a']) it->links[c - 'a'] = new TrieNode();
-			it = it->links[c - 'a'];
+		Node *it = root;
+		for (const auto &ch : word) {
+			int d = ch - 'a';
+			if(!it->next[d]) {
+				it->next[d] = new Node();
+			}
+			it = it->next[d];
 		}
 		it->isEnd = true;
 	}
 
-	// Returns if the word is in the trie.
+	/** Returns if the word is in the trie. */
 	bool search(string word) {
-		TrieNode *it = this->find(word);
-		return it && it->isEnd;
+		Node *it = f(word);
+		return it and it->isEnd;
 	}
 
-	// Returns if there is any word in the trie
-	// that starts with the given prefix.
-	bool startsWith(string word) {
-		TrieNode *it = this->find(word);
+	/** Returns if there is any word in the trie that starts with the given prefix. */
+	bool startsWith(string prefix) {
+		return f(prefix);
+	}
+private:
+	struct Node {
+		Node(void) {
+			isEnd = false;
+			memset(next, 0, sizeof(next));
+		}
+		bool isEnd;
+		Node* next[26];
+	};
+	Node *root;
+	Node *f(const string& word) {
+		Node *it = root;
+		for (const auto &ch : word) {
+			int d = ch - 'a';
+			if (it->next[d]) {
+				it = it->next[d];
+			}
+			else {
+				return NULL;
+			}
+		}
 		return it;
 	}
-
-	TrieNode *find(string word) {
-		TrieNode *it = root;
-		for (const auto &c : word) {
-			if (!it->links[c - 'a']) return it->links[c - 'a'];
-			it = it->links[c - 'a'];
-		}
-		return it;		
-	}
-
-private:
-	TrieNode *root;
 };
 
-// Your Trie object will be instantiated and called as such:
-// Trie trie;
-// trie.insert("somestring");
-// trie.search("key");
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
 
 int main(void) {
 	Trie trie;
-	trie.insert("somestring");
-	cout << boolalpha << trie.search("key");
-	cout << "\tPassed\n";
+	string s;
+	bool answer, result;
+
+	s = "somestring";
+	trie.insert(s);
+	s = "key";
+	answer = false;
+	result = trie.search("key");
+	assert(answer == result);
+	s = "some";
+	answer = true;
+	result = trie.startsWith(s);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
