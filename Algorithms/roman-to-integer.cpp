04751bc82736c01ea2cec69145d9a9af47e0a69a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-22 11:05:04 -0500

    13. Roman to Integer

--- Algorithms/roman-to-integer.cpp
+++ Algorithms/roman-to-integer.cpp
@@ -13,17 +13,16 @@ using namespace std;
 class Solution {
 public:
 	int romanToInt(string s) {
-		unordered_map<char, int> h = {
-			{'I', 1},
-			{'V', 5},
-			{'X', 10},
-			{'L', 50},
-			{'C', 100},
-			{'D', 500},
-			{'M', 1000}
-		};
+		unordered_map<char, int> h;
+		h['I'] = 1;
+		h['V'] = 5;
+		h['X'] = 10;
+		h['L'] = 50;
+		h['C'] = 100;
+		h['D'] = 500;
+		h['M'] = 1000;
 		int result = h[s.back()];
-		for (int n = s.size(), i = n - 2; i >= 0; i--) {
+		for (int n = s.size(), i = n - 2; i >= 0; --i) {
 			if (h[s[i]] < h[s[i + 1]]) {
 				result -= h[s[i]];
 			}
