a51807629fddb91c69877d98eebd3b105ffd4b22
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-22 09:45:09 -0500

    201. Bitwise AND of Numbers Range

--- Algorithms/bitwise-and-of-numbers-range.cpp
+++ Algorithms/bitwise-and-of-numbers-range.cpp
@@ -1,26 +1,53 @@
 // 201. Bitwise AND of Numbers Range
 // https://leetcode.com/problems/bitwise-and-of-numbers-range/
-// https://discuss.leetcode.com/topic/13508/one-line-c-solution
-// https://discuss.leetcode.com/topic/12133/bit-operation-solution-java
-#include <iostream>
+
+/*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
 	int rangeBitwiseAnd(int m, int n) {
-		return (n > m) ? (this->rangeBitwiseAnd(m >> 1, n >> 1) << 1) : m;
+		while (n != (m & n)) {
+			n &= n - 1;
+		}
+		return n;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	int m = 5;
-	int n = 7;
-	cout << solution.rangeBitwiseAnd(m, n) << "\tPassed\n";
+	int m, n, answer, result;
+
+	m = 2;
+	n = 3;
+	answer = 2;
+	result = solution.rangeBitwiseAnd(m, n);
+	assert(answer == result);
+
+	m = 5;
+	n = 7;
+	answer = 4;
+	result = solution.rangeBitwiseAnd(m, n);
+	assert(answer == result);
+
 	m = 0;
 	n = 2147483647;
-	cout << solution.rangeBitwiseAnd(m, n) << "\tPassed\n";
+	answer = 0;
+	result = solution.rangeBitwiseAnd(m, n);
+	assert(answer == result);
+
 	m = 20000;
 	n = 2147483647;
-	cout << solution.rangeBitwiseAnd(m, n) << "\tPassed\n";
+	answer = 0;
+	result = solution.rangeBitwiseAnd(m, n);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
