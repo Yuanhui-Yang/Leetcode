fdac8912f956bb5f1cd14fdfe444c2ee141329eb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-21 20:40:02 -0500

    211. Add and Search Word - Data structure design

--- Algorithms/add-and-search-word-data-structure-design.cpp
+++ Algorithms/add-and-search-word-data-structure-design.cpp
@@ -1,66 +1,170 @@
 // 211. Add and Search Word - Data structure design
 // https://leetcode.com/problems/add-and-search-word-data-structure-design/
-#include <iostream>
-#include <string>
-#include <cstring>
+
+/*
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+
+For example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+Note:
+You may assume that all words are consist of lowercase letters a-z.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-class TrieNode {
-public:
-	TrieNode(): isEnd(false) {
-		memset(links, 0, sizeof(links));
-	}
-	TrieNode *links[26];
-	bool isEnd;
-};
+
 class WordDictionary {
 public:
-	WordDictionary(): root(new TrieNode()) {}
-	// Adds a word into the data structure.
+	/** Initialize your data structure here. */
+	WordDictionary() {
+		root = new Node();
+	}
+
+	/** Adds a word into the data structure. */
 	void addWord(string word) {
-		TrieNode *it = this->root;
-		for (size_t i = 0; i < word.size(); ++i) {
-			if (!it->links[word[i] - 'a']) it->links[word[i] - 'a'] = new TrieNode();
-			it = it->links[word[i] - 'a'];
+		Node* it = root;
+		for (const auto &ch : word) {
+			int d = ch - 'a';
+			if (!it->next[d]) {
+				it->next[d] = new Node();
+			}
+			it = it->next[d];
 		}
 		it->isEnd = true;
 	}
 
-	// Returns if the word is in the data structure. A word could
-	// contain the dot character '.' to represent any one letter.
+	/** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
 	bool search(string word) {
-		return this->search(0, word, this->root);
+		return f(root, word);
 	}
 private:
-	TrieNode *root;
-	bool search(size_t start, string word, TrieNode *current) {
-		if (!current) return false;
-		if (word[start] == '.') {
-			if (start + 1 == word.size()) {
-				for (int i = 0; i < 26; ++i) if (current->links[i] && current->links[i]->isEnd) return true;
-				return false;
-			}
-			for (int i = 0; i < 26; ++i) if (this->search(start + 1, word, current->links[i])) return true;
+	struct Node {
+		Node(void) {
+			isEnd = false;
+			memset(next, 0, sizeof(next));
+		}
+		bool isEnd;
+		Node* next[26];
+	};
+	Node *root;
+	bool f(Node* root, const string& s) {
+		if (s.empty()) {
+			return root and root->isEnd;
+		}
+		if (!root) {
 			return false;
 		}
-		if (start + 1 == word.size()) return current->links[word[start] - 'a'] && current->links[word[start] - 'a']->isEnd;
-		return current->links[word[start] - 'a'] && this->search(start + 1, word, current->links[word[start] - 'a']);
+		for (int n = s.size(), i = 0; i < n; ++i) {
+			char ch = s[i];
+			if (ch == '.') {
+				const string t = s.substr(i + 1);
+				for (int j = 0; j < 26; ++j) {
+					if (f(root->next[j], t)) {
+						return true;
+					}
+				}
+				return false;
+			}
+			int d = ch - 'a';
+			if (!root->next[d]) {
+				return false;
+			}
+			root = root->next[d];
+		}
+		return root->isEnd;
 	}
 };
 
-// Your WordDictionary object will be instantiated and called as such:
-// WordDictionary wordDictionary;
-// wordDictionary.addWord("word");
-// wordDictionary.search("pattern");
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * bool param_2 = obj.search(word);
+ */
 
 int main(void) {
 	WordDictionary wordDictionary;
-	wordDictionary.addWord("bad");
-	wordDictionary.addWord("dad");
-	wordDictionary.addWord("mad");
-	cout << boolalpha << wordDictionary.search("pad") << "\tPassed\n";
-	cout << boolalpha << wordDictionary.search("bad") << "\tPassed\n";
-	cout << boolalpha << wordDictionary.search(".ad") << "\tPassed\n";
-	cout << boolalpha << wordDictionary.search("b..") << "\tPassed\n";
+	string word;
+	bool answer, result;
+
+	word = "bad";
+	wordDictionary.addWord(word);
+
+	word = "dad";
+	wordDictionary.addWord(word);
+
+	word = "mad";
+	wordDictionary.addWord(word);
+
+	word = "pad";
+	answer = false;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "bad";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = ".ad";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "b..";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	wordDictionary = WordDictionary();
+
+	word = "a";
+	wordDictionary.addWord(word);
+
+	word = "a";
+	wordDictionary.addWord(word);
+
+	word = ".";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "a";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "aa";
+	answer = false;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "a";
+	answer = true;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = ".a";
+	answer = false;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
+	word = "a.";
+	answer = false;
+	result = wordDictionary.search(word);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
