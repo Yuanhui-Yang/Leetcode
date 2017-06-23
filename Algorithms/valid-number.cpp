e09f44c38ddafa2cf49d024b33f322d92f44ba7b
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-23 07:25:48 -0500

    65. Valid Number

--- Algorithms/valid-number.cpp
+++ Algorithms/valid-number.cpp
@@ -1,130 +1,131 @@
 // 65. Valid Number
 // https://leetcode.com/problems/valid-number/
-#include <iostream>
-#include <string>
-#include <vector>
+
+/*
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+
+Update (2015-02-10):
+The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	bool isNumber(const string& s) {
+	bool isNumber(string s) {
 		if (s.empty()) {
+		return false;
+		}
+		int n = s.size(), i = 0, dot = -1;
+		while (i < n and s[i] == ' ') {
+			++i;
+		}
+		if (i >= n or !(isdigit(s[i]) or s[i] == '+' or s[i] == '-' or s[i] == '.')) {
 			return false;
 		}
-		int indexOfSpace(-1);
-		int numOfSign(0);
-		int indexOfSign(-1);
-		int indexOfDot(-1);
-		int indexOfNumeric(-1);
-		int numOfE(0);
-		int indexOfE(-1);
-		for (size_t i(0); i < s.size(); ++i) {
-			if (indexOfSpace != -1) {
-				if (indexOfDot != -1 && indexOfDot < indexOfSpace) {
-					if (s[i] != ' ') {
-						return false;
-					}
-				}
-				if (indexOfNumeric != -1 && indexOfNumeric < indexOfSpace) {
-					if (s[i] != ' ') {
-						return false;
-					}
-				}
-				if (indexOfE != -1 && indexOfE < indexOfSpace) {
-					if (s[i] != ' ') {
-						return false;
-					}
-				}
-				if (indexOfSign != -1 && indexOfSign < indexOfSpace) {
-					if (s[i] != ' ') {
-						return false;
-					}
-				}
-			}
-			if (indexOfE != -1) {
-				if (s[i] < '0' || s[i] > '9') {
-					if (i == indexOfE + 1 && s[i] != '-' && s[i] != '+') {
-						return false;
-					}
-				}
-
-			}
-			if (s[i] == ' ') {
-				indexOfSpace = i;
-			}
-			else if (s[i] == '-' || s[i] == '+') {
-				if (numOfSign >= 2) {
-					return false;
-				}
-				if (i + 1 == s.size()) {
-					return false;
-				}
-				if (i > 0) {
-					if (s[i - 1] != 'e') {
-						if (indexOfDot + indexOfSign + indexOfE + indexOfNumeric != -4) {
-							return false;
-						}
-					}
-				}
-				indexOfSign = i;
-				++numOfSign;
-			}
-			else if (s[i] == 'e') {
-				if (i + 1 == s.size()) {
-					return false;
-				}
-				if (numOfE >= 2) {
-					return false;
-				}
-				if (indexOfE == -1) {
-					if (indexOfNumeric == -1) {
-						return false;
-					}
-					else {
-						indexOfE = i;
-						++numOfE;
-					}
-				}
-				else {
-					return false;
-				}
-			}
-			else if (s[i] == '.') {
-				if (indexOfDot == -1) {
-					if (indexOfE != -1) {
-						return false;
-					}
-					else {
-						indexOfDot = i;
-					}
-				}
-				else {
-					return false;
-				}
-			}
-			else if (s[i] >= '0' && s[i] <= '9') {
-				indexOfNumeric = i;
-			}
-			else {
+		if (s[i] == '+' or s[i] == '-') {
+			if (i + 1 >= n or !(isdigit(s[i + 1]) or s[i + 1] == '.')) {
 				return false;
 			}
+			++i;
 		}
-		return indexOfNumeric != -1;
+		if (s[i] == '.') {
+			if (i + 1 >= n or !isdigit(s[i + 1])) {
+				return false;
+			}
+			dot = i;
+			++i;
+		}
+		while (i < n and isdigit(s[i])) {
+			++i;
+		}
+		if (i >= n) {
+			return true;
+		}
+		if (!(s[i] == '.' or s[i] == 'e' or s[i] == ' ')) {
+			return false;
+		}
+		if (s[i] == '.') {
+			if (dot >= 0) {
+				return false;
+			}
+			if (i + 1 >= n) {
+				return true;
+			}
+			if (!(isdigit(s[i + 1]) or s[i + 1] == 'e' or s[i + 1] == ' ')) {
+				return false;
+			}
+			++i;
+		}
+		while (i < n and isdigit(s[i])) {
+			++i;
+		}
+		if (s[i] == 'e') {
+			if (i + 1 >= n or !(isdigit(s[i + 1]) or s[i + 1] == '+' or s[i + 1] == '-')) {
+				return false;
+			}
+			++i;
+		}
+		if (s[i] == '+' or s[i] == '-') {
+			if (i + 1 >= n and !isdigit(s[i + 1])) {
+				return false;
+			}
+			++i;
+		}
+		while (i < n and isdigit(s[i])) {
+			++i;
+		}
+		while (i < n and s[i] == ' ') {
+			++i;
+		}
+		return i >= n;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<string> a({ " 0.1 ", " 0", "abc", "1 a", "2e10", ".", "0e ", "-1.", " 005047e+6", "6+1", "4e+" });
-	vector<bool> b({ true, true, false, false, true, false, false, true, true, false, false });
-	for (size_t i(0); i < b.size(); ++i) {
-		if (b[i] ^ solution.isNumber(a[i])) {
-			cout << "\nError\n";
-			getchar();
-			return 0;
-		}
-		else
-			cout << "\nPassed\n";
-	}
+	string s;
+	bool answer, result;
+
+	s = " 005047e+6";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
+	s = "1.431352e7";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
+	s = "3. ";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
+	s = ".1";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
+	s = "1 ";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
+	s = "1 ";
+	answer = true;
+	result = solution.isNumber(s);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
-	getchar();
 	return 0;
 }
