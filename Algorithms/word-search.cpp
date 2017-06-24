232d7347a5d5f24083fef7405e9c4a33c3d25801
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-24 07:35:53 -0500

    79. Word Search

--- Algorithms/word-search.cpp
+++ Algorithms/word-search.cpp
@@ -1,185 +1,108 @@
 // 79. Word Search
 // https://leetcode.com/problems/word-search/
-#include <iostream>
-#include <vector>
-#include <string>
+
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,
+Given board =
+
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-	bool exist(const vector<vector<char>>& board, const string& word) {
-		vector<vector<int>> path(board.size(), vector<int>(board.front().size(), 0));
-		bool result(false);
-		this->exist(result, 0, 0, 0, path, board, word);
-		return result;
+	bool exist(vector<vector<char>>& board, string word) {
+		int m = board.size(), n = !m ? 0 : board.front().size(), l = word.size();
+		if (!m or !n or !l) {
+			return false;
+		}
+		int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (dfs(board, word, dx, dy, i, j, 0)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 private:
-	void exist(bool& result,
-		const size_t& start,
-		const size_t& row, 
-		const size_t& column,
-		vector<vector<int>>& path,
-		const vector<vector<char>>& board, 
-		const string& word) {
-		if (word.size() == 0 
-			|| board.size() == 0 
-			|| word.size() > board.size() * board.front().size()) {
-			result = false;
-			return;
+	bool dfs(vector<vector<char>>& board, string& word, int *dx, int *dy, int i, int j, int k) {
+		int m = board.size(), n = !m ? 0 : board.front().size(), l = word.size();
+		if (k >= l) {
+			return true;
 		}
-		if (word.size() == 1) {
-			for (const auto& i : board) {
-				for (const auto& j : i) {
-					if (j == word[0]) {
-						result = true;
-						return;
-					}
-				}
-			}
-			result = false;
-			return;
+		if (i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]) {
+			return false;
 		}
-		if (start == word.size() - 1) {
-			if (column >= 1
-				&& path[row][column - 1] == 0
-				&& board[row][column - 1] == word[start]) {
-				result = true;
-				return;
+		char ch = board[i][j];
+		board[i][j] = ' ';
+		for (int l = 0; l < 4; ++l) {
+			int nx = i + dx[l], ny = j + dy[l];
+			if (dfs(board, word, dx, dy, nx, ny, k + 1)) {
+				return true;
 			}
-			if (column + 1 < board.front().size()
-				&& path[row][column + 1] == 0
-				&& board[row][column + 1] == word[start]) {
-				result = true;
-				return;
-			}
-			if (row >= 1
-				&& path[row - 1][column] == 0
-				&& board[row - 1][column] == word[start]) {
-				result = true;
-				return;
-			}
-			if (row + 1 < board.size()
-				&& path[row + 1][column] == 0
-				&& board[row + 1][column] == word[start]) {
-				result = true;
-				return;			
-			}
-			result = false;
-			return;
 		}
-		if (start == 0) {
-			for (size_t i(0); i < board.size(); ++i) {
-				for (size_t j(0); j < board.front().size(); ++j) {
-					if (board[i][j] == word[start]) {
-						result = false;
-						path[i][j] = 1;
-						this->exist(result, start + 1, i, j, path, board, word);
-						path[i][j] = 0;
-						if (result == true) {
-							return;
-						}
-					}
-				}
-			}
-			result = false;
-			return;
-		}
-		else {
-			if (column >= 1
-				&& path[row][column - 1] == 0
-				&& board[row][column - 1] == word[start]) {
-				path[row][column - 1] = 1;
-				this->exist(result, start + 1, row, column - 1, path, board, word);
-				path[row][column - 1] = 0;
-				if (result == true) {
-					return;
-				}
-			}
-			if (column + 1 < board.front().size()
-				&& path[row][column + 1] == 0
-				&& board[row][column + 1] == word[start]) {
-				path[row][column + 1] = 1;
-				this->exist(result, start + 1, row, column + 1, path, board, word);
-				path[row][column + 1] = 0;
-				if (result == true) {
-					return;
-				}
-			}
-			if (row >= 1
-				&& path[row - 1][column] == 0
-				&& board[row - 1][column] == word[start]) {
-				path[row - 1][column] = 1;
-				this->exist(result, start + 1, row - 1, column, path, board, word);
-				path[row - 1][column] = 0;
-				if (result == true) {
-					return;
-				}
-			}
-			if (row + 1 < board.size()
-				&& path[row + 1][column] == 0
-				&& board[row + 1][column] == word[start]) {
-				path[row + 1][column] = 1;
-				this->exist(result, start + 1, row + 1, column, path, board, word);
-				path[row + 1][column] = 0;
-				if (result == true) {
-					return;
-				}			
-			}
-			result = false;
-			return;
-		}
+		board[i][j] = ch;
+		return false;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<vector<char>> board({
-								vector<char>({'A','B','C','E'}),
-								vector<char>({'S','F','C','S'}),
-								vector<char>({'A','D','E','E'})
-								});
-	string word("ABCCED");
-	cout << boolalpha << true << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
+	vector<vector<char>> board;
+	string word;
+	bool answer, result;
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+	word = "ABCCED";
+	answer = true;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
 	word = "SEE";
-	cout << boolalpha << true << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
+	answer = true;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
 	word = "ABCB";
-	cout << boolalpha << false << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
-	board = vector<vector<char>>({
-								vector<char>({'a', 'b'}),
-								vector<char>({'c', 'd'})
-								});
-	word = "cabd";
-	cout << boolalpha << true << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
-	board = vector<vector<char>>({
-								vector<char>({'a', 'b', 'c'}),
-								vector<char>({'a', 'e', 'd'}),
-								vector<char>({'a', 'f', 'g'}),
-								});
-	word = "abcdefg";
-	cout << boolalpha << true << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
-	board = vector<vector<char>>({
-								vector<char>({'F', 'Y', 'C', 'E', 'N', 'R', 'D'}),
-								vector<char>({'K', 'L', 'N', 'F', 'I', 'N', 'U'}),
-								vector<char>({'A', 'A', 'A', 'R', 'A', 'H', 'R'}),
-								vector<char>({'N', 'D', 'K', 'L', 'P', 'N', 'E'}),
-								vector<char>({'A', 'L', 'A', 'N', 'S', 'A', 'P'}),
-								vector<char>({'O', 'O', 'G', 'O', 'T', 'P', 'N'}),
-								vector<char>({'H', 'P', 'O', 'L', 'A', 'N', 'O'})
-							});
-	word = "INDIA";
-	cout << boolalpha << false << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";
-	board = vector<vector<char>>({
-								vector<char>({'b', 'b'}),
-								vector<char>({'a', 'b'}),
-								vector<char>({'b', 'a'})
-							});
+	answer = false;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
+	board = {{'a', 'b'}};
+	word = "ba";
+	answer = true;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
+	board = {{'a'}};
 	word = "a";
-	cout << boolalpha << true << '\t' << solution.exist(board, word) << '\n';
-	cout << "\nPassed\n";	
+	answer = true;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
+	board = {{'a', 'a'}};
+	word = "aaa";
+	answer = false;
+	result = solution.exist(board, word);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
