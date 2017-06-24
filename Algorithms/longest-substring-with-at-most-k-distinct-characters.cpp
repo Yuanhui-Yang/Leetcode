6be764e2a031080e78ee615decd4d33f985bfea3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-24 06:15:39 -0500

    340. Longest Substring with At Most K Distinct Characters

--- Algorithms/longest-substring-with-at-most-k-distinct-characters.cpp
+++ Algorithms/longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,37 +1,79 @@
 // 340. Longest Substring with At Most K Distinct Characters
 // https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
-#include <iostream>
-#include <utility>
-#include <string>
-#include <vector>
-#include <iterator>
-#include <algorithm>
-#include <unordered_map>
+
+/*
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = “eceba” and k = 2,
+
+T is "ece" which its length is 3.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/7399/share-my-c-solution
+
 class Solution {
 public:
 	int lengthOfLongestSubstringKDistinct(string s, int k) {
-		const int n = s.size();
-		int result = 0;
-		vector<int> hashmap(256, 0);
-		for (int i = 0, begin = 0, count = 0; i < n; i++) {
-			hashmap[s[i]]++;
-			if (hashmap[s[i]] == 1) count++;
-			while (count > k) {
-				hashmap[s[begin]]--;
-				if (hashmap[s[begin]] == 0) count--;
-				begin++;
+		if (k <= 0) {
+			return 0;
+		}
+		int n = s.size(), i = 0, j = 0, result = 0, h[256];
+		memset(h, 0, sizeof(h));
+		while (j < n) {
+			while (j < n and (k > 0 or h[s[j] - 0] > 0)) {
+				if (h[s[j++] - 0]++ == 0) {
+					--k;
+				}
 			}
-			result = max(result, i - begin + 1);
+			if (result == 0 or j - i > result) {
+				result = j - i;
+			}
+			while (k == 0) {
+				if (--h[s[i++] - 0] == 0) {
+					++k;
+				}
+			}
 		}
 		return result;
 	}
 };
-// END: https://discuss.leetcode.com/topic/7399/share-my-c-solution
+
 int main(void) {
 	Solution solution;
-	cout << solution.lengthOfLongestSubstringKDistinct("eceba", 2) << "\tPasse\n";
+	string s;
+	int k, answer, result;
+
+	s = "nfhiexxjrtvpfhkrxcutexxcodfioburrtjefrgwrnqtyzelvtpvwdvvpsbudwtiryqzzy";
+	k = 25;
+	answer = 70;
+	result = solution.lengthOfLongestSubstringKDistinct(s, k);
+	assert(answer == result);
+
+	s = "eceba";
+	k = 2;
+	answer = 3;
+	result = solution.lengthOfLongestSubstringKDistinct(s, k);
+	assert(answer == result);
+
+	s = "a";
+	k = 0;
+	answer = 0;
+	result = solution.lengthOfLongestSubstringKDistinct(s, k);
+	assert(answer == result);
+
+	s = "a";
+	k = 2;
+	answer = 1;
+	result = solution.lengthOfLongestSubstringKDistinct(s, k);
+	assert(answer == result);
+
+	s = "aa";
+	k = 1;
+	answer = 2;
+	result = solution.lengthOfLongestSubstringKDistinct(s, k);
+	assert(answer == result);
+
 	cout << "\nPassed All\n";
 	return 0;
 }
