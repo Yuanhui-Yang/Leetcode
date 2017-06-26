fb3cabbc0955e252ad90d8aaf2411c4b97f99188
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-26 11:07:33 -0500

    206. Reverse Linked List

--- Algorithms/reverse-linked-list.cpp
+++ Algorithms/reverse-linked-list.cpp
@@ -1,39 +1,68 @@
-//206. Reverse Linked List
-//https://leetcode.com/problems/reverse-linked-list/
-//https://leetcode.com/discuss/34474/in-place-iterative-and-recursive-java-solution
-#include <iostream>
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+/*
+Reverse a singly linked list.
+
+click to show more hints.
+
+Hint:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 struct ListNode {
 	int val;
 	ListNode *next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
-class Solution {
-public:
-	ListNode* reverseList(ListNode* head) {
-		ListNode *beforehead = NULL;
-		while (head) {
-			ListNode *nexthead = head->next;
-			head->next = beforehead;
-			beforehead = head;
-			head = nexthead;
-		}
-		return beforehead;
-	}
-};
-int main(int argc, char** argv) {
-	Solution solution;
-	ListNode* head = new ListNode(0);
-	ListNode* it = head;
-	for (int i = 1; i <= 10; ++i) {
+
+ListNode *f(vector<int>& v) {
+	ListNode dummy(0), *it = &dummy;
+	for (const auto &i : v) {
 		it->next = new ListNode(i);
 		it = it->next;
 	}
-	ListNode* newHead = solution.reverseList(head);
-	it = newHead;
-	while (it) {
-		cout << it->val << '\n';
-		it = it->next;
+	return dummy.next;
+}
+
+vector<int> g(ListNode* head) {
+	vector<int> v;
+	while (head) {
+		v.push_back(head->val);
+		head = head->next;
 	}
+	return v;
+}
+
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode *result = NULL;
+		while (head) {
+			ListNode *next = head->next;
+			head->next = result;
+			result = head;
+			head = next;
+		}
+		return result;
+	}
+};
+
+int main(void) {
+	Solution solution;
+	ListNode* head;
+	vector<int> u, v;
+
+	u = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	v = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	head = f(u);
+	head = solution.reverseList(head);
+	u = g(head);
+	assert(u == v);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
