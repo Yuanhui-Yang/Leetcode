96f3d769ea2891f7e3f87bfca480dd34708f147a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-26 13:51:11 -0500

    92. Reverse Linked List II

--- Algorithms/reverse-linked-list-ii.cpp
+++ Algorithms/reverse-linked-list-ii.cpp
@@ -1,53 +1,94 @@
-//92. Reverse Linked List II
-//https ://leetcode.com/problems/reverse-linked-list-ii/
-#include <iostream>
+// 92. Reverse Linked List II
+// https://leetcode.com/problems/reverse-linked-list-ii/
+
+/*
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ? m ? n ? length of list.
+*/
+
+#include <bits/stdc++.h>
 using namespace std;
+
 struct ListNode {
 	int val;
-	ListNode* next;
-	ListNode(const int& x) : val(x), next(0) {}
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
 };
+
+ListNode *f(vector<int>& v) {
+	ListNode dummy(0), *it = &dummy;
+	for (const auto &i : v) {
+		it->next = new ListNode(i);
+		it = it->next;
+	}
+	return dummy.next;
+}
+
+vector<int> g(ListNode* head) {
+	vector<int> v;
+	while (head) {
+		v.push_back(head->val);
+		head = head->next;
+	}
+	return v;
+}
+
 class Solution {
 public:
-	ListNode* reverseBetween(ListNode* head, const int& m, const int& n) {
-		if (!head || !head->next || m == n) return head;
-		ListNode dummy(0);
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		if (!head or m >= n) {
+			return head;
+		}
+		ListNode dummy(0), *a = &dummy;
 		dummy.next = head;
-		ListNode* previous_start(&dummy);
-		for (int i = 0; i < m - 1; ++i) {
-			previous_start = previous_start->next;
+		for (int i = 1; i < m; ++i) {
+			a = a->next;
 		}
-		//ListNode* next_end(&dummy);
-		//for (int i = 0; i < n + 1; ++i) {
-		//	next_end = next_end->next;
-		//}
-		ListNode* previous = previous_start->next;
-		ListNode* current = previous->next;
-		ListNode* next = current->next;
-		for (int i = 0; i < n - m; ++i) {
-			next = current->next;
-			current->next = previous;
-			previous = current;
-			current = next;
+		ListNode *b = a->next, *c = b->next;
+		for (int i = m; i < n; ++i) {
+			ListNode *d = c->next;
+			c->next = b;
+			b = c;
+			c = d;
 		}
-		ListNode* next_end = current;
-		previous_start->next->next = next_end;
-		previous_start->next = previous;
+		a->next->next = c;
+		a->next = b;
 		return dummy.next;
 	}
 };
-int main(int argc, char** argv) {
+
+int main(void) {
 	Solution solution;
-	ListNode* head = new ListNode(1);
-	//head->next = new ListNode(2);
-	//head->next->next = new ListNode(3);
-	//head->next->next->next = new ListNode(4);
-	//head->next->next->next->next = new ListNode(5);
-	for (ListNode* it = solution.reverseBetween(head, 1, 1); it; it = it->next) {
-		cout << it->val << '\t';
-	}
-	cout << '\n';
-	cout << "Passed\n";
-	getchar();
+	ListNode* head;
+	vector<int> u, v;
+	int m, n;
+
+	u = {3, 5};
+	v = {5, 3};
+	m = 1;
+	n = 2;
+	head = f(u);
+	head = solution.reverseBetween(head, m, n);
+	u = g(head);
+	assert(u == v);
+
+	u = {1, 2, 3, 4, 5};
+	v = {1, 4, 3, 2, 5};
+	m = 2;
+	n = 4;
+	head = f(u);
+	head = solution.reverseBetween(head, m, n);
+	u = g(head);
+	assert(u == v);
+
+	cout << "\nPassed All\n";
 	return 0;
 }
