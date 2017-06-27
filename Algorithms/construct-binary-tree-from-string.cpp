49e083dca5f6d7256075f85218b152ef54171522
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-27 00:48:06 -0500

    536. Construct Binary Tree from String

--- Algorithms/construct-binary-tree-from-string.cpp
+++ Algorithms/construct-binary-tree-from-string.cpp
@@ -1,48 +1,28 @@
 // 536. Construct Binary Tree from String
 // https://leetcode.com/problems/construct-binary-tree-from-string/
 
-// You need to construct a binary tree from a string consisting of parenthesis and integers.
+/*
+You need to construct a binary tree from a string consisting of parenthesis and integers.
 
-// The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
 
-// You always start to construct the left child node of the parent first if it exists.
+You always start to construct the left child node of the parent first if it exists.
 
-// Example:
-// Input: "4(2(3)(1))(6(5))"
-// Output: return the tree root node representing the following tree:
+Example:
+Input: "4(2(3)(1))(6(5))"
+Output: return the tree root node representing the following tree:
 
-//        4
-//      /   \
-//     2     6
-//    / \   / 
-//   3   1 5   
-// Note:
-// There will only be '(', ')', '-' and '0' ~ '9' in the input string.
+       4
+     /   \
+    2     6
+   / \   / 
+  3   1 5   
+Note:
+There will only be '(', ')', '-' and '0' ~ '9' in the input string.
+An empty tree is represented by "" instead of "()".
+*/
 
-#include <iostream> // std::cout; std::cin
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // memset
-#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::string::npos
-#include <list>
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct TreeNode {
@@ -52,84 +32,78 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+void gc(TreeNode* t) {
+	if (t) {
+		gc(t->left);
+		gc(t->right);
+		delete t;
+	}
+}
+
+bool isSameTree(TreeNode *p, TreeNode *q) {
+	if (!p and !q) {
+		return true;
+	}
+	if (!p and q) {
+		return false;
+	}
+	if (p and !q) {
+		return false;
+	}
+	if (p->val != q->val) {
+		return false;
+	}
+	return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
+}
+
 class Solution {
 public:
 	TreeNode* str2tree(string s) {
 		if (s.empty()) {
 			return NULL;
 		}
-		const size_t n = s.size();
-		size_t i = 0;
-		while (i < n and (isdigit(s.at(i)) or s.at(i) == '-')) {
-			i++;
+		int n = s.size(), i = 0;
+		while (i < n and s[i] != '(') {
+			++i;
 		}
-		if (i == n) {
-			return new TreeNode(stoi(s));
+		TreeNode *root = new TreeNode(stoi(s.substr(0, i)));
+		if (i >= n) {
+			return root;
 		}
-		stack<size_t> stk;
-		TreeNode *root = NULL;
-		string left = "", right = "";
-		for (size_t i = 0, l1 = string::npos, r1 = string::npos, l2 = string::npos, r2 = string::npos; i < n; i++) {
-			if (s.at(i) == '(') {
-				if (root == NULL) {
-					// cout << stoi(s.substr(0, i)) << '\n';
-					root = new TreeNode(stoi(s.substr(0, i)));
-					l1 = i;
-					stk.push(i);
-					continue;
-				}
-				if (stk.empty()) {
-					l2 = i;
-				}
-				stk.push(i);
-				continue;
+		int j = ++i, k = 1;
+		while (j < n and k > 0) {
+			if (s[j] == '(') {
+				++k;
 			}
-			if (s.at(i) == ')') {
-				stk.pop();
-				if (stk.empty()) {
-					if (left.empty() and right.empty()) {
-						r1 = i;
-						left = s.substr(l1 + 1, r1 - l1 - 1);
-						// cout << left << '\n';
-						continue;
-					}
-					if (!left.empty() and right.empty()) {
-						r2 = i;
-						right = s.substr(l2 + 1, r2 - l2 - 1);
-						// cout << right << '\n';
-						continue;
-					}
-				}
-				continue;
+			else if (s[j] == ')') {
+				--k;
 			}
+			++j;
 		}
-		root->left = str2tree(left);
-		root->right = str2tree(right);
-		// cout << root->val << '\n';
+		root->left = str2tree(s.substr(i, j - i - 1));
+		if (j >= n) {
+			return root;
+		}
+		i = ++j;
+		k = 1;
+		while (i < n and k > 0) {
+			if (s[i] == '(') {
+				++k;
+			}
+			else if (s[i] == ')') {
+				--k;
+			}
+			++i;
+		}
+		root->right = str2tree(s.substr(j, i - j - 1));
 		return root;
-	}
-public:
-	bool isSameTree(TreeNode *p, TreeNode *q) {
-		if (!p and !q) {
-			return true;
-		}
-		if (!p and q) {
-			return false;
-		}
-		if (p and !q) {
-			return false;
-		}
-		if (p->val != q->val) {
-			return false;
-		}
-		return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
 	}
 };
 
 int main(void) {
 	Solution solution;
 	string s;
-	TreeNode *result = NULL, *answer = NULL;
+	TreeNode *result, *answer;
 
 	s = "4(2(3)(1))(6(5))";
 	answer = new TreeNode(4);
@@ -139,7 +113,7 @@ int main(void) {
 	answer->left->right = new TreeNode(1);
 	answer->right->left = new TreeNode(5);
 	result = solution.str2tree(s);
-	assert(solution.isSameTree(result, answer));
+	assert(isSameTree(result, answer));
 
 	cout << "\nPassed All\n";
 	return 0;
