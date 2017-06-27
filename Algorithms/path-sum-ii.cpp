c6bba295d6bf23ca307a2d7d9ff3f3a72d20595a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-27 14:29:36 -0500

    113. Path Sum II

--- Algorithms/path-sum-ii.cpp
+++ Algorithms/path-sum-ii.cpp
@@ -1,57 +1,60 @@
 // 113. Path Sum II
 // https://leetcode.com/problems/path-sum-ii/
-#include <iostream>
-#include <vector>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	vector<vector<int>> pathSum(TreeNode* root, int sum) {
 		vector<vector<int>> result;
-		vector<int> path;
-		this->pathSum(result, path, root, sum);
+		vector<int> v;
+		f(result, v, root, sum);
 		return result;
 	}
 private:
-	void pathSum(vector<vector<int>>& result, vector<int>& path, TreeNode* root, int sum) {
-		if (!root) return;
-		if (root->val == sum && !root->left && !root->right) {
-			path.push_back(root->val);
-			result.push_back(path);
-			path.pop_back();
+	void f(vector<vector<int>>& result, vector<int>& v, TreeNode* root, int sum) {
+		if (!root) {
 			return;
 		}
-		path.push_back(root->val);
-		this->pathSum(result, path, root->left, sum - root->val);
-		path.pop_back();
-		path.push_back(root->val);
-		this->pathSum(result, path, root->right, sum - root->val);
-		path.pop_back();
-	}	
-};
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(5);
-	root->left = new TreeNode(4);
-	root->right = new TreeNode(8);
-	root->left->left = new TreeNode(11);
-	root->right->left = new TreeNode(13);
-	root->right->right = new TreeNode(4);
-	root->left->left->left = new TreeNode(7);
-	root->left->left->right = new TreeNode(2);
-	root->right->right->left = new TreeNode(5);
-	root->right->right->right = new TreeNode(1);
-	for (const auto &i : solution.pathSum(root, 22)) {
-		for (const auto &j : i)
-			cout << j << '\t';
-		cout << '\n';
+		if (!root->left and !root->right) {
+			if (sum == root->val) {
+				v.push_back(root->val);
+				result.push_back(v);
+				v.pop_back();
+			}
+			return;
+		}
+		v.push_back(root->val);
+		f(result, v, root->left, sum - root->val);
+		v.pop_back();
+		v.push_back(root->val);
+		f(result, v, root->right, sum - root->val);
+		v.pop_back();
 	}
-	cout << "\nPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
+};
