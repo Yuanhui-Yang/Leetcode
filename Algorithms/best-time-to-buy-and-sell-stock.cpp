1b19488acf9b0c3b9a9630e62bcce5f4017d2b63
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-28 02:58:37 -0500

    121. Best Time to Buy and Sell Stock

--- Algorithms/best-time-to-buy-and-sell-stock.cpp
+++ Algorithms/best-time-to-buy-and-sell-stock.cpp
@@ -1,57 +1,31 @@
 // 121. Best Time to Buy and Sell Stock
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-// https://discuss.leetcode.com/topic/52580/java-solution-in-o-n-time-and-o-k-space/2
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
-#include <climits>
-using namespace std;
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+*/
+
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        const int n = prices.size();
-        if (n < 2) return 0;
-        int result = 0;
-        for (int i = 1, previous = prices[0]; i < n; i++) {
-            int current = prices[i];
-            result = max(result, current - previous);
-            previous = min(previous, current);
-        }
-        return result;
-    }
+	int maxProfit(vector<int>& prices) {
+		int buy = INT_MIN, sell = 0;
+		for (const auto &i : prices) {
+			sell = max(sell, buy + i);
+			buy = max(buy, -i);
+		}
+		return sell;
+	}
 };
-// class Solution {
-// public:
-// 	int maxProfit(const vector<int>& prices) {
-// 		return this->maxProfit(1, prices);
-// 	}
-// private:
-// 	int maxProfit(const size_t& k, const vector<int>& prices) {
-// 		if (prices.empty()) return 0;
-// 		if (k >= prices.size() / 2) {
-// 			int result = 0;
-// 			for (size_t i = 1; i < prices.size(); ++i) result += max(0, prices[i] - prices[i - 1]);
-// 			return result;
-// 		}
-// 		vector<int> buy(k + 1, INT_MIN);
-// 		vector<int> sell(k + 1, 0);
-// 		for (const auto& price : prices) {
-// 			for (size_t i = 1; i < k + 1; ++i) {
-// 				buy[i] = max(buy[i], sell[i - 1] - price);
-// 				sell[i] = max(sell[i], buy[i] + price);
-// 			}
-// 		}
-// 		return sell[k];
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	vector<int> prices;
-	prices = {7, 1, 5, 3, 6, 4};
-	assert(5 == solution.maxProfit(prices));
-	prices = {7, 6, 4, 3, 1};
-	assert(0 == solution.maxProfit(prices));
-	cout << "\nPassed All\n";
-	return 0;
-}
