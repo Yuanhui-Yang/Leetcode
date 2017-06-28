ed7a401581e88d757502931c561ab57afa0ca18f
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-28 06:17:09 -0500

    123. Best Time to Buy and Sell Stock III

--- Algorithms/best-time-to-buy-and-sell-stock-iii.cpp
+++ Algorithms/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,49 +1,25 @@
 // 123. Best Time to Buy and Sell Stock III
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
-#include <climits>
-using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/52580/java-solution-in-o-n-time-and-o-k-space
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
-		return this->maxProfit(2, prices);
-	}
-private:
-	int maxProfit(int k, vector<int>& prices) {
-		const int n = prices.size();
-		if (n < 2) return 0;
-		if (k >= n / 2) {
-			int result = 0;
-			for (int i = 1; i < n; i++) result += max(0, prices[i] - prices[i - 1]);
-			return result;
-		} 
-		vector<int> buys(k + 1, INT_MIN);
-		vector<int> sells(k + 1, 0);
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < k + 1; j++) {
-				buys[j] = max(sells[j - 1] - prices[i], buys[j]);
-				sells[j] = max(buys[j] + prices[i], sells[j]);
+		vector<int> buys(3, INT_MIN), sells(3, 0);
+		for (const auto &price : prices) {
+			for (int i = 1; i <= 2; ++i) {
+				sells[i] = max(sells[i], buys[i] + price);
+				buys[i] = max(buys[i], sells[i - 1] - price);
 			}
 		}
-		return sells[k];
+		return sells[2];
 	}
 };
-// END: https://discuss.leetcode.com/topic/52580/java-solution-in-o-n-time-and-o-k-space
-int main(void) {
-	Solution solution;
-	vector<int> prices;
-	prices = {1, 2, 4};
-	assert(3 == solution.maxProfit(prices));
-	prices = {7, 6, 4, 3, 1};
-	assert(0 == solution.maxProfit(prices));
-	prices = {6, 1, 3, 2, 4, 7};
-	assert(7 == solution.maxProfit(prices));
-	prices = {3, 3, 5, 0, 0, 3, 1, 4};
-	assert(6 == solution.maxProfit(prices));
-	cout << "\nPassed All\n";
-	return 0;
-}
