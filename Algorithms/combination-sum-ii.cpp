ae672d60838405138b36568ebeb975d277d96276
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-29 17:44:37 -0500

    40. Combination Sum II

--- Algorithms/combination-sum-ii.cpp
+++ Algorithms/combination-sum-ii.cpp
@@ -19,56 +19,31 @@ A solution set is:
 ]
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-		if (candidates.empty()) {
-			return {};
-		}
 		sort(begin(candidates), end(candidates));
 		vector<vector<int>> result;
 		vector<int> v;
-		dfs(result, candidates, v, target, 0, candidates.size());
+		f(result, v, candidates, target, 0);
 		return result;
 	}
 private:
-	void dfs(vector<vector<int>>& result, const vector<int>& candidates, vector<int>& v, int target, size_t i, const size_t n) {
+	void f(vector<vector<int>>& result, vector<int>& v, vector<int>& candidates, int target, int i) {
+		if (target < 0) {
+			return;
+		}
 		if (target == 0) {
 			result.push_back(v);
 			return;
 		}
-		if (target < 0) {
-			return;
-		}
-		for (size_t j = i; j < n; j++) {
-			if (j > i and candidates[j] == candidates[j - 1]) {
+		for (int n = candidates.size(), j = i; j < n; ++j) {
+			if (j > i and candidates[j - 1] == candidates[j]) {
 				continue;
 			}
-			int val = candidates[j];
-			v.push_back(val);
-			dfs(result, candidates, v, target - val, j + 1, n);
+			v.push_back(candidates[j]);
+			f(result, v, candidates, target - candidates[j], j + 1);
 			v.pop_back();
 		}
 	}
 };
-
-int main(void) {
-	Solution solution;
-	int target;
-	vector<int> candidates;
-	vector<vector<int>> result, answer;
-
-	target = 8;
-	candidates = {10, 1, 2, 7, 6, 1, 5};
-	answer = {{1, 7}, {1, 2, 5}, {2, 6}, {1, 1, 6}};
-	sort(begin(answer), end(answer));
-	result = solution.combinationSum2(candidates, target);
-	sort(begin(result), end(result));
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
