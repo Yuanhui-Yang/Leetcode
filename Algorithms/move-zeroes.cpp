f9345f74e08b8c5c423ac83bafec31874651c2e2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-29 18:17:43 -0500

    283. Move Zeroes

--- Algorithms/move-zeroes.cpp
+++ Algorithms/move-zeroes.cpp
@@ -11,68 +11,16 @@ You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
-// BEGIN: https://discuss.leetcode.com/topic/24716/simple-o-n-java-solution-using-insert-index
 class Solution {
 public:
 	void moveZeroes(vector<int>& nums) {
-		if (nums.empty()) {
-			return;
-		}
-		int n = nums.size(), idx = 0;
-		for (const auto &i : nums) {
-			if (i) {
-				nums.at(idx++) = i; 
+	int n = nums.size(), i = 0, j = 0;
+		while (j < n) {
+			if (!nums[j]) {
+				++j;
+				continue;
 			}
-		}
-		for (int i = idx; i < n; i++) {
-			nums.at(i) = 0;
+			swap(nums[i++], nums[j++]);
 		}
 	}
 };
-// END: https://discuss.leetcode.com/topic/24716/simple-o-n-java-solution-using-insert-index
-
-int main(void) {
-	Solution solution;
-	vector<int> nums, answer;
-
-	nums = {0, 0, 1};
-	answer = {1, 0, 0};
-	solution.moveZeroes(nums);
-	assert(answer == nums);
-
-	nums = {0, 1, 0, 3, 12};
-	answer = {1, 3, 12, 0, 0};
-	solution.moveZeroes(nums);
-	assert(answer == nums);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
