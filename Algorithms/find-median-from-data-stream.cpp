486e3173b74fef12311354b34c67f56a314ff2ec
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-29 13:49:08 -0500

    295. Find Median from Data Stream

--- Algorithms/find-median-from-data-stream.cpp
+++ Algorithms/find-median-from-data-stream.cpp
@@ -1,172 +1,64 @@
 // 295. Find Median from Data Stream
 // https://leetcode.com/problems/find-median-from-data-stream/
-#include <iostream>
-#include <cassert>
-#include <cmath>
-#include <set>
-#include <cfloat>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+
+Examples: 
+[2,3,4] , the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+For example:
+
+addNum(1)
+addNum(2)
+findMedian() -> 1.5
+addNum(3) 
+findMedian() -> 2
+*/
+
 class MedianFinder {
 public:
+	/** initialize your data structure here. */
+	MedianFinder() {}
 
-	// Adds a number into the data structure.
 	void addNum(int num) {
-		if (minSet.empty() && maxSet.empty()) {
-			minSet.insert(num);
-			return;
+		if (a.size() <= b.size()) {
+			a.push(num);
 		}
-		if (minSet.size() < maxSet.size()) {
-			if (num <= *begin(maxSet)) {
-				minSet.insert(num);
-				return;
-			}
-			if (num > *begin(maxSet)) {
-				minSet.insert(*begin(maxSet));
-				maxSet.erase(begin(maxSet));
-				maxSet.insert(num);
-				return;
-			}
-			return;
+		else {
+			b.push(num);
 		}
-		if (minSet.size() > maxSet.size()) {
-			if (num >= *prev(end(minSet))) {
-				maxSet.insert(num);
-				return;
-			}
-			if (num < *prev(end(minSet))) {
-				maxSet.insert(*prev(end(minSet)));
-				minSet.erase(prev(end(minSet)));
-				minSet.insert(num);
-				return;
-			}
-			return;
+		if (!a.empty() and !b.empty() and a.top() > b.top()) {
+			int x = a.top(), y = b.top();
+			a.pop(); b.pop();
+			a.push(y);
+			b.push(x);
 		}
-		if (minSet.size() == maxSet.size()) {
-			if (num <= *begin(maxSet)) {
-				minSet.insert(num);
-				return;
-			}
-			if (num > *begin(maxSet)) {
-				maxSet.insert(num);
-				return;
-			}
-			return;
-		}
-		return;
 	}
 
-	// Returns the median of current data stream
 	double findMedian() {
-		if (minSet.size() < maxSet.size()) {
-			return double(*begin(maxSet));
+		if (a.size() < b.size()) {
+			return b.top();
 		}
-		if (minSet.size() > maxSet.size()) {
-			return double(*prev(end(minSet)));
+		if (a.size() > b.size()) {
+			return a.top();
 		}
-		if (minSet.size() == maxSet.size()) {
-			double minSetMaxVal = double(*prev(end(minSet)));
-			double maxSetMinVal = double(*begin(maxSet));	
-			return minSetMaxVal / 2.0 + maxSetMinVal / 2.0;
-		}
-		return 0.0;
+		return 0.5 * a.top() + 0.5 * b.top();
 	}
-
 private:
-	multiset<int> minSet;
-	multiset<int> maxSet;
+	priority_queue<int> a;
+	priority_queue<int, vector<int>, greater<int>> b;
 };
 
-// BEGIN: Time Limit Exceeded
-// class MedianFinder {
-// public:
-
-// 	// Adds a number into the data structure.
-// 	void addNum(int num) {
-// 		rbtree.insert(num);
-// 	}
-
-// 	// Returns the median of current data stream
-// 	double findMedian() {
-// 		if (rbtree.size() & 1) {
-// 			return *next(begin(rbtree), rbtree.size() >> 1);
-// 		}
-// 		double left = double(*next(begin(rbtree), (rbtree.size() >> 1) - 1));
-// 		double right = double(*next(begin(rbtree), rbtree.size() >> 1));
-// 		return left / 2.0 + right / 2.0;
-// 	}
-
-// private:
-// 	multiset<int> rbtree;
-// };
-// END: Time Limit Exceeded
-
-// Your MedianFinder object will be instantiated and called as such:
-// MedianFinder mf;
-// mf.addNum(1);
-// mf.findMedian();
-
-int main(void) {
-	MedianFinder mf;
-	double result = 0.0;
-	mf.addNum(1);
-	mf.addNum(2);
-
-	result = mf.findMedian();
-	assert(fabs(1.5 - result) < DBL_EPSILON);
-	
-	mf.addNum(3);
-	
-	result = mf.findMedian();
-	assert(fabs(2.0 - result) < DBL_EPSILON);
-
-	mf = MedianFinder();
-
-	mf.addNum(6);
-	result = mf.findMedian();
-	assert(fabs(6.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(10);
-	result = mf.findMedian();
-	assert(fabs(8.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(2);
-	result = mf.findMedian();
-	assert(fabs(6.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(6);
-	result = mf.findMedian();
-	assert(fabs(6.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(5);
-	result = mf.findMedian();
-	assert(fabs(6.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(0);
-	result = mf.findMedian();
-	assert(fabs(5.5 - result) < DBL_EPSILON);
-	
-	mf.addNum(6);
-	result = mf.findMedian();
-	assert(fabs(6.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(3);
-	result = mf.findMedian();
-	assert(fabs(5.5 - result) < DBL_EPSILON);
-	
-	mf.addNum(1);
-	result = mf.findMedian();
-	assert(fabs(5.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(0);
-	result = mf.findMedian();
-	assert(fabs(4.0 - result) < DBL_EPSILON);
-	
-	mf.addNum(0);
-	result = mf.findMedian();
-	assert(fabs(3.0 - result) < DBL_EPSILON);
-	
-	cout << "\nPassed All\n";
-	return 0;
-}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
