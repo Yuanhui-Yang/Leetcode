146da0b27a66167e4cf488fbde231338723e2dfa
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-06-29 11:56:24 -0500

    82. Remove Duplicates from Sorted List II

--- Algorithms/remove-duplicates-from-sorted-list-ii.cpp
+++ Algorithms/remove-duplicates-from-sorted-list-ii.cpp
@@ -9,76 +9,33 @@ Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
-
-ListNode *f(vector<int>& v) {
-	ListNode dummy(0), *it = &dummy;
-	for (const auto &i : v) {
-		it->next = new ListNode(i);
-		it = it->next;
-	}
-	return dummy.next;
-}
-
-vector<int> g(ListNode* head) {
-	vector<int> v;
-	while (head) {
-		v.push_back(head->val);
-		head = head->next;
-	}
-	return v;
-}
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	ListNode* deleteDuplicates(ListNode* head) {
-		ListNode dummy(0);
+		ListNode dummy(0), *a = &dummy;
 		dummy.next = head;
-		ListNode *a = &dummy;
 		while (a->next) {
 			ListNode *b = a->next;
-			int l = 0;
-			while (b->next and b->next->val == a->next->val) {
+			bool flag = false;
+			while(b->next and b->next->val == b->val) {
 				b = b->next;
-				++l;
+				flag = true;
 			}
-			if (l > 0) {
+			if (flag) {
 				a->next = b->next;
 			}
 			else {
-				a = a->next;   
+				a = b;
 			}
 		}
 		return dummy.next;
 	}
 };
-
-int main(void) {
-	Solution solution;
-	vector<int> x, y;
-	ListNode* head;
-
-	x = {1, 2, 3, 3, 4, 4, 5};
-	y = {1, 2, 5};
-	head = f(x);
-	head = solution.deleteDuplicates(head);
-	x = g(head);
-	assert(y == x);
-
-	x = {1, 1, 1, 2, 3};
-	y = {2, 3};
-	head = f(x);
-	head = solution.deleteDuplicates(head);
-	x = g(head);
-	assert(y == x);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
