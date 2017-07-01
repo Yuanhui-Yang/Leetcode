57bc1a50570fd009744d5b51709af60e971f0028
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-01 16:13:07 -0500

    246. Strobogrammatic Number

--- Algorithms/strobogrammatic-number.cpp
+++ Algorithms/strobogrammatic-number.cpp
@@ -1,52 +1,39 @@
 // 246. Strobogrammatic Number
 // https://leetcode.com/problems/strobogrammatic-number/
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <iterator>
-using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/20837/4-lines-in-java
+
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+For example, the numbers "69", "88", and "818" are all strobogrammatic.
+*/
+
 class Solution {
 public:
-	bool isStrobogrammatic(string num) {
-		const int n = num.size();
-		if (n == 0) return true;
-		string checkStr = "00 11 88 696";
-		for (int i = 0, j = n - 1; i <= j; i++, j--) {
-			string s;
-			s.push_back(num[i]);
-			s.push_back(num[j]);
-			if (checkStr.find(s) == string::npos) return false;
-		}
-		return true;
-	}
+    bool isStrobogrammatic(string num) {
+        int m[10];
+        m[0] = 0;
+        m[1] = 1;
+        m[2] = -1;
+        m[3] = -1;
+        m[4] = -1;
+        m[5] = -1;
+        m[6] = 9;
+        m[7] = -1;
+        m[8] = 8;
+        m[9] = 6;
+        int n = num.size(), i = 0, j = n - 1;
+        if ((n & 1) and num[n / 2] -'0' != m[num[n / 2] - '0']) {
+            return false;
+        }
+        while (i < j) {
+            if (num[i] - '0' != m[num[j] - '0']) {
+                return false;
+            }
+            ++i;
+            --j;
+        }
+        return true;
+    }
 };
-// END: https://discuss.leetcode.com/topic/20837/4-lines-in-java
-// class Solution {
-// public:
-// 	bool isStrobogrammatic(string num) {
-// 		const int n = num.size();
-// 		if (n == 0) return true;
-// 		vector<char> check({'0', '1', '8'});
-// 		for (int i = 0, j = n - 1; i <= j; i++, j--) {
-// 			if (i == j) return find(begin(check), end(check), num[i]) != end(check);
-// 			if (num[i] == '6' && num[j] != '9') return false;
-// 			if (num[i] == '9' && num[j] != '6') return false;
-// 			if (num[i] == '6' && num[j] == '9') continue;
-// 			if (num[i] == '9' && num[j] == '6') continue;
-// 			if (find(begin(check), end(check), num[i]) == end(check)) return false;
-// 			if (num[i] != num[j]) return false;
-// 		}
-// 		return true;
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	cout << boolalpha << solution.isStrobogrammatic("69") << "\tPassed\n";
-	cout << boolalpha << solution.isStrobogrammatic("88") << "\tPassed\n";
-	cout << boolalpha << solution.isStrobogrammatic("818") << "\tPassed\n";
-	cout << boolalpha << solution.isStrobogrammatic("10") << "\tPassed\n";
-	cout << boolalpha << solution.isStrobogrammatic("1") << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
