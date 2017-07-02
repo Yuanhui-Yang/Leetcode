26795c2ae1293d376d66c972d4c391a9ad10edd9
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-02 11:01:56 -0500

    130. Surrounded Regions

--- Algorithms/surrounded-regions.cpp
+++ Algorithms/surrounded-regions.cpp
@@ -1,71 +1,79 @@
 // 130. Surrounded Regions
 // https://leetcode.com/problems/surrounded-regions/
-// https://discuss.leetcode.com/topic/17224/a-really-simple-and-readable-c-solution-only-cost-12ms
-#include <iostream>
-#include <vector>
-using namespace std;
+
+/*
+Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+For example,
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+*/
+
 class Solution {
 public:
 	void solve(vector<vector<char>>& board) {
-		if (board.empty()) return;
-		const size_t &m = board.size();
-		const size_t &n = board.front().size();
-		for (size_t i = 0; i < m; ++i) {
-			if (board[i][0] == 'O') this->solve(i, 0, board);
-			if (n > 1 && board[i][n - 1] == 'O') this->solve(i, n - 1, board);
+		if (board.empty() or board.front().empty()) {
+			return;
 		}
-		for (size_t i = 1; i < n; ++i) {
-			if (board[0][i] == 'O') this->solve(0, i, board);
-			if (m > 1 && board[m - 1][i] == 'O') this->solve(m - 1, i, board);
+		int m = board.size(), n = board.front().size(), dx[4] = {-1, 0, 0, 1}, dy[4] = {0, -1, 1, 0};
+		for (int i = 0; i < m; ++i) {
+			f1(board, m, n, dx, dy, i, 0);
+			f1(board, m, n, dx, dy, i, n - 1);
 		}
-		for (auto &i : board) {
-			for (auto &j : i) {
-				if (j == 'O') j = 'X';
-				if (j == '+') j = 'O';
-			}
+		for (int j = 0; j < n; ++j) {
+			f1(board, m, n, dx, dy, 0, j);
+			f1(board, m, n, dx, dy, m - 1, j);
 		}
-		return;
+		f2(board);
+		f3(board);
 	}
 private:
-	void solve(const size_t &i, const size_t &j, vector<vector<char>>& board) {
-		const size_t &m = board.size();
-		const size_t &n = board.front().size();
-		if (board[i][j] == 'O') board[i][j] = '+';
-		if (i > 1 && board[i - 1][j] == 'O') this->solve(i - 1, j, board);
-		if (i + 2 < m && board[i + 1][j] == 'O') this->solve(i + 1, j, board);
-		if (j > 1 && board[i][j - 1] == 'O') this->solve(i, j - 1, board);
-		if (j + 2 < n && board[i][j + 1] == 'O') this->solve(i, j + 1, board);
-		return;
+	void f1(vector<vector<char>>& board, int m, int n, int * dx, int * dy, int x, int y) {
+		if (x < 0 or x >= m or y < 0 or y >= n or board[x][y] != 'O') {
+			return;
+		}
+		stack<array<int, 2>> s;
+		s.push({x, y});
+		while (!s.empty()) {
+			array<int, 2> t = s.top();
+			s.pop();
+			x = t[0];
+			y = t[1];
+			board[x][y] = 'Y';
+			for (int i = 0; i < 4; ++i) {
+				int nx = x + dx[i], ny = y + dy[i];
+				if (nx >= 0 and nx < m and ny >= 0 and ny < n and board[nx][ny] == 'O') {
+					s.push({nx, ny});
+				} 
+			}
+		}
+	}
+	void f2(vector<vector<char>>& board) {
+		for (auto & i : board) {
+			for (auto & j : i) {
+				if (j == 'O') {
+					j = 'X';
+				}
+			}
+		}
+	}
+	void f3(vector<vector<char>>& board) {
+		for (auto & i : board) {
+			for (auto & j : i) {
+				if (j == 'Y') {
+					j = 'O';
+				}
+			}
+		}
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<vector<char>> board = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
-	solution.solve(board);
-	for (const auto &i : board) {
-		for (const auto &j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
-	cout << "\nPassed\n";
-	board = {{'O', 'O'}, {'O', 'O'}};
-	solution.solve(board);
-	for (const auto &i : board) {
-		for (const auto &j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
-	cout << "\nPassed\n";
-	board = {{'O', 'O', 'O'}, {'O', 'O', 'O'}, {'O', 'O', 'O'}};
-	solution.solve(board);
-	for (const auto &i : board) {
-		for (const auto &j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
-	cout << "\nPassed All\n";
-	return 0;
-}
