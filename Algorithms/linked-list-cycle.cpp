305dcc66e28063543e33d8210bb6c8734fff5471
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-04 17:05:27 -0500

    141. Linked List Cycle

--- Algorithms/linked-list-cycle.cpp
+++ Algorithms/linked-list-cycle.cpp
@@ -1,35 +1,33 @@
 // 141. Linked List Cycle
 // https://leetcode.com/problems/linked-list-cycle/
-// https://en.wikipedia.org/wiki/Cycle_detection
-#include <iostream>
-using namespace std;
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
+
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	bool hasCycle(ListNode *head) {
-		if (!head) return false;
-		ListNode *slow = head, *fast = head;
-		while (fast && fast->next) {
+		ListNode * slow = head, * fast = head;
+		while (slow and fast and fast->next) {
 			slow = slow->next;
-			fast = fast->next->next;
-			if (slow == fast) return true;
+			fast = fast->next;
+			fast = fast->next;
+			if (slow == fast) {
+				return true;
+			}
 		}
 		return false;
 	}
 };
-int main(void) {
-	Solution solution;
-	ListNode *head = new ListNode(1);
-	head->next = new ListNode(2);
-	head->next->next = new ListNode(3);
-	head->next->next->next = new ListNode(4);
-	head->next->next->next->next = new ListNode(3);
-	head->next->next->next->next->next = head->next->next->next;
-	cout << boolalpha << solution.hasCycle(head) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
