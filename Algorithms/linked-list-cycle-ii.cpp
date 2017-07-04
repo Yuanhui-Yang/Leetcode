3b3bb6cc6e6fc49346cd61a6f864afd12357a2de
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-04 17:11:52 -0500

    142. Linked List Cycle II

--- Algorithms/linked-list-cycle-ii.cpp
+++ Algorithms/linked-list-cycle-ii.cpp
@@ -1,42 +1,43 @@
 // 142. Linked List Cycle II
 // https://leetcode.com/problems/linked-list-cycle-ii/
-// https://www.youtube.com/watch?v=FkBm3NeWqak
-// https://en.wikipedia.org/wiki/Cycle_detection
-#include <iostream>
-using namespace std;
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
+
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	ListNode *detectCycle(ListNode *head) {
-		if (!head) return NULL;
-		ListNode *slow = head, *fast = head;
-		while (fast && fast->next) {
+		ListNode * slow = head, * fast = head;
+		while (slow and fast and fast->next) {
 			slow = slow->next;
-			fast = fast->next->next;
-			if (slow == fast) break;
+			fast = fast->next;
+			fast = fast->next;
+			if (slow == fast) {
+				break;
+			}
 		}
-		if (!fast || !fast->next) return NULL;
+		if (!(slow and fast and fast->next)) {
+			return NULL;
+		}
 		slow = head;
 		while (slow != fast) {
 			slow = slow->next;
-			fast = fast->next;			
+			fast = fast->next;
 		}
 		return slow;
 	}
 };
-int main(void) {
-	Solution solution;
-	ListNode *head = new ListNode(1);
-	head->next = new ListNode(2);
-	head->next->next = new ListNode(3);
-	head->next->next->next = new ListNode(4);
-	head->next->next->next->next = new ListNode(3);
-	head->next->next->next->next->next = head->next->next->next;
-	cout << solution.detectCycle(head) << '\t' << head->next->next->next << "\tPassed\n";
-	cout << "\nPassed All\n";	
-	return 0;
-}
