648a1ab9cda20c5247232b47d9b28ba25fdebef2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-04 17:20:20 -0500

    287. Find the Duplicate Number

--- Algorithms/find-the-duplicate-number.cpp
+++ Algorithms/find-the-duplicate-number.cpp
@@ -1,35 +1,30 @@
 // 287. Find the Duplicate Number
 // https://leetcode.com/problems/find-the-duplicate-number/
-// https://www.youtube.com/watch?v=FkBm3NeWqak
-// https://en.wikipedia.org/wiki/Cycle_detection
-#include <iostream>
-#include <vector>
-using namespace std;
+
+/*
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+*/
+
 class Solution {
 public:
 	int findDuplicate(vector<int>& nums) {
-		int slow = nums[0], fast = nums[0];
+		int slow = 0, fast = 0;
 		do {
 			slow = nums[slow];
 			fast = nums[fast];
 			fast = nums[fast];
 		} while (slow != fast);
-		slow = nums[0];
-		while (slow != fast) {
+		slow = 0;
+		do {
 			slow = nums[slow];
-			fast = nums[fast];			
-		}
+			fast = nums[fast];
+		} while (slow != fast);
 		return slow;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums = {1, 2, 2, 3, 4, 5, 6, 7, 8, 9};
-	cout << solution.findDuplicate(nums) << "\tPassed\n";
-	nums = {1, 3, 4, 2, 2};
-	cout << solution.findDuplicate(nums) << "\tPassed\n";
-	nums = {1, 3, 4, 2, 1};
-	cout << solution.findDuplicate(nums) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
