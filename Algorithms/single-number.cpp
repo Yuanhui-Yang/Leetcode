a36de5e938e15289f2ea8f2f4599ced478350c4a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-04 17:27:06 -0500

    136. Single Number

--- Algorithms/single-number.cpp
+++ Algorithms/single-number.cpp
@@ -8,26 +8,9 @@ Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
 		return accumulate(begin(nums), end(nums), 0, bit_xor<int>());
 	}
 };
-
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int answer, result;
-
-	nums = {1, 1, 2, 2, 3};
-	answer = 3;
-	result = solution.singleNumber(nums);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
