2c391d3a0001385c49c319a0ad7cb9b976357bd0
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-05 01:18:56 -0500

    148. Sort List

--- Algorithms/sort-list.cpp
+++ Algorithms/sort-list.cpp
@@ -1,58 +1,57 @@
 // 148. Sort List
 // https://leetcode.com/problems/sort-list/
-// https://github.com/soulmachine/leetcode
-#include <iostream>
-#include <limits>
-using namespace std;
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
+
+/*
+Sort a linked list in O(n log n) time using constant space complexity.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	ListNode* sortList(ListNode* head) {
-		if (!head || !head->next) return head;
-		ListNode *slow = head;
-		ListNode *fast = head->next;
-		while (fast && fast->next) {
-			slow = slow->next;
-			fast = fast->next->next;
+		if (!head or !head->next) {
+			return head;
 		}
-		ListNode *l1 = head;
-		ListNode *l2 = slow->next;
-		slow->next = NULL;
-		return this->mergeTwoSortedLists(sortList(l1), sortList(l2));
+		ListNode * slow = head, * fast = head, * prevSlow = NULL;
+		while (slow and fast and fast->next) {
+			prevSlow = slow;
+			slow = slow->next;
+			fast = fast->next;
+			fast = fast->next;
+		}
+		prevSlow->next = NULL;
+		ListNode * l1 = sortList(head), * l2 = sortList(slow);
+		return f(l1, l2);
 	}
 private:
-	ListNode *mergeTwoSortedLists(ListNode *l1, ListNode *l2) {
-		ListNode *head = new ListNode(numeric_limits<int>::min());
-		for (ListNode *it = head; l1 || l2; ) {
+	ListNode * f(ListNode * l1, ListNode * l2) {
+		ListNode dummy(0), * l = &dummy;
+		while (l1 or l2) {
 			if (!l1) {
-				it->next = l2;
+				l->next = l2;
 				break;
 			}
 			if (!l2) {
-				it->next = l1;
-				break;				
+				l->next = l1;
+				break;
 			}
 			if (l1->val < l2->val) {
-				it->next = l1;
+				l->next = l1;
 				l1 = l1->next;
-				it = it->next;
-				continue;
 			}
-			it->next = l2;
-			l2 = l2->next;
-			it = it->next;
-			continue;
+			else {
+				l->next = l2;
+				l2 = l2->next;
+			}
+			l = l->next;
 		}
-		return head->next;
+		return dummy.next;
 	}
 };
-int main(void) {
-	// Solution solution;
-	cout << "\nPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
