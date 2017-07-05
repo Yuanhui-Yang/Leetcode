70d39fc96173e71c6a8ac3d0796482e077d360e8
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-05 11:57:23 -0500

    157. Read N Characters Given Read4

--- Algorithms/read-n-characters-given-read4.cpp
+++ Algorithms/read-n-characters-given-read4.cpp
@@ -1,5 +1,17 @@
 // 157. Read N Characters Given Read4
 // https://leetcode.com/problems/read-n-characters-given-read4/
+
+/*
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+Note:
+The read function will only be called once for each test case.
+*/
+
 // Forward declaration of the read4 API.
 int read4(char *buf);
 
@@ -11,19 +23,12 @@ public:
 	* @return    The number of characters read
 	*/
 	int read(char *buf, int n) {
-		int k = n / 4;
-		int b = n % 4;
-		int c = 4;
-		int result = 0;
-		for (int i = 0; i < k; ++i) {
-			int r = read4(buf + c * i);
-			result += r;
-			if (r < c) return result;
-		}
-		if (b == 0) return result;
-		char *remain = new char[c];
-		int r = read4(remain);
-		for (int i = 0; i < r && i < b; ++i) buf[k * c + i] = remain[i];
-		return result + min(r, b);
+		int result = 0, l = 0;
+		do {
+			l = min(read4(buf + result), n);
+			result += l;
+			n -= l;
+		} while (l == 4 and n > 0);
+		return result;
 	}
 };
