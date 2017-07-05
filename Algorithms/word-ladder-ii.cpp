03ffe5f5501cbb2e5d8bf5da896e7e6ffc5613c4
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-05 17:26:51 -0500

    126. Word Ladder II

--- Algorithms/word-ladder-ii.cpp
+++ Algorithms/word-ladder-ii.cpp
@@ -32,6 +32,72 @@ public:
 	vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
 		vector<vector<string>> result;
 		if (beginWord == endWord) {
+			result.push_back({beginWord, endWord});
+			return result;
+		}
+		unordered_map<string, unordered_set<string>> rpath;
+		unordered_set<string> words(begin(wordList), end(wordList));
+		if (!words.count(endWord)) {
+			return result;
+		}
+		int shortest = numeric_limits<int>::max();
+		unordered_map<string, int> dist;
+		dist[beginWord] = 0;
+		queue<string> q;
+		q.push(beginWord);
+		while (!q.empty()) {
+			string s = q.front();
+			q.pop();
+			if (dist[s] > shortest) {
+			break;
+			}
+			for (int n = s.size(), i = 0; i < n; ++i) {
+				for (char ch = 'a'; ch <= 'z'; ++ch) {
+					if (s[i] == ch) {
+						continue;
+					}
+					string t(s);
+					t[i] = ch;
+					if (!words.count(t)) {
+						continue;
+					}
+					if (!dist.count(t)) {
+						dist[t] = dist[s] + 1;
+						q.push(t);
+						if (t == endWord) {
+							shortest = dist[t];
+						}
+					}
+					if (dist[t] > dist[s]) {
+						rpath[t].insert(s);
+					}
+				}
+			}
+		}
+		vector<string> v;
+		f(result, v, rpath, endWord, beginWord);
+		return result;
+	}
+private:
+	void f(vector<vector<string>> & result, vector<string> & v, const unordered_map<string, unordered_set<string>> & rpath, const string & endWord, const string & beginWord) {
+		v.insert(begin(v), endWord);
+		if (endWord == beginWord) {
+			result.push_back(v);
+		}
+		else if (rpath.count(endWord)) {
+			for (const auto & i : rpath.at(endWord)) {
+				f(result, v, rpath, i, beginWord);
+			}
+		}
+		v.erase(begin(v));
+	}
+};
+
+class Solution {
+public:
+	vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+		vector<vector<string>> result;
+		if (beginWord == endWord) {
 			result.push_back(vector<string>({beginWord, endWord}));
 			return result;
 		}
@@ -78,8 +144,7 @@ private:
 			result.push_back(v);
 		}
 		else if (rpath.count(endWord)) {
-			const unordered_set<string> & w = rpath.at(endWord);
-			for (const auto & i : w) {
+			for (const auto & i : rpath.at(endWord)) {
 				f(result, v, rpath, i, beginWord);
 			}
 		}
