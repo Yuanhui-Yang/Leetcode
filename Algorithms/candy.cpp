5c2c366dd7b3f3617e3d4de5e2f87182429c9acf
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-06 17:11:39 -0500

    135. Candy

--- Algorithms/candy.cpp
+++ Algorithms/candy.cpp
@@ -1,46 +1,35 @@
 // 135. Candy
 // https://leetcode.com/problems/candy/
-// https://discuss.leetcode.com/topic/5243/a-simple-solution
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <numeric>
-using namespace std;
+
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+*/
+
 class Solution {
 public:
-	int candy(const vector<int>& ratings) {
-		const int n = ratings.size();
-		if (n <= 1) return n;
-		vector<int> OPT(n, 1);
-		for (int i = 1; i < n; ++i) if (ratings[i] > ratings[i - 1]) OPT[i] = max(OPT[i], 1 + OPT[i - 1]);
-		for (int i = n - 2; i >= 0; --i) if (ratings[i] > ratings[i + 1]) OPT[i] = max(OPT[i], 1 + OPT[i + 1]);
-		return accumulate(begin(OPT), end(OPT), 0);
+	int candy(vector<int>& ratings) {
+		if (ratings.empty()) {
+			return 0;
+		}
+		int n = ratings.size(), result = 0;
+		vector<int> x(n, 0), y(x);
+		x.front() = 1;
+		y.back() = 1;
+		for (int i = 1; i < n; ++i) {
+			x[i] = ratings[i] > ratings[i - 1] ? x[i - 1] + 1 : 1;
+		}
+		for (int i = n - 2; i >= 0; --i) {
+			y[i] = ratings[i] > ratings[i + 1] ? y[i + 1] + 1 : 1;
+		}
+		for (int i = 0; i < n; ++i) {
+			result += max(x[i], y[i]);
+		}
+		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> ratings = {0};
-	if (solution.candy(ratings) == 1) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	ratings = {2, 2};
-	if (solution.candy(ratings) == 2) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	ratings = {1, 2};
-	if (solution.candy(ratings) == 3) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	cout << "\nPassed All\n";
-	return 0;
-}
