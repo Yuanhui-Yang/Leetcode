ebecd501f506b2145001af6eac7bafbf12d23d9d
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-06 16:43:20 -0500

    132. Palindrome Partitioning II

--- Algorithms/palindrome-partitioning-ii.cpp
+++ Algorithms/palindrome-partitioning-ii.cpp
@@ -1,33 +1,29 @@
 // 132. Palindrome Partitioning II
 // https://leetcode.com/problems/palindrome-partitioning-ii/
-// https://discuss.leetcode.com/topic/2840/my-solution-does-not-need-a-table-for-palindrome-is-it-right-it-uses-only-o-n-space/2
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
+
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+*/
+
 class Solution {
 public:
-	int minCut(const string& s) {
-		const int n = s.size();
-		vector<int> OPT(n + 1, 0);
-		for (int i = 0; i < n + 1; ++i) OPT[i] = i - 1;
-		for (int i = 0; i < n + 1; ++i) {
-			for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j]; ++j) OPT[i + j + 1] = min(OPT[i + j + 1], 1 + OPT[i - j]);
-			for (int j = 0; i - j >= 0 && i + j < n + 1 && s[i - j] == s[i + j - 1]; ++j) OPT[i + j] = min(OPT[i + j], 1 + OPT[i - j]);
+	int minCut(string s) {
+		int n = s.size();
+		vector<int> vec(n + 1, 0);
+		iota(begin(vec), end(vec), -1);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; i - j >= 0 and i + j < n and s[i - j] == s[i + j]; ++j) {
+				vec[i + j + 1] = min(vec[i + j + 1], 1 + vec[i - j]);
+			}
+			for (int j = 1; i + 1 - j >= 0 and i + j < n and s[i + 1 - j] == s[i + j]; ++j) {
+				vec[i + j + 1] = min(vec[i + j + 1], 1 + vec[i + 1 - j]);
+			}
 		}
-		return OPT[n];
+		return vec[n];
 	}
 };
-int main(void) {
-	Solution solution;
-	string s = "aab";
-	if (solution.minCut(s) == 1) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	cout << "\nPassed All\n";
-	return 0;
-}
