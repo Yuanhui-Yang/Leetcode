957be0ab170160a40c32b47f29d3d89042ca04f3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-07 17:36:18 -0500

    150. Evaluate Reverse Polish Notation

--- Algorithms/evaluate-reverse-polish-notation.cpp
+++ Algorithms/evaluate-reverse-polish-notation.cpp
@@ -1,60 +1,41 @@
 // 150. Evaluate Reverse Polish Notation
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
+
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+*/
+
 class Solution {
 public:
 	int evalRPN(vector<string>& tokens) {
-		vector<int> stack;
-		size_t i = 0;
-		while (i < tokens.size()) {
-			if (isdigit(tokens[i].back())) {
-				int val = stoi(tokens[i]);
-				stack.push_back(val);
-				++i;
-				continue;
-			}
-			int second = stack.back();
-			stack.pop_back();
-			int first = stack.back();
-			stack.pop_back();
-			int result = 0;
-			if (tokens[i] == "+") result = first + second;
-			else if (tokens[i] == "-") result = first - second;
-			else if (tokens[i] == "*") result = first * second;
-			else result = first / second;
-			stack.push_back(result);
-			++i;
-			continue;
+		if (tokens.empty()) {
+			return 0;
 		}
-		return stack.back();
+		stack<int> stk;
+		unordered_map<string, int> h;
+		h["+"] = 1;
+		h["-"] = 1;
+		h["*"] = 2;
+		h["/"] = 2;
+		for (const auto & i : tokens) {
+			if (h.count(i)) {
+				int y = stk.top();
+				stk.pop();
+				int x = stk.top();
+				stk.pop();
+				int z = i == "*" ? x * y : i == "/" ? x / y : i == "+" ? x + y : x - y;
+				stk.push(z);
+			}
+			else {
+				stk.push(stoi(i));
+			}
+		}
+		return stk.top();
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<string> tokens = {"0", "3", "/"};
-	if (solution.evalRPN(tokens) == 0) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	tokens = {"-3", "9", "*"};
-	if (solution.evalRPN(tokens) == -27) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	tokens = {"18"};
-	if (solution.evalRPN(tokens) == 18) {
-		cout << "\nPassed\n";
-	} else {
-		cout << "\nError\n";
-		return 0;
-	}
-	cout << "\nPassed All\n";
-	return 0;
-}
