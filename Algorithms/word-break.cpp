24db66bf3078caec887ac8ff32a1e7dd8939ebcd
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-07 01:48:59 -0500

    139. Word Break

--- Algorithms/word-break.cpp
+++ Algorithms/word-break.cpp
@@ -17,16 +17,13 @@ The wordDict parameter had been changed to a list of strings (instead of a set o
 class Solution {
 public:
 	bool wordBreak(string s, vector<string>& wordDict) {
-		if (s.empty() or wordDict.empty()) {
-			return false;
-		}
 		int n = s.size();
 		unordered_set<string> h(begin(wordDict), end(wordDict));
 		vector<bool> vec(n + 1, false);
 		vec[0] = true;
-		for (int j = 1; j <= n; ++j) {
-			for (int i = 0; i < j and !vec[j]; ++i) {
-				vec[j] = vec[j] or (vec[i] and h.count(s.substr(i, j - i)));
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 0; j < i and !vec[i]; ++j) {
+				vec[i] = vec[i] or (vec[j] and h.count(s.substr(j, i - j)));
 			}
 		}
 		return vec[n];
