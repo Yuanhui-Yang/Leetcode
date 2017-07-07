856b2fff5dcdd1429fc079656d0f884938ab18f4
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-07 00:18:52 -0500

    139. Word Break

--- Algorithms/word-break.cpp
+++ Algorithms/word-break.cpp
@@ -14,79 +14,21 @@ UPDATE (2017/1/4):
 The wordDict parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
 */
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
 class Solution {
 public:
 	bool wordBreak(string s, vector<string>& wordDict) {
-		if (s.empty()) {
-			return true;
+		if (s.empty() or wordDict.empty()) {
+			return false;
 		}
+		int n = s.size();
 		unordered_set<string> h(begin(wordDict), end(wordDict));
-		unordered_map<string, bool> m;
-		return helper(s, h, m);
-	}
-private:
-	bool helper(string s, unordered_set<string>& h, unordered_map<string, bool>& m) {
-		if (s.empty()) {
-			return true;
-		}
-		if (!m.empty() and m.count(s)) {
-			return m.at(s);
-		}
-		for (size_t i = 1; i <= s.size(); i++) {
-			if (!h.empty() and h.count(s.substr(0, i)) and helper(s.substr(i), h, m)) {
-				return m[s] = true;
+		vector<bool> vec(n + 1, false);
+		vec[0] = true;
+		for (int j = 1; j <= n; ++j) {
+			for (int i = 0; i < j and !vec[j]; ++i) {
+				vec[j] = vec[j] or (vec[i] and h.count(s.substr(i, j - i)));
 			}
 		}
-		return m[s] = false;
+		return vec[n];
 	}
 };
-
-int main(void) {
-	Solution solution;
-	string s;
-	vector<string> wordDict;
-	bool answer, result;
-
-	s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
-	wordDict = {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
-	answer = false;
-	result = solution.wordBreak(s, wordDict);
-	assert(answer == result);
-
-	s = "leetcode";
-	wordDict = {"leet", "code"};
-	answer = true;
-	result = solution.wordBreak(s, wordDict);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
