556fc65ad7a95565e0673a050fe9aa1fbd4aa63e
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-06 23:38:19 -0500

    138. Copy List with Random Pointer

--- Algorithms/copy-list-with-random-pointer.cpp
+++ Algorithms/copy-list-with-random-pointer.cpp
@@ -1,31 +1,38 @@
 // 138. Copy List with Random Pointer
 // https://leetcode.com/problems/copy-list-with-random-pointer/
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-using namespace std;
-struct RandomListNode {
-	int label;
-	RandomListNode *next, *random;
-	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
-};
+
+/*
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+*/
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	RandomListNode *copyRandomList(RandomListNode *head) {
-		if (!head) return NULL;
-		if (this->hashMap.count(head->label)) return this->hashMap[head->label];
-		RandomListNode *clone =  new RandomListNode(head->label);
-		this->hashMap[head->label] = clone;
-		clone->next = this->copyRandomList(head->next);
-		clone->random = this->copyRandomList(head->random);
-		return clone;
+		if (!head) {
+			return NULL;
+		}
+		if (h.count(head)) {
+			return h[head];
+		}
+		RandomListNode dummy(0);
+		for (RandomListNode * it = head, * jt = &dummy; it; it = it->next, jt = jt->next) {
+			jt->next = h.count(it) ? h[it] : h[it] = new RandomListNode(it->label);
+		}
+		for (RandomListNode * it = head, * jt = dummy.next; it and jt; it = it->next, jt = jt->next) {
+			jt->random = h.count(it->random) ? h[it->random] : NULL;
+		}
+		return dummy.next;
 	}
 private:
-	unordered_map<int, RandomListNode *> hashMap;
+	unordered_map<RandomListNode*, RandomListNode*> h;
 };
-int main(void) {
-	Solution solution;
-	cout << "\nPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
