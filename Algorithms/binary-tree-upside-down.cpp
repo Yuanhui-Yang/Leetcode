1a9edb3afd2310aaf35b598f0ef4bf067daeccb8
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-07 22:01:05 -0500

    156. Binary Tree Upside Down

--- Algorithms/binary-tree-upside-down.cpp
+++ Algorithms/binary-tree-upside-down.cpp
@@ -1,70 +1,67 @@
 // 156. Binary Tree Upside Down
 // https://leetcode.com/problems/binary-tree-upside-down/
-// http://blog.csdn.net/whuwangyi/article/details/43186045
-// https://lefttree.gitbooks.io/leetcode/content/binaryTree/binaryTreeUpsideDown.html
-// https://discuss.leetcode.com/topic/18273/clean-java-solution
-#include <iostream>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+
+For example:
+Given a binary tree {1,2,3,4,5},
+	1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+	/ \
+   3   1  
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+
+
+OJ's Binary Tree Serialization:
+The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+
+Here's an example:
+   1
+  / \
+ 2   3
+	/
+   4
+	\
+	 5
+The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	TreeNode* upsideDownBinaryTree(TreeNode* root) {
-		if (!root) return root;
-		if (!root->left && !root->right) return root;
-		TreeNode *rootleft = root->left;
-		root->left = NULL;
-		TreeNode *rootright = root->right;
-		root->right = NULL;
-		TreeNode *result = this->upsideDownBinaryTree(rootleft);
-		if (rootleft) {
-			rootleft->left = rootright;
-			rootleft->right = root;
+		if (!root) {
+			return NULL;
 		}
+		if (!root->left) {
+			return root;
+		}
+		TreeNode * left = root->left, * right = root->right;
+		root->left = NULL;
+		root->right = NULL;
+		TreeNode * result = upsideDownBinaryTree(left);
+		TreeNode * node = result;
+		while (node->right) {
+			node = node->right;
+		}
+		node->left = right;
+		node->right = root;
 		return result;
 	}
 };
-void inorderTraverse(TreeNode* root) {
-	if (!root) return;
-	TreeNode *cur = root;
-	while (cur) {
-		if (!cur->left) {
-			cout << cur->val << '\t';
-			cur = cur->right;
-			continue;
-		}
-		TreeNode *pred = cur->left;
-		while(pred->right && pred->right != cur) pred = pred->right;
-		if (!pred->right) {
-			pred->right = cur;
-			cur = cur->left;
-			continue;
-		}
-		cout << cur->val << '\t';
-		pred->right =  NULL;
-		cur = cur->right;
-		continue;
-	}
-}
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(3);
-	root->left->left = new TreeNode(4);
-	root->left->right = new TreeNode(5);
-	cout << '\n';
-	inorderTraverse(root);
-	cout << '\n';
-	root = solution.upsideDownBinaryTree(root);
-	cout << '\n';
-	inorderTraverse(root);
-	cout << '\n';
-	cout << "\nPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
