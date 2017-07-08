ec4d938c65d3d4d59b8824618539909e6ec9c124
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-08 02:53:47 -0500

    340. Longest Substring with At Most K Distinct Characters

--- Algorithms/longest-substring-with-at-most-k-distinct-characters.cpp
+++ Algorithms/longest-substring-with-at-most-k-distinct-characters.cpp
@@ -9,71 +9,35 @@ For example, Given s = “eceba” and k = 2,
 T is "ece" which its length is 3.
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	int lengthOfLongestSubstringKDistinct(string s, int k) {
 		if (k <= 0) {
 			return 0;
 		}
-		int n = s.size(), i = 0, j = 0, result = 0, h[256];
+		int i = 0, j = 0, n = s.size(), result = 0, h[256], cnt = 0;
 		memset(h, 0, sizeof(h));
 		while (j < n) {
-			while (j < n and (k > 0 or h[s[j] - 0] > 0)) {
-				if (h[s[j++] - 0]++ == 0) {
-					--k;
+			while (j < n and cnt < k) {
+				if (!h[s[j]]) {
+					++cnt;
 				}
+				++h[s[j]];
+				++j;
 			}
-			if (result == 0 or j - i > result) {
-				result = j - i;
+			while (j < n and h[s[j]]) {
+				++h[s[j]];
+				++j;
 			}
-			while (k == 0) {
-				if (--h[s[i++] - 0] == 0) {
-					++k;
+			result = max(result, j - i);
+			while (i < j and cnt >= k) {
+				--h[s[i]];
+				if (!h[s[i]]) {
+					--cnt;
 				}
+				++i;
 			}
 		}
 		return result;
 	}
 };
-
-int main(void) {
-	Solution solution;
-	string s;
-	int k, answer, result;
-
-	s = "nfhiexxjrtvpfhkrxcutexxcodfioburrtjefrgwrnqtyzelvtpvwdvvpsbudwtiryqzzy";
-	k = 25;
-	answer = 70;
-	result = solution.lengthOfLongestSubstringKDistinct(s, k);
-	assert(answer == result);
-
-	s = "eceba";
-	k = 2;
-	answer = 3;
-	result = solution.lengthOfLongestSubstringKDistinct(s, k);
-	assert(answer == result);
-
-	s = "a";
-	k = 0;
-	answer = 0;
-	result = solution.lengthOfLongestSubstringKDistinct(s, k);
-	assert(answer == result);
-
-	s = "a";
-	k = 2;
-	answer = 1;
-	result = solution.lengthOfLongestSubstringKDistinct(s, k);
-	assert(answer == result);
-
-	s = "aa";
-	k = 1;
-	answer = 2;
-	result = solution.lengthOfLongestSubstringKDistinct(s, k);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
