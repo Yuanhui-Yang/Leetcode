5faad38e49452f524603269f342ac60d165be811
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-07 23:31:13 -0500

    158. Read N Characters Given Read4 II - Call multiple times

--- Algorithms/read-n-characters-given-read4-ii-call-multiple-times.cpp
+++ Algorithms/read-n-characters-given-read4-ii-call-multiple-times.cpp
@@ -1,32 +1,49 @@
 // 158. Read N Characters Given Read4 II - Call multiple times
 // https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
-// https://discuss.leetcode.com/topic/7094/a-simple-java-code/4
-// http://www.cnblogs.com/EdwardLiu/p/4240616.html
+
+/*
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+Note:
+The read function may be called multiple times.
+*/
+
 // Forward declaration of the read4 API.
 int read4(char *buf);
 
 class Solution {
 public:
 	/**
-	* @param buf Destination buffer
-	* @param n   Maximum number of characters to read
-	* @return    The number of characters read
-	*/
+	 * @param buf Destination buffer
+	 * @param n   Maximum number of characters to read
+	 * @return    The number of characters read
+	 */
 	int read(char *buf, int n) {
-		int result = 0;
-		while (result < n) {
-			if (cachePtr == 0) cacheCnt = read4(cache);
-			if (cacheCnt == 0) break;
-			while (result < n && cachePtr < cacheCnt) buf[result++] = cache[cachePtr++];
-			if (cachePtr >= cacheCnt) cachePtr = 0;
+		if (n <= 0) {
+			return 0;
 		}
-		return result;
+		int i = 0;
+		while (i < n and id < cnt) {
+			buf[i++] = cache[id++];
+		}
+		while (i < n) {
+			cnt = read4(cache);
+			id = 0;
+			while (i < n and id < cnt) {
+				buf[i++] = cache[id++];
+			}
+			if (cnt != 4) {
+				break;
+			}
+		}
+		return i;
 	}
 private:
-	char *cache = new char[4];
-	int cacheCnt = 0;
-	int cachePtr = 0;
+	int id = 0;
+	int cnt = 0;
+	char cache[4];
 };
-
-// "ab"
-// [read(1),read(2)]
