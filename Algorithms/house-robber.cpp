d2938c5eca46ab667c0312b10fd6659841619501
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-10 18:39:48 -0500

    198. House Robber

--- Algorithms/house-robber.cpp
+++ Algorithms/house-robber.cpp
@@ -1,30 +1,38 @@
 // 198. House Robber
 // https://leetcode.com/problems/house-robber/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
-using namespace std;
+
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+*/
+
 class Solution {
 public:
 	int rob(vector<int>& nums) {
-		const int nums_size = nums.size();
-		if (nums_size == 0) return 0;
-		vector<int> OPT(nums_size, 0);
-		OPT[0] = nums[0];
-		OPT[1] = max(nums[0], nums[1]);
-		for (int i = 2; i < nums_size; i++) {
-			OPT[i] = max(OPT[i - 2] + nums[i], OPT[i - 1]);
+		int n = nums.size();
+		if (n == 0) {
+			return 0;
 		}
-		return OPT[nums_size - 1];
+		if (n == 1) {
+			return nums[0];
+		}
+		if (n == 2) {
+			return max(nums[0], nums[1]);
+		}
+		int result = nums[0];
+		nums[1] = max(nums[0], nums[1]);
+		result = max(result, nums[1]);
+		for (int i = 2; i < n; ++i) {
+			int val = nums[0];
+			for (int j = 1; j <= i - 2; ++j) {
+				if (val < nums[j]) {
+					val = nums[j];
+				}
+			}
+			nums[i] += val;
+			result = max(result, nums[i]);
+		}
+		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int result = 0;
-	result = solution.rob(nums);
-	assert(0 == result);
-	cout << "\nPassed All\n";
-	return 0;
-}
