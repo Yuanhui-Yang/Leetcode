7d3f0c6f899393bf1fef66be0ed8e63dea97684a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-09 22:43:56 -0500

    172. Factorial Trailing Zeroes

--- Algorithms/factorial-trailing-zeroes.cpp
+++ Algorithms/factorial-trailing-zeroes.cpp
@@ -10,8 +10,8 @@ Note: Your solution should be in logarithmic time complexity.
 class Solution {
 public:
 	int trailingZeroes(int n) {
-		int m = log(n) / log(5), result = 0;
-		for (int i = 1; i <= m; ++i) {
+		int result = 0;
+		for (int m = log(n) / log(5), i = 1; i <= m; ++i) {
 			result += n / pow(5, i);
 		}
 		return result;
