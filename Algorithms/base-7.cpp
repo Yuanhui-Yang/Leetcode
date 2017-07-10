b375d08024c6ee165e82a8e806a9a908cab973e7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-10 00:59:16 -0500

    504. Base 7

--- Algorithms/base-7.cpp
+++ Algorithms/base-7.cpp
@@ -1,49 +1,33 @@
 // 504. Base 7
 // https://leetcode.com/problems/base-7/
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+Example 2:
+Input: -7
+Output: "-10"
+Note: The input will be in range of [-1e7, 1e7].
+*/
+
 class Solution {
 public:
 	string convertToBase7(int num) {
-		if (!num) {
+		if (num < 0) {
+			return "-" + convertToBase7(-num);
+		}
+		if (num == 0) {
 			return "0";
 		}
-		bool negative = num < 0;
 		string result;
-		if (negative) {
-			num = -num;
-		}
-		while (num > 0) {
-			result.push_back(char(num % 7 + '0'));
+		while (num) {
+			result.push_back('0' + num % 7);
 			num /= 7;
-		}
-		if (negative) {
-			result.push_back('-');
 		}
 		reverse(begin(result), end(result));
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	int num = 0;
-	string result;
-	string answer;
-
-	num = 100;
-	result = solution.convertToBase7(num);
-	answer = "202";
-	assert(answer == result);
-
-	num = -7;
-	result = solution.convertToBase7(num);
-	answer = "-10";
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
