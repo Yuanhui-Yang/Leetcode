7f19de53765e0873dee83b24a47abcf817bbd3b2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-10 21:16:13 -0500

    199. Binary Tree Right Side View

--- Algorithms/binary-tree-right-side-view.cpp
+++ Algorithms/binary-tree-right-side-view.cpp
@@ -1,40 +1,83 @@
 // 199. Binary Tree Right Side View
 // https://leetcode.com/problems/binary-tree-right-side-view/
-// https://discuss.leetcode.com/topic/11768/my-simple-accepted-solution-java
-#include <iostream>
-#include <vector>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	vector<int> rightSideView(TreeNode* root) {
+		if (!root) {
+			return {};
+		}
 		vector<int> result;
-		this->preorderTraveral(root, 0, result);
+		list<TreeNode*> current = {root};
+		while (!current.empty()) {
+			result.push_back(current.back()->val);
+			list<TreeNode*> next;
+			for (const auto & i : current) {
+				if (i->left) {
+					next.push_back(i->left);
+				}
+				if (i->right) {
+					next.push_back(i->right);
+				}
+			}
+			current = next;
+		}
+		return result;
+	}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> rightSideView(TreeNode* root) {
+		if (!root) {
+			return {};
+		}
+		int maxDepth = -1;
+		vector<int> result;
+		f(result, maxDepth, 0, root);
 		return result;
 	}
 private:
-	void preorderTraveral(TreeNode *root, size_t depth, vector<int>& result) {
-		if (!root) return;
-		if (depth == result.size()) result.push_back(root->val);
-		this->preorderTraveral(root->right, depth + 1, result);
-		this->preorderTraveral(root->left, depth + 1, result);
-		return;
+	void f(vector<int> & result, int & maxDepth, int depth, TreeNode* node) {
+		if (!node) {
+			return;
+		}
+		if (depth > maxDepth) {
+			maxDepth = max(maxDepth, depth);
+			result.push_back(node->val);
+		}
+		f(result, maxDepth, depth + 1, node->right);
+		f(result, maxDepth, depth + 1, node->left);
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode* root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->left->right = new TreeNode(5);
-	root->right = new TreeNode(3);
-	root->right->right = new TreeNode(4);
-	for (const auto &i : solution.rightSideView(root)) cout << i << '\t';
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
