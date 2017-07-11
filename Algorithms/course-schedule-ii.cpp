59ffdbf62bdd49a057a4643b585659ecfa3a576c
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-11 13:01:47 -0500

    210. Course Schedule II

--- Algorithms/course-schedule-ii.cpp
+++ Algorithms/course-schedule-ii.cpp
@@ -1,50 +1,68 @@
 // 210. Course Schedule II
 // https://leetcode.com/problems/course-schedule-ii/
-// https://discuss.leetcode.com/topic/17276/20-lines-c-bfs-dfs-solutions
-#include <iostream>
-#include <vector>
-#include <deque>
-#include <unordered_set>
-using namespace std;
+
+/*
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+For example:
+
+2, [[1,0]]
+There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+
+4, [[1,0],[2,0],[3,1],[3,2]]
+There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+click to show more hints.
+
+Hints:
+This problem is equivalent to finding the topological order in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
+Topological sort could also be done via BFS.
+*/
+
 class Solution {
 public:
 	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
-		vector<unordered_set<int>> graph = this->generateGraph(numCourses, prerequisites);
-		vector<int> indegrees = this->generateIndegrees(numCourses, graph);
-		deque<int> zeros;
-		for (int i = 0; i < numCourses; ++i) if (!indegrees[i]) zeros.push_back(i);
+		vector<list<int>> M(numCourses);
+		vector<int> A(numCourses, 0);
+		for (const auto & i : prerequisites) {
+			M[i.second].push_back(i.first);
+			++A[i.first];
+		}
 		vector<int> result;
+		list<int> current;
 		for (int i = 0; i < numCourses; ++i) {
-			if (zeros.empty()) return {};
-			int zero = zeros.front();
-			zeros.pop_front();
-			result.push_back(zero);
-			for (const auto &j : graph[zero]) if (!--indegrees[j]) zeros.push_back(j);
+			if (A[i] == 0) {
+				current.push_back(i);
+			}
+		}
+		while (!current.empty()) {
+			list<int> next;
+			for (const auto & i : current) {
+				result.push_back(i);
+				for (const auto & j : M[i]) {
+					if (--A[j] == 0) {
+						next.push_back(j);
+					}
+				}
+				M[i].clear();
+			}
+			current = next;
+		}
+		for (const auto & i : M) {
+			if (!i.empty()) {
+				return {};
+			}
 		}
 		return result;
 	}
-private:
-	vector<unordered_set<int>> generateGraph(int numCourses, vector<pair<int, int>>& prerequisites) {
-		vector<unordered_set<int>> result(numCourses);
-		for (const auto &i : prerequisites) result[i.second].insert(i.first);
-		return result;
-	}
-	vector<int> generateIndegrees(int numCourses, vector<unordered_set<int>>& graph) {
-		vector<int> result(numCourses, 0);
-		for (const auto &i : graph) for (const auto &j : i) ++result[j];
-		return result;
-	}
 };
-int main(void) {
-	Solution solution;
-	int numCourses = 2;
-	vector<pair<int, int>> prerequisites = {{1, 0}};
-	for (const auto &i : solution.findOrder(numCourses, prerequisites)) cout << i << '\t';
-	cout << "\tPassed\n";
-	numCourses = 4;
-	prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
-	for (const auto &i : solution.findOrder(numCourses, prerequisites)) cout << i << '\t';
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
