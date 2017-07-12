1597ea7fff2560c8815d6b3eb056c5d0f83c4da7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-12 00:45:32 -0500

    218. The Skyline Problem

--- Algorithms/the-skyline-problem.cpp
+++ Algorithms/the-skyline-problem.cpp
@@ -1,108 +1,79 @@
 // 218. The Skyline Problem
 // https://leetcode.com/problems/the-skyline-problem/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <utility>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <iterator>
-#include <functional>
-using namespace std;
-struct Skyline {
-	Skyline(int axis, int height, bool up) {
-		this->axis = axis;
-		this->height = height;
-		this->up = up;
-	}
-	int axis;
-	int height;
-	bool up;
-};
-struct SkylineComp {
-	bool operator() (const Skyline& i, const Skyline& j) {
-		if (i.axis == j.axis) {
-			if (i.height == j.height) {
-				return i.up > j.up;
-			}
-			return i.height > j.height;
-		}
-		return i.axis < j.axis;
-	}
-};
+
+/*
+A city's skyline is the outer contour of the silhouette formed by all the buildings in that city when viewed from a distance. Now suppose you are given the locations and height of all the buildings as shown on a cityscape photo (Figure A), write a program to output the skyline formed by these buildings collectively (Figure B).
+
+ Buildings  Skyline Contour
+The geometric information of each building is represented by a triplet of integers [Li, Ri, Hi], where Li and Ri are the x coordinates of the left and right edge of the ith building, respectively, and Hi is its height. It is guaranteed that 0 ? Li, Ri ? INT_MAX, 0 < Hi ? INT_MAX, and Ri - Li > 0. You may assume all buildings are perfect rectangles grounded on an absolutely flat surface at height 0.
+
+For instance, the dimensions of all buildings in Figure A are recorded as: [ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ] .
+
+The output is a list of "key points" (red dots in Figure B) in the format of [ [x1,y1], [x2, y2], [x3, y3], ... ] that uniquely defines a skyline. A key point is the left endpoint of a horizontal line segment. Note that the last key point, where the rightmost building ends, is merely used to mark the termination of the skyline, and always has zero height. Also, the ground in between any two adjacent buildings should be considered part of the skyline contour.
+
+For instance, the skyline in Figure B should be represented as:[ [2 10], [3 15], [7 12], [12 0], [15 10], [20 8], [24, 0] ].
+
+Notes:
+
+The number of buildings in any input list is guaranteed to be in the range [0, 10000].
+The input list is already sorted in ascending order by the left x position Li.
+The output list must be sorted by the x position.
+There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable; the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
+*/
+
 class Solution {
 public:
 	vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
-		if (buildings.empty()) {
-			return {};
+		vector<Line> A;
+		for (const auto & i : buildings) {
+			A.push_back(Line(i[0], i[2], -1));
+			A.push_back(Line(i[1], i[2], 1));
 		}
-		multiset<Skyline, SkylineComp> sorted_buildings;
-		for (const auto &building : buildings) {
-			sorted_buildings.insert(Skyline(building[0], building[2], true));
-			sorted_buildings.insert(Skyline(building[1], building[2], false));
-		}
-		multiset<int, greater<int>> max_heap;
+		sort(begin(A), end(A));
+		multiset<int> B;
 		vector<pair<int, int>> result;
-		for (multiset<Skyline, SkylineComp>::iterator sorted_building = begin(sorted_buildings);
-			sorted_building != end(sorted_buildings);
-			sorted_building++) {
-			if (sorted_building->up) {
-				if (max_heap.empty() || sorted_building->height > result.back().second) {
-					result.push_back(make_pair(sorted_building->axis, sorted_building->height));
+		for (const auto & i : A) {
+			if (i.z < 0) {
+				if (B.empty()) {
+					result.push_back({i.x, i.y});
 				}
-				max_heap.insert(sorted_building->height);
-				while (next(sorted_building) != end(sorted_buildings)
-					&& next(sorted_building)->axis == sorted_building->axis
-					&& next(sorted_building)->height <= sorted_building->height
-					&& next(sorted_building)->up) {
-					sorted_building++;
-					max_heap.insert(sorted_building->height);
+				else if (*prev(end(B)) < i.y) {
+					result.push_back({i.x, i.y});
 				}
-				continue;
+				B.insert(i.y);
 			}
-			multiset<int, greater<int>>::iterator max_heap_iter = max_heap.lower_bound(sorted_building->height);
-			max_heap.erase(max_heap_iter);
-			while (next(sorted_building) != end(sorted_buildings)
-				&& next(sorted_building)->axis == sorted_building->axis
-				&& next(sorted_building)->height <= sorted_building->height
-				&& !next(sorted_building)->up) {
-				sorted_building++;
-				max_heap_iter = max_heap.lower_bound(sorted_building->height);
-				max_heap.erase(max_heap_iter);
-			}
-			if (max_heap.empty()) {
-				result.push_back(make_pair(sorted_building->axis, 0));
-				continue;
-			}
-			multiset<int, greater<int>>::iterator max_element = begin(max_heap);
-			if (*max_element < result.back().second) {
-				result.push_back(make_pair(sorted_building->axis, *max_element));
-				continue;
+			else {
+				int h = *prev(end(B));
+				B.erase(B.find(i.y));
+				if (B.empty()) {
+					result.push_back({i.x, 0});
+				}
+				else if (*prev(end(B)) < h) {
+					result.push_back({i.x, *prev(end(B))});
+				}
 			}
 		}
 		return result;
 	}
+private:
+	struct Line {
+		int x, y, z;
+		Line(int x, int y, int z) {
+			this->x = x;
+			this->y = y;
+			this->z = z;
+		}
+		bool operator< (const Line & other) {
+			if (this->x == other.x) {
+				if (this->z == other.z) {
+					if (this->z < 0) {
+						return this->y > other.y;
+					}
+					return this->y < other.y;
+				}
+				return this->z < other.z;
+			}
+			return this->x < other.x;
+		}
+	};
 };
-int main(void) {
-	Solution solution;
-	vector<vector<int>> buildings;
-	vector<pair<int, int>> result;
-	vector<pair<int, int>> answer;
-
-	buildings = {{1, 2, 1}, {1, 2, 2}, {1, 2, 3}};
-	result = solution.getSkyline(buildings);
-	answer = {{1, 3}, {2, 0}};
-	assert(answer == result);	
-
-	buildings = {{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
-	result = solution.getSkyline(buildings);
-	answer = {{2, 10}, {3, 15}, {7, 12}, {12, 0}, {15, 10}, {20, 8}, {24, 0}};
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
