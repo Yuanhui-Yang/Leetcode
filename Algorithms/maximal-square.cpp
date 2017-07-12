fb805141fe8ad9516d86cb1559f61397b1b2a2a2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-12 02:55:13 -0500

    221. Maximal Square

--- Algorithms/maximal-square.cpp
+++ Algorithms/maximal-square.cpp
@@ -1,94 +1,76 @@
 // 221. Maximal Square
 // https://leetcode.com/problems/maximal-square/
-// https://leetcode.com/articles/maximal-square/
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-// BEGIN: Better Dynamic Programming
+
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 4.
+*/
+
 class Solution {
 public:
 	int maximalSquare(vector<vector<char>>& matrix) {
-		const int m = matrix.size();
-		if (m == 0) return 0;
-		const int n = matrix.front().size();
-		if (n == 0) return 0;
-		vector<int> OPT(n + 1, 0);
-		int result = 0;
-		for (int i = 0; i < m; ++i) {
-			int previous = 0;
-			for (int j = 0; j < n; ++j) {
-				if (matrix[i][j] == '0') {
-					previous = OPT[j + 1];
-					OPT[j + 1] = 0;
-					continue;
+		int result = 0, p = matrix.size(), q = p > 0 ? matrix.front().size() : 0;
+		vector<vector<int>> A(2, vector<int>(q, 0));
+		for (int i = 0, j = 0; j < q; ++j) {
+			if (matrix[i][j] == '1') {
+				A[i % 2][j] = 1;
+				result = 1;
+			}
+			else {
+				A[i % 2][j] = 0;
+			}
+		}
+		for (int i = 1; i < p; ++i) {
+			int x = i % 2, y = 1 - x;
+			A[x][0] = matrix[i][0] == '1';
+			result = max(result, A[x][0]);
+			for (int j = 1; j < q; ++j) {
+				if (matrix[i][j] == '1') {
+					A[x][j] = 1 + min(A[y][j - 1], min(A[x][j - 1], A[y][j]));
+					result = max(result, A[x][j]);
 				}
-				int tmp = OPT[j + 1];
-				OPT[j + 1] = 1 + min(min(OPT[j] ,OPT[j + 1]), previous);
-				previous = tmp;
-				result = max(result, OPT[j + 1]);
+				else {
+					A[x][j] = 0;
+				}
 			}
 		}
 		return result * result;
 	}
 };
-// END: Better Dynamic Programming
-// BEGIN: Dynamic Programming
-// class Solution {
-// public:
-// 	int maximalSquare(vector<vector<char>>& matrix) {
-// 		const int m = matrix.size();
-// 		if (m == 0) return 0;
-// 		const int n = matrix.front().size();
-// 		if (n == 0) return 0;
-// 		int result = 0;
-// 		vector<vector<int>> OPT(m + 1, vector<int>(n + 1, 0));
-// 		for (int i = 0; i < m; ++i) {
-// 			for (int j = 0; j < n; ++j) {
-// 				if (matrix[i][j] == '0') continue;
-// 				OPT[i + 1][j + 1] = 1 + min(min(OPT[i][j + 1], OPT[i + 1][j]), OPT[i][j]);
-// 				result = max(result, OPT[i + 1][j + 1]);
-// 			}
-// 		}
-// 		return result * result;
-// 	}
-// };
-// END: Dynamic Programming
-// BEGIN: Brute Force Approach
-// class Solution {
-// public:
-// 	int maximalSquare(vector<vector<char>>& matrix) {
-// 		int n = 0;
-// 		for (int i = 0; i < (int)matrix.size(); ++i) {
-// 			for (int j = 0; j < (int)matrix.front().size(); ++j) {
-// 				if (matrix[i][j] == '0') continue;
-// 				while (this->help(matrix, n + 1, i, j)) ++n;
-// 			}
-// 		}
-// 		return n * n;
-// 	}
-// private:
-// 	bool help(vector<vector<char>>& matrix, int n, int i, int j) {
-// 		if ((int)matrix.size() < n + i || (int)matrix.front().size() < n + j) return false;
-// 		for (int p = 0; p < n; ++p)
-// 			for (int q = 0; q < n; ++q)
-// 				if (matrix[i + p][j + q] == '0') return false;
-// 		return true;
-// 	}
-// };
-// END: Brute Force Approach
-int main(void) {
-	Solution solution;
-	vector<vector<char>> matrix = {{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}};
-	cout << solution.maximalSquare(matrix) << "\tPassed\n";
-	matrix = {{'0', '1'}};
-	cout << solution.maximalSquare(matrix) << "\tPassed\n";
-	matrix = {{'0', '0', '0'}, {'0', '0', '0'}, {'1', '1', '1'}};
-	cout << solution.maximalSquare(matrix) << "\tPassed\n";
-	matrix = {{'1', '0', '1', '0'}, {'1', '0', '1', '1'}, {'1', '0', '1', '1'}, {'1', '1', '1', '1'}};
-	cout << solution.maximalSquare(matrix) << "\tPassed\n";
-	matrix = {{'1', '0', '1', '1', '1'}, {'0', '1', '0', '1', '0'}, {'1', '1', '0', '1', '1'}, {'1', '1', '0', '1', '1'}, {'0', '1', '1', '1', '1'}};
-	cout << solution.maximalSquare(matrix) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
+
+class Solution {
+public:
+	int maximalSquare(vector<vector<char>>& matrix) {
+		int result = 0, p = matrix.size(), q = p > 0 ? matrix.front().size() : 0;
+		vector<vector<int>> A(p, vector<int>(q, 0));
+		for (int i = 0, j = 0; i < p; ++i) {
+			if (matrix[i][j] == '1') {
+				A[i][j] = 1;
+				result = 1;
+			}
+		}
+		for (int i = 0, j = 0; j < q; ++j) {
+			if (matrix[i][j] == '1') {
+				A[i][j] = 1;
+				result = 1;
+			}
+		}
+		for (int i = 1; i < p; ++i) {
+			for (int j = 1; j < q; ++j) {
+				if (matrix[i][j] != '1') {
+					continue;
+				}
+				A[i][j] = 1 + min(A[i - 1][j - 1], min(A[i - 1][j], A[i][j - 1]));
+				result = max(result, A[i][j]);
+			}
+		}
+		return result * result;
+	}
+};
