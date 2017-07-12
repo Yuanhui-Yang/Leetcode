3ae298ce50bf357d054c6af608efaf092bc9d2f3
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-12 11:13:16 -0500

    222. Count Complete Tree Nodes

--- Algorithms/count-complete-tree-nodes.cpp
+++ Algorithms/count-complete-tree-nodes.cpp
@@ -1,36 +1,41 @@
 // 222. Count Complete Tree Nodes
 // https://leetcode.com/problems/count-complete-tree-nodes/
-#include <iostream>
-#include <cassert>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a complete binary tree, count the number of nodes.
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	int countNodes(TreeNode* root) {
-		if (!root) return 0;
-		if (!root->left) return 1;
-		if (!root->right) return 2;
-		int h1 = 0;
-		for (TreeNode *it = root; it; it = it->left) h1++;
-		int h2 = 0;
-		for (TreeNode *it = root; it; it = it->right) h2++;
-		if (h1 == h2) return ((1 << h1) - 1);
-		return countNodes(root->left) + countNodes(root->right) + 1;
+		if (!root) {
+			return 0;
+		}
+		TreeNode * n1 = root, * n2 = root;
+		int l1 = 0, l2 = 0;
+		while (n1) {
+			++l1;
+			n1 = n1->left;
+		}
+		while (n2) {
+			++l2;
+			n2 = n2->right;
+		}
+		if (l1 == l2) {
+			return (1 << l1) - 1;
+		}
+		return 1 + countNodes(root->left) + countNodes(root->right);
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(0);
-	root->left = new TreeNode(1);
-	root->right = new TreeNode(2);
-	root->left->left = new TreeNode(3);
-	root->left->right = new TreeNode(4);
-	assert(5 == solution.countNodes(root));
-	cout << "\nPassed All\n";
-	return 0;
-}
