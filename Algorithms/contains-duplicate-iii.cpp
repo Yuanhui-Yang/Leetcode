c4038df98fc6c3adb34cf5a3aaf6d3437143aa68
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-11 21:56:17 -0500

    220. Contains Duplicate III

--- Algorithms/contains-duplicate-iii.cpp
+++ Algorithms/contains-duplicate-iii.cpp
@@ -11,6 +11,46 @@ public:
 		if (nums.empty() or k <= 0 or t < 0) {
 			return false;
 		}
+		if (t == 0) {
+			return unordered_set<int>(begin(nums), end(nums)).size() < nums.size();
+		}
+		pair<vector<int>::iterator, vector<int>::iterator> p = minmax_element(begin(nums), end(nums));
+		long minVal = *p.first, maxVal = *p.second;
+		long m = nums.size(), n = (maxVal - minVal) / t + 1;
+		unordered_map<long, long> dict;
+		for (long i = 0; i < m; ++i) {
+			if (i > k) {
+				dict.erase(f1(nums[i - k - 1], minVal, t));
+			}
+			long id = f1(nums[i], minVal, t);
+			if (dict.count(id) and f2(nums[dict[id]], nums[i]) <= t) {
+				return true;
+			}
+			if (dict.count(id - 1) and f2(nums[dict[id - 1]], nums[i]) <= t) {
+				return true;
+			}
+			if (dict.count(id + 1) and f2(nums[dict[id + 1]], nums[i]) <= t) {
+				return true;
+			}
+			dict[id] = i;
+		}
+		return false;
+	}
+private:
+	long f1(long val, long minVal, long t) {
+		return (val - minVal) / t;
+	}
+	long f2(long a, long b) {
+		return a < b ? b - a : a - b;
+	}
+};
+
+class Solution {
+public:
+	bool containsNearbyAlmostDuplicate(vector<int>& nums, long k, long t) {
+		if (nums.empty() or k <= 0 or t < 0) {
+			return false;
+		}
 		multiset<long> s;
 		for (long n = nums.size(), i = 0; i < n; ++i) {
 			multiset<long>::iterator iter = s.lower_bound(nums[i] - t);
