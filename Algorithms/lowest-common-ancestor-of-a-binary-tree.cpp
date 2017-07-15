b833febabdcf33852a41d3f83b843ecb8989e3ac
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-15 13:04:53 -0500

    236. Lowest Common Ancestor of a Binary Tree

--- Algorithms/lowest-common-ancestor-of-a-binary-tree.cpp
+++ Algorithms/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,42 +1,37 @@
 // 236. Lowest Common Ancestor of a Binary Tree
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
-#include <iostream>
-#include <cassert>
-#include <queue>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-// BEGIN: https://discuss.leetcode.com/topic/20063/accepted-24ms-dfs-c-solution-only-3-lines
+
+/*
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+		_______3______
+	   /              \
+	___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+		 /  \
+		 7   4
+For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		if (!root || root == p || root == q) return root;
-		if (!p) return q;
-		if (!q) return p;
-		if (p == q) return p;
-		TreeNode *left = this->lowestCommonAncestor(root->left, p, q);
-		TreeNode *right = this->lowestCommonAncestor(root->right, p, q);
-		return left && right ? root : left ? left : right;
+		if (!root or root == p or root == q) {
+			return root;
+		}
+		TreeNode * left = lowestCommonAncestor(root->left, p, q), * right = lowestCommonAncestor(root->right, p, q);
+		return left and right ? root : left ? left : right;
 	}
 };
-// END: https://discuss.leetcode.com/topic/20063/accepted-24ms-dfs-c-solution-only-3-lines
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(3);
-	root->left = new TreeNode(5);
-	root->right = new TreeNode(1);
-	root->left->left = new TreeNode(6);
-	root->left->right = new TreeNode(2);
-	root->left->right->left = new TreeNode(7);
-	root->left->right->right = new TreeNode(4);
-	root->right->left = new TreeNode(0);
-	root->right->right = new TreeNode(8);
-	assert(root == solution.lowestCommonAncestor(root, root->left, root->right));
-	assert(root->left == solution.lowestCommonAncestor(root, root->left, root->left->right->right));
-	cout << "\nPassed All\n";
-	return 0;
-}
