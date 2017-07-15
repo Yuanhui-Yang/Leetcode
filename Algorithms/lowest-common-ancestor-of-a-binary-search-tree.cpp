bbd43b16734898ba619c9248cf6220d738c3dace
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-15 13:34:51 -0500

    235. Lowest Common Ancestor of a Binary Search Tree

--- Algorithms/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ Algorithms/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,41 +1,67 @@
 // 235. Lowest Common Ancestor of a Binary Search Tree
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
-#include <iostream>
-#include <cassert>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+		_______6______
+	   /              \
+	___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+		 /  \
+		 3   5
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-		if (!root) return NULL;
-		if (!p) return q;
-		if (!q) return p;
-		if (p == q) return p;
-		if (p->val > q->val) return this->lowestCommonAncestor(root, q, p);
-		if (p->val <= root->val && root->val <= q->val) return root;
-		if (p->val < root->val && q->val < root->val) return this->lowestCommonAncestor(root->left, p, q);
-		if (p->val > root->val && q->val > root->val) return this->lowestCommonAncestor(root->right, p, q);
-		return NULL;
+		if (p->val > q->val) {
+			return lowestCommonAncestor(root, q, p);
+		}
+		while (!(p->val <= root->val and root->val <= q->val)) {
+			root = root->val < p->val ? root->right : root->left;
+		}
+		return root;
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(6);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(8);
-	root->left->left = new TreeNode(0);
-	root->left->right = new TreeNode(4);
-	root->left->right->left = new TreeNode(3);
-	root->left->right->right = new TreeNode(5);
-	root->right->left = new TreeNode(7);
-	root->right->right = new TreeNode(9);
-	assert(solution.lowestCommonAncestor(root, root->left, root->right) == root);
-	assert(solution.lowestCommonAncestor(root, root->left, root->left->right) == root->left);
-	cout << "\nPassed All\n";
-	return 0;
-}
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root) {
+			return root;
+		}
+		if (p->val > q->val) {
+			return lowestCommonAncestor(root, q, p);
+		}
+		if (p->val <= root->val and root->val <= q->val) {
+			return root;
+		}
+		if (root->val < p->val) {
+			return lowestCommonAncestor(root->right, p, q);
+		}
+		return lowestCommonAncestor(root->left, p, q);
+	}
+};
