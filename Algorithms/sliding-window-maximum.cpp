648a4fc92f871a95242c30e277df9b406406b360
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-15 16:08:22 -0500

    239. Sliding Window Maximum

--- Algorithms/sliding-window-maximum.cpp
+++ Algorithms/sliding-window-maximum.cpp
@@ -1,63 +1,41 @@
 // 239. Sliding Window Maximum
 // https://leetcode.com/problems/sliding-window-maximum/
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <deque>
-#include <algorithm>
-using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/19055/java-o-n-solution-using-deque-with-explanation
+
+/*
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+
+Note: 
+You may assume k is always valid, ie: 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+*/
+
 class Solution {
 public:
 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+		multiset<int, greater<int>> tree;
 		vector<int> result;
-		deque<int> dq;
-		const int n = nums.size();
-		for (int i = 0; i < n; i++) {
-			while (!dq.empty() && i - dq.front() > k - 1) dq.pop_front();
-			while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
-			dq.push_back(i);
-			if (i >= k - 1) result.push_back(nums[dq.front()]);
+		for (int n = nums.size(), i = 0; i < n; ++i) {
+			tree.insert(nums[i]);
+			if (tree.size() == k) {
+				result.push_back(*begin(tree));
+				tree.erase(tree.find(nums[i + 1 - k]));
+			}
 		}
 		return result;
 	}
 };
-// END: https://discuss.leetcode.com/topic/19055/java-o-n-solution-using-deque-with-explanation
-// class Solution {
-// public:
-// 	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-// 		vector<int> result;
-// 		const int n = nums.size();
-// 		if (n == 0) return result;
-// 		deque<int> dq;
-// 		int maxVal = INT_MIN;
-// 		for (int i = 0; i < n; i++) {
-// 			maxVal = INT_MIN;
-// 			if ((int)dq.size() + 1 < k) {
-// 				dq.push_back(nums[i]);
-// 				maxVal = max(maxVal, nums[i]);
-// 				continue;
-// 			}
-// 			dq.push_back(nums[i]);
-// 			for (const auto &j : dq) maxVal = max(maxVal, j);
-// 			result.push_back(maxVal);
-// 			dq.pop_front();
-// 		}
-// 		if (n < k) result.push_back(maxVal);
-// 		return result;
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	for (const auto &i : solution.maxSlidingWindow(nums, 0)) cout << i << '\t';
-	cout << "\tPassed\n";
-	nums = {1,3,-1,-3,5,3,6,7};
-	for (const auto &i : solution.maxSlidingWindow(nums, 3)) cout << i << '\t';
-	cout << "\tPassed\n";
-	nums = {1,-1};
-	for (const auto &i : solution.maxSlidingWindow(nums, 1)) cout << i << '\t';
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
