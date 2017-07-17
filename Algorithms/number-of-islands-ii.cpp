4556ea3b9235bc883ed860017d91819f3b69b742
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-17 05:32:03 -0500

    305. Number of Islands II

--- Algorithms/number-of-islands-ii.cpp
+++ Algorithms/number-of-islands-ii.cpp
@@ -1,108 +1,87 @@
 // 305. Number of Islands II
 // https://leetcode.com/problems/number-of-islands-ii/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <utility>
-#include <iterator>
-using namespace std;
-// BEGIN: http://www.jiuzhang.com/solutions/number-of-islands-ii/
+
+/*
+A 2d grid map of m rows and n columns is initially filled with water. We may perform an addLand operation which turns the water at position (row, col) into a land. Given a list of positions to operate, count the number of islands after each addLand operation. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example:
+
+Given m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]].
+Initially, the 2d grid grid is filled with water. (Assume 0 represents water and 1 represents land).
+
+0 0 0
+0 0 0
+0 0 0
+Operation #1: addLand(0, 0) turns the water at grid[0][0] into a land.
+
+1 0 0
+0 0 0   Number of islands = 1
+0 0 0
+Operation #2: addLand(0, 1) turns the water at grid[0][1] into a land.
+
+1 1 0
+0 0 0   Number of islands = 1
+0 0 0
+Operation #3: addLand(1, 2) turns the water at grid[1][2] into a land.
+
+1 1 0
+0 0 1   Number of islands = 2
+0 0 0
+Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
+
+1 1 0
+0 0 1   Number of islands = 3
+0 1 0
+We return the result as an array: [1, 1, 2, 3]
+
+Challenge:
+
+Can you do it in time complexity O(k log mn), where k is the length of the positions?
+*/
+
 class Solution {
 public:
 	vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
+		int sz = m * n, cnt = 0;
+		vector<int> A(sz, -1);
 		vector<int> result;
-		const int positions_size = positions.size();
-		vector<int> father(m * n, 0);
-		for (int i = 0; i < m * n; i++) father[i] = i;
-		vector<vector<bool>> graph(m, vector<bool>(n, false));
-		const vector<int> dx = {-1, 0, 1, 0};
-		const vector<int> dy = {0, -1, 0, 1};
-		for (int i = 0, cnt = 0; i < positions_size; i++) {
-			int x_i = positions[i].first;
-			int y_i = positions[i].second;
-			if (!graph[x_i][y_i]) {
-				graph[x_i][y_i] = true;
-				cnt++;
-				for (int j = 0; j < 4; j++) {
-					int x_j = x_i + dx[j];
-					int y_j = y_i + dy[j];
-					if (x_j >= 0 && x_j < m && y_j >= 0 && y_j < n && graph[x_j][y_j]) {
-						int key_i = y_i + n * x_i;
-						int root_i = find(key_i, father);
-						int key_j = y_j + n * x_j;
-						int root_j = find(key_j, father);
-						if (root_i != root_j) {
-							father[root_i] = root_j;
-							cnt--;
-						}
-					}
-				}
-				result.push_back(cnt);
-			}
+		for (const auto & i : positions) {
+			cnt = f(A, m, n, i.first, i.second, cnt);
+			result.push_back(cnt);
 		}
 		return result;
 	}
 private:
-	int find(int x, vector<int>& father) {
-		if (x == father[x]) return x;
-		return father[x] = find(father[x], father);
+	int f(vector<int> & A, int m, int n, int x, int y, int cnt) {
+		int id = y + x * n;
+		if (A[id] >= 0) {
+			return 0;
+		}
+		A[id] = id;
+		int result = 1 + cnt, dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+		for (int i = 0; i < 4; ++i) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx >= 0 and nx < m and ny >= 0 and ny < n) {
+				int nid = ny + n * nx;
+				if (A[nid] >= 0) {
+					int r = g(A, id), nr = g(A, nid);
+					if (r != nr) {
+						--result;
+						A[r] = nr;
+					}
+				}
+			}
+		}
+		return result;
 	}
+	int g(vector<int> & A, int id) {
+		return A[id] == id ? id : A[id] = g(A, A[id]);
+	}
+	// int g(vector<int> & A, int id) {
+	//     while (A[id] != id) {
+	//         A[id] = A[A[id]];
+	//         id = A[id];
+	//     }
+	//     return id;
+	// }
 };
-// END: http://www.jiuzhang.com/solutions/number-of-islands-ii/
-// BEGIN: https://discuss.leetcode.com/topic/29613/easiest-java-solution-with-explanations/
-// class Solution {
-// public:
-// 	vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
-// 		vector<int> result;
-// 		vector<int> roots(m * n, -1);
-// 		vector<pair<int, int>> signs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-// 		int count = 0;
-// 		for (const auto &i : positions) {
-// 			int current = i.first * n + i.second;
-// 			roots[current] = current;
-// 			++count;
-// 			for (const auto &j : signs) {
-// 				int row = i.first + j.first;
-// 				int column = i.second + j.second;
-// 				int neighbor = row * n + column;
-// 				if (row < 0 || row >= m || column < 0 || column >= n || roots[neighbor] == -1) continue;
-// 				while (neighbor != roots[neighbor]) neighbor = roots[neighbor] = roots[roots[neighbor]];
-// 				if (neighbor == current) continue;
-// 				roots[current] = neighbor;
-// 				current = neighbor;
-// 				--count;
-// 			}
-// 			result.push_back(count);
-// 		}
-// 		return result;
-// 	}
-// };
-// END: https://discuss.leetcode.com/topic/29613/easiest-java-solution-with-explanations/
-int main(void) {
-	Solution solution;
-	vector<pair<int, int>> positions;
-	vector<int> result;
-
-	positions = {{0, 0}, {0, 1}, {1, 2}, {2, 1}};
-	result = solution.numIslands2(3, 3, positions);
-	assert(vector<int>({1, 1, 2, 3}) == result);
-
-	positions = {{7, 0}};
-	result = solution.numIslands2(8, 2, positions);
-	assert(vector<int>({1}) == result);
-
-	positions = {{0, 0}, {1, 1}, {0, 1}};
-	result = solution.numIslands2(2, 2, positions);
-	assert(vector<int>({1, 2, 1}) == result);
-
-	positions = {{0, 1}, {1, 2}, {2, 1}, {1, 0}, {0, 2}, {0, 0},{1, 1}};
-	result = solution.numIslands2(3, 3, positions);
-	assert(vector<int>({1, 2, 3, 4, 3, 2, 1}) == result);
-	
-	positions = {{0, 1}, {0, 0}};
-	result = solution.numIslands2(1, 2, positions);
-	assert(vector<int>({1, 1}) == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
