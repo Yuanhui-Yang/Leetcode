c445d313592533678da32330b5fa0081e9f1cfd2
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-18 03:28:52 -0500

    415. Add Strings

--- Algorithms/add-strings.cpp
+++ Algorithms/add-strings.cpp
@@ -1,40 +1,33 @@
 // 415. Add Strings
 // https://leetcode.com/problems/add-strings/
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Note:
+
+The length of both num1 and num2 is < 5100.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+*/
+
 class Solution {
 public:
 	string addStrings(string num1, string num2) {
-		if (num1 == "0") return num2;
-		if (num2 == "0") return num1;
-		if (num1.size() < num2.size()) swap(num1, num2);
-		reverse(begin(num1), end(num1));
-		reverse(begin(num2), end(num2));
-		const size_t m = num1.size(), n = num2.size();
-		int carry = 0;
-		for (size_t i = 0; i < m; i++) {
-			if (i < n) {
-				int val1 = num1[i] - '0', val2 = num2[i] - '0';
-				int val = val1 + val2 + carry;
-				num1[i] = '0' + val % 10;
-				carry = val / 10;
-				continue;
-			}
-			int val = num1[i] - '0' + carry;
-			num1[i] = '0' + val % 10;
-			carry = val / 10;
+		int M = num1.size(), N = num2.size(), i = M - 1, j = N - 1, k = M + N, carry = 0, base = 10;
+		string result(M + N + 1, '0');
+		while (i >= 0 or j >= 0 or carry > 0) {
+			int x = i >= 0 ? num1[i--] - '0' : 0;
+			int y = j >= 0 ? num2[j--] - '0' : 0;
+			int z = x + y + carry;
+			carry = z / base;
+			z %= base;
+			result[k--] = '0' + z;
 		}
-		if (carry) num1.push_back('0' + carry);
-		reverse(begin(num1), end(num1));
-		return num1;
+		while (!result.empty() and result.front() == '0') {
+			result.erase(begin(result));
+		}
+		return result.empty() ? string("0") : result;
 	}
 };
-int main(void) {
-	Solution solution;
-	cout << solution.addStrings("9", "99") << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
