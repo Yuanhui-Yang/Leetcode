905419c4bdb08e49175fdbce6c0266a6b4d550ef
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-19 21:32:30 -0500

    523. Continuous Subarray Sum

--- Algorithms/continuous-subarray-sum.cpp
+++ Algorithms/continuous-subarray-sum.cpp
@@ -1,116 +1,48 @@
 // 523. Continuous Subarray Sum
 // https://leetcode.com/problems/continuous-subarray-sum/
-#include <iostream>
-#include <cstdlib>
-#include <cassert>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+
+Example 1:
+Input: [23, 2, 4, 6, 7],  k=6
+Output: True
+Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+Example 2:
+Input: [23, 2, 6, 4, 7],  k=6
+Output: True
+Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+Note:
+The length of the array won't exceed 10,000.
+You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+*/
+
 class Solution {
 public:
 	bool checkSubarraySum(vector<int>& nums, int k) {
-		if (nums.size() < 2) {
-			return false;
-		}
-		for (size_t i = 1; i < nums.size(); i++) {
-			if (nums.at(i) == 0 && nums.at(i - 1) == 0) {
-				return true;
-			}
-		}
+		int sz = nums.size();
 		if (k == 0) {
-			return false;
-		}
-		k = abs(k);
-		if (k == 1) {
-			return true;
-		}
-		set<int> tree_set;
-		tree_set.insert(0);
-		int cnt = 0;
-		for (const auto &num : nums) {
-			const int up = num + *prev(end(tree_set));
-			const int low = num;
-			for (int i = 1; k && i <= (up - low) / k + 1; i++) {
-				const set<int>::iterator it = tree_set.lower_bound(up - k * i);
-				if (cnt >= 1 && it != end(tree_set) && *it == (up - k * i)) {
+			for (int i = 1; i < sz; ++i) {
+				if (nums[i - 1] == 0 and nums[i] == 0) {
 					return true;
 				}
 			}
-			tree_set.insert(up);
-			cnt++;
-		} 
+			return false;
+		}
+		unordered_map<int, int> A;
+		A[0] = -1;
+		for (int sum = 0, i = 0; i < sz; ++i) {
+			sum += nums[i] % k;
+			sum %= k;
+			if (A.count(sum)) {
+				if (i - A[sum] > 1) {
+					return true;
+				}
+			}
+			else {
+				A[sum] = i;
+			}
+		}
 		return false;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int k = 0;
-	bool result = false;
-	bool answer = false;
-
-	nums = {1, 1000000000};
-	k = 1;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {5, 2, 4};
-	k = 5;
-	answer = false;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {0, 0};
-	k = -1;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {0, 1, 0};
-	k = 0;
-	answer = false;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {23, 2, 4, 6, 7};
-	k = -6;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {0};
-	k = 0;
-	answer = false;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {0, 0};
-	k = 0;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {23, 2, 6, 4, 7};
-	k = 0;
-	answer = false;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {23, 2, 4, 6, 7};
-	k = 6;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	nums = {23, 2, 6, 4, 7};
-	k = 6;
-	answer = true;
-	result = solution.checkSubarraySum(nums, k);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
