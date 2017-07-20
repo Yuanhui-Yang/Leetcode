127e69561bcf7aec12767d62c5170beb27996b5f
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-20 03:44:18 -0500

    414. Third Maximum Number

--- Algorithms/third-maximum-number.cpp
+++ Algorithms/third-maximum-number.cpp
@@ -1,29 +1,63 @@
 // 414. Third Maximum Number
 // https://leetcode.com/problems/third-maximum-number/
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+Example 2:
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+*/
+
 class Solution {
 public:
 	int thirdMax(vector<int>& nums) {
-		sort(begin(nums), end(nums));
-		reverse(begin(nums), end(nums));
-		size_t i = 0, n = nums.size();
-		while (i + 1 < n && nums[i + 1] == nums[i]) i++;
-		if (i + 1 == n) return nums[0];
-		i++;
-		while (i + 1 < n && nums[i + 1] == nums[i]) i++;
-		if (i + 1 == n) return nums[0];
-		return nums[++i];
+		array<int, 3> A;
+		A[0] = -1;
+		A[1] = -1;
+		A[2] = -1;
+		for (int sz = nums.size(), i = 0; i < sz; ++i) {
+			if (A[0] >= 0 and nums[A[0]] == nums[i]) {
+				continue;
+			}
+			if (A[1] >= 0 and nums[A[1]] == nums[i]) {
+				continue;
+			}
+			if (A[2] >= 0 and nums[A[2]] == nums[i]) {
+				continue;
+			}
+			if (A[0] < 0 or nums[A[0]] < nums[i]) {
+				A[2] = A[1];
+				A[1] = A[0];
+				A[0] = i;
+				continue;
+			}
+			if (A[1] < 0 or nums[A[1]] < nums[i]) {
+				A[2] = A[1];
+				A[1] = i;
+				continue;
+			}
+			if (A[2] < 0 or nums[A[2]] < nums[i]) {
+				A[2] = i;
+				continue;
+			}
+		}
+		return A[2] >= 0 ? nums[A[2]] : nums[A[0]];
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums = {1, 2, 2, 5, 3, 5};
-	cout << solution.thirdMax(nums) << "\tPassed\n";
-	nums = {-2147483648, 1, 2};
-	cout << solution.thirdMax(nums) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
