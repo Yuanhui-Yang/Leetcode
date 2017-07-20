255a73d873251b3f3d319523f0e46f212294ba7b
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-20 03:01:28 -0500

    329. Longest Increasing Path in a Matrix

--- Algorithms/longest-increasing-path-in-a-matrix.cpp
+++ Algorithms/longest-increasing-path-in-a-matrix.cpp
@@ -30,6 +30,51 @@ The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 class Solution {
 public:
 	int longestIncreasingPath(vector<vector<int>>& matrix) {
+		int M = matrix.size(), N = M == 0 ? 0 : matrix[0].size();
+		vector<vector<int>> A(M, vector<int>(N, 0));
+		array<int, 4> dx = {0, -1, 0, 1}, dy = {-1, 0, 1, 0};
+		for (int x = 0; x < M; ++x) {
+			for (int y = 0; y < N; ++y) {
+				for (int i = 0; i < 4; ++i) {
+					int nx = x + dx[i], ny = y + dy[i];
+					if (nx >= 0 and nx < M and ny >= 0 and ny < N and matrix[x][y] < matrix[nx][ny]) {
+						++A[nx][ny];
+					}
+				}
+			}
+		}
+		int result = 0;
+		list<array<int, 2>> curr;
+		for (int i = 0; i < M; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if (A[i][j] == 0) {
+					curr.push_back({i, j});
+				}
+			}
+		}
+		while (!curr.empty()) {
+			++result;
+			list<array<int, 2>> next;
+			for (const auto & p : curr) {
+				int x = p[0], y = p[1];
+				for (int i = 0; i < 4; ++i) {
+					int nx = x + dx[i], ny = y + dy[i];
+					if (nx >= 0 and nx < M and ny >= 0 and ny < N and matrix[x][y] < matrix[nx][ny]) {
+						if (--A[nx][ny] == 0) {
+							next.push_back({nx, ny});
+						}
+					}
+				}
+			}
+			curr = next;
+		}
+		return result;
+	}
+};
+
+class Solution {
+public:
+	int longestIncreasingPath(vector<vector<int>>& matrix) {
 		int M = matrix.size(), N = M == 0 ? 0 : matrix[0].size(), result = 0;
 		vector<vector<int>> A(M, vector<int>(N, -1));
 		for (int i = 0; i < M; ++i) {
