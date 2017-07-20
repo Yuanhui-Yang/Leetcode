2fe2af1ba01a5ff5282a1fe16abad46e5e70accb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-20 05:22:29 -0500

    290. Word Pattern

--- Algorithms/word-pattern.cpp
+++ Algorithms/word-pattern.cpp
@@ -1,52 +1,46 @@
 // 290. Word Pattern
 // https://leetcode.com/problems/word-pattern/
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+*/
+
 class Solution {
 public:
 	bool wordPattern(string pattern, string str) {
-		vector<string> str_vec;
-		for (size_t i = 0, j = 0; i < str.size(); i++) {
-			if (i + 1 == str.size() || str[i] == ' ') {
-				string s = i + 1 == str.size() ? str.substr(j, str.size() - j) : str.substr(j, i - j);
-				str_vec.push_back(s);
-				j = i + 1;
+		array<int, 256> A;
+		A.fill(-1);
+		unordered_map<string, int> B;
+		int X = pattern.size(), Y = str.size(), i = 0, j = 0;
+		for (; i < X and j < Y; ++i, ++j) {
+			int k = j;
+			while (j < Y and str[j] != ' ') {
+				++j;
 			}
-		}
-		if (pattern.size() != str_vec.size()) {
-			return false;
-		}
-		unordered_map<char, vector<size_t>> pattern_hashmap;
-		unordered_map<string, vector<size_t>> str_hashmap;
-		for (size_t i = 0; i < pattern.size(); i++) {
-			pattern_hashmap[pattern[i]].push_back(i);
-			str_hashmap[str_vec[i]].push_back(i);
-		}
-		for (size_t i = 0; i < pattern.size(); i++) {
-			if (pattern_hashmap[pattern[i]] != str_hashmap[str_vec[i]]) {
+			char key1 = pattern[i];
+			string key2 = str.substr(k, j - k);
+			if (A[key1] < 0 and !B.count(key2)) {
+				A[key1] = i;
+				B[key2] = i;
+			}
+			else if (A[key1] >= 0 and B.count(key2) and A[key1] == B[key2]) {
+				A[key1] = i;
+				B[key2] = i;
+			}
+			else {
 				return false;
 			}
 		}
-		return true;
+		return i == X and --j == Y;
 	}
 };
-int main(void) {
-	Solution solution;
-	bool result;
-	result = solution.wordPattern("abba", "dog cat cat dog");
-	assert(true == result);
-	result = solution.wordPattern("abba", "dog cat cat fish");
-	assert(false == result);
-	result = solution.wordPattern("aaaa", "dog cat cat dog");
-	assert(false == result);
-	result = solution.wordPattern("abba", "dog dog dog dog");
-	assert(false == result);
-	cout << "\nPassed All\n";
-	return 0;
-}
