31b1f0c7b0e627530af0b822363260b1c0a94540
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-22 09:15:23 -0500

    434. Number of Segments in a String

--- Algorithms/number-of-segments-in-a-string.cpp
+++ Algorithms/number-of-segments-in-a-string.cpp
@@ -1,25 +1,31 @@
 // 434. Number of Segments in a String
 // https://leetcode.com/problems/number-of-segments-in-a-string/
-#include <iostream>
-#include <string>
-#include <cassert>
-using namespace std;
+
+/*
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+*/
+
 class Solution {
 public:
 	int countSegments(string s) {
-		int result = 0;
-		const int n = s.size();
-		for (int i = 0; i < n; i++) {
-			if (s[i] == ' ') continue;
-			result++;
-			while (i < n && s[i] != ' ') i++;
+		int result = 0, sz = s.size(), i = 0;
+		while (i < sz) {
+			if (s[i] == ' ') {
+				++i;
+				continue;
+			}
+			++result;
+			while (i < sz and s[i] != ' ') {
+				++i;
+			}
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	assert(5 == solution.countSegments("Hello, my name is John"));
-	cout << "\nPassed All\n";
-	return 0;
-}
