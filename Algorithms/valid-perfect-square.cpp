45baf0d7506f4f1552f6445967aa45bff733fbf5
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-22 03:17:41 -0500

    367. Valid Perfect Square

--- Algorithms/valid-perfect-square.cpp
+++ Algorithms/valid-perfect-square.cpp
@@ -1,24 +1,34 @@
 // 367. Valid Perfect Square
 // https://leetcode.com/problems/valid-perfect-square/
-#include <iostream>
-using namespace std;
+
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Returns: True
+Example 2:
+
+Input: 14
+Returns: False
+*/
+
 class Solution {
 public:
-	bool isPerfectSquare(int num) {
-		size_t x = this->mySqrt(num);
-		return x * x == num;
-	}
-private:
-	int mySqrt(int x) {
-		size_t r = x;
-		while (r * r > x)
-			r = (r + x / r) / 2;
-		return r;
+	bool isPerfectSquare(long long num) {
+		long long left = 1, right = num;
+		while (left < right) {
+			long long mid = (left + right) / 2;
+			if (mid * mid < num) {
+				left = mid + 1;
+			}
+			else {
+				right = mid;
+			}
+		}
+		return left * left == num;
 	}
 };
-int main(void) {
-	Solution solution;
-	cout << boolalpha << solution.isPerfectSquare(16) << "\tPassed\n";
-	cout << boolalpha << solution.isPerfectSquare(14) << "\tPassed\n";
-	return 0;
-}
