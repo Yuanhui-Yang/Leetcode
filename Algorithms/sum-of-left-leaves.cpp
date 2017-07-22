dd440aa4e024eafee1f66d81b86fcf095e9a0574
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-22 04:15:29 -0500

    404. Sum of Left Leaves

--- Algorithms/sum-of-left-leaves.cpp
+++ Algorithms/sum-of-left-leaves.cpp
@@ -1,37 +1,42 @@
 // 404. Sum of Left Leaves
 // https://leetcode.com/problems/sum-of-left-leaves/
-#include <iostream>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+	3
+   / \
+  9  20
+	/  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	int sumOfLeftLeaves(TreeNode* root) {
-		TreeNode *parent = NULL;
-		int result = 0;
-		this->sumOfLeftLeaves(root, parent, result);
-		return result;
+		return f(root, NULL);
 	}
 private:
-	void sumOfLeftLeaves(TreeNode* root, TreeNode* parent, int& result) {
-		if (!root) return;
-		if (parent && parent->left == root && !root->left && !root->right) result += root->val;
-		this->sumOfLeftLeaves(root->left, root, result);
-		this->sumOfLeftLeaves(root->right, root, result);
+	int f(TreeNode * node, TreeNode* parent) {
+		if (!node) {
+			return 0;
+		}
+		if (!node->left and !node->right) {
+			return parent and parent->left == node ? node->val : 0;
+		}
+		return f(node->left, node) + f(node->right, node);
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(3);
-	root->left = new TreeNode(9);
-	root->right = new TreeNode(20);
-	root->right->left = new TreeNode(15);
-	root->right->right = new TreeNode(7);
-	cout << solution.sumOfLeftLeaves(root) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
