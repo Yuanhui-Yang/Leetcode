579ad328e814805f4aed1ea183a5a33a32ea6ccb
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-22 04:05:56 -0500

    342. Power of Four

--- Algorithms/power-of-four.cpp
+++ Algorithms/power-of-four.cpp
@@ -1,17 +1,31 @@
-//342. Power of Four
-//https ://leetcode.com/problems/power-of-four/?sort=hot
-//https ://leetcode.com/discuss/97930/java-1-line-cheating-for-the-purpose-of-not-using-loops
-#include <iostream>
-using namespace std;
+// 342. Power of Four
+// https://leetcode.com/problems/power-of-four/
+
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
 class Solution {
 public:
 	bool isPowerOfFour(int num) {
-		return num > 0 && !(num & (num - 1)) && (0x5555555 & num);
+		return num > 0 and (num - 1) % 3 == 0 and (num & (num - 1)) == 0;
 	}
 };
-int main(int argc, char** argv) {
-	Solution solution;
-	cout << "Passed!\n";
-	getchar();
-	return 0;
-}
+
+class Solution {
+public:
+	bool isPowerOfFour(int num) {
+		if (num <= 0) {
+			return false;
+		}
+		while (num % 4 == 0) {
+			num /= 4;
+		}
+		return num == 1;
+	}
+};
