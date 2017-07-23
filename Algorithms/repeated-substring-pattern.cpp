f166517645155cd16e94ad531707c27902117af1
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-23 02:08:04 -0500

    459. Repeated Substring Pattern

--- Algorithms/repeated-substring-pattern.cpp
+++ Algorithms/repeated-substring-pattern.cpp
@@ -1,28 +1,44 @@
 // 459. Repeated Substring Pattern
 // https://leetcode.com/problems/repeated-substring-pattern/
-#include <iostream>
-#include <string>
-using namespace std;
+
+/*
+Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+Example 1:
+Input: "abab"
+
+Output: True
+
+Explanation: It's the substring "ab" twice.
+Example 2:
+Input: "aba"
+
+Output: False
+Example 3:
+Input: "abcabcabcabc"
+
+Output: True
+
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+*/
+
 class Solution {
 public:
-	bool repeatedSubstringPattern(string str) {
-		const int n = str.size();
-		for (int i = 1; i <= n / 2; i++) {
-			if (n % i == 0) {
-				string pat;
-				for (int j = 0; j < n / i; j++)
-					pat += str.substr(0, i);
-				if (str == pat) return true;
+	bool repeatedSubstringPattern(string s) {
+		if (s.size() <= 1) {
+			return false;
+		}
+		int sz = s.size(), i = 0, j = -1;
+		vector<int> A(sz + 1);
+		A[0] = -1;
+		while (i < sz) {
+			if (j == -1 or s[i] == s[j]) {
+				A[++i] = ++j;
+			}
+			else {
+				j = A[j];
 			}
 		}
-		return false;
+		return 0 < A[sz] and A[sz] < sz and sz % (sz - A[sz]) == 0;
 	}
 };
-int main(void) {
-	Solution solution;
-	cout << boolalpha << solution.repeatedSubstringPattern("abab") << "\tPassed\n";
-	cout << boolalpha << solution.repeatedSubstringPattern("aba") << "\tPassed\n";
-	cout << boolalpha << solution.repeatedSubstringPattern("abcabcabcabc") << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
