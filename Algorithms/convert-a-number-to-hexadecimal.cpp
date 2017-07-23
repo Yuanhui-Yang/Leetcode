e6359c47d0004db8ba2f9e51c32a24fd2f5b75ac
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-23 00:41:39 -0500

    405. Convert a Number to Hexadecimal

--- Algorithms/convert-a-number-to-hexadecimal.cpp
+++ Algorithms/convert-a-number-to-hexadecimal.cpp
@@ -1,58 +1,62 @@
 // 405. Convert a Number to Hexadecimal
 // https://leetcode.com/problems/convert-a-number-to-hexadecimal/
-// https://discuss.leetcode.com/topic/60367/simple-c-15-lines-accepted-solution
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+Note:
+
+All letters in hexadecimal (a-f) must be in lowercase.
+The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+The given number is guaranteed to fit within the range of a 32-bit signed integer.
+You must not use any method provided by the library which converts/formats the number to hex directly.
+Example 1:
+
+Input:
+26
+
+Output:
+"1a"
+Example 2:
+
+Input:
+-1
+
+Output:
+"ffffffff"
+*/
+
 class Solution {
 public:
 	string toHex(int num) {
-		if (num == 0) return "0"; 
-		string result;
-		unsigned int Num;
-		if (num < 0) {
-			Num = -num;
-			Num = ~Num + 1;
+		if (num == 0) {
+			return "0";
 		}
-		else Num = num;
+		array<char, 16> A = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+		string result;
+		unsigned int Num = num;
 		while (Num) {
-			unsigned int x = Num & 15;
-			if (x < 10) result.push_back('0' + x);
-			else result.push_back('a' + (x - 10));
-			Num >>= 4;
+			result.push_back(A[Num & 15]);
+			Num /= 16;
 		}
 		reverse(begin(result), end(result));
 		return result;
 	}
 };
-// class Solution {
-// public:
-// 	string toHex(int num) {
-// 		if (num == 0) return "0";
-// 		string result;
-// 		unsigned int Num;
-// 		if (num < 0) {
-// 			Num = -num;
-// 			Num = ~Num + 1;
-// 		}
-// 		else Num = num;
-// 		while (Num) {
-// 			unsigned int n = Num % 16;
-// 			if (n < 10) result.push_back('0' + n);
-// 			else result.push_back('a' + (n - 10));
-// 			Num /= 16;
-// 		}
-// 		reverse(begin(result), end(result));
-// 		return result;
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	cout << solution.toHex(26) << "\tPassed\n";
-	cout << solution.toHex(-1) << "\tPassed\n";
-	cout << solution.toHex(0) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
+
+class Solution {
+public:
+	string toHex(int num) {
+		if (num == 0) {
+			return "0";
+		}
+		array<char, 16> A = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+		string result;
+		for (int i = 0; i < 8 and num; ++i) {
+			result.push_back(A[num & 15]);
+			num >>= 4;
+		}
+		reverse(begin(result), end(result));
+		return result;
+	}
+};
