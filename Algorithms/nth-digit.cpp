9f97150792293c9b2132c501a4807040157bec15
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-24 18:56:51 -0500

    400. Nth Digit

--- Algorithms/nth-digit.cpp
+++ Algorithms/nth-digit.cpp
@@ -1,25 +1,43 @@
 // 400. Nth Digit
 // https://leetcode.com/problems/nth-digit/
-#include <iostream>
-#include <cmath>
-using namespace std;
+
+/*
+Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+
+Note:
+n is positive and will fit within the range of a 32-bit signed integer (n < 231).
+
+Example 1:
+
+Input:
+3
+
+Output:
+3
+Example 2:
+
+Input:
+11
+
+Output:
+0
+
+Explanation:
+The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+*/
+
 class Solution {
 public:
-	int findNthDigit(size_t n) {
-		if (n < 10) return n;
-		size_t i = 1;
-		for (i = 1; n >= 9 * i * size_t(pow(10, i - 1)); ++i) n -= 9 * i * size_t(pow(10, i - 1));
-		size_t x = size_t(pow(10, i - 1)) + (n - 1) / i;
-		return to_string(x)[(n - 1) % i] - '0';
+	int findNthDigit(int n) {
+		int i = 0;
+		while (n - 9 * pow(10, i) * (i + 1) > 0) {
+			n -= 9 * pow(10, i) * (i + 1);
+			++i;
+		}
+		int val = pow(10, i);
+		val += (n - 1) / (i + 1);
+		string s = to_string(val);
+		n = (n - 1) % (i + 1);
+		return s[n] - '0';
 	}
 };
-int main(void) {
-	Solution solution;
-	cout << solution.findNthDigit(3) << "\tPassed\n";
-	cout << solution.findNthDigit(10) << "\tPassed\n";
-	cout << solution.findNthDigit(11) << "\tPassed\n";
-	cout << solution.findNthDigit(100) << "\tPassed\n";
-	cout << solution.findNthDigit(1000000000) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
