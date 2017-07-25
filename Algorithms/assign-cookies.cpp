651a90ef7ba58c04300f93b571d3510dff075c71
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-25 16:12:53 -0500

    455. Assign Cookies

--- Algorithms/assign-cookies.cpp
+++ Algorithms/assign-cookies.cpp
@@ -1,39 +1,48 @@
 // 455. Assign Cookies
 // https://leetcode.com/problems/assign-cookies/
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie. Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be content with; and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+Note:
+You may assume the greed factor is always positive. 
+You cannot assign more than one cookie to one child.
+
+Example 1:
+Input: [1,2,3], [1,1]
+
+Output: 1
+
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
+You need to output 1.
+Example 2:
+Input: [1,2], [1,2,3]
+
+Output: 2
+
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
+You have 3 cookies and their sizes are big enough to gratify all of the children, 
+You need to output 2.
+*/
+
 class Solution {
 public:
 	int findContentChildren(vector<int>& g, vector<int>& s) {
-		if (g.empty() || s.empty()) return 0;
-		const int m = g.size(), n = s.size();
 		sort(begin(g), end(g));
 		sort(begin(s), end(s));
 		int result = 0;
-		for (int i = 0, j = 0; i < m && j < n; i++) {
-			if (g[i] > s.back()) return result;
-			vector<int>::iterator it = lower_bound(begin(s) + j, end(s), g[i]);
-			j = distance(begin(s), it) + 1;
-			result++;
+		vector<int>::iterator jt = begin(s);
+		for (int M = g.size(), i = 0; i < M; ++i) {
+			jt = lower_bound(jt, end(s), g[i]);
+			if (jt == end(s)) {
+				break;
+			}
+			else {
+				++result;
+				++jt;
+			}
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> g, s;
-	g = {1,2,3};
-	s = {1,1};
-	cout << solution.findContentChildren(g, s) << "\tPassed\n";
-	g = {1,2};
-	s = {1,2,3};
-	cout << solution.findContentChildren(g, s) << "\tPassed\n";
-	g = {1,2,3};
-	s = {3};
-	cout << solution.findContentChildren(g, s) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
