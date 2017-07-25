450ab6a7091a0c0fe6e986e4908a32a597e12a26
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-24 21:58:10 -0500

    437. Path Sum III

--- Algorithms/path-sum-iii.cpp
+++ Algorithms/path-sum-iii.cpp
@@ -1,52 +1,57 @@
 // 437. Path Sum III
 // https://leetcode.com/problems/path-sum-iii/
-#include <iostream>
-#include <queue>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+	  10
+	 /  \
+	5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	int pathSum(TreeNode* root, int sum) {
-		if (!root) return 0;
-		int result = 0;
-		queue<TreeNode *> q;
-		q.push(root);
-		while (!q.empty()) {
-			TreeNode *front = q.front();
-			q.pop();
-			this->helper(front, result, sum);
-			if (front->left) q.push(front->left);
-			if (front->right) q.push(front->right);
+		if (!root) {
+			return 0;
 		}
-		return result;
+		return f(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
 	}
 private:
-	void helper(TreeNode* root, int& result, int sum) {
-		if (!root) return;
-		if (root->val == sum) {
-			result++;
+	int f(TreeNode * node, int prev, int sum) {
+		if (!node) {
+			return 0;
 		}
-		this->helper(root->left, result, sum - root->val);
-		this->helper(root->right, result, sum - root->val);
+		prev += node->val;
+		return int(prev == sum) + f(node->left, prev, sum) + f(node->right, prev, sum);
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(10);
-	root->left = new TreeNode(5);
-	root->right = new TreeNode(-3);
-	root->left->left = new TreeNode(3);
-	root->left->right = new TreeNode(2);
-	root->left->left->left = new TreeNode(3);
-	root->left->left->right = new TreeNode(-2);
-	root->left->right->right = new TreeNode(1);
-	root->right->right = new TreeNode(11);
-	cout << solution.pathSum(root, 8) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
