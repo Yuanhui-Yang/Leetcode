21b06a5a893841bb7a2f216674caafeef1ef8e63
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-25 05:59:14 -0500

    447. Number of Boomerangs

--- Algorithms/number-of-boomerangs.cpp
+++ Algorithms/number-of-boomerangs.cpp
@@ -1,35 +1,42 @@
 // 447. Number of Boomerangs
 // https://leetcode.com/problems/number-of-boomerangs/
-#include <iostream>
-#include <utility>
-#include <unordered_map>
-using namespace std;
+
+/*
+Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
+
+Find the number of boomerangs. You may assume that n will be at most 500 and coordinates of points are all in the range [-10000, 10000] (inclusive).
+
+Example:
+Input:
+[[0,0],[1,0],[2,0]]
+
+Output:
+2
+
+Explanation:
+The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
+*/
+
 class Solution {
 public:
 	int numberOfBoomerangs(vector<pair<int, int>>& points) {
-		int result = 0;
-		for (size_t i = 0; i < points.size(); i++) {
-			unordered_map<int, int> hashmap;
-			for (size_t j = 0; j < points.size(); j++) {
-				if (i == j) continue;
-				int distance = (points[i].first - points[j].first) * (points[i].first - points[j].first) + (points[i].second - points[j].second) * (points[i].second - points[j].second);
-				hashmap[distance]++;
+		int sz = points.size(), result = 0;
+		for (int i = 0; i < sz; ++i) {
+			unordered_map<int, int> A;
+			for (int j = 0; j < sz; ++j) {
+				if (i == j) {
+					continue;
+				}
+				++A[f(points[i], points[j])];
 			}
-			for (const auto &k : hashmap)
-				if (k.second >= 2)
-				result += k.second * (k.second - 1);
+			for (const auto & i : A) {
+				result += i.second * (i.second  - 1);
+			}
 		}
 		return result;
 	}
+private:
+	int f(const pair<int, int> & a,  const pair<int, int> & b) {
+		return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+	}
 };
-int main(void) {
-	Solution solution;
-	vector<pair<int, int>> points;
-	cout << solution.numberOfBoomerangs(points) << "\tPassed\n";
-	points = {{0,0},{1,0},{2,0}};
-	cout << solution.numberOfBoomerangs(points) << "\tPassed\n";
-	points = {{1,1},{2,2},{3,3},{4,4},{5,5},{6,6},{7,7},{8,8}};
-	cout << solution.numberOfBoomerangs(points) << "\tPassed\n";	
-	cout << "\nPassed All\n";
-	return 0;
-}
