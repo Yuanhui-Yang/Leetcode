d02474a0207a1af1312511b8eed34a4e8eb754e0
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-25 16:30:24 -0500

    475. Heaters

--- Algorithms/heaters.cpp
+++ Algorithms/heaters.cpp
@@ -1,44 +1,46 @@
 // 475. Heaters
 // https://leetcode.com/problems/heaters/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Winter is coming! Your first job during the contest is to design a standard heater with fixed warm radius to warm all the houses.
+
+Now, you are given positions of houses and heaters on a horizontal line, find out minimum radius of heaters so that all houses could be covered by those heaters.
+
+So, your input will be the positions of houses and heaters seperately, and your expected output will be the minimum radius standard of heaters.
+
+Note:
+Numbers of houses and heaters you are given are non-negative and will not exceed 25000.
+Positions of houses and heaters you are given are non-negative and will not exceed 10^9.
+As long as a house is in the heaters' warm radius range, it can be warmed.
+All the heaters follow your radius standard and the warm radius will the same.
+Example 1:
+Input: [1,2,3],[2]
+Output: 1
+Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+Example 2:
+Input: [1,2,3,4],[1,4]
+Output: 1
+Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard, then all the houses can be warmed.
+*/
+
 class Solution {
 public:
 	int findRadius(vector<int>& houses, vector<int>& heaters) {
-		if (houses.empty() || heaters.empty()) return 0;
-		int result = 0;
+		sort(begin(houses), end(houses));
 		sort(begin(heaters), end(heaters));
-		for (const auto &i : houses) {
-			vector<int>::iterator it = lower_bound(begin(heaters), end(heaters), i);
-			vector<int>::iterator jt = upper_bound(begin(heaters), end(heaters), i);
-			if (it == end(heaters)) {
-				result = max(result, abs(i - heaters.back()));
-				continue;	
+		int M = houses.size(), N = heaters.size(), result = 0;
+		for (int i = 0; i < M; ++i) {
+			vector<int>::iterator jt = lower_bound(begin(heaters), end(heaters), houses[i]);
+			if (jt == begin(heaters)) {
+				result = max(result, *jt - houses[i]);
 			}
-			if (it == begin(heaters)) {
-				result = max(result, abs(heaters.front() - i));
-				continue;
+			else if (jt == end(heaters)) {
+				result = max(result, houses[i] - *(prev(jt)));
 			}
-			if (*it == i) continue;
-			it--;
-			result = max(result, min(abs(*it - i), abs(*jt - i)));
+			else {
+				result = max(result, min(*jt - houses[i], houses[i] - *(prev(jt))));
+			}
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> houses = {1, 2, 3};
-	vector<int> heaters = {2};
-	assert(1 == solution.findRadius(houses, heaters));
-	houses = {1, 2, 3, 4};
-	heaters = {1, 4};
-	assert(1 == solution.findRadius(houses, heaters));
-	cout << "\nPassed All\n";
-	return 0;
-}
