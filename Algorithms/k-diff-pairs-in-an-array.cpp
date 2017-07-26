ee835b8fe64aa7cca109b6c32328bd8c44c8c992
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 02:21:54 -0500

    532. K-diff Pairs in an Array

--- Algorithms/k-diff-pairs-in-an-array.cpp
+++ Algorithms/k-diff-pairs-in-an-array.cpp
@@ -1,83 +1,57 @@
 // 532. K-diff Pairs in an Array
 // https://leetcode.com/problems/k-diff-pairs-in-an-array/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+
+Example 1:
+Input: [3, 1, 4, 1, 5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+Example 2:
+Input:[1, 2, 3, 4, 5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+Example 3:
+Input: [1, 3, 1, 5, 4], k = 0
+Output: 1
+Explanation: There is one 0-diff pair in the array, (1, 1).
+Note:
+The pairs (i, j) and (j, i) count as the same pair.
+The length of the array won't exceed 10,000.
+All the integers in the given input belong to the range: [-1e7, 1e7].
+*/
+
 class Solution {
 public:
 	int findPairs(vector<int>& nums, int k) {
-		if (k < 0 or nums.empty()) {
+		int sz = nums.size();
+		if (sz == 0 or k < 0) {
 			return 0;
 		}
-		int result = 0;
+		sort(begin(nums), end(nums));
 		if (k == 0) {
-			unordered_map<int, int> h;
-			for (const auto &i : nums) {
-				h[i]++;
-			}
-			for (const auto &i : h) {
-				result += i.second > 1;
+			int result = 0;
+			for (int i = 1; i < sz; ++i) {
+				if (nums[i - 1] == nums[i]) {
+					++result;
+					while (i + 1 < sz and nums[i] == nums[i + 1]) {
+						++i;
+					}
+				}
 			}
 			return result;
 		}
-		unordered_set<int> s;
-		sort(begin(nums), end(nums));
-		for (const auto &i : nums) {
-			if (s.empty()) {
-				s.insert(i);
+		unordered_set<int> A;
+		int result = 0;
+		for (int i = 0; i < sz; ++i) {
+			if (i > 0 and nums[i - 1] == nums[i]) {
 				continue;
 			}
-			if (!s.count(i)) {
-				result += s.count(i - k);
-				s.insert(i);
-				continue;
-			}
+			result += A.count(nums[i] - k);
+			A.insert(nums[i]);
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int k, result, answer;
-
-	nums = {1, 1, 1, 2, 2};
-	k = 1;
-	answer = 1;
-	result = solution.findPairs(nums, k);
-	assert(answer == result);
-
-	nums = {1, 1, 1, 1, 1};
-	k = 0;
-	answer = 1;
-	result = solution.findPairs(nums, k);
-	assert(answer == result);
-
-	nums = {3, 1, 4, 1, 5};
-	k = 2;
-	answer = 2;
-	result = solution.findPairs(nums, k);
-	assert(answer == result);
-
-	nums = {1, 2, 3, 4, 5};
-	k = 1;
-	answer = 4;
-	result = solution.findPairs(nums, k);
-	assert(answer == result);
-
-	nums = {1, 3, 1, 5, 4};
-	k = 0;
-	answer = 1;
-	result = solution.findPairs(nums, k);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
