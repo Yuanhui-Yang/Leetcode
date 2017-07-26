7b42017829b3ea0c002d0887cbf191b3ad26f96d
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 05:05:23 -0500

    501. Find Mode in Binary Search Tree

--- Algorithms/find-mode-in-binary-search-tree.cpp
+++ Algorithms/find-mode-in-binary-search-tree.cpp
@@ -1,55 +1,92 @@
 // 501. Find Mode in Binary Search Tree
 // https://leetcode.com/problems/find-mode-in-binary-search-tree/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <iterator>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary search tree (BST) with duplicates, find all the mode(s) (the most frequently occurred element) in the given BST.
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than or equal to the node's key.
+The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+Both the left and right subtrees must also be binary search trees.
+For example:
+Given BST [1,null,2,2],
+   1
+	\
+	 2
+	/
+   2
+return [2].
+
+Note: If a tree has more than one mode, you can return them in any order.
+
+Follow up: Could you do that without using any extra space? (Assume that the implicit stack space incurred due to recursion does not count).
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	vector<int> findMode(TreeNode* root) {
-		if (!root) {
-			return {};
+		vector<int> result;
+		int maxCnt = 0, cnt = 0;
+		TreeNode * prev = NULL;
+		while (root) {
+			if (!root->left) {
+				if (prev == NULL or prev->val != root->val) {
+					cnt = 1;
+					prev = root;
+				}
+				else {
+					++cnt;
+				}
+				if (maxCnt < cnt) {
+					maxCnt = cnt;
+					result.clear();
+					result.push_back(root->val);
+				}
+				else if (maxCnt == cnt) {
+					result.push_back(root->val);
+				}
+				root = root->right;
+			}
+			else {
+				TreeNode * pred = root->left;
+				while (pred->right and pred->right != root) {
+					pred = pred->right;
+				}
+				if (pred->right == root) {
+					if (prev == NULL or prev->val != root->val) {
+						cnt = 1;
+						prev = root;
+					}
+					else {
+						++cnt;
+					}
+					if (maxCnt < cnt) {
+						maxCnt = cnt;
+						result.clear();
+						result.push_back(root->val);
+					}
+					else if (maxCnt == cnt) {
+						result.push_back(root->val);
+					}
+					pred->right = NULL;
+					root = root->right;
+				}
+				else {
+					pred->right = root;
+					root = root->left;
+				}
+			}
 		}
-		unordered_map<int, int> OPT;
-		inorderTraversal(root, OPT);
-		map<int, vector<int>> treeMap;
-		for (const auto &i : OPT) {
-			treeMap[i.second].push_back(i.first);
-		}
-		return prev(end(treeMap))->second;
-	}
-private:
-	void inorderTraversal(TreeNode* root, unordered_map<int, int>& OPT) {
-		if (root) {
-			inorderTraversal(root->left, OPT);
-			OPT[root->val]++;
-			inorderTraversal(root->right, OPT);
-		}
+		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = NULL;
-	vector<int> result;
-
-	result = solution.findMode(root);
-	assert(vector<int>({}) == result);	
-
-	root = new TreeNode(1);
-	root->right = new TreeNode(2);
-	root->right->left = new TreeNode(2);
-	result = solution.findMode(root);
-	assert(vector<int>({2}) == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
