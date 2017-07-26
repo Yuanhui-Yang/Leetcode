38e61a0b5e960b2a458dc24bb92fd0cc4b31dfa8
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 12:25:35 -0500

    540. Single Element in a Sorted Array

--- Algorithms/single-element-in-a-sorted-array.cpp
+++ Algorithms/single-element-in-a-sorted-array.cpp
@@ -13,103 +13,22 @@ Output: 10
 Note: Your solution should run in O(log n) time and O(1) space.
 */
 
-#include <iostream> // std::cout; std::cin
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
 class Solution {
 public:
 	int singleNonDuplicate(vector<int>& nums) {
-		size_t left = 0, right = nums.size() - 1;
-		if (nums.at(left) != nums.at(left + 1)) {
-			return nums.front();
-		}
-		if (nums.at(right - 1) != nums.at(right)) {
-			return nums.back();
-		}
-		while (left + 1 < right) {
-			size_t mid = left + (right - left) / 2;
-			if (nums.at(mid - 1) != nums.at(mid) and nums.at(mid + 1) != nums.at(mid)) {
-				return nums.at(mid);
+		int sz = nums.size(), i = 0, j = sz;
+		while (i < j) {
+			int k = i + (j - i) / 2;
+			if (k + 1 < sz and nums[k] == nums[k + 1] and k % 2 == 0) {
+				i = k + 1;
 			}
-			if (mid % 2) {
-				if (nums.at(mid - 1) == nums.at(mid)) {
-					left = mid;
-				}
-				else {
-					right = mid;
-				}
+			else if (k + 1 < sz and nums[k] != nums[k + 1] and k % 2 == 1) {
+				i = k + 1;
 			}
 			else {
-				if (nums.at(mid + 1) == nums.at(mid)) {
-					left = mid;
-				}
-				else {
-					right = mid;
-				}
+				j = k;
 			}
 		}
-		if (nums.at(left - 1) != nums.at(left) and nums.at(left + 1) != nums.at(left)) {
-			return nums.at(left);
-		}
-		return nums.at(right);
+		return nums[i];
 	}
 };
-
-// class Solution {
-// public:
-// 	int singleNonDuplicate(vector<int>& nums) {
-// 		int result = 0;
-// 		for (const auto &i : nums) {
-// 			result ^= i;
-// 		}
-// 		return result;
-// 	}
-// };
-
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int result = 0, answer = 0;
-
-	nums = {0, 1, 1};
-	answer = 0;
-	result = solution.singleNonDuplicate(nums);
-	assert(answer == result);
-
-	nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
-	answer = 2;
-	result = solution.singleNonDuplicate(nums);
-	assert(answer == result);
-
-	nums = {3, 3, 7, 7, 10, 11, 11};
-	answer = 10;
-	result = solution.singleNonDuplicate(nums);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
