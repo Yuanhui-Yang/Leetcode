49467d4e8a121e4fd6087a887f4ab91686eb716e
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 11:27:22 -0500

    366. Find Leaves of Binary Tree

--- Algorithms/find-leaves-of-binary-tree.cpp
+++ Algorithms/find-leaves-of-binary-tree.cpp
@@ -1,53 +1,74 @@
 // 366. Find Leaves of Binary Tree
 // https://leetcode.com/problems/find-leaves-of-binary-tree/
-#include <iostream>
-#include <vector>
-#include <iterator>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+
+Example:
+Given binary tree 
+		  1
+		 / \
+		2   3
+	   / \     
+	  4   5    
+Returns [4, 5, 3], [2], [1].
+
+Explanation:
+1. Removing the leaves [4, 5, 3] would result in this tree:
+
+		  1
+		 / 
+		2          
+2. Now removing the leaf [2] would result in this tree:
+
+		  1          
+3. Now removing the leaf [1] would result in the empty tree:
+
+		  []         
+Returns [4, 5, 3], [2], [1].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	vector<vector<int>> findLeaves(TreeNode* root) {
 		vector<vector<int>> result;
-		while (root) result.push_back(this->dfs(root));
+		if (!root) {
+			return result;
+		}
+		for (vector<int> v = f(NULL, root); !v.empty(); v = f(NULL, root)) {
+			result.push_back(v);
+		}
 		return result;
 	}
 private:
-	vector<int> dfs(TreeNode* &root) {
+	vector<int> f(TreeNode * parent, TreeNode * & current) {
 		vector<int> result;
-		if (!root) return result;
-		if (!root->left && !root->right) {
-			result = {root->val};
-			// delete root;
-			root = NULL;
+		if (!current) {
 			return result;
 		}
-		vector<int> l = this->dfs(root->left);
-		vector<int> r = this->dfs(root->right);
-		result.insert(end(result), begin(l), end(l));
-		result.insert(end(result), begin(r), end(r));
+		if (!current->left and !current->right) {
+			result.push_back(current->val);
+			if (parent and parent->left == current) {
+				parent->left = NULL;
+			}
+			if (parent and parent->right == current) {
+				parent->right = NULL;
+			}
+			current = NULL;
+			return result;
+		}
+		vector<int> a = f(current, current->left), b = f(current, current->right);
+		result.insert(end(result), begin(a), end(a));
+		result.insert(end(result), begin(b), end(b));
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(3);
-	root->left->left = new TreeNode(4);
-	root->left->right = new TreeNode(5);
-	for (const auto &i : solution.findLeaves(root)) {
-		for (const auto &j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
-	cout << "\nPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
