0b65cd6ada29ec0156182ff1d704c18a593981b6
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 05:13:12 -0500

    551. Student Attendance Record I

--- Algorithms/student-attendance-record-i.cpp
+++ Algorithms/student-attendance-record-i.cpp
@@ -19,69 +19,29 @@ Input: "PPALLL"
 Output: False
 */
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close;
-#include <ctime>
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
 class Solution {
 public:
 	bool checkRecord(string s) {
-		for (size_t i = 0, j = 0; i < s.size(); i++) {
-			if (s.at(i) == 'A') {
-				if (j >= 1) {
-					return false;
-				}
-				j++;
+		for (int sz = s.size(), i = 0, A = -1; i < sz; ++i) {
+			if (s[i] == 'P') {
 				continue;
 			}
-			if (i >= 2 and s.at(i - 2) == 'L' and s.at(i - 1) == 'L' and s.at(i) == 'L') {
+			if (s[i] == 'A') {
+				if (A == -1) {
+					A = i;
+					continue;
+				}
+				return false;
+			}
+			int cnt = 1;
+			while (i + 1 < sz and s[i + 1] == 'L') {
+				++i;
+				++cnt;
+			}
+			if (cnt > 2) {
 				return false;
 			}
 		}
 		return true;
 	}
 };
-
-int main(void) {
-	Solution solution;
-	string s;
-	bool result, answer;
-
-	s = "PPALLP";
-	answer = true;
-	result = solution.checkRecord(s);
-	assert(answer == result);
-
-	s = "PPALLL";
-	answer = false;
-	result = solution.checkRecord(s);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
