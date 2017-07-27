0efbea6dc7a09fc15482075594dd029c43a4c47c
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-27 02:38:23 -0500

    274. H-Index

--- Algorithms/h-index.cpp
+++ Algorithms/h-index.cpp
@@ -1,60 +1,24 @@
 // 274. H-Index
 // https://leetcode.com/problems/h-index/
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-using namespace std;
-// BEGIN: https://leetcode.com/articles/h-index/
+
+/*
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+*/
+
 class Solution {
 public:
 	int hIndex(vector<int>& citations) {
-		if (citations.empty()) return 0;
 		sort(begin(citations), end(citations));
-		const int n = citations.size();
-		int result = n - 1;
-		while (result >= 0 && citations[result] >= (n - result)) result--;
-		return n - 1 - result;
+		int sz = citations.size(), i = 0;
+		while (i < sz and citations[i] + i < sz) {
+			++i;
+		}
+		return sz - i;
 	}
 };
-// BEGIN: https://discuss.leetcode.com/topic/40765/java-bucket-sort-o-n-solution-with-detail-explanation
-// class Solution {
-// public:
-// 	int hIndex(vector<int>& citations) {
-// 		if (citations.empty()) return 0;
-// 		const int n = citations.size();
-// 		vector<int> buckets(n + 1, 0);
-// 		for (const auto &i : citations) buckets[i >= n ? n : i]++;
-// 		for (int i = n, cnt = 0; i >= 0; i--)
-// 			if ((cnt += buckets[i]) >= i)
-// 				return i;
-// 		return 0;
-// 	}
-// };
-// class Solution {
-// public:
-// 	int hIndex(vector<int>& citations) {
-// 		if (citations.empty()) return 0;
-// 		const int n = *max_element(begin(citations), end(citations));
-// 		vector<int> buckets(n + 1, 0);
-// 		for (const auto &i : citations) buckets[i]++;
-// 		for (int i = n, cnt = 0; i >= 0; i--)
-// 			if ((cnt += buckets[i]) >= i)
-// 				return i;
-// 		return 0;
-// 	}
-// };
-// END: https://discuss.leetcode.com/topic/40765/java-bucket-sort-o-n-solution-with-detail-explanation
-int main(void) {
-	Solution solution;
-	vector<int> citations;
-	cout << solution.hIndex(citations) << "\tPassed\n";
-	citations = {3, 0, 6, 1, 5};
-	cout << solution.hIndex(citations) << "\tPassed\n";
-	citations = {100};
-	cout << solution.hIndex(citations) << "\tPassed\n";
-	citations ={0};
-	cout << solution.hIndex(citations) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
