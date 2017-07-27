bcbc4411505c30340cf6c8c866f8103ea337d493
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 20:37:19 -0500

    515. Find Largest Value in Each Tree Row

--- Algorithms/find-largest-value-in-each-tree-row.cpp
+++ Algorithms/find-largest-value-in-each-tree-row.cpp
@@ -1,65 +1,53 @@
 // 515. Find Largest Value in Each Tree Row
 // https://leetcode.com/problems/find-largest-value-in-each-tree-row/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <iterator>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+You need to find the largest value in each row of a binary tree.
+
+Example:
+Input: 
+
+		  1
+		 / \
+		3   2
+	   / \   \  
+	  5   3   9 
+
+Output: [1, 3, 9]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	vector<int> largestValues(TreeNode* root) {
-		if (!root) {
-			return {};
-		}
 		vector<int> result;
-		multiset<TreeNode*, MaxHeapComp> max_heap;
-		max_heap.insert(root);
-		while (!max_heap.empty()) {
-			result.push_back((*begin(max_heap))->val);
-			vector<TreeNode*> stack(begin(max_heap), end(max_heap));
-			max_heap.clear();
-			for (const auto &i : stack) {
+		if (!root) {
+			return result;
+		}
+		for (list<TreeNode*> curr({root}), next; !curr.empty(); curr = next) {
+			next.clear();
+			int maxVal = curr.front()->val;
+			for (const auto & i : curr) {
+				if (maxVal < i->val) {
+					maxVal = i->val;
+				}
 				if (i->left) {
-					max_heap.insert(i->left);
+					next.push_back(i->left);
 				}
 				if (i->right) {
-					max_heap.insert(i->right);
+					next.push_back(i->right);
 				}
 			}
+			result.push_back(maxVal);
 		}
 		return result;
 	}
-private:
-	struct MaxHeapComp {
-		bool operator() (const TreeNode* i, const TreeNode* j) {
-			return i->val > j->val;
-		}
-	};
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = NULL;
-	vector<int> result;
-	vector<int> answer;
-
-	root = new TreeNode(1);
-	root->left = new TreeNode(3);
-	root->right = new TreeNode(2);
-	root->left->left = new TreeNode(5);
-	root->left->right = new TreeNode(3);
-	root->right->right = new TreeNode(9);
-	answer = {1, 3, 9};
-	result = solution.largestValues(root);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
