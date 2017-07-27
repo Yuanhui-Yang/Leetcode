0ff2b01a5bddbcdf34f8f35735aa3307080b4f4e
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-26 20:30:35 -0500

    280. Wiggle Sort

--- Algorithms/wiggle-sort.cpp
+++ Algorithms/wiggle-sort.cpp
@@ -1,34 +1,24 @@
 // 280. Wiggle Sort
 // https://leetcode.com/problems/wiggle-sort/
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+
+/*
+Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+*/
+
 class Solution {
 public:
 	void wiggleSort(vector<int>& nums) {
-		const int n = nums.size();
-		if (n < 2) return;
-		for (int i = 1; i < n; i++) {
-			if (i & 1) {
-				if (nums[i - 1] <= nums[i]) continue;
-				swap(nums[i - 1], nums[i]);
+		for (int sz = nums.size(), i = 0; i + 1 < sz; ++i) {
+			if (i % 2 == 0 and nums[i] > nums[i + 1]) {
+				swap(nums[i], nums[i + 1]);
 				continue;
 			}
-			if (nums[i - 1] <= nums[i]) {
-				swap(nums[i - 1], nums[i]);
-				continue;				
+			if (i % 2 == 1 and nums[i] < nums[i + 1]) {
+				swap(nums[i], nums[i + 1]);
+				continue;
 			}
 		}
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	nums = {3, 5, 2, 1, 6, 4};
-	solution.wiggleSort(nums);
-	for (const auto &i : nums) cout << i << '\t';
-	cout << '\n';
-	cout << "\nPassed All\n";
-	return 0;
-}
