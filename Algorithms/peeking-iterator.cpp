3392a82e8ec8d77cd8f73cb17e8164aab9d676fe
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-27 09:19:01 -0500

    284. Peeking Iterator

--- Algorithms/peeking-iterator.cpp
+++ Algorithms/peeking-iterator.cpp
@@ -1,6 +1,20 @@
 // 284. Peeking Iterator
 // https://leetcode.com/problems/peeking-iterator/
-// BEGIN: https://discuss.leetcode.com/topic/24909/simple-c-solution-1-line-per-method-without-extra-member-variables
+
+/*
+Given an Iterator class interface with methods: next() and hasNext(), design and implement a PeekingIterator that support the peek() operation -- it essentially peek() at the element that will be returned by the next call to next().
+
+Here is an example. Assume that the iterator is initialized to the beginning of the list: [1, 2, 3].
+
+Call next() gets you 1, the first element in the list.
+
+Now you call peek() and it returns 2, the next element. Calling next() after that still return 2.
+
+You call next() the final time and it returns 3, the last element. Calling hasNext() after that should return false.
+
+Follow up: How would you extend your design to be generic and work with all types, not just integer?
+*/
+
 // Below is the interface for Iterator, which is already defined for you.
 // **DO NOT** modify the interface for Iterator.
 class Iterator {
@@ -23,7 +37,7 @@ public:
 		// Initialize any member here.
 		// **DO NOT** save a copy of nums and manipulate it directly.
 		// You should only use the Iterator interface methods.
-
+		
 	}
 
 	// Returns the next element in the iteration without advancing the iterator.
@@ -34,11 +48,10 @@ public:
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	int next() {
-		return this->Iterator::next();
+		return Iterator::next();
 	}
 
 	bool hasNext() const {
-		return this->Iterator::hasNext();
+		return Iterator::hasNext();
 	}
 };
-// END: https://discuss.leetcode.com/topic/24909/simple-c-solution-1-line-per-method-without-extra-member-variables
