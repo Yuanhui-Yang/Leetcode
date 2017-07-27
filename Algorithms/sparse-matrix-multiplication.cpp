54f981a7dff5f412513378d0fcabe36bf4d4d507
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-27 08:17:04 -0500

    311. Sparse Matrix Multiplication

--- Algorithms/sparse-matrix-multiplication.cpp
+++ Algorithms/sparse-matrix-multiplication.cpp
@@ -28,6 +28,42 @@ AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
 class Solution {
 public:
 	vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+		int P = A.size(), Q = P == 0 ? 0 : A[0].size(), R = B.size(), S = R == 0 ? 0 : B[0].size();
+		if (Q != R) {
+			return {};
+		}
+		vector<list<array<int, 2>>> X(P), Y(R);
+		for (int i = 0; i < P; ++i) {
+			for (int j = 0; j < Q; ++j) {
+				int val = A[i][j];
+				if (val) {
+					X[i].push_back({j, val});
+				}
+			}
+		}
+		for (int i = 0; i < R; ++i) {
+			for (int j = 0; j < S; ++j) {
+				int val = B[i][j];
+				if (val) {
+					Y[i].push_back({j, val});
+				}
+			}
+		}
+		vector<vector<int>> result(P, vector<int>(S, 0));
+		for (int i = 0; i < P; ++i) {
+			for (const auto & j : X[i]) {
+				for (const auto & k : Y[j[0]]) {
+					result[i][k[0]] += j[1] * k[1];
+				}
+			}
+		}
+		return result;
+	}
+};
+
+class Solution {
+public:
+	vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
 		vector<array<int, 3>> P, Q;
 		for (int M = A.size(), i = 0; i < M; ++i) {
 			for (int N = A[i].size(), j = 0; j < N; ++j) {
