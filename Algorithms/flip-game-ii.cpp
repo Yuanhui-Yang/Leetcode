e02906afb0017f2ae7e9b9aee1d82696bf98f721
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-27 15:28:11 -0500

    294. Flip Game II

--- Algorithms/flip-game-ii.cpp
+++ Algorithms/flip-game-ii.cpp
@@ -1,27 +1,31 @@
 // 294. Flip Game II
 // https://leetcode.com/problems/flip-game-ii/
-// https://discuss.leetcode.com/topic/27282/theory-matters-from-backtracking-128ms-to-dp-0ms
-#include <iostream>
-#include <string>
-using namespace std;
+
+/*
+You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+Write a function to determine if the starting player can guarantee a win.
+
+For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
+
+Follow up:
+Derive your algorithm's runtime complexity.
+*/
+
 class Solution {
 public:
 	bool canWin(string s) {
-		for (size_t i = 0; i + 1 < s.size(); ++i) {
-			if (s.substr(i, 2) != "++") continue;
-			s[i] = s[i + 1] = '-';
-			if (!this->canWin(s)) return true;
-			s[i] = s[i + 1] = '+';
+		for (int sz = s.size(), i = 0; i + 1 < sz; ++i) {
+			if (s[i] == '+' and s[i + 1] == '+') {
+				s[i] = '-';
+				s[i + 1] = '-';
+				if (!canWin(s)) {
+					return true;
+				}
+				s[i] = '+';
+				s[i + 1] = '+';
+			}
 		}
 		return false;
 	}
 };
-int main(void) {
-	Solution solution;
-	string s = "++++";
-	cout << boolalpha << solution.canWin(s) << "\tPassed\n";
-	s = "+++++";
-	cout << boolalpha << solution.canWin(s) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
