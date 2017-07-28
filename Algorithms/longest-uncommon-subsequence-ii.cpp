c75909504b2087c8ad7f1eb595de63454512dec7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-28 17:20:21 -0500

    522. Longest Uncommon Subsequence II

--- Algorithms/longest-uncommon-subsequence-ii.cpp
+++ Algorithms/longest-uncommon-subsequence-ii.cpp
@@ -17,84 +17,38 @@ All the given strings' lengths will not exceed 10.
 The length of the given list will be in the range of [2, 50].
 */
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close;
-#include <ctime>
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push; std::priority_queue; std::priority_queue::top; std::priority_queue::push; std::priority_queue::pop
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
-// BEGIN: https://discuss.leetcode.com/topic/85044/python-simple-explanation
-// BEGIN: https://leetcode.com/articles/longest-uncommon-subsequence-ii/#approach-3-sorting-and-checking-subsequence-accepted
-// BEGIN: Time Complexity O(l * n ^ 2) and Space Complexity O(1)
 class Solution {
 public:
 	int findLUSlength(vector<string>& strs) {
 		sort(begin(strs), end(strs), Comp());
-		for (size_t i = 0, j = 0; i < strs.size(); i++) {
-			for (j = 0; j < strs.size(); j++) {
-				if (j != i and isSubsequence(strs.at(i), strs.at(j))) {
+		for (int sz = strs.size(), i = 0; i < sz; ++i) {
+			bool flag = true;
+			for (int j = 0; j < sz; ++j) {
+				if (i != j and f(strs[j], strs[i])) {
+					flag = false;
 					break;
 				}
 			}
-			if (j == strs.size()) {
-				return strs.at(i).size();
+			if (flag) {
+				return strs[i].size();
 			}
 		}
 		return -1;
 	}
 private:
 	struct Comp {
-		bool operator() (const string& a, const string& b) {
+		bool operator() (const string & a, const string & b) {
 			return a.size() > b.size();
 		}
 	};
-	bool isSubsequence(const string& a, const string& b) {
-		size_t i = 0;
-		for (size_t j = 0; i < a.size() and j < b.size(); j++) {
-			if (a.at(i) == b.at(j)) {
-				i++;
+	bool f(const string & s, const string & t) {
+		int m = s.size(), n = t.size(), i = 0, j = 0;
+		while (i < m and j < n) {
+			if (s[i] == t[j]) {
+				++j;
 			}
+			++i;
 		}
-		return i == a.size();
+		return j == n;
 	}
 };
-// END: Time Complexity O(l * n ^ 2) and Space Complexity O(1)
-// END: https://leetcode.com/articles/longest-uncommon-subsequence-ii/#approach-3-sorting-and-checking-subsequence-accepted
-// END: https://discuss.leetcode.com/topic/85044/python-simple-explanation
-
-int main(void) {
-	Solution solution;
-	vector<string> strs;
-	int result = 0, answer = 0;
-
-	strs = {"aba", "cdc", "eae"};
-	answer = 3;
-	result = solution.findLUSlength(strs);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
