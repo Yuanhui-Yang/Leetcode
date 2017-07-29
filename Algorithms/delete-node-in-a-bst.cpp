937f5188f3209174a69426535edbb86d48fd49ac
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-29 12:01:59 -0500

    450. Delete Node in a BST

--- Algorithms/delete-node-in-a-bst.cpp
+++ Algorithms/delete-node-in-a-bst.cpp
@@ -1,51 +1,97 @@
 // 450. Delete Node in a BST
 // https://leetcode.com/problems/delete-node-in-a-bst/
-#include <iostream>
-#include <cassert>
-#include <vector>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+
+Example:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+	5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+	5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+	5
+   / \
+  2   6
+   \   \
+	4   7
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	TreeNode* deleteNode(TreeNode* root, int key) {
-		if (!root) return NULL;
-		if (root->val == key) {
-			if (!root->left && !root->right) return NULL;
-			if (!root->left) return root->right;
-			if (!root->right) return root->left;
-			TreeNode *it = root->right;
-			while (it->left) it = it->left;
-			it->left = root->left;
-			return root->right;		
+		TreeNode * dummy = new TreeNode(-1);
+		dummy->left = root;
+		TreeNode * prev = dummy, * curr = root;
+		while (curr and curr->val != key) {
+			prev = curr;
+			if (curr->val < key) {
+				curr = curr->right;
+			}
+			else {
+				curr = curr->left;
+			}
 		}
-		root->left = deleteNode(root->left, key);
-		root->right = deleteNode(root->right, key);
-		return root;
+		if (curr) {
+			if (curr->right) {
+				TreeNode * left = curr->left, * right = curr->right, * leftmost = min_element(right);
+				if (prev->left == curr) {
+					prev->left = right;
+					leftmost->left = left;
+				}
+				else {
+					prev->right = right;
+					leftmost->left = left;
+				}
+			}
+			else {
+				if (prev->left == curr) {
+					prev->left = curr->left;
+				}
+				else {
+					prev->right = curr->left;
+				}
+			}
+		}
+		return dummy->left;
+	}
+private:
+	TreeNode * min_element(TreeNode * node) {
+		while (node and node->left) {
+			node = node->left;
+		}
+		return node;
 	}
 };
-void inorderTraversal(TreeNode *root) {
-	if (root) {
-		inorderTraversal(root->left);
-		cout << root->val << '\t';
-		inorderTraversal(root->right);
-	}
-}
-int main(void) {
-	Solution solution;
-	TreeNode *root = NULL;
-	root = new TreeNode(5);
-	root->left = new TreeNode(3);
-	root->right = new TreeNode(6);
-	root->left->left = new TreeNode(2);
-	root->left->right = new TreeNode(4);
-	root->right->right = new TreeNode(7);
-	TreeNode *result = solution.deleteNode(root, 3);
-	inorderTraversal(result);
-	cout << "\nPassed All\n";
-	return 0;
-}
