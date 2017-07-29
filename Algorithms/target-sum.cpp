d8c33c59b7d0df2d40c7f48bff3605513012ab12
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-29 03:49:51 -0500

    494. Target Sum

--- Algorithms/target-sum.cpp
+++ Algorithms/target-sum.cpp
@@ -1,45 +1,62 @@
 // 494. Target Sum
 // https://leetcode.com/problems/target-sum/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <unordered_map>
-using namespace std;
+
+/*
+You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+Note:
+The length of the given array is positive and will not exceed 20.
+The sum of elements in the given array will not exceed 1000.
+Your output answer is guaranteed to be fitted in a 32-bit integer.
+*/
+
 class Solution {
 public:
 	int findTargetSumWays(vector<int>& nums, int S) {
-		if (nums.empty()) {
+		int sum = accumulate(begin(nums), end(nums), 0);
+		if (S < -sum or sum < S) {
 			return 0;
 		}
-		vector<unordered_map<int, size_t>> OPT(nums.size());
-		for (size_t i = 0; i < nums.size(); i++) {
-			if (i == 0) {
-				OPT[0][nums[0]]++;
-				OPT[0][-nums[0]]++;
-				continue;
-			}
-			if (i >= 1) {
-				for (const auto &k : OPT[i - 1]) {
-					OPT[i][k.first + nums[i]] += k.second;
-					OPT[i][k.first - nums[i]] += k.second;
-				}
-				continue;
-			}
+		int sz = nums.size();
+		vector<vector<int>> A(sz, vector<int>(2001, -1));
+		return f(A, nums, 0, sz, 0, S);
+	}
+private:
+	int f(vector<vector<int>> & A, vector<int>& nums, int index, int sz, int curr, int S) {
+		if (index == sz) {
+			return curr == S;
 		}
-		return OPT[nums.size() - 1].count(S) ? OPT[nums.size() - 1][S] : 0;
+		if (A[index][curr + 1000] >= 0) {
+			return A[index][curr + 1000];
+		}
+		return A[index][curr + 1000] = f(A, nums, index + 1, sz, curr - nums[index], S) + f(A, nums, index + 1, sz, curr + nums[index], S);
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int S;
-	int result;
 
-	nums = {1, 1, 1, 1, 1};
-	S = 3;
-	result = solution.findTargetSumWays(nums, S);
-	assert(5 == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
+class Solution {
+public:
+	int findTargetSumWays(vector<int>& nums, int S) {
+		return f(nums, 0, nums.size(), S);
+	}
+private:
+	int f(vector<int>& nums, int index, int sz, int S) {
+		if (index == sz) {
+			return S == 0;
+		}
+		return f(nums, index + 1, sz, S - nums[index]) + f(nums, index + 1, sz, S + nums[index]);
+	}
+};
