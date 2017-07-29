5553974815332319b8e87efee1b242f55d638032
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-29 14:37:32 -0500

    592. Fraction Addition and Subtraction

--- Algorithms/fraction-addition-and-subtraction.cpp
+++ Algorithms/fraction-addition-and-subtraction.cpp
@@ -24,109 +24,140 @@ The number of given fractions will be in the range [1,10].
 The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	string fractionAddition(string expression) {
 		if (expression.empty()) {
 			return "0";
 		}
-		list<pair<int, int>> l1;
-		list<char> l2;
-		unordered_map<char, int> h;
-		h['+'] = 2;
-		h['-'] = 2;
-		h['*'] = 3;
-		h['/'] = 3;
-		int i = 0, n = expression.size();
-		if (expression.front() == '-') {
-			l1.push_back(make_pair(0, 1));
-			l2.push_back('-');
-			i++;
+		int sz = expression.size(), i = 0, base = 10;
+		stack<array<int, 2>> a;
+		stack<char> b;
+		array<int, 256> A;
+		A.fill(0);
+		A['+'] = 1;
+		A['-'] = 1;
+		A['/'] = 2;
+		if (expression[0] == '-') {
+			a.push({0, 1});
+			b.push('-');
+			++i;
 		}
-		for ( ; i < n; i++) {
-			int j = i;
-			char ch = expression[i];
-			if (ch == ' ') {
+		for (; i < sz; ++i) {
+			if (expression[i] == ' ') {
 				continue;
 			}
-			if (isdigit(ch)) {
-				while (i < n and isdigit(expression[i])) {
-					i++;
+			if (isdigit(expression[i])) {
+				int val = expression[i] - '0';
+				while (i + 1 < sz and isdigit(expression[i + 1])) {
+					val = base * val + expression[i + 1] - '0';
+					++i;
 				}
-				l1.push_back(make_pair(stoi(expression.substr(j, i-- - j)), 1));
+				a.push({val, 1});
 				continue;
 			}
-			if (h.count(ch)) {
-				while (!l2.empty() and h[l2.back()] >= h[ch]) {
-					char op = l2.back();
-					l2.pop_back();
-					pair<int, int> b = l1.back();
-					l1.pop_back();
-					pair<int, int> a = l1.back();
-					l1.pop_back();
-					pair<int, int> c = op == '+' ? make_pair(a.first * b.second + a.second * b.first, a.second * b.second) : op == '-' ? make_pair(a.first * b.second - a.second * b.first, a.second * b.second) : op == '*' ? make_pair(a.first * b.first, a.second * b.second) : make_pair(a.first * b.second, a.second * b.first);
-					int d = gcd(c.first, c.second);
-					c.first /= d;
-					c.second /= d;
-					l1.push_back(c);
+			if (A[expression[i]] > 0) {
+				while (!b.empty() and A[b.top()] >= A[expression[i]]) {
+					array<int, 2> y = a.top();
+					a.pop();
+					array<int, 2> x = a.top();
+					a.pop();
+					char op = b.top();
+					b.pop();
+					if (op == '+') {
+						a.push(f1(x, y));
+					}
+					else if (op == '-') {
+						a.push(f2(x, y));
+					}
+					else if (op == '*') {
+						a.push(f3(x, y));
+					}
+					else if (op == '/') {
+						a.push(f4(x, y));
+					}
 				}
-				l2.push_back(ch);
+				b.push(expression[i]);
 				continue;
 			}
 		}
-		while (!l2.empty()) {
-			char op = l2.back();
-			l2.pop_back();
-			pair<int, int> b = l1.back();
-			l1.pop_back();
-			pair<int, int> a = l1.back();
-			l1.pop_back();
-			pair<int, int> c = op == '+' ? make_pair(a.first * b.second + a.second * b.first, a.second * b.second) : op == '-' ? make_pair(a.first * b.second - a.second * b.first, a.second * b.second) : op == '*' ? make_pair(a.first * b.first, a.second * b.second) : make_pair(a.first * b.second, a.second * b.first);
-			int d = gcd(c.first, c.second);
-			c.first /= d;
-			c.second /= d;
-			l1.push_back(c);
+		while (!b.empty()) {
+			array<int, 2> y = a.top();
+			a.pop();
+			array<int, 2> x = a.top();
+			a.pop();
+			char op = b.top();
+			b.pop();
+			if (op == '+') {
+				a.push(f1(x, y));
+			}
+			else if (op == '-') {
+				a.push(f2(x, y));
+			}
+			else if (op == '*') {
+				a.push(f3(x, y));
+			}
+			else if (op == '/') {
+				a.push(f4(x, y));
+			}
 		}
-		pair<int, int>& result = l1.front();
-		if (result.second < 0) {
-			result.first = -result.first;
-			result.second = -result.second;
+		return to_string(a.top()[0]) + "/" + to_string(a.top()[1]);
+	}
+private:
+	array<int, 2> f1(const array<int, 2> & x, const array<int, 2> & y) {
+		array<int, 2> result;
+		result[0] = x[0] * y[1] + x[1] * y[0];
+		result[1] = x[1] * y[1];
+		int d = gcd(result[0], result[1]);
+		result[0] /= d;
+		result[1] /= d;
+		if (result[1] < 0) {
+			result[0] = -result[0];
+			result[1] = -result[1];
 		}
-		return to_string(result.first) + "/" + to_string(result.second);
+		return result;
+	}
+	array<int, 2> f2(const array<int, 2> & x, const array<int, 2> & y) {
+		array<int, 2> result;
+		result[0] = x[0] * y[1] - x[1] * y[0];
+		result[1] = x[1] * y[1];
+		int d = gcd(result[0], result[1]);
+		result[0] /= d;
+		result[1] /= d;
+		if (result[1] < 0) {
+			result[0] = -result[0];
+			result[1] = -result[1];
+		}
+		return result;
+	}
+	array<int, 2> f3(const array<int, 2> & x, const array<int, 2> & y) {
+		array<int, 2> result;
+		result[0] = x[0] * y[0];
+		result[1] = x[1] * y[1];
+		int d = gcd(result[0], result[1]);
+		result[0] /= d;
+		result[1] /= d;
+		if (result[1] < 0) {
+			result[0] = -result[0];
+			result[1] = -result[1];
+		}
+		return result;
+	}
+	array<int, 2> f4(const array<int, 2> & x, const array<int, 2> & y) {
+		array<int, 2> result;
+		result[0] = x[0] * y[1];
+		result[1] = x[1] * y[0];
+		int d = gcd(result[0], result[1]);
+		result[0] /= d;
+		result[1] /= d;
+		if (result[1] < 0) {
+			result[0] = -result[0];
+			result[1] = -result[1];
+		}
+		return result;
 	}
 private:
 	int gcd(int a, int b) {
 		return b == 0 ? a : gcd(b, a % b);
 	}
 };
-
-int main(void) {
-	Solution solution;
-	string expression, result, answer;
-
-	expression = "-1/2+1/2";
-	answer = "0/1";
-	result = solution.fractionAddition(expression);
-	assert(answer == result);
-
-	expression = "-1/2+1/2+1/3";
-	answer = "1/3";
-	result = solution.fractionAddition(expression);
-	assert(answer == result);
-
-	expression = "1/3-1/2";
-	answer = "-1/6";
-	result = solution.fractionAddition(expression);
-	assert(answer == result);
-
-	expression = "5/3+1/3";
-	answer = "2/1";
-	result = solution.fractionAddition(expression);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
