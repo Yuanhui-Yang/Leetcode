ffcb0701c3047a8f8ef482d2c4ebda08b0b26d20
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-07-30 00:12:25 -0500

    333. Largest BST Subtree

--- Algorithms/largest-bst-subtree.cpp
+++ Algorithms/largest-bst-subtree.cpp
@@ -1,58 +1,61 @@
 // 333. Largest BST Subtree
 // https://leetcode.com/problems/largest-bst-subtree/
-#include <iostream>
-#include <algorithm>
-using namespace std;
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+
+/*
+Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+
+Note:
+A subtree must include all of its descendants.
+Here's an example:
+	10
+	/ \
+   5  15
+  / \   \ 
+ 1   8   7
+The Largest BST Subtree in this case is the highlighted one. 
+The return value is the subtree's size, which is 3.
+Follow up:
+Can you figure out ways to solve it with O(n) time complexity?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	int largestBSTSubtree(TreeNode* root) {
-		if (!root) return 0;
-		if (!root->left && !root->right) return 1;
-		if (this->isValidBST(root)) return this->countNodes(root);
-		return max(this->largestBSTSubtree(root->left), this->largestBSTSubtree(root->right));
+		return f(root)[0];
 	}
 private:
-	bool isValidBST(TreeNode* root) {
-		TreeNode *previous = NULL;
-		return this->isValidBST(root, previous);
-	}
-	bool isValidBST(TreeNode* root, TreeNode*& previous) {
-		if (!root) return true;
-		if (!this->isValidBST(root->left, previous)) return false;
-		if (previous && previous->val >= root->val) return false;
-		previous = root;
-		if (!this->isValidBST(root->right, previous)) return false;
-		return true;
-	}
-	int countNodes(TreeNode* root) {
-		if (!root) return 0;
-		return this->countNodes(root->left) + 1 + this->countNodes(root->right);
+	array<int, 4> f(TreeNode * node) { // largest number, valid, min val, max val
+		if (!node) {
+			return {0, 0, 0, 0};
+		}
+		if (!node->left and !node->right) {
+			return {1, 1, node->val, node->val};
+		}
+		array<int, 4> left = f(node->left), right = f(node->right);
+		if (node->left and !node->right) {
+			if (left[1] and left[3] < node->val) {
+				return {left[0] + 1, 1, left[2], node->val};
+			}
+			return {left[0], 0, 0, 0};
+		}
+		if (!node->left and node->right) {
+			if (right[1] and node->val < right[2]) {
+				return {right[0] + 1, 1, node->val, right[3]};
+			}
+			return {right[0], 0, 0, 0};
+		}
+		if (left[1] and right[1] and left[3] < node->val and node->val < right[2]) {
+			return {left[0] + right[0] + 1, 1, left[2], right[3]};
+		}
+		return {max(left[0], right[0]), 0, 0, 0};
 	}
 };
-int main(void) {
-	Solution solution;
-	TreeNode *root = new TreeNode(10);
-	root->left = new TreeNode(5);
-	root->right = new TreeNode(15);
-	root->left->left = new TreeNode(1);
-	root->left->right = new TreeNode(8);
-	root->right->right = new TreeNode(7);
-	cout << solution.largestBSTSubtree(root) << "\tPassed\n";
-	root = new TreeNode(3);
-	root->right = new TreeNode(1);
-	root->right->left = new TreeNode(2);
-	cout << solution.largestBSTSubtree(root) << "\tPassed\n";
-	root = new TreeNode(3);
-	root->left = new TreeNode(1);
-	root->left->left = new TreeNode(2);
-	root->left->left->right = new TreeNode(4);
-	cout << solution.largestBSTSubtree(root) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
