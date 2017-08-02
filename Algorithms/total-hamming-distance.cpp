9dc51a1c78e0a8f98246e520cbceb640173e047e
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-02 15:56:21 -0500

    477. Total Hamming Distance

--- Algorithms/total-hamming-distance.cpp
+++ Algorithms/total-hamming-distance.cpp
@@ -1,37 +1,35 @@
 // 477. Total Hamming Distance
 // https://leetcode.com/problems/total-hamming-distance/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <bitset>
-using namespace std;
+
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Now your job is to find the total Hamming distance between all pairs of the given numbers.
+
+Example:
+Input: 4, 14, 2
+
+Output: 6
+
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case). So the answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+Note:
+Elements of the given array are in the range of 0 to 10^9
+Length of the array will not exceed 10^4.
+*/
+
 class Solution {
 public:
 	int totalHammingDistance(vector<int>& nums) {
-		const int n = nums.size();
-		if (n < 2) return 0;
-		vector<bitset<32>> vec(n);
-		for (int i = 0; i < n; i++) {
-			vec[i] = nums[i];
-		}
-		vector<int> OPT(32, 0);
-		for (int i = 0; i < 32; i++) {
-			for (int j = 0; j < n; j++) {
-				OPT[i] += vec[j][i];
+		int result = 0, sz = nums.size();
+		for (int i = 0; i < 32; ++i) {
+			int cnt = 0;
+			for (const auto & j : nums) {
+				cnt += (j >> i) & 1;
 			}
-		}
-		int result = 0;
-		for (int i = 0; i < 32; i++) {
-			result += OPT[i] * (n - OPT[i]);
+			result += cnt * (sz - cnt);
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	nums = {4, 14, 2};
-	assert(6 == solution.totalHammingDistance(nums));
-	cout << "\nPassed All\n";
-	return 0;
-}
