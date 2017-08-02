3ed6c50e28fcc25a67299b3229da8493ada90cb8
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-02 15:35:24 -0500

    556. Next Greater Element III

--- Algorithms/next-greater-element-iii.cpp
+++ Algorithms/next-greater-element-iii.cpp
@@ -12,58 +12,12 @@ Input: 21
 Output: -1
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	int nextGreaterElement(int n) {
 		string s = to_string(n);
-		bool l = nextPermutation(s);
-		long long m = stoll(s);
-		return (l and m >= 0 and m <= (long long)INT_MAX) ? m : -1; 
-	}
-private:
-	bool nextPermutation(string& s) {
-		if (s.empty()) {
-			return "";
-		}
-		int n = s.size(), i = n - 2;
-		while (i >= 0 and s[i] >= s[i + 1]) {
-			--i;
-		}
-		bool result = (i >= 0);
-		if (result) {
-			int j = n - 1;
-			while (j > i and s[j] <= s[i]) {
-				--j;
-			}
-			swap(s[i], s[j]);
-		}
-		reverse(next(begin(s), i + 1), end(s));
-		return result;
+		bool flag = next_permutation(begin(s), end(s));
+		long long result = stoll(s);
+		return flag and result < INT_MAX ? result : -1;
 	}
 };
-
-int main(void) {
-	Solution solution;
-	int n, answer, result;
-
-	n = 1999999999;
-	answer = -1;
-	result = solution.nextGreaterElement(n);
-	assert(answer == result);
-
-	n = 12;
-	answer = 21;
-	result = solution.nextGreaterElement(n);
-	assert(answer == result);
-
-	n = 21;
-	answer = -1;
-	result = solution.nextGreaterElement(n);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
