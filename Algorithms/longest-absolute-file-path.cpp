ad40c9b2dfb7a7dc95bf2ae41a95b363bf61cb98
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-04 13:41:40 -0500

    388. Longest Absolute File Path

--- Algorithms/longest-absolute-file-path.cpp
+++ Algorithms/longest-absolute-file-path.cpp
@@ -1,65 +1,182 @@
 // 388. Longest Absolute File Path
 // https://leetcode.com/problems/longest-absolute-file-path/
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <vector>
-using namespace std;
+
+/*
+Suppose we abstract our file system by a string in the following manner:
+
+The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
+
+dir
+	subdir1
+	subdir2
+		file.ext
+The directory dir contains an empty sub-directory subdir1 and a sub-directory subdir2 containing a file file.ext.
+
+The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" represents:
+
+dir
+	subdir1
+		file1.ext
+		subsubdir1
+	subdir2
+		subsubdir2
+			file2.ext
+The directory dir contains two sub-directories subdir1 and subdir2. subdir1 contains a file file1.ext and an empty second-level sub-directory subsubdir1. subdir2 contains a second-level sub-directory subsubdir2 containing a file file2.ext.
+
+We are interested in finding the longest (number of characters) absolute path to a file within our file system. For example, in the second example above, the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and its length is 32 (not including the double quotes).
+
+Given a string representing the file system in the above format, return the length of the longest absolute path to file in the abstracted file system. If there is no file in the system, return 0.
+
+Note:
+The name of a file contains at least a . and an extension.
+The name of a directory or sub-directory will not contain a ..
+Time complexity required: O(n) where n is the size of the input string.
+
+Notice that a/aa/aaa/file1.txt is not the longest file path, if there is another path aaaaaaaaaaaaaaaaaaaaa/sth.png.
+*/
+
 class Solution {
 public:
 	int lengthLongestPath(string input) {
-		if (input.empty()) return 0;
-		const int n = input.size();
-		int NDE = 0;
-		for (int i = 0; i < n; i++) {
-			if (i == 0) {
-				while (i < n && input[i] != '\n') i++;
-				i--;
-				NDE = max(NDE, 1);
-				continue;
+		int M = input.size(), N = f(input), result = 0;
+		vector<int> A(N + 1);
+		for (int i = 0; i < M; ++i) {
+			int index = 0;
+			while (i < M and input[i] == '\t') {
+				++i;
+				++index;
 			}
-			if (i < n && input[i] == '\n') {
-				int j = ++i;
-				while (i < n && input[i] == '\t') i++;
-				int cnt = i - j;
-				NDE = max(NDE, cnt + 1);
-				continue;
+			int j = i;
+			bool flag = false;
+			while (i < M and input[i] != '\n') {
+				if (input[i] == '.') {
+					flag = true;
+				}
+				++i;
+			}
+			if (index == 0) {
+				A[0] = i - j;
+			}
+			else {
+				A[index] = A[index - 1] + i - j;
+			}
+			if (flag) {
+				result = max(result, index + A[index]);
 			}
 		}
-		vector<vector<string>> DES(NDE);
+		return result;
+	}
+private:
+	int f(const string & s) {
 		int result = 0;
-		for (int i = 0; i < n; i++) {
-			if (i == 0) {
-				bool isFile = false;
-				while (i < n && input[i] != '\n') if (input[i++] == '.') isFile = true;
-				DES[0].push_back(input.substr(0, i));
-				if (isFile) result = max(result, i);
-				i--;
-				continue;
-			}
-			if (i < n && input[i] == '\n') {
-				int j = ++i;
-				while (i < n && input[i] == '\t') i++;
-				int cnt = i - j;
-				j = i;
-				bool isFile = false;
-				while (i < n && input[i] != '\n') if (input[i++] == '.') isFile = true;
-				DES[cnt].push_back(input.substr(j, i - j));
-				if (isFile) {
-					int l = 0;
-					for (int k = 0; k <= cnt; k++) l += DES[k].back().size();
-					result = max(result, l + cnt);
+		for (int sz = s.size(), i = 0; i < sz; ++i) {
+			if (s[i] != '\t') {
+				int t = 1;
+				while (i + 1 < sz and s[i + 1] == '\t') {
+					++t;
+					++i;
 				}
-				i--;
-				continue;
+				result = max(result, t);
 			}
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	assert(32 == solution.lengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"));
-	cout << "\nPassed All\n";
-	return 0;
-}
+
+class Solution {
+public:
+	int lengthLongestPath(string input) {
+		int M = input.size(), N = f(input), result = 0;
+		vector<int> A(N + 1);
+		for (int i = 0; i < M; ++i) {
+			int index = 0;
+			while (i < M and input[i] == '\t') {
+				++i;
+				++index;
+			}
+			int j = i;
+			bool flag = false;
+			while (i < M and input[i] != '\n') {
+				if (input[i] == '.') {
+					flag = true;
+				}
+				++i;
+			}
+			A[index] = i - j;
+			if (flag) {
+				int t = index;
+				for (int k = 0; k <= index; ++k) {
+				   t += A[k]; 
+				}
+				result = max(result, t);
+			}
+		}
+		return result;
+	}
+private:
+	int f(const string & s) {
+		int result = 0;
+		for (int sz = s.size(), i = 0; i < sz; ++i) {
+			if (s[i] != '\t') {
+				int t = 1;
+				while (i + 1 < sz and s[i + 1] == '\t') {
+					++t;
+					++i;
+				}
+				result = max(result, t);
+			}
+		}
+		return result;
+	}
+};
+
+class Solution {
+public:
+	int lengthLongestPath(string input) {
+		int result = 0, M = input.size(), N = f(input);
+		vector<stack<string>> A(N + 1);
+		for (int i = 0; i < M; ++i) {
+			int index = 0;
+			while (i < M and input[i] == '\t') {
+				++index;
+				++i;
+			}
+			int j = i;
+			bool flag = false;
+			while (i < M and input[i] != '\n') {
+				if (input[i] == '.') {
+					flag = true;
+				}
+				++i;
+			}
+			string s = input.substr(j, i - j);
+			A[index].push(s);
+			if (flag) {
+				int t = index;
+				for (int k = 0; k <= index; ++k) {
+					t += A[k].top().size();
+				}
+				result = max(result, t);
+			}
+		}
+		return result;
+	}
+private:
+	int f(const string & s) {
+		int result = 0;
+		for (int sz = s.size(), i = 0; i < sz; ++i) {
+			if (s[i] != '\t') {
+				continue;
+			}
+			int t = 0;
+			while (i < sz and s[i] == '\t') {
+				++i;
+				++t;
+			}
+			if (result < t) {
+				result = t;
+			}
+		}
+		return result;
+	}
+};
