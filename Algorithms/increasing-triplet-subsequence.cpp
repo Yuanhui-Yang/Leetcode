59dec65601dbf816028c0dca2c591929396c90ce
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-04 15:37:56 -0500

    334. Increasing Triplet Subsequence

--- Algorithms/increasing-triplet-subsequence.cpp
+++ Algorithms/increasing-triplet-subsequence.cpp
@@ -1,44 +1,37 @@
 // 334. Increasing Triplet Subsequence
 // https://leetcode.com/problems/increasing-triplet-subsequence/
-// https://discuss.leetcode.com/topic/37281/clean-and-short-with-comments-c
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
+
+/*
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+
+Formally the function should:
+Return true if there exists i, j, k 
+such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+Your algorithm should run in O(n) time complexity and O(1) space complexity.
+
+Examples:
+Given [1, 2, 3, 4, 5],
+return true.
+
+Given [5, 4, 3, 2, 1],
+return false.
+*/
+
 class Solution {
 public:
 	bool increasingTriplet(vector<int>& nums) {
-		int c1 = INT_MAX, c2 = INT_MAX;
-		for (const auto& i : nums) {
-			if (i <= c1)
-				c1 = i;
-			else if (i <= c2)
-				c2 = i;
-			else
+		int a = INT_MAX, b = INT_MAX;
+		for (const auto & i : nums) {
+			if (i <= a) {
+				a = i;
+			}
+			else if (i <= b) {
+				b = i;
+			}
+			else {
 				return true;
+			}
 		}
 		return false;
 	}
 };
-// class Solution {
-// public:
-// 	bool increasingTriplet(vector<int>& nums) {
-// 		vector<int> result;
-// 		for (const auto& i : nums) {
-// 			vector<int>::iterator it = lower_bound(begin(result), end(result), i);
-// 			if (it == end(result)) result.push_back(i);
-// 			else *it = i;
-// 			if (result.size() >= 3) return true;
-// 		}
-// 		return false;
-// 	}
-// };
-int main(void) {
-	Solution solution;
-	vector<int> nums {1, 2, 3, 4, 5};
-	cout << boolalpha << solution.increasingTriplet(nums) << "\tPassed\n";
-	nums = {5, 4, 3, 2, 1};
-	cout << boolalpha << solution.increasingTriplet(nums) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
