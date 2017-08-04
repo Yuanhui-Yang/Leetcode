a38483e0cf78075a771688346fc76774158f85ed
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-03 22:26:38 -0500

    406. Queue Reconstruction by Height

--- Algorithms/queue-reconstruction-by-height.cpp
+++ Algorithms/queue-reconstruction-by-height.cpp
@@ -1,29 +1,35 @@
 // 406. Queue Reconstruction by Height
 // https://leetcode.com/problems/queue-reconstruction-by-height/
-// https://discuss.leetcode.com/topic/60394/easy-concept-with-python-c-java-solution
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+Example
+
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
+
 class Solution {
 public:
 	vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
-		sort(begin(people), end(people), [](const pair<int, int>& i, const pair<int, int>& j) {
-			return (i.first == j.first) ? i.second < j.second : i.first > j.first; 
-		});
 		vector<pair<int, int>> result;
-		for (const auto &i : people)
-		    result.insert(begin(result) + i.second, i);
+		sort(begin(people), end(people), Comp());
+		for (const auto & i : people) {
+			result.insert(next(begin(result), i.second), i);
+		}
 		return result;
 	}
+private:
+	struct Comp {
+		bool operator() (const pair<int, int> & a, const pair<int, int> & b) {
+			return a.first == b.first ? a.second < b.second : a.first > b.first;
+		}
+	};
 };
-int main(void) {
-	Solution solution;
-	vector<pair<int, int>> people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
-	for (const auto&i : solution.reconstructQueue(people)) cout << i.first << ", " << i.second << "; ";
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
