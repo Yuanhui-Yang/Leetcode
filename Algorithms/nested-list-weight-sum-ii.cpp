7d2afd895fe2dd69c32ad18f657f0f01757775f8
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-05 11:48:30 -0500

    364. Nested List Weight Sum II

--- Algorithms/nested-list-weight-sum-ii.cpp
+++ Algorithms/nested-list-weight-sum-ii.cpp
@@ -1,5 +1,20 @@
 // 364. Nested List Weight Sum II
 // https://leetcode.com/problems/nested-list-weight-sum-ii/
+
+/*
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Different from the previous question where weight is increasing from root to leaf, now the weight is defined from bottom up. i.e., the leaf level integers have weight 1, and the root level integers have the largest weight.
+
+Example 1:
+Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2)
+
+Example 2:
+Given the list [1,[4,[6]]], return 17. (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)
+*/
+
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
@@ -32,23 +47,20 @@
 class Solution {
 public:
 	int depthSumInverse(vector<NestedInteger>& nestedList) {
-		return this->depthSumInverse(nestedList, this->depth(nestedList));
-	}
-private:
-	int depth(const vector<NestedInteger>& nestedList) {
-		if (nestedList.empty()) return 0;
-		int result = 1;
-		for (const auto &i : nestedList)
-			if (!i.isInteger())
-				result = max(result, 1 + this->depth(i.getList()));
-		return result;
-	}
-	int depthSumInverse(const vector<NestedInteger>& nestedList, int depth) {
-		int result = 0;
-		for (const auto &i : nestedList) {
-			if (i.isInteger()) result += depth * i.getInteger();
-			else result += this->depthSumInverse(i.getList(), depth - 1);
+		int a = 0, b = 0;
+		while (!nestedList.empty()) {
+			vector<NestedInteger> next;
+			for (const auto & i : nestedList) {
+				if (i.isInteger()) {
+					b += i.getInteger();
+				}
+				else {
+					next.insert(end(next), begin(i.getList()), end(i.getList()));
+				}
+			}
+			a += b;
+			nestedList = next;
 		}
-		return result;
+		return a;
 	}
 };
