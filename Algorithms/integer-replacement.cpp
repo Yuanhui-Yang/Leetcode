80ea03fe1a0d050ded1627004a7f878d35719aff
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-05 14:00:23 -0500

    397. Integer Replacement

--- Algorithms/integer-replacement.cpp
+++ Algorithms/integer-replacement.cpp
@@ -1,23 +1,56 @@
 // 397. Integer Replacement
 // https://leetcode.com/problems/integer-replacement/
-#include <iostream>
-#include <algorithm>
-#include <climits>
-using namespace std;
+
+/*
+Given a positive integer n and you can do operations as follow:
+
+If n is even, replace n with n/2.
+If n is odd, you can replace n with either n + 1 or n - 1.
+What is the minimum number of replacements needed for n to become 1?
+
+Example 1:
+
+Input:
+8
+
+Output:
+3
+
+Explanation:
+8 -> 4 -> 2 -> 1
+Example 2:
+
+Input:
+7
+
+Output:
+4
+
+Explanation:
+7 -> 8 -> 4 -> 2 -> 1
+or
+7 -> 6 -> 3 -> 2 -> 1
+*/
+
 class Solution {
 public:
 	int integerReplacement(int n) {
-		if (n == 1) return 0;
-		if (n == INT_MAX) return 32;
-		if (n & 1) return 1 + min(this->integerReplacement(n - 1), this->integerReplacement(n + 1));
-		return 1 + this->integerReplacement(n >> 1);
+		int result = 0;
+		long long N = n;
+		while (N > 1) {
+			++result;
+			if (N & 1) {
+				if (N != 3 and (N & 2)) {
+					++N;
+				}
+				else {
+					--N;
+				}
+			}
+			else {
+				N >>= 1;
+			}
+		}
+		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	cout << solution.integerReplacement(8) << "\tPassed\n";
-	cout << solution.integerReplacement(7) << "\tPassed\n";
-	cout << solution.integerReplacement(2147483647) << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
