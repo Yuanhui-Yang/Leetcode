6749562055cf8da523154904ae2cb158b41fd13a
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-17 16:10:24 -0500

    526. Beautiful Arrangement

--- Algorithms/beautiful-arrangement.cpp
+++ Algorithms/beautiful-arrangement.cpp
@@ -1,99 +1,76 @@
 // 526. Beautiful Arrangement
 // https://leetcode.com/problems/beautiful-arrangement/
-#include <iostream>
-#include <cassert>
-#include <cctype>
-#include <vector>
-#include <bitset>
-#include <algorithm>
-#include <iterator>
-using namespace std;
 
-// BEGIN: http://www.liuchuo.net/archives/3295
+/*
+Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers successfully if one of the following is true for the ith position (1 ? i ? N) in this array:
+
+The number at the ith position is divisible by i.
+i is divisible by the number at the ith position.
+Now given N, how many beautiful arrangements can you construct?
+
+Example 1:
+Input: 2
+Output: 2
+Explanation: 
+
+The first beautiful arrangement is [1, 2]:
+
+Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+The second beautiful arrangement is [2, 1]:
+
+Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+
+Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
+Note:
+N is a positive integer and will not exceed 15.
+*/
+
 class Solution {
 public:
 	int countArrangement(int N) {
-		int result = 0;
-		bitset<15> visited;
-		vector<int> candidates;
-		for (int i = 1; i <= N; i++) {
-			candidates.push_back(i);
-		}
-		countArrangement(result, visited, candidates, N);
-		return result;
+		vector<int> A(N);
+		iota(begin(A), end(A), 1);
+		return f(A, N, 0);
 	}
 private:
-	void countArrangement(int& result,
-		bitset<15>& visited,
-		vector<int>& candidates,
-		const int N) {
-		if (candidates.empty()) {
-			result++;
-			return;
+	int f(vector<int> & A, int N, int start) {
+		if (start >= N) {
+			return 1;
 		}
-		for (int i = 0; i < N; i++) {
-			if (!visited[i]) {
-				const int candidates_back = candidates.back();
-				if (candidates_back % (i + 1) == 0 || (i + 1) % candidates_back == 0) {
-					visited[i] = 1;
-					candidates.pop_back();
-					countArrangement(result, visited, candidates, N);
-					candidates.push_back(candidates_back);
-					visited[i] = 0;
-				}
+		int result = 0;
+		for (int i = start; i < N; ++i) {
+			swap(A[start], A[i]);
+			if ((start + 1) % A[start] == 0 or A[start] % (start + 1) == 0) {
+				result += f(A, N, start + 1);
 			}
+			swap(A[start], A[i]);
 		}
+		return result;
 	}
 };
-// END: http://www.liuchuo.net/archives/3295
 
-// class Solution {
-// public:
-// 	int countArrangement(int N) {
-// 		int OPT[15] = {1, 2, 3, 8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679};
-// 		return OPT[N - 1];
-// 	}
-// };
-
-// BEGIN: Time Limit Exceeded
-// class Solution {
-// public:
-// 	int countArrangement(int N) {
-// 		int result = 0;
-// 		vector<int> OPT;
-// 		for (int i = 1; i <= N; i++) {
-// 			OPT.push_back(i);
-// 		}
-// 		do {
-// 			bool x = true;
-// 			for (int i = 0; x && i < N; i++) {
-// 				if (OPT[i] % (i + 1) && (i + 1) % OPT[i]) {
-// 					x = false;
-// 				}
-// 			}
-// 			result += x;
-// 		} while (next_permutation(begin(OPT), end(OPT)));
-// 		return result;
-// 	}
-// };
-// END: Time Limit Exceeded
-
-int main(void) {
-	Solution solution;
-	int N = 0;
-	int result = 0;
-	int answer = 0;
-
-	N = 12;
-	result = solution.countArrangement(N);
-	answer = 4010;
-	assert(answer == result);
-
-	N = 2;
-	result = solution.countArrangement(N);
-	answer = 2;
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
+class Solution {
+public:
+	int countArrangement(int N) {
+		vector<bool> A(N, true);
+		return f(A, N, 0);
+	}
+private:
+	int f(vector<bool> & A, int N, int start) {
+		if (start >= N) {
+			return 1;
+		}
+		int result = 0;
+		for (int i = 0; i < N; ++i) {
+			if (A[i] and ((start + 1) % (i + 1) == 0 or (i + 1) % (start + 1) == 0)) {
+				A[i] = false;
+				result += f(A, N, start + 1);
+				A[i] = true;
+			}
+		}
+		return result;
+	}
+};
