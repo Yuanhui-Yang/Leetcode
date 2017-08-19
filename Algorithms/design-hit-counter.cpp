9896b4c6d7f1e88a979fde153f99d819ea222be7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-19 00:53:17 -0500

    362. Design Hit Counter

--- Algorithms/design-hit-counter.cpp
+++ Algorithms/design-hit-counter.cpp
@@ -1,64 +1,75 @@
 // 362. Design Hit Counter
 // https://leetcode.com/problems/design-hit-counter/
-// https://discuss.leetcode.com/topic/48752/simple-java-solution-with-explanation
-// https://discuss.leetcode.com/topic/48758/super-easy-design-o-1-hit-o-s-gethits-no-fancy-data-structure-is-needed
-#include <iostream>
-#include <deque>
-#include <cstring>
-using namespace std;
+
+/*
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+
+Each function accepts a timestamp parameter (in seconds granularity) and you may assume that calls are being made to the system in chronological order (ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+It is possible that several hits arrive roughly at the same time.
+
+Example:
+HitCounter counter = new HitCounter();
+
+// hit at timestamp 1.
+counter.hit(1);
+
+// hit at timestamp 2.
+counter.hit(2);
+
+// hit at timestamp 3.
+counter.hit(3);
+
+// get hits at timestamp 4, should return 3.
+counter.getHits(4);
+
+// hit at timestamp 300.
+counter.hit(300);
+
+// get hits at timestamp 300, should return 4.
+counter.getHits(300);
+
+// get hits at timestamp 301, should return 3.
+counter.getHits(301); 
+Follow up:
+What if the number of hits per second could be very large? Does your design scale?
+*/
+
 class HitCounter {
 public:
 	/** Initialize your data structure here. */
-	HitCounter() {}
+	HitCounter() {
+		A.fill(0);
+		B.fill(0);
+	}
 
 	/** Record a hit.
 		@param timestamp - The current timestamp (in seconds granularity). */
 	void hit(int timestamp) {
-		cache.push_back(timestamp);
-		while (!cache.empty() && cache.front() + 300 <= timestamp) cache.pop_front();
+		int id = timestamp % 300;
+		if (A[id] == timestamp) {
+			++B[id];
+		}
+		else {
+			A[id] = timestamp;
+			B[id] = 1;
+		}
 	}
 
 	/** Return the number of hits in the past 5 minutes.
 		@param timestamp - The current timestamp (in seconds granularity). */
 	int getHits(int timestamp) {
-		while (!cache.empty() && cache.front() + 300 <= timestamp) cache.pop_front();
-		return cache.size();
+		int result = 0;
+		for (int i = 0; i < 300; ++i) {
+			if (A[i] > 0 and timestamp - A[i] < 300) {
+				result += B[i];
+			}
+		}
+		return result;
 	}
 private:
-	deque<int> cache;
+	array<int, 300> A, B;
 };
-// class HitCounter {
-// public:
-// 	/** Initialize your data structure here. */
-// 	HitCounter(): times(new int[300]), hits(new int[300]) {
-// 		memset(times, 0, sizeof(int) * 300);
-// 		memset(hits, 0, sizeof(int) * 300);
-// 	}
-
-// 	/** Record a hit.
-// 		@param timestamp - The current timestamp (in seconds granularity). */
-// 	void hit(int timestamp) {
-// 		int index = timestamp % 300;
-// 		if (times[index] == timestamp) {
-// 			++hits[index];
-// 			return;
-// 		}
-// 		times[index] = timestamp;
-// 		hits[index] = 1;
-// 		return;
-// 	}
-
-// 	/** Return the number of hits in the past 5 minutes.
-// 		@param timestamp - The current timestamp (in seconds granularity). */
-// 	int getHits(int timestamp) {
-// 		int result = 0;
-// 		for (int i = 0; i < 300; ++i) if (timestamp - times[i] < 300) result += hits[i];
-// 		return result;
-// 	}
-// private:
-// 	int *times;
-// 	int *hits;
-// };
 
 /**
  * Your HitCounter object will be instantiated and called as such:
@@ -66,28 +77,3 @@ private:
  * obj.hit(timestamp);
  * int param_2 = obj.getHits(timestamp);
  */
-int main(void) {
-	HitCounter counter;
-	// hit at timestamp 1.
-	counter.hit(1);
-
-	// hit at timestamp 2.
-	counter.hit(2);
-
-	// hit at timestamp 3.
-	counter.hit(3);
-
-	// get hits at timestamp 4, should return 3.
-	cout << counter.getHits(4) << "\tPassed\n";
-
-	// hit at timestamp 300.
-	counter.hit(300);
-
-	// get hits at timestamp 300, should return 4.
-	cout << counter.getHits(300) << "\tPassed\n";
-
-	// get hits at timestamp 301, should return 3.
-	cout << counter.getHits(301) << "\tPassed\n";
-	cout << "\nPassed All\n";
- 	return 0;
-}
