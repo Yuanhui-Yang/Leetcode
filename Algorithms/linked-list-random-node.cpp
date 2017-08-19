dc5e0e6287e7bf111205ebb2728a1549fa63c172
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-19 15:09:02 -0500

    382. Linked List Random Node

--- Algorithms/linked-list-random-node.cpp
+++ Algorithms/linked-list-random-node.cpp
@@ -1,52 +1,56 @@
 // 382. Linked List Random Node
 // https://leetcode.com/problems/linked-list-random-node/
-// https://en.wikipedia.org/wiki/Reservoir_sampling
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
-// BEGIN: https://discuss.leetcode.com/topic/53812/using-reservoir-sampling-o-1-space-o-n-time-complexity-c/
+
+/*
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	/** @param head The linked list's head.
 		Note that the head is guaranteed to be not null, so it contains at least one node. */
 	Solution(ListNode* head) {
-		srand (time(NULL));
 		this->head = head;
 	}
 
 	/** Returns a random node's value. */
 	int getRandom() {
-		int result = 0, n = 1;
-		for (ListNode *it = this->head; it; it = it->next)
-			if (rand() % n++ == 0)
+		int result = 0, n = 0;
+		for (ListNode * it = head; it; it = it->next) {
+			if (rand() % (++n) == 0) {
 				result = it->val;
+			}
+		}
 		return result;
 	}
 private:
-	ListNode *head;
+	ListNode * head;
 };
-// END: https://discuss.leetcode.com/topic/53812/using-reservoir-sampling-o-1-space-o-n-time-complexity-c/
+
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(head);
  * int param_1 = obj.getRandom();
  */
-
-int main(void) {
-	// Init a singly linked list [1,2,3].
-	ListNode *head = new ListNode(1);
-	head->next = new ListNode(2);
-	head->next->next = new ListNode(3);
-	Solution solution(head);
-
-	// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
-	cout << solution.getRandom() << "\tPassed\n";
-	cout << "\nPassed All\n";
-	return 0;
-}
