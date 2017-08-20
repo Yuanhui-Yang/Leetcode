6c37c6cfd8adadb44670ebd982ee235fb9540df7
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-20 14:13:52 -0500

    651. 4 Keys Keyboard

---
+++ Algorithms/4-keys-keyboard.cpp
@@ -0,0 +1,57 @@
+// 651. 4 Keys Keyboard
+// https://leetcode.com/problems/4-keys-keyboard/
+
+/*
+Imagine you have a special keyboard with the following keys:
+
+Key 1: (A): Print one 'A' on screen.
+
+Key 2: (Ctrl-A): Select the whole screen.
+
+Key 3: (Ctrl-C): Copy selection to buffer.
+
+Key 4: (Ctrl-V): Print buffer on screen appending it after what has already been printed.
+
+Now, you can only press the keyboard for N times (with the above four keys), find out the maximum numbers of 'A' you can print on screen.
+
+Example 1:
+Input: N = 3
+Output: 3
+Explanation: 
+We can at most get 3 A's on screen by pressing following key sequence:
+A, A, A
+Example 2:
+Input: N = 7
+Output: 9
+Explanation: 
+We can at most get 9 A's on screen by pressing following key sequence:
+A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
+Note:
+1 <= N <= 50
+Answers will be in the range of 32-bit signed integer.
+*/
+
+class Solution {
+public:
+	int maxA(int N) {
+		vector<int> A(N + 1, 0);
+		iota(begin(A), end(A), 0);
+		for (int i = 4; i <= N; ++i) {
+			for (int j = 1; j <= i - 3; ++j) {
+				A[i] = max(A[i], A[j] * (i - j - 1));
+			}
+		}
+		return A[N];
+	}
+};
+
+class Solution {
+public:
+	int maxA(int N) {
+		int result = N;
+		for (int i = 1; i <= N - 3; ++i) {
+			result = max(result, maxA(i) * (N - i - 1));
+		}
+		return result;
+	}
+};
