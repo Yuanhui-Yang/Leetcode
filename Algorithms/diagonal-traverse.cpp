5b988350ba7456c80a4d6cfbbe4c0ed311788882
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-19 21:29:21 -0500

    498. Diagonal Traverse

--- Algorithms/diagonal-traverse.cpp
+++ Algorithms/diagonal-traverse.cpp
@@ -1,105 +1,61 @@
 // 498. Diagonal Traverse
 // https://leetcode.com/problems/diagonal-traverse/
-#include <iostream>
-#include <cassert>
-#include <vector>
-using namespace std;
+
+/*
+Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order as shown in the below image.
+
+Example:
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output:  [1,2,4,7,5,3,6,8,9]
+Explanation:
+
+Note:
+The total number of elements of the given matrix will not exceed 10,000.
+*/
+
 class Solution {
 public:
 	vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-		if (matrix.empty() || matrix.front().empty()) {
-			return {};
-		}
+		int P = matrix.size(), Q = P == 0 ? 0 : matrix[0].size();
 		vector<int> result;
-		const vector<int> dx = {-1, 1};
-		const vector<int> dy = {1, -1};
-		const int m = matrix.size();
-		const int n = matrix.front().size();
-		int currentX = 0;
-		int currentY = 0;
-		int direction = 0;
-		int cnt = 0;
-		while (cnt < m * n) {
-			cnt++;
-			if (direction == 0) {
-				result.push_back(matrix[currentX][currentY]);
-				int nextX = currentX + dx[direction];
-				int nextY = currentY + dy[direction];
-				if (nextX < 0 && nextY < n) {
-					direction = (direction + 1) % 2;
-					currentX = 0;
-					currentY = nextY;
-					continue;
+		if (P == 0 or Q == 0) {
+			return result;
+		}
+		array<int, 2> curr({0, 0});
+		for (int i = 0, j = P * Q, d = -1; i < j; ++i) {
+			result.push_back(matrix[curr[0]][curr[1]]);
+			if (d < 0) {
+				--curr[0];
+				++curr[1];
+				if (curr[1] >= Q) {
+					curr[0] += 2;
+					--curr[1];
+					d = -d;
 				}
-				if (nextX < 0 && nextY >= n) {
-					direction = (direction + 1) % 2;
-					currentX = 1;
-					currentY = currentY;
-					continue;
+				else if (curr[0] < 0) {
+					++curr[0];
+					d = -d;
 				}
-				if (nextX >= 0 && nextY >= n) {
-					direction = (direction + 1) % 2;
-					currentX++;
-					currentY = currentY;
-					continue;
-				}
-				if (nextX >= 0 && nextY < n) {
-					currentX = nextX;
-					currentY = nextY;
-					continue;
-				}
-				continue;
 			}
-			if (direction == 1) {
-				result.push_back(matrix[currentX][currentY]);
-				int nextX = currentX + dx[direction];
-				int nextY = currentY + dy[direction];
-				if (nextY < 0 && nextX < m) {
-					direction = (direction + 1) % 2;
-					currentX = nextX;
-					currentY = 0;
-					continue;
+			else {
+				++curr[0];
+				--curr[1];
+				if (curr[0] >= P) {
+					--curr[0];
+					curr[1] += 2;
+					d = -d;
 				}
-				if (nextY < 0 && nextX >= m) {
-					direction = (direction + 1) % 2;
-					currentX = currentX;
-					currentY++;
-					continue;
+				else if (curr[1] < 0) {
+					++curr[1];
+					d = -d;
 				}
-				if (nextY >= 0 && nextX >= m) {
-					direction = (direction + 1) % 2;
-					currentX = m - 1;
-					currentY++;
-					continue;
-				}
-				if (nextY >= 0 && nextX < m) {
-					currentX = nextX;
-					currentY = nextY;
-					continue;
-				}
-				continue;
 			}
 		}
 		return result;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<vector<int>> matrix;
-	vector<int> result;
-
-	matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	result =  solution.findDiagonalOrder(matrix);
-	assert(vector<int>({1, 2, 4, 7, 5, 3, 6, 8, 9}) == result);
-
-	matrix = {{2, 5, 8}, {4, 0, -1}};
-	result =  solution.findDiagonalOrder(matrix);
-	assert(vector<int>({2, 5, 4, 0, 8, -1}) == result);	
-
-	matrix = {{2, 5}, {8, 4}, {0, -1}};
-	result =  solution.findDiagonalOrder(matrix);
-	assert(vector<int>({2, 5, 8, 0, 4, -1}) == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
