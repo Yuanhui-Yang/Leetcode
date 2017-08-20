9cc28d8b94f2b39df49af6467bb34fb781c83558
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-20 18:39:37 -0500

    486. Predict the Winner

--- Algorithms/predict-the-winner.cpp
+++ Algorithms/predict-the-winner.cpp
@@ -1,55 +1,48 @@
 // 486. Predict the Winner
 // https://leetcode.com/problems/predict-the-winner/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+
+/*
+Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+
+Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
+
+Example 1:
+Input: [1, 5, 2]
+Output: False
+Explanation: Initially, player 1 can choose between 1 and 2. 
+If he chooses 2 (or 1), then player 2 can choose from 1 (or 2) and 5. If player 2 chooses 5, then player 1 will be left with 1 (or 2). 
+So, final score of player 1 is 1 + 2 = 3, and player 2 is 5. 
+Hence, player 1 will never be the winner and you need to return False.
+Example 2:
+Input: [1, 5, 233, 7]
+Output: True
+Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 1 can choose 233.
+Finally, player 1 has more score (234) than player 2 (12), so you need to return True representing player1 can win.
+Note:
+1 <= length of the array <= 20.
+Any scores in the given array are non-negative integers and will not exceed 10,000,000.
+If the scores of both players are equal, then player 1 is still the winner.
+*/
+
 class Solution {
 public:
 	bool PredictTheWinner(vector<int>& nums) {
-		if (nums.size() <= 2) {
+		int sz = nums.size();
+		if (sz == 0) {
 			return true;
 		}
-		return PredictTheWinner(0, 0, 0, nums.size() - 1, nums);
-	}
-private:
-	bool PredictTheWinner(const int x, const int y, const size_t i, const size_t j, const vector<int>& nums) {
-		if (i == j) {
-			if (x + nums[i] > y) {
-				return true;
+		vector<vector<int>> A(sz, vector<int>(sz, 0));
+		for (int len = 1; len <= sz; ++len) {
+			for (int i = 0; i + len <= sz; ++i) {
+				int j = i + len - 1;
+				if (len == 1) {
+					A[i][j] = nums[i];
+				}
+				else {
+					A[i][j] = max(nums[i] - A[i + 1][j], nums[j] - A[i][j - 1]);
+				}
 			}
-			return false;
 		}
-		return !PredictTheWinner(y, x + nums[i], i + 1, j, nums) || !PredictTheWinner(y, x + nums[j], i, j - 1, nums);
+		return A[0][sz - 1] >= 0;
 	}
 };
-int main(void) {
-	Solution solution;
-	vector<int> nums;
-	bool result;
-
-	nums = {1, 5, 2};
-	result = solution.PredictTheWinner(nums);
-	assert(!result);
-
-	nums = {1, 5, 233, 7};
-	result = solution.PredictTheWinner(nums);
-	assert(result);
-
-	nums = {0};
-	result = solution.PredictTheWinner(nums);
-	assert(result);
-
-	nums = {1, 1};
-	result = solution.PredictTheWinner(nums);
-	assert(result);
-
-	nums = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-	result = solution.PredictTheWinner(nums);
-	assert(result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
