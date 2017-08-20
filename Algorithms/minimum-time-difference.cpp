03c393edd43225d9e3cd8bfbb9123f8fe2d0a328
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-19 23:54:02 -0500

    539. Minimum Time Difference

--- Algorithms/minimum-time-difference.cpp
+++ Algorithms/minimum-time-difference.cpp
@@ -1,95 +1,43 @@
 // 539. Minimum Time Difference
 // https://leetcode.com/problems/minimum-time-difference/
 
-// Given a list of 24-hour clock time points in "Hour:Minutes" format, find the minimum minutes difference between any two time points in the list.
+/*
+Given a list of 24-hour clock time points in "Hour:Minutes" format, find the minimum minutes difference between any two time points in the list.
 
-// Example 1:
-// Input: ["23:59","00:00"]
-// Output: 1
-// Note:
-// The number of time points in the given list is at least 2 and won't exceed 20000.
-// The input time is legal and ranges from 00:00 to 23:59.
-
-#include <iostream> // std::cout; std::cin
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // memset
-#include <algorithm> // max; min; min_element; max_element; minmax_element; next_permutation; prev_permutation; nth_element; sort; swap; lower_bound; upper_bound; reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::string::npos
-#include <list>
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
+Example 1:
+Input: ["23:59","00:00"]
+Output: 1
+Note:
+The number of time points in the given list is at least 2 and won't exceed 20000.
+The input time is legal and ranges from 00:00 to 23:59.
+*/
 
 class Solution {
 public:
 	int findMinDifference(vector<string>& timePoints) {
-		if (timePoints.size() <= 1) {
-			return 0;
+		vector<int> A;
+		int sz = timePoints.size();
+		for (int i = 0; i < sz; ++i) {
+			A.push_back(f(timePoints[i]));
 		}
-		set<pair<int, int>> rbtree;
-		for (const auto &i : timePoints) {
-			int h = stoi(i.substr(0, 2));
-			int m = stoi(i.substr(3, 2));
-			pair<int, int> t = make_pair(h, m);
-			if (!rbtree.empty() and rbtree.count(t)) {
-				return 0;
-			}
-			rbtree.insert(t);
+		sort(begin(A), end(A));
+		int result = INT_MAX;
+		for (int i = 0; i + 1 < sz; ++i) {
+			int diff = A[i + 1] - A[i];
+			result = min(result, diff);
 		}
-		if (rbtree.size() <= 1) {
-			return 0;
+		if (sz >= 2) {
+			int diff = A[0] + 24 * 60 - A[sz - 1];
+			result = min(result, diff);
 		}
-		int dh = prev(end(rbtree))->first - begin(rbtree)->first;
-		int dm = prev(end(rbtree))->second - begin(rbtree)->second;
-		int result = dh * 60 + dm;
-		result = min(result, 24 * 60 - result);
-		int ph = INT_MIN;
-		int pm = INT_MIN;
-		for (const auto &i : rbtree) {
-			if (ph == INT_MIN and pm == INT_MIN) {
-				ph = i.first;
-				pm = i.second;
-			}
-			else {
-				dh = i.first - ph;
-				dm = i.second - pm;
-				int dt = dh * 60 + dm;
-				dt = min(dt, 24 * 60 - dt);
-				result = min(result, dt);
-				ph = i.first;
-				pm = i.second;
-			}
+		return result == INT_MAX ? 0 : result;
+	}
+private:
+	int f(const string & s) {
+		int sz = s.size(), i = 0;
+		while (i < sz and s[i] != ':') {
+			++i;
 		}
-		return result;
+		return stoi(s.substr(0, i)) * 60 + stoi(s.substr(i + 1));
 	}
 };
-
-int main(void) {
-	Solution solution;
-	vector<string> timePoints;
-	int result = 0, answer = 0;
-
-	timePoints = {"23:59", "00:00"};
-	answer = 1;
-	result = solution.findMinDifference(timePoints);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
