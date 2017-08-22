8bac6e88de6518bd3dc27b9db4924266ae87b946
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-21 19:31:17 -0500

    353. Design Snake Game

--- Algorithms/design-snake-game.cpp
+++ Algorithms/design-snake-game.cpp
@@ -1,57 +1,111 @@
 // 353. Design Snake Game
 // https://leetcode.com/problems/design-snake-game/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <list>
-#include <algorithm>
-#include <utility>
-using namespace std;
+
+/*
+Design a Snake game that is played on a device with screen size = width x height. Play the game online if you are not familiar with the game.
+
+The snake is initially positioned at the top left corner (0,0) with length = 1 unit.
+
+You are given a list of food's positions in row-column order. When a snake eats the food, its length and the game's score both increase by 1.
+
+Each food appears one by one on the screen. For example, the second food will not appear until the first food was eaten by the snake.
+
+When a food does appear on the screen, it is guaranteed that it will not appear on a block occupied by the snake.
+
+Example:
+Given width = 3, height = 2, and food = [[1,2],[0,1]].
+
+Snake snake = new Snake(width, height, food);
+
+Initially the snake appears at position (0,0) and the food at (1,2).
+
+|S| | |
+| | |F|
+
+snake.move("R"); -> Returns 0
+
+| |S| |
+| | |F|
+
+snake.move("D"); -> Returns 0
+
+| | | |
+| |S|F|
+
+snake.move("R"); -> Returns 1 (Snake eats the first food and right after that, the second food appears at (0,1) )
+
+| |F| |
+| |S|S|
+
+snake.move("U"); -> Returns 1
+
+| |F|S|
+| | |S|
+
+snake.move("L"); -> Returns 2 (Snake eats the second food)
+
+| |S|S|
+| | |S|
+
+snake.move("U"); -> Returns -1 (Game over because snake collides with border)
+*/
+
 class SnakeGame {
 public:
 	/** Initialize your data structure here.
-	@param width - screen width
-	@param height - screen height 
-	@param food - A list of food positions
-	E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
+		@param width - screen width
+		@param height - screen height 
+		@param food - A list of food positions
+		E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
 	SnakeGame(int width, int height, vector<pair<int, int>> food) {
-		this->current = 0;
-		this->width = width;
 		this->height = height;
-		this->food = food;
-		this->snake.clear();
-		this->snake.push_back(make_pair(0, 0));
+		this->width = width;
+		this->counter = 0;
+		curr.push_back({0, 0});
+		for (const auto & i : food) {
+			this->food.push_back({i.first, i.second});
+		}
 	}
 
 	/** Moves the snake.
-	@param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
-	@return The game's score after the move. Return -1 if game over. 
-	Game over when snake crosses the screen boundary or bites its body. */
+		@param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
+		@return The game's score after the move. Return -1 if game over. 
+		Game over when snake crosses the screen boundary or bites its body. */
 	int move(string direction) {
-		int snake_length = this->snake.size();
-		if (snake_length > this->width * this->height) return -1;
-		const int food_size = food.size();
-		pair<int, int> snake_head = this->snake.front();
-		int snake_head_x = snake_head.first;
-		int snake_head_y = snake_head.second;
-		int new_snake_head_x = (direction == "U") ? snake_head_x - 1 : (direction == "D") ? snake_head_x + 1 : snake_head_x;
-		int new_snake_head_y = (direction == "L") ? snake_head_y - 1 : (direction == "R") ? snake_head_y + 1 : snake_head_y;
-		if (new_snake_head_x < 0 || new_snake_head_x >= this->height) return -1;
-		if (new_snake_head_y < 0 || new_snake_head_y >= this->width) return -1;	
-		pair<int, int> new_snake_head = make_pair(new_snake_head_x, new_snake_head_y);
-		if (this->current < food_size && new_snake_head == food[this->current]) this->current++;
-		else this->snake.pop_back();	
-		if (find(begin(this->snake), end(this->snake), new_snake_head) != end(this->snake)) return -1;
-		this->snake.push_front(new_snake_head);
-		return this->current;
+		int x = curr.front()[0], y = curr.front()[1];
+		if (direction == "U") {
+			--x;
+		}
+		else if (direction == "D") {
+			++x;
+		}
+		else if (direction == "L") {
+			--y;
+		}
+		else if (direction == "R") {
+			++y;
+		}
+		if (x < 0 or x >= height or y < 0 or y >= width) {
+		   return -1; 
+		}
+		if (food.empty() or food.front()[0] != x or food.front()[1] != y) {
+			curr.pop_back();
+		}
+		for (const auto & i : curr) {
+			if (x == i[0] and y == i[1]) {
+				return -1;
+			}
+		}
+		curr.push_front({x, y});
+		if (!food.empty() and food.front()[0] == x and food.front()[1] == y) {
+			food.pop_front();
+			++counter;
+		}
+		return counter;
 	}
-
 private:
-	int current;
-	int width;
-	int height;
-	vector<pair<int, int>> food;
-	list<pair<int, int>> snake;
+	int height, width, counter;
+	list<array<int, 2>> curr, food;
 };
 
 /**
@@ -59,30 +113,3 @@ private:
  * SnakeGame obj = new SnakeGame(width, height, food);
  * int param_1 = obj.move(direction);
  */
-
-int main(void) {
-	vector<pair<int, int>> food;
-
-	// food = {{1, 2}, {0, 1}};
-	SnakeGame snakeGame(3, 2, food);
-	// assert(0 == snakeGame.move("R"));
-	// assert(0 == snakeGame.move("D"));
-	// assert(1 == snakeGame.move("R"));
-	// assert(1 == snakeGame.move("U"));
-	// assert(2 == snakeGame.move("L"));
-	// assert(-1 == snakeGame.move("U"));
-
-	food = {{1, 1}, {1, 0}, {0, 0}};
-	snakeGame = SnakeGame(2, 2, food);
-	assert(0 == snakeGame.move("R"));
-	assert(1 == snakeGame.move("D"));
-	assert(2 == snakeGame.move("L"));
-	assert(3 == snakeGame.move("U"));
-	assert(3 == snakeGame.move("R"));
-	assert(3 == snakeGame.move("D"));
-	assert(3 == snakeGame.move("L"));
-	assert(-1 == snakeGame.move("L"));
-
-	cout << "\nPassed All\n";
-	return 0;
-}
