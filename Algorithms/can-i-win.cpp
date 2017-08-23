2f0638ff3c23f40d494664ad0e2025aeb3fc66be
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-23 16:38:34 -0500

    464. Can I Win

--- Algorithms/can-i-win.cpp
+++ Algorithms/can-i-win.cpp
@@ -1,202 +1,62 @@
 // 464. Can I Win
 // https://leetcode.com/problems/can-i-win/
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <bitset>
-#include <unordered_map>
-using namespace std;
+
+/*
+In the "100 game," two players take turns adding, to a running total, any integer from 1..10. The player who first causes the running total to reach or exceed 100 wins.
+
+What if we change the game so that players cannot re-use integers?
+
+For example, two players might take turns drawing from a common pool of numbers of 1..15 without replacement until they reach a total >= 100.
+
+Given an integer maxChoosableInteger and another integer desiredTotal, determine if the first player to move can force a win, assuming both players play optimally.
+
+You can always assume that maxChoosableInteger will not be larger than 20 and desiredTotal will not be larger than 300.
+
+Example
+
+Input:
+maxChoosableInteger = 10
+desiredTotal = 11
+
+Output:
+false
+
+Explanation:
+No matter which integer the first player choose, the first player will lose.
+The first player can choose an integer from 1 up to 10.
+If the first player choose 1, the second player can only choose integers from 2 up to 10.
+The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
+Same with other integers chosen by the first player, the second player will always win.
+*/
 
 class Solution {
 public:
 	bool canIWin(int maxChoosableInteger, int desiredTotal) {
-		if (maxChoosableInteger >= desiredTotal) return true;
-		if (maxChoosableInteger * (1 + maxChoosableInteger) / 2 < desiredTotal) return false;
-		vector<unordered_map<bitset<20>, bool>> OPT(1 + desiredTotal);
-		bitset<20> visited;
-		return canIWin(visited, 0, maxChoosableInteger, desiredTotal, OPT);
+		if (maxChoosableInteger * (1 + maxChoosableInteger) < desiredTotal * 2) {
+			return false;
+		}
+		if (desiredTotal <= 0) {
+			return true;
+		}
+		unordered_map<bitset<20>, bool> A;
+		bitset<20> B;
+		return f(A, B, maxChoosableInteger, desiredTotal);
 	}
 private:
-	bool canIWin(bitset<20> visited, int sum, const int maxChoosableInteger, const int desiredTotal, vector<unordered_map<bitset<20>, bool>>& OPT) {
-		if (sum >= desiredTotal) return false;
-		if (OPT[sum].count(visited)) return OPT[sum][visited];
-		for (int i = 0; i < maxChoosableInteger; i++) {
-			bitset<20> j = visited;
-			if (!(j[i])) {
-				j[i] = 1;
-				if (sum + i + 1 >= desiredTotal) {
-					OPT[sum][visited] = true;
-					return true;
+	bool f(unordered_map<bitset<20>, bool> & A, bitset<20> & B, int maxVal, int curr) {
+		if (A.count(B)) {
+			return A[B];
+		}
+		for (int i = 0; i < maxVal; ++i) {
+			if (!B[i]) {
+				B[i] = 1;
+				if (i + 1 >= curr or !f(A, B, maxVal, curr - i - 1)) {
+					B[i] = 0;
+					return A[B] = true;
 				}
-				if (OPT[sum + i + 1].count(j) && !OPT[sum + i + 1][j]) {
-					OPT[sum][visited] = true;
-					return true;
-				}
-				if (!OPT[sum + i + 1].count(j) && !canIWin(j, sum + i + 1, maxChoosableInteger, desiredTotal, OPT)) {
-					OPT[sum][visited] = true;
-					return true;					
-				}
+				B[i] = 0;
 			}
 		}
-		OPT[sum][visited] = false;
-		return false;
+		return A[B] = false;
 	}
 };
-
-// class Solution {
-// public:
-// 	bool canIWin(int maxChoosableInteger, int desiredTotal) {
-// 		if (maxChoosableInteger >= desiredTotal) return true;
-// 		if (maxChoosableInteger * (1 + maxChoosableInteger) / 2 < desiredTotal) return false;
-// 		vector<unordered_map<int, bool>> OPT(1 + desiredTotal);
-// 		return canIWin(0, 0, maxChoosableInteger, desiredTotal, OPT);
-// 	}
-// private:
-// 	bool canIWin(int visited, int sum, const int maxChoosableInteger, const int desiredTotal, vector<unordered_map<int, bool>>& OPT) {
-// 		if (sum >= desiredTotal) return false;
-// 		if (OPT[sum].count(visited)) return OPT[sum][visited];
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if (!(visited & j)) {
-// 				j |= visited;
-// 				if (sum + i + 1 >= desiredTotal) {
-// 					OPT[sum][visited] = true;
-// 					return true;
-// 				}
-// 				if (OPT[sum + i + 1].count(j) && !OPT[sum + i + 1][j]) {
-// 					OPT[sum][visited] = true;
-// 					return true;
-// 				}
-// 				if (!OPT[sum + i + 1].count(j) && !canIWin(j, sum + i + 1, maxChoosableInteger, desiredTotal, OPT)) {
-// 					OPT[sum][visited] = true;
-// 					return true;					
-// 				}
-// 			}
-// 		}
-// 		OPT[sum][visited] = false;
-// 		return false;
-// 	}
-// };
-
-// BEGIN: Time Limit Exceeded
-// class Solution {
-// public:
-// 	bool canIWin(int maxChoosableInteger, int desiredTotal) {
-// 		if (maxChoosableInteger >= desiredTotal) return true;
-// 		if (maxChoosableInteger * (1 + maxChoosableInteger) / 2 < desiredTotal) return false;
-// 		vector<vector<bool>> OPT(1 << maxChoosableInteger, vector<bool>(1 + desiredTotal, false));
-// 		vector<vector<bool>> PATH(1 << maxChoosableInteger, vector<bool>(1 + desiredTotal, false));
-// 		return canIWin(0, 0, maxChoosableInteger, desiredTotal, OPT, PATH);
-// 	}
-// private:
-// 	bool canIWin(int visited, int sum, const int maxChoosableInteger, const int desiredTotal, vector<vector<bool>>& OPT, vector<vector<bool>>& PATH) {
-// 		if (sum >= desiredTotal) return false;
-// 		if (OPT[visited][sum]) return true;
-// 		if (PATH[visited][sum]) return OPT[visited][sum];
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if (!(j & visited)) {
-// 				j |= visited;
-// 				if (sum + i + 1 >= desiredTotal) {
-// 					OPT[visited][sum] = true;
-// 					PATH[visited][sum] = true;
-// 					return true;
-// 				}
-// 				if (!canIWin(j, sum + i + 1, maxChoosableInteger, desiredTotal, OPT, PATH)) {
-// 					OPT[visited][sum] = true;
-// 					PATH[visited][sum] = true;
-// 					return true;
-// 				}
-// 			}
-// 		}
-// 		PATH[visited][sum] = true;
-// 		return false;
-// 	}
-// };
-// END: Time Limit Exceeded
-
-// BEGIN: Memory Limit Exceeded
-// class Solution {
-// public:
-// 	bool canIWin(int maxChoosableInteger, int desiredTotal) {
-// 		if (maxChoosableInteger >= desiredTotal) return true;
-// 		if (maxChoosableInteger * (1 + maxChoosableInteger) / 2 < desiredTotal) return false;
-// 		vector<vector<vector<bool>>> F(2, vector<vector<bool>>(1 << maxChoosableInteger, vector<bool>(1 + desiredTotal, false)));
-// 		vector<vector<vector<bool>>> G(2, vector<vector<bool>>(1 << maxChoosableInteger, vector<bool>(1 + desiredTotal, false)));
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if (g(1, j, i + 1, maxChoosableInteger, desiredTotal, F, G)) {
-// 				return true;
-// 			}
-// 		}
-// 		return false;
-// 	}
-// private:
-// 	// player has chance to win
-// 	bool f(int player, int visited, int sum, int maxChoosableInteger, int desiredTotal, vector<vector<vector<bool>>>& F, vector<vector<vector<bool>>>& G) {
-// 		if (sum >= desiredTotal) return false;
-// 		if (F[player % 2][visited][sum]) return true;
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if ((j & visited) == 0) {
-// 				j |= visited;
-// 				if (sum + i + 1 < 1 + desiredTotal && G[(player + 1) % 2][j][sum + i + 1]) {
-// 					F[player % 2][visited][sum] = true;
-// 					return true;
-// 				}
-// 				if (sum + i + 1 >= desiredTotal) {
-// 					F[player % 2][visited][sum] = true;
-// 					return true;
-// 				}
-// 			}
-// 		}
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if ((j & visited) == 0) {
-// 				j |= visited;
-// 				if (g((player + 1) % 2, j, sum + i + 1, maxChoosableInteger, desiredTotal, F, G)) {
-// 					F[player % 2][visited][sum] = true;
-// 					return true;					
-// 				}
-// 			}			
-// 		}
-// 		return false;
-// 	}
-// 	// player has no chance to win
-// 	bool g(int player, int visited, int sum, int maxChoosableInteger, int desiredTotal, vector<vector<vector<bool>>>& F, vector<vector<vector<bool>>>& G) {
-// 		if (sum >= desiredTotal) return true;
-// 		if (G[player % 2][visited][sum]) return true;
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if ((j & visited) == 0) {
-// 				j |= visited;
-// 				if (sum + i + 1 >= desiredTotal) {
-// 					return false;
-// 				}
-// 			}
-// 		}
-// 		for (int i = 0; i < maxChoosableInteger; i++) {
-// 			int j = 1 << i;
-// 			if ((j & visited) == 0) {
-// 				j |= visited;
-// 				if (!f((player + 1) % 2, j, sum + i + 1, maxChoosableInteger, desiredTotal, F, G)) {
-// 					return false;
-// 				}
-// 			}
-// 		}
-// 		G[player % 2][visited][sum] = true;
-// 		return true;
-// 	}
-// };
-// END: Memory Limit Exceeded
-int main(void) {
-	Solution solution;
-	assert(false == solution.canIWin(10, 11));
-	assert(true == solution.canIWin(10, 0));
-	assert(false == solution.canIWin(10, 40));
-	assert(true == solution.canIWin(18, 79));
-	assert(true == solution.canIWin(20, 54));
-	assert(false == solution.canIWin(5, 50));
-	cout << "\nPassed All\n";
-	return 0;
-}
