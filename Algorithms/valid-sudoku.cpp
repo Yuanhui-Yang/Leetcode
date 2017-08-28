ad7388deb0685f42a2e86752b3863e1904b4702b
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-27 23:28:24 -0500

    36. Valid Sudoku

--- Algorithms/valid-sudoku.cpp
+++ Algorithms/valid-sudoku.cpp
@@ -13,50 +13,28 @@ Note:
 A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	bool isValidSudoku(vector<vector<char>>& board) {
-		int a[9], b[9], c[9];
-		memset(a, 0, sizeof(a));
-		memset(b, 0, sizeof(b));
-		memset(c, 0, sizeof(c));
+		array<int, 9> X, Y, Z;
+		X.fill(0);
+		Y.fill(0);
+		Z.fill(0);
 		for (int i = 0; i < 9; ++i) {
 			for (int j = 0; j < 9; ++j) {
-				char ch = board[i][j];
-				if (ch == '.') {
-					continue;
+				if (isdigit(board[i][j])) {
+					int k = i / 3 * 3 + j / 3, mask = 1 << (board[i][j] - '0');
+					if ((X[i] & mask) or (Y[j] & mask) or (Z[k] & mask)) {
+						return false;
+					}
+					else {
+						X[i] |= mask;
+						Y[j] |= mask;
+						Z[k] |= mask;
+					}
 				}
-				int mask = 1 << (ch - '1'), k = i / 3 * 3 + j / 3;
-				if ((a[i] & mask) or (b[j] & mask) or (c[k] & mask)) {
-					return false;
-				}
-				a[i] |= mask;
-				b[j] |= mask;
-				c[k] |= mask;
 			}
 		}
 		return true;
 	}
 };
-
-int main(void) {
-	Solution solution;
-	vector<vector<char>> board;
-	vector<string> matrix;
-	bool answer, result;
-
-	matrix = {".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........"};
-	answer = true;
-	board.clear();
-	for (const auto &i : matrix) {
-		board.push_back(vector<char>(begin(i), end(i)));
-	}
-	result = solution.isValidSudoku(board);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
