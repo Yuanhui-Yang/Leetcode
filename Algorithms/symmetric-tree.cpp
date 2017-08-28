519e181bb73ade9723854d0d363842b4725e78e0
Yuanhui Yang <yuanhui.yang@outlook.com>
2017-08-27 22:06:15 -0500

    101. Symmetric Tree

--- Algorithms/symmetric-tree.cpp
+++ Algorithms/symmetric-tree.cpp
@@ -6,13 +6,13 @@ Given a binary tree, check whether it is a mirror of itself (ie, symmetric aroun
 
 For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 
-    1
+	1
    / \
   2   2
  / \ / \
 3  4 4  3
 But the following [1,2,2,null,3,null,3] is not:
-    1
+	1
    / \
   2   2
    \   \
@@ -21,94 +21,66 @@ Note:
 Bonus points if you could solve it both recursively and iteratively.
 */
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
-
-struct TreeNode {
-int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 	bool isSymmetric(TreeNode* root) {
-		if (!root) {
-			return true;
-		}
-		return helper(root->left, root->right);
+		return f(root, root);
 	}
 private:
-	bool helper(TreeNode* p, TreeNode* q) {
-		if (!p or !q) {
-			return p == q;
+	bool f(TreeNode * p, TreeNode * q) {
+		if (!p and !q) {
+			return true;
 		}
-		return p->val == q->val and helper(p->left, q->right) and helper(p->right, q->left);
+		if (!p or !q) {
+			return false;
+		}
+		return p->val == q->val and f(p->left, q->right) and f(p->right, q->left);
 	}
 };
 
-void gc(TreeNode*& root) {
-	if (root) {
-		gc(root->left);
-		gc(root->right);
-		delete root;
-		root = NULL;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool isSymmetric(TreeNode* root) {
+		queue<TreeNode*> curr;
+		curr.push(root);
+		curr.push(root);
+		while (!curr.empty()) {
+			TreeNode * a = curr.front();
+			curr.pop();
+			TreeNode * b = curr.front();
+			curr.pop();
+			if (!a and !b) {
+				continue;
+			}
+			if (!a or !b) {
+				return false;
+			}
+			if (a->val != b->val) {
+				return false;
+			}
+			curr.push(a->left);
+			curr.push(b->right);
+			curr.push(a->right);
+			curr.push(b->left);
+		}
+		return true;
 	}
-}
-
-int main(void) {
-	Solution solution;
-	TreeNode *root = NULL;
-	bool result, answer;
-
-	root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(2);
-	root->left->left = new TreeNode(3);
-	root->left->right = new TreeNode(4);
-	root->right->left = new TreeNode(4);
-	root->right->right = new TreeNode(3);
-	answer = true;
-	result = solution.isSymmetric(root);
-	assert(answer == result);
-	gc(root);
-
-	root = new TreeNode(1);
-	root->left = new TreeNode(2);
-	root->right = new TreeNode(2);
-	root->left->right = new TreeNode(3);
-	root->right->right = new TreeNode(3);
-	answer = false;
-	result = solution.isSymmetric(root);
-	assert(answer == result);
-	gc(root);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
+};
