57544e3b74b09841e614b89e400a6d6610d6b84a
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-19 16:33:03 -0700

    5. Longest Palindromic Substring

--- Algorithms/longest-palindromic-substring.java
+++ Algorithms/longest-palindromic-substring.java
@@ -20,6 +20,48 @@ Output: "bb"
 
 class Solution {
 	public String longestPalindrome(String s) {
+		if (s == null || s.length() <= 1) {
+			return s;
+		}
+		char[] t = new char[2 * s.length() + 1];
+		int len = 2 * s.length() + 1, i = 0, j = 0, k = 0, x = 0, y = 0, z = 0, maxCenter = 0, maxLen = 0;
+		t[0] = '#';
+		for (i = 0, j = 1; i < s.length(); ++i) {
+			t[j] = s.charAt(i);
+			++j;
+			t[j] = '#';
+			++j;    
+		}
+		int[] A = new int[len];
+		A[0] = 0;
+		for (i = 1; i < len; ++i) {
+			j = 2 * x - i;
+			z = 2 * x - y;
+			if (j >= 0 && j - A[j] > z) {
+				A[i] = A[j];
+			}
+			else {
+				k = 0;
+				while (i - k >= 0 && i + k < len && t[i - k] == t[i + k]) {
+					++k;
+				}
+				A[i] = k - 1;
+			}
+			if (i + A[i] > x + y) {
+				x = i;
+				y = A[i];
+			}
+			if (A[i] > maxLen) {
+				maxCenter = i;
+				maxLen = A[i];
+			}
+		}
+		return s.substring((maxCenter - maxLen) / 2, (maxCenter - maxLen) / 2 + maxLen);
+	}
+}
+
+class Solution {
+	public String longestPalindrome(String s) {
 		int len = s.length(), i, j, k, x = 0, y = 0;
 		boolean[][] DP = new boolean[len][len];
 		for (i = 0; i < len; ++i) {
