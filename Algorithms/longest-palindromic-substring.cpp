5fcd02b7a36a2c227dba2c5413ed2452de50b4b4
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-20 00:51:27 -0700

    5. Longest Palindromic Substring

--- Algorithms/longest-palindromic-substring.cpp
+++ Algorithms/longest-palindromic-substring.cpp
@@ -19,33 +19,31 @@ Output: "bb"
 */
 
 class Solution {
-	public String longestPalindrome(String s) {
-		if (s == null || s.length() <= 1) {
+public:
+	string longestPalindrome(string s) {
+		if (s.size() <= 1) {
 			return s;
 		}
-		char[] t = new char[2 * s.length() + 1];
-		int len = 2 * s.length() + 1, i = 0, j = 0, k = 0, x = 0, y = 0, z = 0, maxCenter = 0, maxLen = 0;
-		t[0] = '#';
-		for (i = 0, j = 1; i < s.length(); ++i) {
-			t[j] = s.charAt(i);
-			++j;
-			t[j] = '#';
-			++j;    
+		string t;
+		t.push_back('#');
+		for (const auto & i : s) {
+			t.push_back(i);
+			t.push_back('#');
 		}
-		int[] A = new int[len];
-		A[0] = 0;
+		int len = t.size(), i = 0, j = 0, k = 0, x = 0, y = 0, z = 0, maxCenter = 0, maxLen = 0;
+		vector<int> A(len, 0);
 		for (i = 1; i < len; ++i) {
 			j = 2 * x - i;
 			z = x - y;
-			if (j >= 0 && j - A[j] > z) {
+			if (j >= 0 and j - A[j] > z) {
 				A[i] = A[j];
 			}
 			else {
 				k = 0;
-				while (i - k >= 0 && i + k < len && t[i - k] == t[i + k]) {
+				while (i - k >= 0 and i + k < len and t[i - k] == t[i + k]) {
 					++k;
 				}
-				A[i] = k - 1;
+				A[i] = --k;
 			}
 			if (i + A[i] > x + y) {
 				x = i;
@@ -56,6 +54,6 @@ class Solution {
 				maxLen = A[i];
 			}
 		}
-		return s.substring((maxCenter - maxLen) / 2, (maxCenter - maxLen) / 2 + maxLen);
+		return s.substr((maxCenter - maxLen) / 2, maxLen);
 	}
-}
+};
