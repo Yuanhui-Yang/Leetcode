656d47f3467e8e8fd21383e2b6c55b252c46f9a4
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-24 19:36:22 -0700

    351. Android Unlock Patterns

--- Algorithms/android-unlock-patterns.cpp
+++ Algorithms/android-unlock-patterns.cpp
@@ -1,49 +1,95 @@
 // 351. Android Unlock Patterns
 // https://leetcode.com/problems/android-unlock-patterns/
+
+/*
+Given an Android 3x3 key lock screen and two integers m and n, where 1 ≤ m ≤ n ≤ 9, count the total number of unlock patterns of the Android lock screen, which consist of minimum of m keys and maximum n keys.
+
+Rules for a valid pattern:
+Each pattern must connect at least m keys and at most n keys.
+All the keys must be distinct.
+If the line connecting two consecutive keys in the pattern passes through any other keys, the other keys must have previously selected in the pattern. No jumps through non selected key is allowed.
+The order of keys used matters.
+
+Explanation:
+| 1 | 2 | 3 |
+| 4 | 5 | 6 |
+| 7 | 8 | 9 |
+Invalid move: 4 - 1 - 3 - 6 
+Line 1 - 3 passes through key 2 which had not been selected in the pattern.
+
+Invalid move: 4 - 1 - 9 - 2
+Line 1 - 9 passes through key 5 which had not been selected in the pattern.
+
+Valid move: 2 - 4 - 1 - 3 - 6
+Line 1 - 3 is valid because it passes through key 2, which had been selected in the pattern
+
+Valid move: 6 - 5 - 4 - 1 - 9 - 2
+Line 1 - 9 is valid because it passes through key 5, which had been selected in the pattern.
+
+Example:
+Given m = 1, n = 1, return 9.
+*/
+
 #include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <algorithm>
+#include <cassert>
+#include <array>
+
 using namespace std;
+
 class Solution {
 public:
 	int numberOfPatterns(int m, int n) {
-		vector<vector<bool>> pattern(3, vector<bool>(3, false));
+		array<bool, 9> A;
+		A.fill(false);
 		int result = 0;
-		this->numberOfPatterns(pattern, m, n, result, 0, -1, -1);
+		for (int step = m; step <= n; ++step) {
+			result += f1(A, step, -1);
+		}
 		return result;
 	}
 private:
-	void numberOfPatterns(vector<vector<bool>>& pattern, int m, int n, int& result, int count, int i0, int j0) {
-		if (m > n) return;
-		if (count > n) return;
-		if (count >= m) ++result;
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				if (!this->isValid(pattern, i, j, i0, j0)) continue;
-				pattern[i][j] = true;
-				this->numberOfPatterns(pattern, m, n, result, count + 1, i, j);
-				pattern[i][j] = false;
+	int f1(array<bool, 9> & A, int step, int prev) {
+		if (step == 0) {
+			return 1;
+		}
+		int result = 0;
+		for (int i = 0; i < 9; ++i) {
+			if (f2(A, i, prev)) {
+				A[i] = true;
+				result += f1(A, step - 1, i);
+				A[i] = false;            
 			}
 		}
+		return result;
 	}
-	bool isValid(vector<vector<bool>>& pattern, int i, int j, int i0, int j0) {
-		if (i0 < 0 && j0 < 0) return true;
-		if (i == i0 && j == j0) return false;
-		if (pattern[i][j]) return false;
-		if (abs(i - i0) <= 1 && abs(j - j0) <= 1) return true;
-		if (abs(i - i0) == 2 && abs(j - j0) == 2) return pattern[1][1];
-		if (abs(i - i0) == 2 && abs(j - j0) == 0) return pattern[min(i0, i) + 1][j0];
-		if (abs(i - i0) == 2 && abs(j - j0) == 1) return true;
-		if (abs(i - i0) == 0 && abs(j - j0) == 2) return pattern[i0][min(j0, j) + 1];
-		if (abs(i - i0) == 1 && abs(j - j0) == 2) return true;
-		return false;
+	bool f2(array<bool, 9> & A, int curr, int prev) {
+		if (prev == -1) {
+			return true;
+		}
+		if (A[curr]) {
+			return false;
+		}
+		if ((prev + curr) % 2) {
+			return true;
+		}
+		int mid = (prev + curr) / 2;
+		if (mid == 4 or curr / 3 == prev / 3 or curr % 3 == prev % 3) {
+			return A[mid];
+		}
+		return true;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	cout << solution.numberOfPatterns(1, 1) << "\tPassed\n";
-	cout << solution.numberOfPatterns(1, 2) << "\tPassed\n";
+	int m, n, answer, result;
+
+	m = 1;
+	n = 2;
+	answer = 65;
+	result = solution.numberOfPatterns(m, n);
+	assert(answer == result);
+	
 	cout << "\nPassed All\n";
 	return 0;
 }
