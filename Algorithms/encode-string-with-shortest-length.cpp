73f848fe3b2577cbad0709ab6124fc0817647e60
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-25 14:38:26 -0700

    471. Encode String with Shortest Length

--- Algorithms/encode-string-with-shortest-length.cpp
+++ Algorithms/encode-string-with-shortest-length.cpp
@@ -1,55 +1,111 @@
 // 471. Encode String with Shortest Length
 // https://leetcode.com/problems/encode-string-with-shortest-length/
+
+/*
+Given a non-empty string, encode the string such that its encoded length is the shortest.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
+
+Note:
+k will be a positive integer and encoded string will not be empty or have extra space.
+You may assume that the input string contains only lowercase English letters. The string's length is at most 160.
+If an encoding process does not make the string shorter, then do not encode it. If there are several solutions, return any of them is fine.
+Example 1:
+
+Input: "aaa"
+Output: "aaa"
+Explanation: There is no way to encode it such that it is shorter than the input string, so we do not encode it.
+Example 2:
+
+Input: "aaaaa"
+Output: "5[a]"
+Explanation: "5[a]" is shorter than "aaaaa" by 1 character.
+Example 3:
+
+Input: "aaaaaaaaaa"
+Output: "10[a]"
+Explanation: "a9[a]" or "9[a]a" are also valid solutions, both of them have the same length = 5, which is the same as "10[a]".
+Example 4:
+
+Input: "aabcaabcd"
+Output: "2[aabc]d"
+Explanation: "aabc" occurs twice, so one answer can be "2[aabc]d".
+Example 5:
+
+Input: "abbbabbbcabbbabbbc"
+Output: "2[2[abbb]c]"
+Explanation: "abbbabbbc" occurs twice, but "abbbabbbc" can also be encoded to "2[abbb]c", so one answer can be "2[2[abbb]c]".
+*/
+
 #include <iostream>
-#include <cassert>
 #include <string>
 #include <vector>
-#include <unordered_set>
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/71541/easy-to-understand-c-o-n-3-solution
+
 class Solution {
 public:
 	string encode(string s) {
-		if (s.size() < 5) return s;
-		vector<vector<string>> OPT(s.size(), vector<string>(s.size()));
-		for (size_t len = 1; len <= s.size(); len++) {
-			for (size_t i = 0; i + len <= s.size(); i++) {
-				size_t j = i + len - 1;
-				const string s_substr = s.substr(i, len);
-				OPT[i][j] = s_substr;
-				if (len >= 5) {
-					const string s_collapse = collapse(s, i, j, OPT);
-					OPT[i][j] = s_collapse.size() < OPT[i][j].size() ? s_collapse : OPT[i][j];
-					for (size_t l = 1; l < len; l++) {
-						const string s_replace = OPT[i][i + l - 1] + OPT[i + l][j];
-						OPT[i][j] = s_replace.size() < OPT[i][j].size() ? s_replace : OPT[i][j];
+		int sz = s.size(), i = 0, j = 0, k = 0, l = 0, m = 0, n = 0;
+		if (sz  < 5) {
+			return s;
+		}
+		string t, u;
+		vector<vector<string>> A(sz, vector<string>(sz, ""));
+		for (i = 1; i <= sz; ++i) {
+			for (j = 0; i + j <= sz; ++j) {
+				k = i + j - 1;
+				A[j][k] = s.substr(j, i);
+				if (i >= 5) {
+					for (l = 1; l < i; ++l) {
+						if (A[j][j + l - 1].size() + A[j + l][k].size() < A[j][k].size()) {
+							A[j][k] = A[j][j + l - 1] + A[j + l][k];
+						}
+					}
+					for (l = 1; l <= i / 2; ++l) {
+						if (i % l == 0) {
+							t.clear();
+							n = i / l;
+							for (m = 0; m < n; ++m) {
+								t.append(s.substr(j, l));
+							}
+							if (t == s.substr(j, i)) {
+								u = to_string(n) + "[" + A[j][j + l - 1] + "]";
+								if (u.size() < A[j][k].size()) {
+									A[j][k] = u;
+								}
+							}
+						}
 					}
 				}
 			}
 		}
-		return OPT[0][s.size() - 1];
-	}
-private:
-	string collapse(const string& s, const size_t i, const size_t j, const vector<vector<string>>& OPT) {
-		const string s_substr = s.substr(i, j - i + 1);
-		size_t pos = (s_substr + s_substr).find(s_substr, 1);
-		return to_string(s_substr.size() / pos) + '[' + OPT[i][i + pos - 1] + ']';
+		return A[0][sz - 1];
 	}
 };
-// END: https://discuss.leetcode.com/topic/71541/easy-to-understand-c-o-n-3-solution
+
 int main(void) {
 	Solution solution;
-	string result;
-	result = solution.encode("aaa");
-	assert(unordered_set<string>({"aaa"}).count(result));
-	result = solution.encode("aaaaa");
-	assert(unordered_set<string>({"5[a]"}).count(result));
-	result = solution.encode("aaaaaaaaaa");
-	assert(unordered_set<string>({"10[a]", "a9[a]", "9[a]a"}).count(result));
-	result = solution.encode("aabcaabcd");
-	assert(unordered_set<string>({"2[aabc]d"}).count(result));
-	result = solution.encode("abbbabbbcabbbabbbc");
-	assert(unordered_set<string>({"2[2[abbb]c]"}).count(result));
-	cout << "\nPassed All\n";
+	string s, result;
+
+	s = "aaa";
+	result = solution.encode(s);
+	cout << result << '\n';
+
+	s = "aaaaa";
+	result = solution.encode(s);
+	cout << result << '\n';
+	
+	s = "aaaaaaaaaa";
+	result = solution.encode(s);
+	cout << result << '\n';
+	
+	s = "aabcaabcd";
+	result = solution.encode(s);
+	cout << result << '\n';
+	
+	s = "abbbabbbcabbbabbbc";
+	result = solution.encode(s);
+	cout << result << '\n';
+	
 	return 0;
 }
