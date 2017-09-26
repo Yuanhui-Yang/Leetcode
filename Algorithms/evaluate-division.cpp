1ce656f72dbde2cb6d1ba62c65c0a2cb6b6b73a6
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-25 21:46:44 -0700

    399. Evaluate Division

--- Algorithms/evaluate-division.cpp
+++ Algorithms/evaluate-division.cpp
@@ -1,61 +1,186 @@
 // 399. Evaluate Division
 // https://leetcode.com/problems/evaluate-division/
+
+/*
+Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+
+Example:
+Given a / b = 2.0, b / c = 3.0. 
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? . 
+return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+
+The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries , where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+
+According to the example above:
+
+equations = [ ["a", "b"], ["b", "c"] ],
+values = [2.0, 3.0],
+queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]. 
+The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+*/
+
 #include <iostream>
-#include <string>
-#include <vector>
 #include <queue>
-#include <iterator>
+#include <utility>
+#include <vector>
 #include <unordered_map>
-#include <unordered_set>
+
 using namespace std;
+
 class Solution {
 public:
 	vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
-		unordered_map<string, unordered_map<string, double>> graph;
-		for (size_t i = 0; i < equations.size(); i++) {
-			graph[equations[i].first][equations[i].second] = values[i];
-			graph[equations[i].second][equations[i].first] = 1.0 / values[i];
+		int sz = equations.size(), i = 0, j = 0, k = 0, n = 0;
+		unordered_map<string, int> A;
+		for (const auto & i : equations) {
+			if (!A.count(i.first)) {
+				A[i.first] = n++;
+			}
+			if (!A.count(i.second)) {
+				A[i.second] = n++;
+			}
+		}
+		vector<vector<pair<int, double>>> B(n);
+		for (i = 0; i < sz; ++i) {
+			j = A[equations[i].first];
+			k = A[equations[i].second];
+			B[j].push_back({k, values[i]});
+			B[k].push_back({j, 1 / values[i]});
 		}
 		vector<double> result;
-		for (const auto &i : queries) result.push_back(this->findparent(i.first, i.second, graph));
+		for (const auto & i : queries) {
+			if (!A.count(i.first) or !A.count(i.second)) {
+				result.push_back(-1);
+			}
+			else {
+				j = A[i.first];
+				k = A[i.second];
+				result.push_back(f(B, j, k));
+			}
+		}
 		return result;
 	}
 private:
-	double findparent(const string& begin, const string& end, unordered_map<string, unordered_map<string, double>>& graph) {
-		if (!graph.count(begin) || !graph.count(end)) return -1.0;
-		if (begin == end) return 1.0;
-		unordered_map<string, string> parent;
-		unordered_set<string> visited;
-		queue<string> q;
-		q.push(begin);
-		visited.insert(begin);
-		while (!q.empty()) {
-			for (const auto &i : graph[q.front()]) {
-				if (visited.count(i.first)) continue;
-				parent[i.first] = q.front();
-				q.push(i.first);
-				visited.insert(i.first);
+	double f(const vector<vector<pair<int, double>>> & B, int x, int y) {
+		int n = B.size(), front = 0;
+		vector<double> C(n, -1);
+		C[x] = 1;
+		queue<int> curr;
+		curr.push(x);
+		while (!curr.empty()) {
+			front = curr.front();
+			curr.pop();
+			if (front == y) {
+				return C[y];
 			}
-			q.pop();
+			for (const auto & i : B[front]) {
+				if (C[i.first] < 0) {
+					curr.push(i.first);
+					C[i.first] = C[front] * i.second;
+				}
+			}
 		}
-		if (!parent.count(end)) return -1.0;
-		double result = 1.0;
-		for (string i = end; i != begin; i = parent[i]) result *= graph[parent[i]][i];
-		return result;
+		return -1;
 	}
 };
+
 int main(void) {
 	Solution solution;
-	vector<pair<string, string>> equations = { {"a", "b"}, {"b", "c"} };
-	vector<double> values = {2.0, 3.0};
-	vector<pair<string, string>> queries = { {"a", "c"}, {"b", "a"}, {"a", "e"}, {"a", "a"}, {"x", "x"} };
-	for (const auto &i : solution.calcEquation(equations, values, queries)) cout << i << '\t';
-	cout << "\tPassed\n";
-	equations = { {"a","b"},{"b","c"} };
-	values = {2.0,3.0};
-	queries = { {"a","c"},{"b","c"},{"a","e"},{"a","a"},{"x","x"} };
-	for (const auto &i : solution.calcEquation(equations, values, queries)) cout << i << '\t';
-	cout << "\tPassed\n";	
-	cout << "\nPassed All\n";
+	vector<pair<string, string>> equations;
+	vector<double> values, result;
+	vector<pair<string, string>> queries;
+	
+	equations = {{"a", "b"}, {"b", "c"}};
+	values = {2.0, 3.0};
+	queries = {{"a", "c"}, {"b", "a"}, {"a", "e"}, {"a", "a"}, {"x", "x"}};
+	result = solution.calcEquation(equations, values, queries);
+	for (const auto & i : result) {
+		cout << i << '\t';
+	}
+	cout << '\n';
+	
+	return 0;
+}
+
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
+		int sz = equations.size(), i = 0, j = 0, k = 0, n = 0;
+		unordered_map<string, int> A;
+		for (const auto & i : equations) {
+			if (!A.count(i.first)) {
+				A[i.first] = n++;
+			}
+			if (!A.count(i.second)) {
+				A[i.second] = n++;
+			}
+		}
+		vector<vector<double>> B(n, vector<double>(n, -1));
+		for (i = 0; i < sz; ++i) {
+			j = A[equations[i].first];
+			k = A[equations[i].second];
+			B[j][k] = values[i];
+			B[k][j] = 1 / B[j][k];
+		}
+		vector<double> result;
+		for (const auto & i : queries) {
+			if (!A.count(i.first) or !A.count(i.second)) {
+				result.push_back(-1);
+			}
+			else {
+				j = A[i.first];
+				k = A[i.second];
+				result.push_back(f(B, j, k));
+			}
+		}
+		return result;
+	}
+private:
+	double f(const vector<vector<double>> & B, int x, int y) {
+		int n = B.size(), i = 0, front = 0;
+		vector<double> C(n, -1);
+		C[x] = 1;
+		queue<int> curr;
+		curr.push(x);
+		while (!curr.empty()) {
+			front = curr.front();
+			curr.pop();
+			if (front == y) {
+				return C[y];
+			}
+			for (i = 0; i < n; ++i) {
+				if (B[front][i] > 0 and C[i] < 0) {
+					curr.push(i);
+					C[i] = C[front] * B[front][i];
+				}
+			}
+		}
+		return -1;
+	}
+};
+
+int main(void) {
+	Solution solution;
+	vector<pair<string, string>> equations;
+	vector<double> values, result;
+	vector<pair<string, string>> queries;
+	
+	equations = {{"a", "b"}, {"b", "c"}};
+	values = {2.0, 3.0};
+	queries = {{"a", "c"}, {"b", "a"}, {"a", "e"}, {"a", "a"}, {"x", "x"}};
+	result = solution.calcEquation(equations, values, queries);
+	for (const auto & i : result) {
+		cout << i << '\t';
+	}
+	cout << '\n';
+	
 	return 0;
 }
