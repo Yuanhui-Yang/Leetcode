61ea5ac72002c4dd7807bd27c64085817249573e
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-27 22:56:45 -0700

    416. Partition Equal Subset Sum

--- Algorithms/partition-equal-subset-sum.cpp
+++ Algorithms/partition-equal-subset-sum.cpp
@@ -33,45 +33,45 @@ using namespace std;
 
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sz = nums.size(), i = 0, j = 0, target = accumulate(begin(nums), end(nums), 0);
-        if (target % 2) {
-            return false;
-        }
-        target /= 2;
-        sort(begin(nums), end(nums));
-        vector<vector<bool>> A(target + 1, vector<bool>(sz + 1, false));
-        for (i = 0; i <= sz; ++i) {
-            A[0][i] = true;
-        }
-        for (i = 1; i <= target; ++i) {
-            for (j = sz - 1; j >= 0; --j) {
-                A[i][j] = A[i][j + 1];
-                if (i >= nums[j]) {
-                    A[i][j] = A[i][j] or A[i - nums[j]][j + 1];
-                }
-            }
-        }
-        return A[target][0];
-    }
+	bool canPartition(vector<int>& nums) {
+		int sz = nums.size(), i = 0, j = 0, target = accumulate(begin(nums), end(nums), 0);
+		if (target % 2) {
+			return false;
+		}
+		target /= 2;
+		sort(begin(nums), end(nums));
+		vector<vector<bool>> A(target + 1, vector<bool>(sz + 1, false));
+		for (i = 0; i <= sz; ++i) {
+			A[0][i] = true;
+		}
+		for (i = 1; i <= target; ++i) {
+			for (j = sz - 1; j >= 0; --j) {
+				A[i][j] = A[i][j + 1];
+				if (i >= nums[j]) {
+					A[i][j] = A[i][j] or A[i - nums[j]][j + 1];
+				}
+			}
+		}
+		return A[target][0];
+	}
 };
 
 int main(void) {
-    Solution solution;
-    vector<int> nums;
-    bool result;
+	Solution solution;
+	vector<int> nums;
+	bool result;
 
-    nums = {1, 5, 11, 5};
-    result = solution.canPartition(nums);
-    cout << boolalpha << result << '\n';
+	nums = {1, 5, 11, 5};
+	result = solution.canPartition(nums);
+	cout << boolalpha << result << '\n';
 
-    nums = {1, 2, 3, 5};
-    result = solution.canPartition(nums);
-    cout << boolalpha << result << '\n';
+	nums = {1, 2, 3, 5};
+	result = solution.canPartition(nums);
+	cout << boolalpha << result << '\n';
 
-    nums = {1, 2, 3, 4, 5, 6, 7};
-    result = solution.canPartition(nums);
-    cout << boolalpha << result << '\n';
-    
-    return 0;
+	nums = {1, 2, 3, 4, 5, 6, 7};
+	result = solution.canPartition(nums);
+	cout << boolalpha << result << '\n';
+	
+	return 0;
 }
