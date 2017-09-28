40082b517f285b90e295b12e432ca873a83011a0
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-27 22:53:29 -0700

    416. Partition Equal Subset Sum

--- Algorithms/partition-equal-subset-sum.cpp
+++ Algorithms/partition-equal-subset-sum.cpp
@@ -1,55 +1,77 @@
 // 416. Partition Equal Subset Sum
 // https://leetcode.com/problems/partition-equal-subset-sum/
+
+/*
+Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Note:
+Each of the array element will not exceed 100.
+The array size will not exceed 200.
+Example 1:
+
+Input: [1, 5, 11, 5]
+
+Output: true
+
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: [1, 2, 3, 5]
+
+Output: false
+
+Explanation: The array cannot be partitioned into equal sum subsets.
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <numeric>
+
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/62271/c-solution-with-dynamic-programming
+
 class Solution {
 public:
-	bool canPartition(vector<int>& nums) {
-		const int sum = accumulate(begin(nums), end(nums), 0);
-		const int n = nums.size();
-		if (sum & 1) return false;
-		int target = sum >> 1;
-		vector<bool> OPT(1 + target, false);
-		OPT[0] = true;
-		for (int i = 1; i < 1 + target; i++)
-			for (int j = 0; j < n; j++)
-				OPT[i] = OPT[i] || (nums[j] <= i && OPT[i - nums[j]]);
-		return OPT[sum / 2];
-	}
+    bool canPartition(vector<int>& nums) {
+        int sz = nums.size(), i = 0, j = 0, target = accumulate(begin(nums), end(nums), 0);
+        if (target % 2) {
+            return false;
+        }
+        target /= 2;
+        sort(begin(nums), end(nums));
+        vector<vector<bool>> A(target + 1, vector<bool>(sz + 1, false));
+        for (i = 0; i <= sz; ++i) {
+            A[0][i] = true;
+        }
+        for (i = 1; i <= target; ++i) {
+            for (j = sz - 1; j >= 0; --j) {
+                A[i][j] = A[i][j + 1];
+                if (i >= nums[j]) {
+                    A[i][j] = A[i][j] or A[i - nums[j]][j + 1];
+                }
+            }
+        }
+        return A[target][0];
+    }
 };
-// class Solution {
-// public:
-// 	bool canPartition(vector<int>& nums) {
-// 		int sum = accumulate(begin(nums), end(nums), 0);
-// 		if (sum & 1) return false;
-// 		for (size_t i = 1; i <= nums.size() / 2; i++) {
-// 			for (size_t j = 0; j < i; j++) {
-// 				for (size_t k = j; k < nums.size(); k++) {
-// 					swap(nums[j], nums[k]);
-// 					if (accumulate(begin(nums), begin(nums) + i, 0) == sum / 2) return true;
-// 				}
-// 			}
-// 		}
-// 		return false;
-// 	}
-// };
-// END: https://discuss.leetcode.com/topic/62271/c-solution-with-dynamic-programming
+
 int main(void) {
-	Solution solution;
-	vector<int> nums;
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
-	nums = {1, 5, 11, 5};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
-	nums = {1, 2, 3, 5};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
-	nums = {73, 75, 1, 61, 42, 92, 95, 94, 86, 81, 66, 21, 32, 80, 16, 52, 35, 90, 60, 20, 57, 95, 34, 40, 72, 83, 35, 67, 86, 78, 68, 47, 60, 72, 37, 83, 43, 35, 98, 24, 6, 38, 22, 42, 47, 62, 72, 42, 20, 70, 6, 75, 55, 28, 61, 85, 54, 33, 67, 99, 30, 80, 94, 56, 13, 33, 39, 37, 59, 82, 82, 16, 17, 60, 4, 36, 23, 76, 34, 45, 3, 97, 86, 2, 63, 17, 95, 92, 61, 36, 66, 18, 67, 5, 46, 38, 5, 53, 59, 17};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";
-	nums = {74, 95, 89, 56, 83, 77, 58, 40, 8, 74, 37, 64, 89, 85, 51, 35, 5, 35, 79, 99, 95, 94, 38, 78, 39, 49, 36, 1, 39, 68, 41, 25, 12, 36, 24, 98, 34, 19, 9, 89, 48, 30, 47, 42, 49, 53, 18, 99, 52, 86, 59, 28, 56, 99, 34, 1, 11, 43, 75, 13, 29, 84, 92, 14, 13, 38, 14, 85, 16, 24, 20, 31, 96, 30, 88, 95, 90, 69, 73, 42, 47, 23, 67, 56, 85, 10, 97, 59, 98, 19, 10, 10, 25, 39, 1, 14, 9, 4, 76};
-	cout << boolalpha << solution.canPartition(nums) << "\tPassed\n";	
-	cout << "\nPassed All\n";
-	return 0;
+    Solution solution;
+    vector<int> nums;
+    bool result;
+
+    nums = {1, 5, 11, 5};
+    result = solution.canPartition(nums);
+    cout << boolalpha << result << '\n';
+
+    nums = {1, 2, 3, 5};
+    result = solution.canPartition(nums);
+    cout << boolalpha << result << '\n';
+
+    nums = {1, 2, 3, 4, 5, 6, 7};
+    result = solution.canPartition(nums);
+    cout << boolalpha << result << '\n';
+    
+    return 0;
 }
