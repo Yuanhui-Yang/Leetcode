1db8dfbedf295c09df5aa3a9ac1fb1601b5b1274
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-09-29 04:33:16 -0700

    670. Maximum Swap

--- Algorithms/maximum-swap.cpp
+++ Algorithms/maximum-swap.cpp
@@ -26,6 +26,59 @@ using namespace std;
 class Solution {
 public:
 	int maximumSwap(int num) {
+		string s = to_string(num);
+		int sz = s.size(), i = 0, a = -1, b = -1, c = -1, d = -1;
+		for (i = sz - 1; i >= 0; --i) {
+			if (a < s[i]) {
+				a = s[i];
+				b = i;
+			}
+			else if (s[i] < a) {
+				c = i;
+				d = b;
+			}
+		}
+		if (c < 0 and d < 0) {
+			return num;
+		}
+		swap(s[c], s[d]);
+		return stoi(s);
+	}
+};
+
+int main(void) {
+	Solution solution;
+	int num, result;
+
+	num = 1993;
+	result = solution.maximumSwap(num);
+	cout << result << '\n';
+	
+	num = 98368;
+	result = solution.maximumSwap(num);
+	cout << result << '\n';
+	
+	num = 2736;
+	result = solution.maximumSwap(num);
+	cout << result << '\n';
+
+	num = 9973;
+	result = solution.maximumSwap(num);
+	cout << result << '\n';
+
+	return 0;
+}
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+	int maximumSwap(int num) {
 		string s = to_string(num), t, u;
 		int sz = s.size(), i = 0, j = 0;
 		bool flag = false;
