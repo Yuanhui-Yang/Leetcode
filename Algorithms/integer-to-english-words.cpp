9a4f570862c7b04d2dd7bd11931e4ce7e41b8b07
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-01 07:12:19 -0700

    273. Integer to English Words

--- Algorithms/integer-to-english-words.cpp
+++ Algorithms/integer-to-english-words.cpp
@@ -19,6 +19,61 @@ using namespace std;
 class Solution {
 public:
 	string numberToWords(int num) {
+		int ten = 10, hundred = 1e2, thousand = 1e3, million = 1e6, billion = 1e9;
+		array<string, 21> A({"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"});
+		if (num <= 20) {
+			return A[num];
+		}
+		array<string, 10> B({"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"});
+		if (num < hundred) {
+			return num % ten == 0 ? B[num / ten] : B[num / ten] + " " + numberToWords(num % ten); 
+		}
+		if (num < thousand) {
+			return num % hundred == 0 ? numberToWords(num / hundred) + " Hundred" : numberToWords(num / hundred) + " Hundred " + numberToWords(num % hundred);
+		}
+		if (num < million) {
+			return num % thousand == 0 ? numberToWords(num / thousand) + " Thousand" : numberToWords(num / thousand) + " Thousand " + numberToWords(num % thousand);
+		}
+		if (num < billion) {
+			return num % million == 0 ? numberToWords(num / million) + " Million" : numberToWords(num / million) + " Million " + numberToWords(num % million);
+		}
+		return num % billion == 0 ? numberToWords(num / billion) + " Billion" : numberToWords(num / billion) + " Billion " + numberToWords(num % billion);
+	}
+};
+
+int main(void) {
+	Solution solution;
+	int num;
+	string result;
+
+	num = 30;
+	result = solution.numberToWords(num);
+	cout << result << '\n';
+
+	num = 123;
+	result = solution.numberToWords(num);
+	cout << result << '\n';
+
+	num = 12345;
+	result = solution.numberToWords(num);
+	cout << result << '\n';
+
+	num = 1234567;
+	result = solution.numberToWords(num);
+	cout << result << '\n';
+
+	return 0;
+}
+
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	string numberToWords(int num) {
 		array<string, 21> A({"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"});
 		if (num <= 20) {
 			return A[num];
