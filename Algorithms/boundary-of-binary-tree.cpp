87fb8a19ae5df5a3016790b0fbd70271c55938a1
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-01 04:18:47 -0700

    545. Boundary of Binary Tree

--- Algorithms/boundary-of-binary-tree.cpp
+++ Algorithms/boundary-of-binary-tree.cpp
@@ -14,7 +14,7 @@ Example 1
 Input:
   1
    \
-    2
+	2
    / \
   3   4
 
@@ -28,14 +28,14 @@ The right boundary are node 1,2,4. Note the anti-clockwise direction means you s
 So order them in anti-clockwise without duplicates and we have [1,3,4,2].
 Example 2
 Input:
-    ____1_____
+	____1_____
    /          \
   2            3
  / \          / 
 4   5        6   
    / \      / \
   7   8    9  10  
-       
+	   
 Ouput:
 [1,2,4,7,8,9,10,6,3]
 
@@ -46,115 +46,112 @@ The right boundary are node 1,3,6,10. (10 is the right-most node).
 So order them in anti-clockwise without duplicate nodes we have [1,2,4,7,8,9,10,6,3].
 */
 
-#include <iostream> // std::cout; std::cin
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
+#include <iostream>
 #include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
 #include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
+#include <algorithm>
+
 using namespace std;
 
 struct TreeNode {
 	int val;
-	TreeNode *left;
-	TreeNode *right;
+	TreeNode * left, * right;
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void garbageCollection(TreeNode* root) {
+void gc(TreeNode * & root) {
 	if (root) {
-		garbageCollection(root->left);
-		garbageCollection(root->right);
+		gc(root->left);
+		gc(root->right);
 		delete root;
+		root = NULL;
 	}
 }
 
-// BEGIN: http://www.geeksforgeeks.org/boundary-traversal-of-binary-tree/
-// BEGIN: Time Complexity: Linear on size of tree, Space Complexity: Linear on size of tree
 class Solution {
 public:
 	vector<int> boundaryOfBinaryTree(TreeNode* root) {
+		vector<int> result, A, B, C;
 		if (!root) {
-			return {};
+			return result;
 		}
-		vector<int> result;
 		result.push_back(root->val);
-		leftBoundary(root->left, result);
-		leaves(root->left, result);
-		leaves(root->right, result);
-		rightBoundary(root->right, result);
+		f1(root->left, A);
+		f2(root->left, B);
+		f2(root->right, B);
+		f3(root->right, C);
+		result.insert(end(result), begin(A), end(A));
+		result.insert(end(result), begin(B), end(B));
+		result.insert(end(result), begin(C), end(C));
 		return result;
 	}
+	
 private:
-	void leftBoundary(TreeNode* root, vector<int>& result) {
-		if (root) {
+	void f1(TreeNode * root, vector<int> & A) {
+		if (!root) {
+			return;
+		}
+		while (root->left or root->right) {
+			A.push_back(root->val);
 			if (root->left) {
-				result.push_back(root->val);
-				leftBoundary(root->left, result);
+				root = root->left;
 			}
-			else if (root->right) {
-				result.push_back(root->val);
-				leftBoundary(root->right, result);
+			else {
+				root = root->right;
 			}
 		}
 	}
-	void leaves(TreeNode* root, vector<int>& result) {
-		if (root) {
-			leaves(root->left, result);
-			if (!root->left and !root->right) {
-				result.push_back(root->val);
-			}
-			leaves(root->right, result);
+	void f2(TreeNode * root, vector<int> & B) {
+		if (!root) {
+			return;
 		}
+		if (!root->left and !root->right) {
+			B.push_back(root->val);
+			return;
+		}
+		f2(root->left, B);
+		f2(root->right, B);
 	}
-	void rightBoundary(TreeNode* root, vector<int>& result) {
-		if (root) {
+	void f3(TreeNode * root, vector<int> & C) {
+		if (!root) {
+			return;
+		}
+		while (root->left or root->right) {
+			C.push_back(root->val);
 			if (root->right) {
-				rightBoundary(root->right, result);
-				result.push_back(root->val);
+				root = root->right;
 			}
-			else if (root->left) {
-				rightBoundary(root->left, result);
-				result.push_back(root->val);
+			else {
+				root = root->left;
 			}
 		}
+		reverse(begin(C), end(C));
 	}
 };
-// END: Time Complexity: Linear on size of tree, Space Complexity: Linear on size of tree
-// END: http://www.geeksforgeeks.org/boundary-traversal-of-binary-tree/
 
 int main(void) {
 	Solution solution;
-	TreeNode *root = NULL;
-	vector<int> result, answer;
+	TreeNode * root = NULL;
+	vector<int> result;
 
+	root = new TreeNode(1);
+	result = solution.boundaryOfBinaryTree(root);
+	gc(root);
+	for (const auto & i : result) {
+		cout << i << '\t';
+	}
+	cout << '\n';
+	
 	root = new TreeNode(1);
 	root->right = new TreeNode(2);
 	root->right->left = new TreeNode(3);
 	root->right->right = new TreeNode(4);
-	answer = {1, 3, 4, 2};
 	result = solution.boundaryOfBinaryTree(root);
-	garbageCollection(root);
-	assert(answer == result);
+	gc(root);
+	for (const auto & i : result) {
+		cout << i << '\t';
+	}
+	cout << '\n';
 
 	root = new TreeNode(1);
 	root->left = new TreeNode(2);
@@ -166,11 +163,12 @@ int main(void) {
 	root->left->right->right = new TreeNode(8);
 	root->right->left->left = new TreeNode(9);
 	root->right->left->right = new TreeNode(10);
-	answer = {1, 2, 4, 7, 8, 9, 10, 6, 3};
 	result = solution.boundaryOfBinaryTree(root);
-	garbageCollection(root);
-	assert(answer == result);
+	gc(root);
+	for (const auto & i : result) {
+		cout << i << '\t';
+	}
+	cout << '\n';
 
-	cout << "\nPassed All\n";
 	return 0;
 }
