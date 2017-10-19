504c6ef0354289ae22b7650deecaa02c94974937
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-18 22:21:57 -0700

    365. Water and Jug Problem

--- Algorithms/water-and-jug-problem.cpp
+++ Algorithms/water-and-jug-problem.cpp
@@ -1,37 +1,57 @@
 // 365. Water and Jug Problem
 // https://leetcode.com/problems/water-and-jug-problem/
+
+/*
+You are given two jugs with capacities x and y litres. There is an infinite amount of water supply available. You need to determine whether it is possible to measure exactly z litres using these two jugs.
+
+If z liters of water is measurable, you must have z liters of water contained within one or both buckets by the end.
+
+Operations allowed:
+
+Fill any of the jugs completely with water.
+Empty any of the jugs.
+Pour water from one jug into another till the other jug is completely full or the first jug itself is empty.
+Example 1: (From the famous "Die Hard" example)
+
+Input: x = 3, y = 5, z = 4
+Output: True
+Example 2:
+
+Input: x = 2, y = 6, z = 5
+Output: False
+*/
+
 #include <iostream>
-#include <cassert>
+
 using namespace std;
-// BEGIN: https://www.hrwhisper.me/leetcode-water-jug-problem/
-// BEGIN: https://en.wikipedia.org/wiki/Euclidean_algorithm
+
 class Solution {
 public:
 	bool canMeasureWater(int x, int y, int z) {
-		int w = gcd(x , y);
-		if (w == 0) return z == 0;
-		return x + y >= z && z % w == 0;
+		return z == 0 or (x + y >= z and z % f1(x, y) == 0);
 	}
 private:
-	// int gcd(int a, int b) {
-	// 	return b == 0 ? a : gcd(b, a % b);
-	// }
-	int gcd(int a, int b) {
-		while (b) {
-			int tmp = a % b;
-			a = b;
-			b = tmp;
-		}
-		return a;
+	int f1(int x, int y) {
+		return x == 0 ? y : f1(y % x, x);
 	}
 };
-// END: https://en.wikipedia.org/wiki/Euclidean_algorithm
-// END: https://www.hrwhisper.me/leetcode-water-jug-problem/
+
 int main(void) {
 	Solution solution;
-	assert(true == solution.canMeasureWater(3, 5, 4));
-	assert(false == solution.canMeasureWater(2, 6, 5));
-	assert(true == solution.canMeasureWater(0, 0, 0));
-	cout << "\nPassed All\n";
+	int x, y, z;
+	bool result;
+	
+	x = 3;
+	y = 5;
+	z = 4;
+	result = solution.canMeasureWater(x, y, z);
+	cout << boolalpha << result << '\n';
+
+	x = 2;
+	y = 6;
+	z = 5;
+	result = solution.canMeasureWater(x, y, z);
+	cout << boolalpha << result << '\n';
+
 	return 0;
 }
