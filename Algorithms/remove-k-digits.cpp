8683538ed10f67c8dfc3c747b17c3b3e0607191b
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-20 02:47:10 -0700

    402. Remove K Digits

--- Algorithms/remove-k-digits.cpp
+++ Algorithms/remove-k-digits.cpp
@@ -1,43 +1,103 @@
 // 402. Remove K Digits
 // https://leetcode.com/problems/remove-k-digits/
+
+/*
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+*/
+
 #include <iostream>
-#include <cassert>
 #include <string>
-#include <vector>
+#include <iterator>
+
 using namespace std;
+
 class Solution {
 public:
 	string removeKdigits(string num, int k) {
-		string result = helper(num, k);
-		if (result.empty()) return "0";
-		const int result_size = result.size();
-		int i = 0;
-		for (i = 0; i < result_size && result[i] == '0'; i++) ;
-		if (i == result_size) return "0";
-		return result.substr(i);
-	}
-private:
-	string helper(string num, int k) {
-		if (!k) return num;
-		const int num_size = num.size();
-		if (k >= num_size) return "";
-		int j = 0;
-		for (int i = 0; i <= k && i < num_size; i++) {
-			if (num[j] > num[i]) {
-				j = i;
-			}
+		if (k <= 0) {
+			return num;
 		}
-		string result;
-		result.push_back(num[j]);
-		result += helper(num.substr(j + 1), k - j);
-		return result;
+		int sz = num.size(), i = 0;
+		if (sz <= k) {
+			return "0";
+		}
+		while (k > 0 and !num.empty()) {
+			i = 0;
+			sz = num.size();
+			while (i + 1 < sz and num[i] <= num[i + 1]) {
+				++i;
+			}
+			num.erase(next(begin(num), i));
+			--k;
+		}
+		while (!num.empty() and num[0] == '0') {
+			num.erase(begin(num));
+		}
+		return num.empty() ? "0" : num;
 	}
 };
+
+/*
+class Solution {
+public:
+	string removeKdigits(string num, int k) {
+		int sz = num.size(), i = 0;
+		if (sz <= k) {
+			return "0";
+		}
+		if (k <= 0) {
+			return num;
+		}
+		while (i + 1 < sz and num[i] <= num[i + 1]) {
+			++i;
+		}
+		num.erase(next(begin(num), i));
+		while (!num.empty() and num[0] == '0') {
+			num.erase(begin(num));
+		}
+		return removeKdigits(num, --k);
+	}
+};
+*/
+
 int main(void) {
 	Solution solution;
-	assert("1219" == solution.removeKdigits("1432219", 3));
-	assert("200" == solution.removeKdigits("10200", 1));
-	assert("0" == solution.removeKdigits("10", 2));
-	cout << "\nPassed All\n";
+	string num, result;
+	int k;
+	
+	num = "10200";
+	k = 1;
+	result = solution.removeKdigits(num, k);
+	cout << result << '\n';
+
+	num = "10";
+	k = 1;
+	result = solution.removeKdigits(num, k);
+	cout << result << '\n';
+
+	num = "222222222222222222222210";
+	k = 12;
+	result = solution.removeKdigits(num, k);
+	cout << result << '\n';
+
 	return 0;
 }
