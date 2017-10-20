f836c7582eefed8d21bb1ce657b94702de9e6dce
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-20 02:09:44 -0700

    373. Find K Pairs with Smallest Sums

--- Algorithms/find-k-pairs-with-smallest-sums.cpp
+++ Algorithms/find-k-pairs-with-smallest-sums.cpp
@@ -1,92 +1,124 @@
 // 373. Find K Pairs with Smallest Sums
 // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+
+/*
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+
+Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+Example 1:
+Given nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
+
+Return: [1,2],[1,4],[1,6]
+
+The first 3 pairs are returned from the sequence:
+[1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+Given nums1 = [1,1,2], nums2 = [1,2,3],  k = 2
+
+Return: [1,1],[1,1]
+
+The first 2 pairs are returned from the sequence:
+[1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+Example 3:
+Given nums1 = [1,2], nums2 = [3],  k = 3 
+
+Return: [1,3],[2,3]
+
+All possible pairs are returned from the sequence:
+[1,3],[2,3]
+*/
+
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 #include <utility>
+
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/50481/clean-16ms-c-o-n-space-o-klogn-time-solution-using-priority-queue
-// BEGIN: https://discuss.leetcode.com/topic/50421/c-priority_queue-solution
-// BEGIN: http://stackoverflow.com/questions/3810519/how-to-use-a-lambda-expression-as-a-template-parameter
+
+struct Comp {
+	bool operator() (const pair<int, int> & a, const pair<int, int> & b) {
+		return a.first + a.second < b.first + b.second;
+	}
+};
+
 class Solution {
 public:
 	vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-		vector<pair<int, int>> result;
-		if (nums1.empty() || nums2.empty() || k <= 0) return result;
-		priority_queue<pair<int, int>, vector<pair<int, int>>, comp> max_heap;
-		for (const auto &i : nums1) {
-			for (const auto &j : nums2) {
-				if (max_heap.size() < k) {
-					max_heap.push(make_pair(i, j));
-					continue;
+		int sz1 = nums1.size(), sz2 = nums2.size(), sz3;
+		if (sz1 == 0 or sz2 == 0 or k <= 0) {
+			return {};
+		}
+		priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> A;
+		for (const auto & i : nums1) {
+			for (const auto & j : nums2) {
+				if (A.size() == k) {
+					if (i + j >= A.top().first + A.top().second) {
+						break;
+					}
+					else {
+						A.pop();
+						A.push({i, j});
+					}
 				}
-				if (i + j < max_heap.top().first + max_heap.top().second) {
-					max_heap.pop();
-					max_heap.push(make_pair(i, j));
+				else {
+					A.push({i, j});
 				}
 			}
 		}
-		while (!max_heap.empty()) {
-			result.push_back(max_heap.top());
-			max_heap.pop();
+		sz3 = A.size();
+		vector<pair<int, int>> result(sz3);
+		while (!A.empty()) {
+			result[--sz3] = A.top();
+			A.pop();
 		}
 		return result;
 	}
-private:
-	struct comp {
-		bool operator() (pair<int, int>& a, pair<int, int>& b) {
-			return a.first + a.second < b.first + b.second;
-		}
-	};
 };
-// class Solution {
-// public:
-// 	vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-// 		vector<pair<int, int>> result;
-// 		if (nums1.empty() || nums2.empty() || k <= 0) return result;
-// 		auto comp = [](const pair<int, int>& a, const pair<int, int>& b) -> bool {
-// 			return a.first + a.second < b.first + b.second;
-// 		};
-// 		priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> max_heap(comp);
-// 		for (const auto &i : nums1) {
-// 			for (const auto &j : nums2) {
-// 				if (max_heap.size() < k) {
-// 					max_heap.push(make_pair(i, j));
-// 					continue;
-// 				}
-// 				if (i + j < max_heap.top().first + max_heap.top().second) {
-// 					max_heap.pop();
-// 					max_heap.push(make_pair(i, j));
-// 				}
-// 			}
-// 		}
-// 		while (!max_heap.empty()) {
-// 			result.push_back(max_heap.top());
-// 			max_heap.pop();
-// 		}
-// 		return result;
-// 	}
-// };
-// END: http://stackoverflow.com/questions/3810519/how-to-use-a-lambda-expression-as-a-template-parameter
-// END: https://discuss.leetcode.com/topic/50421/c-priority_queue-solution
-// END: https://discuss.leetcode.com/topic/50481/clean-16ms-c-o-n-space-o-klogn-time-solution-using-priority-queue
+
 int main(void) {
 	Solution solution;
 	vector<int> nums1, nums2;
-	for (const auto&i : solution.kSmallestPairs(nums1, nums2, 0)) cout << i.first << ',' << i.second << '\t';
-	cout << "\tPassed\n";
-	nums1 = {1,7,11};
-	nums2 = {2,4,6};
-	for (const auto&i : solution.kSmallestPairs(nums1, nums2, 3)) cout << i.first << ',' << i.second << '\t';
-	cout << "\tPassed\n";
-	nums1 = {1,1,2};
-	nums2 = {1,2,3};
-	for (const auto&i : solution.kSmallestPairs(nums1, nums2, 2)) cout << i.first << ',' << i.second << '\t';
-	cout << "\tPassed\n";
-	nums1 = {1,2};
+	int k;
+	vector<pair<int, int>> result;
+	
+	nums1 = {1, 7, 11};
+	nums2 = {2, 4, 6};
+	k = 3;
+	result = solution.kSmallestPairs(nums1, nums2, k);
+	for (const auto & i : result) {
+		cout << i.first << ',' << i.second << '\t';
+	}
+	cout << '\n';
+
+	nums1 = {1, 1, 2};
+	nums2 = {1, 2, 3};
+	k = 2;
+	result = solution.kSmallestPairs(nums1, nums2, k);
+	for (const auto & i : result) {
+		cout << i.first << ',' << i.second << '\t';
+	}
+	cout << '\n';
+
+	nums1 = {1, 2};
 	nums2 = {3};
-	for (const auto&i : solution.kSmallestPairs(nums1, nums2, 3)) cout << i.first << ',' << i.second << '\t';
-	cout << "\tPassed\n";
-	cout << "\nPassed All\n";
+	k = 3;
+	result = solution.kSmallestPairs(nums1, nums2, k);
+	for (const auto & i : result) {
+		cout << i.first << ',' << i.second << '\t';
+	}
+	cout << '\n';
+
+	nums1 = {-10, -4, 0, 0, 6};
+	nums2 = {3, 5, 6, 7, 8, 100};
+	k = 10;
+	result = solution.kSmallestPairs(nums1, nums2, k);
+	for (const auto & i : result) {
+		cout << i.first << ',' << i.second << '\t';
+	}
+	cout << '\n';
+
 	return 0;
 }
