c6b6c75e19b32690b459e2c2990ca25e10393c9b
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2017-10-20 22:25:51 -0700

    418. Sentence Screen Fitting

--- Algorithms/sentence-screen-fitting.cpp
+++ Algorithms/sentence-screen-fitting.cpp
@@ -1,90 +1,122 @@
 // 418. Sentence Screen Fitting
 // https://leetcode.com/problems/sentence-screen-fitting/
+
+/*
+Given a rows x cols screen and a sentence represented by a list of non-empty words, find how many times the given sentence can be fitted on the screen.
+
+Note:
+
+A word cannot be split into two lines.
+The order of words in the sentence must remain unchanged.
+Two consecutive words in a line must be separated by a single space.
+Total words in the sentence won't exceed 100.
+Length of each word is greater than 0 and won't exceed 10.
+1 ≤ rows, cols ≤ 20,000.
+Example 1:
+
+Input:
+rows = 2, cols = 8, sentence = ["hello", "world"]
+
+Output: 
+1
+
+Explanation:
+hello---
+world---
+
+The character '-' signifies an empty space on the screen.
+Example 2:
+
+Input:
+rows = 3, cols = 6, sentence = ["a", "bcd", "e"]
+
+Output: 
+2
+
+Explanation:
+a-bcd- 
+e-a---
+bcd-e-
+
+The character '-' signifies an empty space on the screen.
+Example 3:
+
+Input:
+rows = 4, cols = 5, sentence = ["I", "had", "apple", "pie"]
+
+Output: 
+1
+
+Explanation:
+I-had
+apple
+pie-I
+had--
+
+The character '-' signifies an empty space on the screen.
+*/
+
 #include <iostream>
-#include <cassert>
 #include <string>
 #include <vector>
+
 using namespace std;
-// BEGIN: https://discuss.leetcode.com/topic/65173/12ms-java-solution-using-dp
+
 class Solution {
 public:
 	int wordsTyping(vector<string>& sentence, int rows, int cols) {
-		if (sentence.empty() || rows == 0 || cols == 0) return 0;
-		const int n = sentence.size();
-		vector<int> OPT(n, 0);
-		for (int i = 0; i < n; i++) {
-			int j = i;
-			int length = 0;
-			if (length == 0) {
-				length += sentence[j++].size();
-				if (length > cols) return 0;
-			}
-			while (length <= cols) length += sentence[(j++) % n].size() + 1;
-			OPT[i] = j - 1;
+		int sz = sentence.size(), i = 0, result = 0;
+		if (sz == 0 or rows <= 0 or cols <= 0) {
+			return 0;
 		}
-		int result = 0;
-		for (int i = 0, j = 0; i < rows; i++, j = OPT[j % n]) result += OPT[j % n] - j % n;
-		return result /= n;
+		vector<int> A(sz);
+		for (i = 0; i < sz; ++i) {
+			if (cols < sentence[i].size()) {
+				return 0;
+			}
+			A[i] = f1(sentence, cols, i);
+		}
+		for (i = 0; i < rows; ++i) {
+			result += A[result % sz];
+		}
+		return result / sz;
+	}
+private:
+	int f1(const vector<string> & sentence, int cols, int index) {
+		int sz = sentence.size(), result = 1;
+		cols -= sentence[index % sz].size();
+		++index;
+		while (cols >= sentence[index % sz].size() + 1) {
+			cols -= sentence[index % sz].size() + 1;
+			++index;
+			++result;
+		}
+		return result;
 	}
 };
-// END: https://discuss.leetcode.com/topic/65173/12ms-java-solution-using-dp
 
-// BEGIN: Time Limit Exceeded
-// class Solution {
-// public:
-// 	int wordsTyping(vector<string>& sentence, int rows, int cols) {
-// 		int result = 0;
-// 		const int m = sentence.size();
-// 		if (m == 1) {
-// 			const int n = sentence[0].size();
-// 			if (n > cols) return 0;
-// 			if (n == cols) return rows;
-// 			if (cols & 0b01) return rows * ((cols + 1) >> 1); 
-// 			else return rows * (cols >> 1);
-// 		}
-// 		int remain = cols;
-// 		while (rows > 0) {
-// 			int i = 0;
-// 			for (i = 0; rows > 0 && i < m; i++) {
-// 				const int n = sentence[i].size();
-// 				if (n > cols) return 0;
-// 				if (remain == cols) {
-// 					remain -= n;
-// 					continue;
-// 				}
-// 				if (remain < n + 1) {
-// 					remain = cols;
-// 					rows--;
-// 					i--;
-// 					continue;
-// 				}
-// 				if (remain == n + 1) {
-// 					remain = cols;
-// 					rows--;
-// 					continue;
-// 				}
-// 				if (remain > n + 1) {
-// 					remain -= n + 1;
-// 					continue;
-// 				}
-// 			}
-// 			if (i == m) result++;
-// 		}
-// 		return result;
-// 	}
-// };
-// END: Time Limit Exceeded
 int main(void) {
 	Solution solution;
 	vector<string> sentence;
+	int rows, cols, result;
+	
 	sentence = {"hello", "world"};
-	assert(1 == solution.wordsTyping(sentence, 2, 8));
+	rows = 2;
+	cols = 8;
+	result = solution.wordsTyping(sentence, rows, cols);
+	cout << result << '\n';
+
 	sentence = {"a", "bcd", "e"};
-	assert(2 == solution.wordsTyping(sentence, 3, 6));
+	rows = 3;
+	cols = 6;
+	result = solution.wordsTyping(sentence, rows, cols);
+	cout << result << '\n';
+
 	sentence = {"I", "had", "apple", "pie"};
-	assert(1 == solution.wordsTyping(sentence, 4, 5));
-	sentence = {"a"};
-	assert(200000000 == solution.wordsTyping(sentence, 20000, 20000));
-	cout << "\nPassed All\n";
+	rows = 4;
+	cols = 6;
+	result = solution.wordsTyping(sentence, rows, cols);
+	cout << result << '\n';
+
 	return 0;
 }
