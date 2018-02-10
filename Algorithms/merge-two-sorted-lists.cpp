63915949083004adda83df7df1227fed5b0d8fb1
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-09 20:59:21 -0800

    21. Merge Two Sorted Lists

--- Algorithms/merge-two-sorted-lists.cpp
+++ Algorithms/merge-two-sorted-lists.cpp
@@ -1,99 +1,44 @@
-// 21. Merge Two Sorted Lists
-// https://leetcode.com/problems/merge-two-sorted-lists/
+21. Merge Two Sorted Lists
+https://leetcode.com/problems/merge-two-sorted-lists/
 
-/*
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
-*/
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
-#include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
-using namespace std;
+Example:
 
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-};
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
 
-void gc(ListNode*& l) {
-	if (l) {
-		gc(l->next);
-		delete l;
-		l = NULL;
-	}
-}
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-		ListNode dummy(0);
-		for (ListNode *l = &dummy; l1 or l2; l = l->next) {
-			if (l1 and l2) {
-				if (l1->val < l2->val) {
-					l->next = l1;
-					l1 = l1->next;
-				}
-				else {
-					l->next = l2;
-					l2 = l2->next;
-				}
-			}
-			else if (l1) {
-				l->next = l1;
-				l1 = l1->next;
-			}
-			else {
-				l->next = l2;
-				l2 = l2->next;
-			}
-		}
-		return dummy.next;
-	}
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode * dummy = new ListNode(-1), * node = dummy;
+        while (l1 or l2) {
+            if (!l1) {
+                node->next = l2;
+                l2 = l2->next;
+            }
+            else if (!l2) {
+                node->next = l1;
+                l1 = l1->next;
+            }
+            else if (l1->val < l2->val) {
+                node->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                node->next = l2;
+                l2 = l2->next;
+            }
+            node = node->next;
+        }
+        return dummy->next;
+    }
 };
-
-int main(void) {
-	Solution solution;
-	ListNode *l1, *l2, *l3;
-	vector<int> result, answer;
-
-	l1 = new ListNode(1);
-	l1->next = new ListNode(2);
-	l1->next->next = new ListNode(3);
-	l2 = new ListNode(2);
-	l2->next = new ListNode(3);
-	l2->next->next = new ListNode(4);
-	l3 = solution.mergeTwoLists(l1, l2);
-	for (ListNode *i = l3; i; i = i->next) {
-		result.push_back(i->val);
-	}
-	answer = {1, 2, 2, 3, 3, 4};
-	assert(answer == result);
-	gc(l3);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
