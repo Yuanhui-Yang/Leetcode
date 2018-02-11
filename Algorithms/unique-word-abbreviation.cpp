64ac88939f8027ad3d6fa21d7bf85e65faaea3a1
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-11 02:50:08 -0800

    288. Unique Word Abbreviation

--- Algorithms/unique-word-abbreviation.cpp
+++ Algorithms/unique-word-abbreviation.cpp
@@ -1,70 +1,67 @@
-// 288. Unique Word Abbreviation
-// https://leetcode.com/problems/unique-word-abbreviation/
+288. Unique Word Abbreviation
+https://leetcode.com/problems/unique-word-abbreviation/
 
-/*
 An abbreviation of a word follows the form <first letter><number><last letter>. Below are some examples of word abbreviations:
 
 a) it                      --> it    (no abbreviation)
 
-	 1
+     1
 b) d|o|g                   --> d1g
 
-			  1    1  1
-	 1---5----0----5--8
+              1    1  1
+     1---5----0----5--8
 c) i|nternationalizatio|n  --> i18n
 
-			  1
-	 1---5----0
+              1
+     1---5----0
 d) l|ocalizatio|n          --> l10n
 Assume you have a dictionary and given a word, find whether its abbreviation is unique in the dictionary. A word's abbreviation is unique if no other word from the dictionary has the same abbreviation.
 
 Example: 
 Given dictionary = [ "deer", "door", "cake", "card" ]
 
-isUnique("dear") -> 
-false
+isUnique("dear") -> false
+isUnique("cart") -> true
+isUnique("cane") -> false
+isUnique("make") -> true
 
-isUnique("cart") -> 
-true
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
-isUnique("cane") -> 
-false
-
-isUnique("make") -> 
-true
-*/
+using namespace std;
 
 class ValidWordAbbr {
 public:
-	ValidWordAbbr(vector<string> dictionary) {
-		for (const auto & i : dictionary) {
-			string s = f(i);
-			if (A.count(s) and A[s] != i) {
-				A[s] = "";
-			}
-			else {
-				A[s] = i;
-			}
-		}
-	}
-	
-	bool isUnique(string word) {
-		string s = f(word);
-		return !A.count(s) or A[s] == word;
-	}
+    ValidWordAbbr(vector<string> dictionary) {
+        A.clear();
+        for (const auto & i : dictionary) {
+            string abbr = f1(i);
+            if (A.count(abbr)) {
+                if (A[abbr] != i) {
+                    A[abbr].clear();
+                }
+            }
+            else {
+                A[abbr] = i;
+            }
+        }
+    }
+    
+    bool isUnique(string word) {
+        string abbr = f1(word);
+        return !A.count(abbr) or A[abbr] == word;
+    }
 private:
-	unordered_map<string, string> A;
-	string f(const string & s) {
-		int sz = s.size();
-		if (sz <= 2) {
-			return s;
-		}
-		string result;
-		result.push_back(s.front());
-		result.append(to_string(sz - 2));
-		result.push_back(s.back());
-		return result;
-	}
+    unordered_map<string, string> A;
+    string f1(const string & word) {
+        int sz = word.size();
+        if (sz <= 2) {
+            return word;
+        }
+        return word.front() + to_string(sz - 2) + word.back();
+    }
 };
 
 /**
@@ -72,3 +69,18 @@ private:
  * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
  * bool param_1 = obj.isUnique(word);
  */
+
+ int main(void) {
+    vector<string> dictionary({"deer", "door", "cake", "card"});
+    ValidWordAbbr obj(dictionary);
+    bool result;
+    result = obj.isUnique("dear");
+    cout << boolalpha << result << '\n';
+    result = obj.isUnique("cart");
+    cout << boolalpha << result << '\n';
+    result = obj.isUnique("cane");
+    cout << boolalpha << result << '\n';
+    result = obj.isUnique("make");
+    cout << boolalpha << result << '\n';
+    return 0;
+ }
