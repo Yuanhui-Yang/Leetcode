39d863174927334072478380979d91cac3f93460
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-14 03:28:47 -0800

    747. Largest Number At Least Twice of Others

--- Algorithms/largest-number-at-least-twice-of-others.cpp
+++ Algorithms/largest-number-at-least-twice-of-others.cpp
@@ -1,7 +1,6 @@
-// 747. Largest Number Greater Than Twice of Others
-// https://leetcode.com/problems/largest-number-greater-than-twice-of-others/
+747. Largest Number At Least Twice of Others
+https://leetcode.com/problems/largest-number-at-least-twice-of-others/
 
-/*
 In a given integer array nums, there is always exactly one largest element.
 
 Find whether the largest element in the array is at least twice as much as every other number in the array.
@@ -9,18 +8,24 @@ Find whether the largest element in the array is at least twice as much as every
 If it is, return the index of the largest element, otherwise return -1.
 
 Example 1:
+
 Input: nums = [3, 6, 1, 0]
 Output: 1
 Explanation: 6 is the largest integer, and for every other number in the array x,
 6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
+
 Example 2:
+
 Input: nums = [1, 2, 3, 4]
 Output: -1
 Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ 
+
 Note:
+
 nums will have a length in the range [1, 50].
 Every nums[i] will be an integer in the range [0, 99].
-*/
 
 #include <iostream>
 #include <vector>
@@ -30,11 +35,14 @@ using namespace std;
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int sz = nums.size(), i = 0, x = -1, y = -1;
-        if (sz <= 1) {
+        int sz = nums.size(), x = -1, y = -1;
+        if (sz == 0) {
             return -1;
         }
-        for (i = 0; i < sz; ++i) {
+        if (sz == 1) {
+            return 0;
+        }
+        for (int i = 0; i < sz; ++i) {
             if (x < 0) {
                 x = i;
             }
@@ -48,7 +56,7 @@ public:
                 }
             }
             else {
-                if (nums[x] <= nums[i]) {
+                if (nums[x] < nums[i]) {
                     y = x;
                     x = i;
                 }
@@ -57,7 +65,7 @@ public:
                 }
             }
         }
-        return nums[x] >= 2 * nums[y] ? x : -1;
+        return 2 * nums[y] <= nums[x] ? x : -1;
     }
 };
 
