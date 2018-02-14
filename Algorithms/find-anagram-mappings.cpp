662f5c263d644d864f4a788ad9ca1b5e42832730
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-14 15:42:19 -0800

    760. Find Anagram Mappings

--- Algorithms/find-anagram-mappings.cpp
+++ Algorithms/find-anagram-mappings.cpp
@@ -22,7 +22,6 @@ A[i], B[i] are integers in range [0, 10^5].
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <queue>
 
 using namespace std;
 
@@ -30,17 +29,13 @@ class Solution {
 public:
     vector<int> anagramMappings(vector<int>& A, vector<int>& B) {
         vector<int> result;
-        unordered_map<int, queue<int>> C;
-        int sz, i, key;
-        sz = B.size();
+        unordered_map<int, int> C;
+        int sz = A.size(), i;
         for (i = 0; i < sz; ++i) {
-            key = B[i];
-            C[key].push(i);
+            C[B[i]] = i;
         }
-        for (const auto & i : A) {
-            queue<int> & q = C[i];
-            result.push_back(q.front());
-            q.pop();
+        for (i = 0; i < sz; ++i) {
+            result.push_back(C[A[i]]);
         }
         return result;
     }
@@ -48,14 +43,15 @@ public:
 
 int main(void) {
     Solution solution;
-    vector<int> A, B, result;
+    vector<int> A, B, C;
     
     A = {12, 28, 46, 32, 50};
     B = {50, 12, 32, 46, 28};
-    result = solution.anagramMappings(A, B);
-    for (const auto & i : result) {
-        cout << i << '\n';
+    C = solution.anagramMappings(A, B);
+    for (const auto & i : C) {
+        cout << i << '\t';
     }
+    cout << '\n';
     
     return 0;
 }
