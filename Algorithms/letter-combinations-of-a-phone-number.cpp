12789ab63518a467aa0c944c0329e4e0b5ae7575
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-17 22:36:32 -0800

    17. Letter Combinations of a Phone Number

--- Algorithms/letter-combinations-of-a-phone-number.cpp
+++ Algorithms/letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,6 @@
-// 17. Letter Combinations of a Phone Number
-// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+17. Letter Combinations of a Phone Number
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
-/*
 Given a digit string, return all possible letter combinations that the number could represent.
 
 A mapping of digit to letters (just like on the telephone buttons) is given below.
@@ -12,60 +11,60 @@ Input:Digit string "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-	vector<string> letterCombinations(string digits) {
-		if (digits.empty()) {
-			return {};
-		}
-		vector<string> table = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, result;
-		string s;
-		dfs(result, table, digits, s, 0, digits.size());
-		return result;
-	}
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.empty()) {
+            return result;
+        }
+        string path;
+        vector<string> A;
+        A.push_back("abc");
+        A.push_back("def");
+        A.push_back("ghi");
+        A.push_back("jkl");
+        A.push_back("mno");
+        A.push_back("pqrs");
+        A.push_back("tuv");
+        A.push_back("wxyz");
+        f1(result, path, A, digits, 0);
+        return result;
+    }
 private:
-	void dfs(vector<string>& result, const vector<string>& table, const string& digits, string& s, size_t i, const size_t n) {
-		if (s.size() == n) {
-			result.push_back(s);
-			return;
-		}
-		for (size_t j = i; j < n; j++) {
-			const int id = digits[j] - '2';
-			const string& t = table[id];
-			for (const auto &k : t) {
-				s.push_back(k);
-				dfs(result, table, digits, s, j + 1, n);
-				s.pop_back();
-			}
-		}
-	}
+    void f1(vector<string> & result, string & path, vector<string> & A, string & digits, int begin) {
+        int end = digits.size();
+        if (begin == end) {
+            result.push_back(path);
+            return;
+        }
+        int id = digits[begin] - '2';
+        for (const auto & i : A[id]) {
+            path.push_back(i);
+            f1(result, path, A, digits, begin + 1);
+            path.pop_back();
+        }
+    }
 };
 
 int main(void) {
-	Solution solution;
-
-	string digits;
-	vector<string> answer, result;
-
-	digits = "";
-	answer = {};
-	sort(begin(answer), end(answer));
-	result = solution.letterCombinations(digits);
-	sort(begin(result), end(result));
-	assert(answer == result);
-
-	digits = "23";
-	answer = {"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
-	sort(begin(answer), end(answer));
-	result = solution.letterCombinations(digits);
-	sort(begin(result), end(result));
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
+    Solution solution;
+    string digits;
+    vector<string> result;
+    
+    digits = "23";
+    result = solution.letterCombinations(digits);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
+    
+    return 0;
 }
