447e88645d5f4233cc3543f47b93089766e385d1
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-17 23:14:21 -0800

    56. Merge Intervals

--- Algorithms/merge-intervals.cpp
+++ Algorithms/merge-intervals.cpp
@@ -1,42 +1,62 @@
-// 56. Merge Intervals
-// https://leetcode.com/problems/merge-intervals/
+56. Merge Intervals
+https://leetcode.com/problems/merge-intervals/
 
-/*
 Given a collection of intervals, merge all overlapping intervals.
 
 For example,
 Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18].
-*/
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Interval {
+    int start, end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+struct Comp {
+    bool operator() (const Interval & a, const Interval & b) {
+        if (a.start == b.start) {
+            return a.end < b.end;
+        }
+        return a.start < b.start;
+    }
+};
+
 class Solution {
 public:
-	vector<Interval> merge(vector<Interval>& intervals) {
-		sort(begin(intervals), end(intervals), Comp());
-		vector<Interval> result;
-		for (int sz = intervals.size(), i = 0; i < sz; ++i) {
-			int a = intervals[i].start, b = intervals[i].end;
-			while (i + 1 < sz and intervals[i + 1].start <= b) {
-				++i;
-				b = max(b, intervals[i].end);
-			}
-			result.push_back({a, b});
-		}
-		return result;
-	}
-private:
-	struct Comp {
-		bool operator () (const Interval & a, const Interval & b) {
-			return a.start < b.start;
-		}
-	};
+    vector<Interval> merge(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), Comp());
+        int sz = intervals.size(), i = 0;
+        vector<Interval> result;
+        while (i < sz) {
+            int a = intervals[i].start, b = intervals[i].end;
+            while (i + 1 < sz and intervals[i + 1].start <= b) {
+                b = max(b, intervals[i + 1].end);
+                ++i;
+            }
+            result.push_back(Interval(a, b));
+            ++i;
+        }
+        return result;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<Interval> intervals, result;
+    
+    intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+    result = solution.merge(intervals);
+    for (const auto & i : result) {
+        cout << i.start << ',' << i.end << '\t';
+    }
+    cout << '\n';
+    
+    return 0;
+}
