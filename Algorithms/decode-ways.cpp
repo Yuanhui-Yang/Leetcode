d2bb30390d8ca1b6f7de2fc8f2a842a69d3ef6cc
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-19 07:19:33 -0800

    91. Decode Ways

--- Algorithms/decode-ways.cpp
+++ Algorithms/decode-ways.cpp
@@ -1,7 +1,6 @@
-// 91. Decode Ways
-// https://leetcode.com/problems/decode-ways/
+91. Decode Ways
+https://leetcode.com/problems/decode-ways/
 
-/*
 A message containing letters from A-Z is being encoded to numbers using the following mapping:
 
 'A' -> 1
@@ -14,107 +13,90 @@ For example,
 Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 
 The number of ways decoding "12" is 2.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-	int numDecodings(string s) {
-		if (s.empty()) {
-			return 0;
-		}
-		int n = s.size(), x = s.back() >= '1', y = 1;
-		for (int i = n - 2; i >= 0; --i) {
-			if (s[i] == '0') {
-				y = x;
-				x = 0;
-				continue;
-			}
-			bool z = s[i] == '1' or (s[i] == '2' and s[i + 1] <= '6');
-			int t = x;
-			x += z ? y : 0;
-			y = t;
-		}
-		return x;
-	}
+    int numDecodings(string s) {
+        int sz = s.size(), i = sz - 1;
+        if (sz == 0) {
+            return 0;
+        }
+        vector<int> A(sz + 1);
+        A[sz] = 1;
+        while (i >= 0) {
+            if (s[i] == '0') {
+                A[i] = 0;
+            }
+            else if (i + 2 <= sz) {
+                int x = stoi(s.substr(i, 2));
+                if (x <= 26) {
+                    A[i] = A[i + 1] + A[i + 2];
+                }
+                else {
+                    A[i] = A[i + 1];
+                }
+            }
+            else {
+                A[i] = A[i + 1];
+            }
+            --i;
+        }
+        return A[0];
+    }
 };
 
-// class Solution {
-// public:
-// 	int numDecodings(string s) {
-// 		if (s.empty()) {
-// 			return 0;
-// 		}
-// 		int n = s.size();
-// 		vector<int> M(n + 1, 0);
-// 		M.back() = 1;
-// 		for (int i = n - 1; i >= 0; --i) {
-// 			if (s[i] == '0') {
-// 				M[i] = 0;
-// 				continue;
-// 			}
-// 			if (i + 1 == n) {
-// 				M[i] = s[i] >= '1';
-// 				continue;
-// 			}
-// 			bool x = s[i] == '1' or (s[i] == '2' and s[i + 1] <= '6');
-// 			M[i] = M[i + 1] + (x ? M[i + 2] : 0);
-// 		}
-// 		return M.front();
-// 	}
-// };
-
 int main(void) {
-	Solution solution;
-	string s;
-	int answer, result;
+    Solution solution;
+    string s;
+    int answer, result;
 
-	s = "";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "";
+    answer = 0;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "12";
-	answer = 2;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "12";
+    answer = 2;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "27";
-	answer = 1;
-	result = solution.numDecodings(s);
+    s = "27";
+    answer = 1;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "00";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "00";
+    answer = 0;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "01";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "01";
+    answer = 0;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "012";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "012";
+    answer = 0;
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "111";
-	answer = 3;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "111";
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "230";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "230";
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	s = "6065812287883668764831544958683283296479682877898293612168136334983851946827579555449329483852397155";
-	answer = 0;
-	result = solution.numDecodings(s);
-	assert(answer == result);
+    s = "6065812287883668764831544958683283296479682877898293612168136334983851946827579555449329483852397155";
+    result = solution.numDecodings(s);
+    cout << result << '\n';
 
-	cout << "\nPassed All\n";
-	return 0;
+    return 0;
 }
