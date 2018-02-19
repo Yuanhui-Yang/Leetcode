71336b8860cf09405f21242947869b230b6e87e8
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-18 21:32:19 -0800

    29. Divide Two Integers

--- Algorithms/divide-two-integers.cpp
+++ Algorithms/divide-two-integers.cpp
@@ -1,46 +1,62 @@
-// 29. Divide Two Integers
-// https://leetcode.com/problems/divide-two-integers/
+29. Divide Two Integers
+https://leetcode.com/problems/divide-two-integers/
 
-/*
 Divide two integers without using multiplication, division and mod operator.
 
 If it is overflow, return MAX_INT.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+
 using namespace std;
 
 class Solution {
 public:
-	int divide(int dividend, int divisor) {
-		if (divisor == 0) {
-			return INT_MAX;
-		}
-		long long a = llabs((long long)dividend), b = llabs((long long)divisor), result = 0, sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
-		while (a >= b) {
-			long long c = b, d = 1;
-			while (a >= (c << 1)) {
-				c <<= 1;
-				d <<= 1;
-			}
-			a -= c;
-			result += d;
-		}
-		result *= sign;
-		return (result < (long long)INT_MIN) ? INT_MIN : (result > (long long)INT_MAX) ? INT_MAX : result;
-	}
+    int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            return INT_MAX;
+        }
+        long c = f1(dividend, divisor);
+        return c >= INT_MAX ? INT_MAX : c <= INT_MIN ? INT_MIN : c;
+    }
+private:
+    long f1(long a, long b) {
+        if (a < 0) {
+            return -f1(-a, b);
+        }
+        if (b < 0) {
+            return -f1(a, -b);
+        }
+        long x = 0, y = a + 1;
+        while (x < y) {
+            long z = x + (y - x) / 2;
+            if (f2(a, b, z)) {
+                x = z + 1;
+            }
+            else {
+                y = z;
+            }
+        }
+        return --x;
+    }
+    bool f2(long a, long b, long z) {
+        return b * z <= a;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	int dividend, divisor, answer, result;
+    Solution solution;
+    int dividend, divisor, result;
+    
+    dividend = -2147483648;
+    divisor = 1;
+    result = solution.divide(dividend, divisor);
+    cout << result << '\n';
 
-	dividend = -2147483647;
-	divisor = -1;
-	answer = 2147483647;
-	result = solution.divide(dividend, divisor);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
+    dividend = 1;
+    divisor = 2;
+    result = solution.divide(dividend, divisor);
+    cout << result << '\n';
+    
+    return 0;
 }
