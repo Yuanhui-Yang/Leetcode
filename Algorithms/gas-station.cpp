50b245e63bad0d4b6155e6b02bb4627964d7d5d3
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-22 00:46:24 -0800

    134. Gas Station

--- Algorithms/gas-station.cpp
+++ Algorithms/gas-station.cpp
@@ -1,7 +1,6 @@
-// 134. Gas Station
-// https://leetcode.com/problems/gas-station/
+134. Gas Station
+https://leetcode.com/problems/gas-station/
 
-/*
 There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
 
 You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
@@ -10,57 +9,54 @@ Return the starting gas station's index if you can travel around the circuit onc
 
 Note:
 The solution is guaranteed to be unique.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-		int n = gas.size(), i = 0;
-		while (i < n) {
-			int b = 0, j = 0;
-			for (j = 0;  j < n; ++j) {
-				int k = i + j;
-				k %= n;
-				b += gas[k] - cost[k];
-				if (b < 0) {
-					break;
-				}
-			}
-			if (j >= n) {
-				return i;
-			}
-			i += j + 1;
-		}
-		return -1;
-	}
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sz = gas.size(), i = 0;
+        while (i < sz) {
+            int sum = 0, j = 0;
+            while (j < sz and sum >= 0) {
+                int k = i + j;
+                k %= sz;
+                sum += gas[k] - cost[k];
+                ++j;
+            }
+            if (sum < 0) {
+                i += j;
+            }
+            else {
+                return i;
+            }
+        }
+        return -1;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<int> gas, cost;
-	int answer, result;
+    Solution solution;
+    vector<int> gas, cost;
+    int result;
 
-	gas = {5};
-	cost = {4};
-	answer = 0;
-	result = solution.canCompleteCircuit(gas, cost);
-	assert(answer == result);
+    gas = {5};
+    cost = {4};
+    result = solution.canCompleteCircuit(gas, cost);
+    cout << result << '\n';
 
-	gas = {4};
-	cost = {5};
-	answer = -1;
-	result = solution.canCompleteCircuit(gas, cost);
-	assert(answer == result);
+    gas = {4};
+    cost = {5};
+    result = solution.canCompleteCircuit(gas, cost);
+    cout << result << '\n';
 
-	gas = {1, 2};
-	cost = {2, 1};
-	answer = 1;
-	result = solution.canCompleteCircuit(gas, cost);
-	assert(answer == result);
+    gas = {1, 2};
+    cost = {2, 1};
+    result = solution.canCompleteCircuit(gas, cost);
+    cout << result << '\n';
 
-	cout << "\nPassed All\n";
-	return 0;
+    return 0;
 }
