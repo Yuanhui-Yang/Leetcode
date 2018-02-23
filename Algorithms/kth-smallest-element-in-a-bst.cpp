030a6906316600127a658cda90df38952cdccfb2
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-23 08:51:13 -0800

    230. Kth Smallest Element in a BST

--- Algorithms/kth-smallest-element-in-a-bst.cpp
+++ Algorithms/kth-smallest-element-in-a-bst.cpp
@@ -1,83 +1,79 @@
-// 230. Kth Smallest Element in a BST
-// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+230. Kth Smallest Element in a BST
+https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 
-/*
 Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
 
 Note: 
-You may assume k is always valid, 1 ? k ? BST's total elements.
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
 
 Follow up:
 What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
-*/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
-public:
-	int kthSmallest(TreeNode* root, int k) {
-		int result;
-		stack<TreeNode*> s;
-		while (k > 0 and (root or !s.empty())) {
-			if (root) {
-				s.push(root);
-				root = root->left;
-			}
-			else {
-				TreeNode * node = s.top();
-				s.pop();
-				result = node->val;
-				--k;
-				root = node->right;
-			}
-		}
-		return result;
-	}
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-	int kthSmallest(TreeNode* root, int k) {
-		int result = 0;
-		while (root and k > 0) {
-			if (!root->left) {
-				result = root->val;
-				root = root->right;
-				--k;
-			}
-			else {
-				TreeNode * pred = root->left;
-				while (pred->right and pred->right != root) {
-					pred = pred->right;
-				}
-				if (!pred->right) {
-					pred->right = root;
-					root = root->left;
-				}
-				else {
-					result = root->val;
-					pred->right = NULL;
-					root = root->right;
-					--k;
-				}
-			}
-		}
-		return result;
-	}
+    int kthSmallest(TreeNode* root, int k) {
+        int result = -1;
+        root = clone(root);
+        while (root and k > 0) {
+            if (root->left) {
+                TreeNode * pred = root->left;
+                while (pred->right and pred->right != root) {
+                    pred = pred->right;
+                }
+                if (pred->right == root) {
+                    --k;
+                    result = root->val;
+                    root = root->right;
+                    pred->right = NULL;
+                }
+                else {
+                    pred->right = root;
+                    root = root->left;
+                }
+            }
+            else {
+                --k;
+                result = root->val;
+                root = root->right;
+            }
+        }
+        return result;
+    }
+private:
+    TreeNode * clone(TreeNode * root) {
+        TreeNode * result = NULL;
+        if (root) {
+            result = new TreeNode(root->val);
+            result->left = clone(root->left);
+            result->right = clone(root->right);
+        }
+        return result;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    
+    TreeNode * root = NULL;
+    int k, result;
+    
+    root = new TreeNode(2);
+    root->left = new TreeNode(1);
+    k = 1;
+    result = solution.kthSmallest(root, k);
+    cout << result << '\n';
+    
+  
+    return 0;
+}
