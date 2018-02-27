fce53823eefc57a18b923989ffeeffd38c54dcb7
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-02-27 02:17:11 -0800

    313. Super Ugly Number

--- Algorithms/super-ugly-number.cpp
+++ Algorithms/super-ugly-number.cpp
@@ -1,7 +1,6 @@
-// 313. Super Ugly Number
-// https://leetcode.com/problems/super-ugly-number/
+313. Super Ugly Number
+https://leetcode.com/problems/super-ugly-number/
 
-/*
 Write a program to find the nth super ugly number.
 
 Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
@@ -11,57 +10,50 @@ Note:
 (2) The given numbers in primes are in ascending order.
 (3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.
 (4) The nth super ugly number is guaranteed to fit in a 32-bit signed integer.
-*/
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <queue>
+
+using namespace std;
 
 class Solution {
 public:
-	int nthSuperUglyNumber(size_t n, vector<int>& primes) {
-		sort(begin(primes), end(primes));
-		long long k = primes.size();
-		vector<long long> A(k, 0LL), B;
-		priority_queue<array<long long, 2>, vector<array<long long, 2>>, greater<array<long long, 2>>> min_heap;
-		for (long long i = 0; i < k; ++i) {
-			min_heap.push({primes[i], i});
-		}
-		B.push_back(1LL);
-		while (B.size() < n) {
-			array<long long, 2> top = min_heap.top();
-			min_heap.pop();
-			if (B.back() < top[0]) {
-				B.push_back(top[0]);
-			}
-			++A[top[1]];
-			min_heap.push({primes[top[1]] * B[A[top[1]]], top[1]});
-		}
-		return B.back();
-	}
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int sz = primes.size();
+        if (n < 1 or sz < 1) {
+            return 0;
+        }
+        vector<int> A, B(sz, 0);
+        A.push_back(1);
+        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>> pq;
+        for (int i = 0; i < sz; ++i) {
+            pq.push({primes[i], i});
+        }
+        for (int i = 1; i < n; ++i) {
+            int val = pq.top()[0];
+            A.push_back(val);
+            while (!pq.empty() and pq.top()[0] == val) {
+                int id = pq.top()[1];
+                pq.pop();
+                ++B[id];
+                pq.push({primes[id] * A[B[id]], id});
+            }
+        }
+        return A.back();
+    }
 };
 
-class Solution {
-public:
-	int nthSuperUglyNumber(size_t n, vector<int>& primes) {
-		sort(begin(primes), end(primes));
-		size_t k = primes.size();
-		vector<int> A(k, 0);
-		vector<long long> B;
-		while (B.size() < n) {
-			if (B.empty()) {
-				B.push_back(1LL);
-				continue;
-			}
-			size_t i = 0;
-			long long minVal = primes[i] * B[A[i]];
-			for (size_t j = 1; j < k; ++j) {
-				if (primes[j] * B[A[j]] < minVal) {
-					i = j;
-					minVal = primes[i] * B[A[i]];
-				}
-			}
-			if (B.back() < minVal) {
-				B.push_back(minVal);
-			}
-			++A[i];
-		}
-		return B.back();
-	}
-};
+int main(void) {
+    Solution solution;
+    vector<int> primes;
+    int n, result;
+    
+    primes = {2, 7, 13, 19};
+    n = 12;
+    result = solution.nthSuperUglyNumber(n, primes);
+    cout << result << '\n';
+    
+    return 0;
+}
