5da74a7a4a1a0da007cecbfa73de3c7aab3fed3f
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-11 22:35:59 -0700

    91. Decode Ways

--- Algorithms/decode-ways.cpp
+++ Algorithms/decode-ways.cpp
@@ -16,85 +16,47 @@ The number of ways decoding "12" is 2.
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int numDecodings(string s) {
-        int sz = s.size(), i = sz - 1;
+        int sz = s.size();
         if (sz == 0) {
             return 0;
         }
-        vector<int> A(sz + 1);
-        A[sz] = 1;
+        int curr = s[sz - 1] != '0', prev = 1, i = sz - 2;
         while (i >= 0) {
             if (s[i] == '0') {
-                A[i] = 0;
-            }
-            else if (i + 2 <= sz) {
-                int x = stoi(s.substr(i, 2));
-                if (x <= 26) {
-                    A[i] = A[i + 1] + A[i + 2];
-                }
-                else {
-                    A[i] = A[i + 1];
-                }
+                prev = curr;
+                curr = 0;
             }
             else {
-                A[i] = A[i + 1];
+                int val = stoi(s.substr(i, 2));
+                if (val <= 26) {
+                    int next = curr + prev;
+                    prev = curr;
+                    curr = next;
+                }
+                else {
+                    int next = curr;
+                    prev = curr;
+                    curr = next;
+                }
             }
             --i;
-        }
-        return A[0];
+        } 
+        return curr;
     }
 };
 
 int main(void) {
     Solution solution;
     string s;
-    int answer, result;
-
-    s = "";
-    answer = 0;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
+    int result;
 
     s = "12";
-    answer = 2;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "27";
-    answer = 1;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "00";
-    answer = 0;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "01";
-    answer = 0;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "012";
-    answer = 0;
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "111";
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "230";
-    result = solution.numDecodings(s);
-    cout << result << '\n';
-
-    s = "6065812287883668764831544958683283296479682877898293612168136334983851946827579555449329483852397155";
     result = solution.numDecodings(s);
     cout << result << '\n';
 
