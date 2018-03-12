e0169ae3986e9288222af8e68ebed6aaeb0d90b0
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-11 23:20:10 -0700

    50. Pow(x, n)

--- Algorithms/powx-n.cpp
+++ Algorithms/powx-n.cpp
@@ -1,46 +1,49 @@
-// 50. Pow(x, n)
-// https://leetcode.com/problems/powx-n/
+50. Pow(x, n)
+https://leetcode.com/problems/powx-n/
 
-/*
 Implement pow(x, n).
-*/
 
-#include <bits/stdc++.h>
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+
+#include <iostream>
+
 using namespace std;
 
 class Solution {
 public:
-	double myPow(double x, int n) {
-		return f(x, n);
-	}
+    double myPow(double x, int n) {
+        return f1(x, n);
+    }
 private:
-	long double f(long double x, long long n) {
-		if (x == 0 or n == 1) {
-			return x;
-		}
-		if (x == 1 or n == 0) {
-			return 1;
-		}
-		if (n < 0) {
-			return 1 / f(x, -n);
-		}
-		long double y = f(x, n / 2);
-		y *= y;
-		return (n & 1) ? x * y : y;
-	}
+    double f1(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 1 / f1(x, -n);
+        }
+        double y = f1(x, n / 2);
+        return n % 2 ? x * y * y : y * y;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	double x, answer, result;
-	int n;
+    Solution solution;
+    double x, answer, result;
+    int n;
 
-	x = 2.00000;
-	n = -2147483648;
-	answer = 0;
-	result = solution.myPow(x, n);
-	assert(fabs(answer - result) < numeric_limits<double>::epsilon());
-
-	cout << "\nPassed All\n";
-	return 0;
+    x = 2.00000;
+    n = -2147483648;
+    result = solution.myPow(x, n);
+    cout << result << '\n';
+    
+    return 0;
 }
