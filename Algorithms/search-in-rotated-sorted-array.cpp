688e818f1ab9f13bb700c6dfcc2b8a9553e95c6e
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-11 20:25:54 -0700

    33. Search in Rotated Sorted Array

--- Algorithms/search-in-rotated-sorted-array.cpp
+++ Algorithms/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,6 @@
-// 33. Search in Rotated Sorted Array
-// https://leetcode.com/problems/search-in-rotated-sorted-array/
+33. Search in Rotated Sorted Array
+https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-/*
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
 (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
@@ -9,67 +8,54 @@ Suppose an array sorted in ascending order is rotated at some pivot unknown to y
 You are given a target value to search. If found in the array return its index, otherwise return -1.
 
 You may assume no duplicate exists in the array.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-	int search(vector<int>& nums, int target) {
-		if (nums.empty()) {
-			return -1;
-		}
-		int n = nums.size(), i = 0, j = n - 1;
-		if (target >= nums.front()) {
-			while (i + 1 < j) {
-				int mid = i + (j - i) / 2;
-				if (nums[mid] < nums.front()) {
-					j = mid;
-				}
-				else if (nums[mid] < target) {
-					i = mid;
-				}
-				else {
-					j = mid;
-				}
-			}
-		}
-		else {
-			while (i + 1 < j) {
-				int mid = i + (j - i) / 2;
-				if (nums[mid] >= nums.front()) {
-					i = mid;
-				}
-				else if (nums[mid] < target) {
-					i = mid;
-				}
-				else {
-					j = mid;
-				}
-			}
-		}
-		if (nums[i] == target) {
-			return i;
-		}
-		if (nums[j] == target) {
-			return j;
-		}
-		return -1;
-	}
+    int search(vector<int>& nums, int target) {
+        int sz = nums.size(), i = 0, j = sz;
+        while (i < j) {
+            int k = i + (j - i) / 2;
+            if (target < nums[0]) {
+                if (nums[0] <= nums[k]) {
+                    i = k + 1;
+                }
+                else if (nums[k] < target) {
+                    i = k + 1;
+                }
+                else {
+                    j = k;
+                }
+            }
+            else {
+                if (nums[k] < nums[0]) {
+                    j = k;
+                }
+                else if (nums[k] < target) {
+                    i = k + 1;
+                }
+                else {
+                    j = k;
+                }
+            }
+        }
+        return i == sz or nums[i] != target ? -1 : i;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int target, answer, result;
-
-	nums = {4, 5, 6, 7, 0, 1, 2};
-	target = 0;
-	answer = 4;
-	result = solution.search(nums, target);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
+    Solution solution;
+    vector<int> nums;
+    int target, result;
+    
+    nums = {4, 5, 6, 7, 0, 1, 2};
+    target = 0;
+    result = solution.search(nums, target);
+    cout << result << '\n';
+    
+    return 0;
 }
