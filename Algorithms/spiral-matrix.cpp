69074a38cfc26a2cdaedbf640a6850116a8adb1c
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-15 23:00:42 -0700

    54. Spiral Matrix

--- Algorithms/spiral-matrix.cpp
+++ Algorithms/spiral-matrix.cpp
@@ -1,7 +1,6 @@
-// 54. Spiral Matrix
-// https://leetcode.com/problems/spiral-matrix/
+54. Spiral Matrix
+https://leetcode.com/problems/spiral-matrix/
 
-/*
 Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
 
 For example,
@@ -13,52 +12,52 @@ Given the following matrix:
  [ 7, 8, 9 ]
 ]
 You should return [1,2,3,6,9,8,7,4,5].
-*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-	vector<int> spiralOrder(vector<vector<int>>& matrix) {
-		vector<int> result;
-		int p = matrix.size(), q = p > 0 ? matrix.front().size() : 0, top = 0, bottom = p - 1, left = 0, right = q - 1, x = 0, y = 0, sz = p * q;
-		for (int i = 0, mode = 0; i < sz; ++i) {
-			result.push_back(matrix[x][y]);
-			if (mode == 0) {
-				if (++y > right) {
-					++x;
-					--y;
-					++top;
-					mode = 1;
-				}
-				continue;
-			}
-			if (mode == 1) {
-				if (++x > bottom) {
-					--x;
-					--y;
-					--right;
-					mode = 2;
-				}
-				continue;
-			}
-			if (mode == 2) {
-				if (--y < left) {
-					--x;
-					++y;
-					--bottom;
-					mode = 3;
-				}
-				continue;
-			}
-			if (mode == 3) {
-				if (--x < top) {
-					++x;
-					++y;
-					++left;
-					mode = 0;
-				}
-				continue;
-			}
-		}
-		return result;
-	}
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        int X = matrix.size(), Y = X == 0 ? 0 : matrix[0].size(), x1 = 0, x2 = X - 1, y1 = 0, y2 = Y - 1;
+        while (x1 <= x2 and y1 <= y2) {
+            for (int j = y1; j <= y2; ++j) {
+                result.push_back(matrix[x1][j]);
+            }
+            for (int i = x1 + 1; i <= x2; ++i) {
+                result.push_back(matrix[i][y2]);
+            }
+            if (x1 < x2 and y1 < y2) {
+                for (int j = y2 - 1; j >= y1; --j) {
+                    result.push_back(matrix[x2][j]);
+                }
+                for (int i = x2 - 1; i > x1; --i) {
+                    result.push_back(matrix[i][y1]);
+                }
+            }
+            ++x1;
+            --x2;
+            ++y1;
+            --y2;
+        }
+        return result;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<vector<int>> matrix;
+    vector<int> result;
+
+    matrix = {{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }};
+    result = solution.spiralOrder(matrix);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
+
+    return 0;
+}
