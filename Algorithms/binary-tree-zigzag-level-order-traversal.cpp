8b098d6706cc78362523becb418cceabbaf6dfde
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-24 20:17:04 -0700

    103. Binary Tree Zigzag Level Order Traversal

--- Algorithms/binary-tree-zigzag-level-order-traversal.cpp
+++ Algorithms/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,7 +1,6 @@
-// 103. Binary Tree Zigzag Level Order Traversal
-// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+103. Binary Tree Zigzag Level Order Traversal
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 
-/*
 Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
 For example:
@@ -17,97 +16,74 @@ return its zigzag level order traversal as:
   [20,9],
   [15,7]
 ]
-*/
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
+#include <iostream>
+#include <queue>
 #include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
-#include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
+
 using namespace std;
 
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
-	vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-		if (!root) {
-			return {};
-		}
-		vector<vector<int>> result;
-		vector<TreeNode*> current{root};
-		while (!current.empty()) {
-			vector<TreeNode*> next;
-			vector<int> v;
-			for (const auto &i : current) {
-				v.push_back(i->val);
-				if (i->left) {
-					next.push_back(i->left);
-				}
-				if (i->right) {
-					next.push_back(i->right);
-				}
-			}
-			if (result.size() % 2) {
-				reverse(begin(v), end(v));
-			}
-			result.push_back(v);
-			current = next;
-		}
-		return result;
-	}
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) {
+            return result;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        bool l2r = true;
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int> line(sz);
+            for (int i = 0; i < sz; ++i) {
+                int j = i;
+                if (!l2r) {
+                    j = sz - 1 - i;
+                }
+                TreeNode * front = q.front();
+                TreeNode * left = front->left;
+                TreeNode * right = front->right;
+                q.pop();
+                line[j] = front->val;
+                if (left) {
+                    q.push(left);
+                }
+                if (right) {
+                    q.push(right);
+                }
+            }
+            l2r = !l2r;
+            result.push_back(line);
+        }
+        return result;
+    }
 };
 
-void gc(TreeNode*& root) {
-	if (root) {
-		gc(root->left);
-		gc(root->right);
-		delete root;
-		root = NULL;
-	}
-}
-
 int main(void) {
-	Solution solution;
-	TreeNode *root = NULL;
-	vector<vector<int>> result, answer;
+    Solution solution;
+    TreeNode * root = NULL;
+    vector<vector<int>> result;
 
-	root = new TreeNode(3);
-	root->left = new TreeNode(9);
-	root->right = new TreeNode(20);
-	root->right->left = new TreeNode(15);
-	root->right->right = new TreeNode(7);
-	answer = {{3}, {20, 9}, {15, 7}};
-	result = solution.zigzagLevelOrder(root);
-	assert(answer == result);
-	gc(root);
-
-	cout << "\nPassed All\n";
-	return 0;
+    root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    result = solution.zigzagLevelOrder(root);
+    for (const auto & i : result) {
+        for (const auto & j : i) {
+            cout << j << '\t';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
 }
