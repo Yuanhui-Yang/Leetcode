ae62d5b6fb54b2a93cdaf62070b58f90a53b4c5d
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-26 01:46:57 -0700

    210. Course Schedule II

--- Algorithms/course-schedule-ii.cpp
+++ Algorithms/course-schedule-ii.cpp
@@ -1,7 +1,6 @@
-// 210. Course Schedule II
-// https://leetcode.com/problems/course-schedule-ii/
+210. Course Schedule II
+https://leetcode.com/problems/course-schedule-ii/
 
-/*
 There are a total of n courses you have to take, labeled from 0 to n - 1.
 
 Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
@@ -19,50 +18,107 @@ There are a total of 2 courses to take. To take course 1 you should have finishe
 There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
 
 Note:
+
 The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
 You may assume that there are no duplicate edges in the input prerequisites.
+ 
+
 click to show more hints.
 
 Hints:
+
 This problem is equivalent to finding the topological order in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
 Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
 Topological sort could also be done via BFS.
-*/
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
+
+using namespace std;
 
 class Solution {
 public:
-	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
-		vector<list<int>> M(numCourses);
-		vector<int> A(numCourses, 0);
-		for (const auto & i : prerequisites) {
-			M[i.second].push_back(i.first);
-			++A[i.first];
-		}
-		vector<int> result;
-		list<int> current;
-		for (int i = 0; i < numCourses; ++i) {
-			if (A[i] == 0) {
-				current.push_back(i);
-			}
-		}
-		while (!current.empty()) {
-			list<int> next;
-			for (const auto & i : current) {
-				result.push_back(i);
-				for (const auto & j : M[i]) {
-					if (--A[j] == 0) {
-						next.push_back(j);
-					}
-				}
-				M[i].clear();
-			}
-			current = next;
-		}
-		for (const auto & i : M) {
-			if (!i.empty()) {
-				return {};
-			}
-		}
-		return result;
-	}
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        f1(numCourses);
+        f2(prerequisites);
+        return f3();
+    }
+private:
+    int N;
+    vector<vector<bool>> A;
+    vector<int> B;
+    void f1(int N) {
+        this->N = N;
+        A.resize(N, vector<bool>(N, false));
+        B.resize(N, 0);
+    }
+    void f2(vector<pair<int, int>> & prerequisites) {
+        for (const auto & i : prerequisites) {
+            int from = i.second, to = i.first;
+            if (0 <= from and from < N and 0 <= to and to < N) {
+                if (!A[from][to]) {
+                    A[from][to] = true;
+                    ++B[to];
+                }
+            }
+        }
+    }
+    vector<int> f3() {
+        vector<int> result;
+        queue<int> q;
+        for (int i = 0; i < N; ++i) {
+            if (B[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int front = q.front();
+            result.push_back(front);
+            q.pop();
+            for (int i = 0; i < N; ++i) {
+                if (A[front][i]) {
+                    A[front][i] = false;
+                    --B[i];
+                    if (B[i] == 0) {
+                        q.push(i);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (A[i][j]) {
+                    return {};
+                }
+            }
+        }
+        return result;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    int numCourses;
+    vector<pair<int, int>> prerequisites;
+    vector<int> result;
+    
+    numCourses = 2;
+    prerequisites = {{1, 0}};
+    result = solution.findOrder(numCourses, prerequisites);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
+
+    numCourses = 4;
+    prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+    result = solution.findOrder(numCourses, prerequisites);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
+    
+    return 0;
+}
