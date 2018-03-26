0c6f18c1c1dde6a33b7f6ea87377fdade02b1e4b
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-03-26 13:43:13 -0700

    142. Linked List Cycle II

--- Algorithms/linked-list-cycle-ii.cpp
+++ Algorithms/linked-list-cycle-ii.cpp
@@ -1,43 +1,65 @@
-// 142. Linked List Cycle II
-// https://leetcode.com/problems/linked-list-cycle-ii/
+142. Linked List Cycle II
+https://leetcode.com/problems/linked-list-cycle-ii/
 
-/*
 Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 Note: Do not modify the linked list.
 
 Follow up:
 Can you solve it without using extra space?
-*/
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
-	ListNode *detectCycle(ListNode *head) {
-		ListNode * slow = head, * fast = head;
-		while (slow and fast and fast->next) {
-			slow = slow->next;
-			fast = fast->next;
-			fast = fast->next;
-			if (slow == fast) {
-				break;
-			}
-		}
-		if (!(slow and fast and fast->next)) {
-			return NULL;
-		}
-		slow = head;
-		while (slow != fast) {
-			slow = slow->next;
-			fast = fast->next;
-		}
-		return slow;
-	}
+    ListNode *detectCycle(ListNode *head) {
+        ListNode * intersec = f1(head);
+        return f2(head, intersec);
+    }
+private:
+    ListNode * f1(ListNode * head) {
+        ListNode * low  = head, * fast = head;
+        while (fast and fast->next) {
+            low = low->next;
+            fast = fast->next->next;
+            if (low == fast) {
+                return low;
+            }
+        }
+        return NULL;
+    }
+    ListNode * f2(ListNode * a, ListNode * b) {
+        while (a and b) {
+            if (a == b) {
+                return a;
+            }
+            a = a->next;
+            b = b->next;
+        }
+        return NULL;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    ListNode * head = NULL, * result = NULL;
+
+    head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = head->next;
+    result = solution.detectCycle(head);
+    cout << result->val << '\n';
+
+    return 0;
+}
