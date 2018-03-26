12e2f8e3850dae2d6d679662a09ea7ecdb351091
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-03-26 01:09:30 -0700

    207. Course Schedule

--- Algorithms/course-schedule.cpp
+++ Algorithms/course-schedule.cpp
@@ -1,7 +1,6 @@
-// 207. Course Schedule
-// https://leetcode.com/problems/course-schedule/
+207. Course Schedule
+https://leetcode.com/problems/course-schedule/
 
-/*
 There are a total of n courses you have to take, labeled from 0 to n - 1.
 
 Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
@@ -25,73 +24,91 @@ Hints:
 This problem is equivalent to finding if a cycle exists in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
 Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
 Topological sort could also be done via BFS.
-*/
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue>
+
+using namespace std;
 
 class Solution {
 public:
-	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-		vector<vector<int>> graph(numCourses);
-		vector<int> indegrees(numCourses, 0), courses(numCourses, 0);
-		for (const auto & i : prerequisites) {
-			graph[i.second].push_back(i.first);
-			++indegrees[i.first];
-			++courses[i.first];
-			++courses[i.second];
-		}
-		list<int> current;
-		for (int i = 0; i < numCourses; ++i) {
-			if (courses[i] > 0 and indegrees[i] == 0) {
-				current.push_back(i);
-			}
-		}
-		while (!current.empty()) {
-			list<int> next;
-			for (const auto & i : current) {
-				for (const auto & j : graph[i]) {
-					if (--indegrees[j] == 0) {
-						next.push_back(j);
-					}
-				}
-				graph[i].clear();
-			}
-			current = next;
-		}
-		for (const auto & i : graph) {
-			if (!i.empty()) {
-				return false;
-			}
-		}
-		return true;
-	}
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        f1(numCourses);
+        f2(prerequisites);
+        return f3();
+    }
+private:
+    vector<vector<bool>> A;
+    vector<bool> B;
+    vector<int> C;
+    int N;
+    void f1(int N) {
+        A.resize(N, vector<bool>(N, false));
+        B.resize(N, false);
+        C.resize(N, 0);
+        this->N = N;
+    }
+    void f2(vector<pair<int, int>> & prerequisites) {
+        for (const auto & i : prerequisites) {
+            int from = i.second, to = i.first;
+            if (0 <= from and from < N and 0 <= to and to < N) {
+                if (!A[from][to]) {
+                    A[from][to] = true;
+                    B[from] = true;
+                    B[to] = true;
+                    ++C[to];
+                }
+            }
+        }
+    }
+    bool f3() {
+        queue<int> q;
+        for (int i = 0; i < N; ++i) {
+            if (B[i] and C[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int front = q.front();
+            q.pop();
+            for (int i = 0; i < N; ++i) {
+                if (A[front][i]) {
+                    A[front][i] = false;
+                    --C[i];
+                    if (C[i] == 0) {
+                        q.push(i);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (A[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 };
 
-class Solution {
-public:
-	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-		vector<vector<int>> graph(numCourses);
-		for (const auto & i : prerequisites) {
-			graph[i.second].push_back(i.first);
-		}
-		for (int i = 0; i < numCourses; ++i) {
-			vector<bool> A(numCourses, true);
-			A[i] = false;
-			list<int> current = {i};
-			while (!current.empty()) {
-				list<int> next;
-				for (const auto & j : current) {
-					for (const auto & k : graph[j]) {
-						if (k == i) {
-							return false;
-						}
-						if (A[k]) {
-							A[k] = false;
-							next.push_back(k);
-						}
-					} 
-				}
-				current = next;
-			}
-		}
-		return true;
-	}
-};
+int main(void) {
+    Solution solution;
+    int numCourses;
+    vector<pair<int, int>> prerequisites;
+    bool result;
+    
+    numCourses = 2;
+    prerequisites = {{1, 0}};
+    result = solution.canFinish(numCourses, prerequisites);
+    cout << boolalpha << result << '\n';
+
+    numCourses = 2;
+    prerequisites = {{1, 0}, {0, 1}};
+    result = solution.canFinish(numCourses, prerequisites);
+    cout << boolalpha << result << '\n';
+    
+    return 0;
+}
