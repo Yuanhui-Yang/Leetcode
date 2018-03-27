0234cab4df608058a8603962fac921a8537e87f1
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-03-26 22:50:43 -0700

    27. Remove Element

--- Algorithms/remove-element.cpp
+++ Algorithms/remove-element.cpp
@@ -1,48 +1,50 @@
-// 27. Remove Element
-// https://leetcode.com/problems/remove-element/
+27. Remove Element
+https://leetcode.com/problems/remove-element/
 
-/*
-Given an array and a value, remove all instances of that value in place and return the new length.
+Given an array and a value, remove all instances of that value in-place and return the new length.
 
-Do not allocate extra space for another array, you must do this in place with constant memory.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 Example:
-Given input array nums = [3,2,2,3], val = 3
+
+Given nums = [3,2,2,3], val = 3,
 
 Your function should return length = 2, with the first two elements of nums being 2.
-*/
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Solution {
 public:
-	int removeElement(vector<int>& nums, int val) {
-		int result = 0;
-		for (int i = 0, n = nums.size(); i < n; ++i) {
-			if (nums[i] != val) {
-				nums[result++] = nums[i];
-			}
-		}
-		return result;
-	}
+    int removeElement(vector<int>& nums, int val) {
+        int sz = nums.size(), i = 0, j = 0;
+        while (j < sz) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                ++i;
+            }
+            ++j;
+        }
+        return i;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<int> nums, answer;
-	int val, result;
-
-	nums = {3, 2, 2, 3};
-	val = 3;
-	answer = {2, 2};
-	sort(begin(answer), end(answer));
-	result = solution.removeElement(nums, val);
-	sort(begin(nums), next(begin(nums), result));
-	assert(answer == vector<int>(begin(nums), next(begin(nums), result)));
-
-	cout << "\nPassed All\n";
-	return 0;
+    Solution solution;
+    vector<int> nums;
+    int val, result;
+    
+    nums = {3, 2, 2, 3};
+    val = 3;
+    result = solution.removeElement(nums, val);
+    for (int i = 0; i < result; ++i) {
+        cout << nums[i] << '\t';
+    }
+    cout << '\n';
+    
+    return 0;
 }
