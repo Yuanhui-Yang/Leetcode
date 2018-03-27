3ca04f304836a26c6ac9bb2510c414f75fd1de94
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-03-26 17:37:54 -0700

    214. Shortest Palindrome

--- Algorithms/shortest-palindrome.cpp
+++ Algorithms/shortest-palindrome.cpp
@@ -1,7 +1,6 @@
-// 214. Shortest Palindrome
-// https://leetcode.com/problems/shortest-palindrome/
+214. Shortest Palindrome
+https://leetcode.com/problems/shortest-palindrome/
 
-/*
 Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
 
 For example:
@@ -9,65 +8,57 @@ For example:
 Given "aacecaaa", return "aaacecaaa".
 
 Given "abcd", return "dcbabcd".
-*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
 public:
-	string shortestPalindrome(string s) {
-		int m = s.size();
-		string t = s;
-		reverse(begin(t), end(t));
-		s.push_back('#');
-		s.append(t);
-		int n = s.size(), i = 0, j = -1;
-		vector<int> A(n + 1, 0);
-		A.front() = -1;
-		while (i < n) {
-			if (j == -1 or s[i] == s[j]) {
-				A[++i] = ++j;
-			}
-			else {
-				j = A[j];
-			}
-		}
-		return t.substr(0, m - A.back()) + s.substr(0, m);
-	}
+    string shortestPalindrome(string s) {
+        int sz = s.size();
+        string t = s;
+        reverse(t.begin(), t.end());
+        s.push_back('#');
+        s.append(t);
+        vector<int> A = f1(s);
+        string result;
+        result = t.substr(0, sz - A.back());
+        result.append(s.substr(0, sz));
+        return result;
+    }
+private:
+    vector<int> f1(string & s) {
+        int sz = s.size();
+        vector<int> result(sz + 1);
+        result[0] = -1;
+        int i = 0, j = -1;
+        while (i < sz) {
+            if (j == -1 or s[i] == s[j]) {
+                result[++i] = ++j;
+            }
+            else {
+                j = result[j];
+            }
+        }
+        return result;
+    }
 };
 
-class Solution {
-public:
-	string shortestPalindrome(string s) {
-		string t;
-		for (const auto & i : s) {
-			t.push_back('#');
-			t.push_back(i);
-		}
-		t.push_back('#');
-		int p = s.size(), q = t.size(), x = 0, y = 0;
-		vector<int> A(q, 0);
-		for (int i = 0; i < q and x - y >= 0 and x + y < q; ++i) {
-			int j = 2 * x - i;
-			if (j >= 0 and j - A[j] > 2 * x - y) {
-				A[i] = A[j];
-			}
-			else {
-				int z = 0;
-				while (i - z >= 0 and i + z < q and t[i - z] == t[i + z]) {
-					++z;
-				}
-				A[i] = z - 1;
-			}
-			if (y < A[i]) {
-				x = i;
-				y = A[i];
-			}
-		}
-		int i = q - 1;
-		while (i > 0 and i != A[i]) {
-			--i;
-		}
-		t = s.substr(i);
-		reverse(begin(t), end(t));
-		return t + s;
-	}
-};
+int main() {
+    Solution solution;
+    string s, result;
+    
+    s = "aacecaaa";
+    result = solution.shortestPalindrome(s);
+    cout << result << '\n';
+
+    s = "abcd";
+    result = solution.shortestPalindrome(s);
+    cout << result << '\n';
+    
+    return 0;
+}
