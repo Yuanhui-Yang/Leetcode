a5711b5735a561683fc4581da682cac79f37a28a
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-09 03:14:55 -0700

    684. Redundant Connection

--- Algorithms/redundant-connection.cpp
+++ Algorithms/redundant-connection.cpp
@@ -1,7 +1,6 @@
-// 684. Redundant Connection
-// https://leetcode.com/problems/redundant-connection/
+684. Redundant Connection
+https://leetcode.com/problems/redundant-connection/
 
-/*
 In this problem, a tree is an undirected graph that is connected and has no cycles.
 
 The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N), with one additional edge added. The added edge has two different vertices chosen from 1 to N, and was not an edge that already existed.
@@ -22,68 +21,82 @@ Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
 Output: [1,4]
 Explanation: The given undirected graph will be like this:
 5 - 1 - 2
-	|   |
-	4 - 3
+    |   |
+    4 - 3
 Note:
 The size of the input 2D-array will be between 3 and 1000.
 Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
 
 Update (2017-09-26):
 We have overhauled the problem description + test cases and specified clearly the graph is an undirected graph. For the directed graph follow up please see Redundant Connection II). We apologize for any inconvenience caused.
-*/
 
 #include <iostream>
 #include <vector>
 #include <numeric>
-#include <iterator>
 
 using namespace std;
 
 class Solution {
 public:
-	vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-		int n = 0;
-		for (const auto & i : edges) {
-			n = max(n, max(i[0], i[1]));
-		}
-		vector<int> A(n + 1);
-		iota(begin(A), end(A), 0);
-		vector<int> result;
-		for (const auto & i : edges) {
-			int ra = f1(A, i[0]), rb = f1(A, i[1]);
-			if (ra != rb) {
-				A[ra] = rb;
-			}
-			else {
-				result = i;
-			}
-		}
-		return result;
-	}
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        cnt = edges.size();
+        A.resize(cnt + 1);
+        B.resize(cnt + 1, 1);
+        iota(A.begin(), A.end(), 0);
+        for (const auto & edge : edges) {
+            int x = edge[0], y = edge[1];
+            if (x > y) {
+                swap(x, y);
+            }
+            int a = find(x), b = find(y);
+            if (a == b) {
+                return {x, y};
+            }
+            unoin(a, b);
+        }
+        return {};
+    }
 private:
-	int f1(vector<int> & A, int id) {
-		return A[id] == id ? id : A[id] = f1(A, A[id]);
-	}
+    vector<int> A, B;
+    int cnt;
+    int find(int id) {
+        while (id != A[id]) {
+            A[id] = A[A[id]];
+            id = A[id];
+        }
+        return id;
+    }
+    void unoin(int x, int y) {
+        int a = find(x), b = find(y);
+        if (a == b) {
+            return;
+        }
+        if (B[a] > B[b]) {
+            swap(a, b);
+        }
+        A[a] = b;
+        B[b] += B[a];
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<vector<int>> edges;
-	vector<int> result;
-	
-	edges = {{1, 2}, {1, 3}, {2, 3}};
-	result = solution.findRedundantConnection(edges);
-	for (const auto & i : result) {
-		cout << i << '\t';
-	}
-	cout << '\n';
+    Solution solution;
+    vector<vector<int>> edges;
+    vector<int> result;
+    
+    edges = {{1, 2}, {1, 3}, {2, 3}};
+    result = solution.findRedundantConnection(edges);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
 
-	edges = {{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}};
-	result = solution.findRedundantConnection(edges);
-	for (const auto & i : result) {
-		cout << i << '\t';
-	}
-	cout << '\n';
+    edges = {{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}};
+    result = solution.findRedundantConnection(edges);
+    for (const auto & i : result) {
+        cout << i << '\t';
+    }
+    cout << '\n';
 
-	return 0;
+    return 0;
 }
