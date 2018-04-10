21e8507391436df27c77cf943be7bb4ce5efba19
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-10 03:24:34 -0700

    163. Missing Ranges

--- Algorithms/missing-ranges.cpp
+++ Algorithms/missing-ranges.cpp
@@ -9,34 +9,22 @@ For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4-
 
 class Solution {
 public:
-	vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
-		if (lower > upper) {
-			return {};
-		}
-		vector<string> result;
-		int n = nums.size(), i = 0;
-		while (i < n and nums[i] < lower) {
-			++i;
-		}
-		if (i >= n) {
-			result.push_back(lower == upper ? to_string(lower) : to_string(lower) + "->" + to_string(upper));
-			return result;
-		}
-		if (nums[i] > lower) {
-			result.push_back((long)lower + 1 == (long)nums[i] ? to_string(lower) : to_string(lower) + "->" + to_string((long)nums[i] - 1));
-			lower = nums[i];
-		}
-		++i;
-		while (i < n and nums[i] <= upper) {
-			if ((long)lower + 1 < (long)nums[i]) {
-				result.push_back((long)lower + 2 == (long)nums[i] ? to_string((long)lower + 1) : to_string((long)lower + 1) + "->" + to_string((long)nums[i] - 1));
-			}
-			lower = nums[i];
-			++i;
-		}
-		if (i >= n and lower < upper) {
-			result.push_back((long)lower + 1 == upper ? to_string(upper) : to_string((long)lower + 1) + "->" + to_string(upper));
-		}
-		return result;
-	}
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<string> result;
+        long a = lower, b;
+        --a;
+        for (int sz = nums.size(), i = 0; i <= sz; ++i) {
+            b = i < sz ? nums[i] : (long)upper + 1;
+            if (a + 2 == b) {
+                string t = to_string(a + 1);
+                result.push_back(t);
+            }
+            else if (a + 2 < b) {
+                string t = to_string(a + 1) + "->" + to_string(b - 1);
+                result.push_back(t);
+            }
+            a = b;
+        }
+        return result;
+    }
 };
