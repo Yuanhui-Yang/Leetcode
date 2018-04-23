ca9c4b16289417e9c07958a597c80c1f39e742ca
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-23 15:14:24 -0700

    Update accounts-merge.cpp

--- Algorithms/accounts-merge.cpp
+++ Algorithms/accounts-merge.cpp
@@ -1,7 +1,6 @@
-// 721. Accounts Merge
-// https://leetcode.com/problems/accounts-merge/
+721. Accounts Merge
+https://leetcode.com/problems/accounts-merge/
 
-/*
 Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
 
 Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
@@ -22,90 +21,88 @@ Note:
 The length of accounts will be in the range [1, 1000].
 The length of accounts[i] will be in the range [1, 10].
 The length of accounts[i][j] will be in the range [1, 30].
-*/
 
 #include <iostream>
 #include <string>
-#include <set>
 #include <vector>
+#include <algorithm>
+#include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-	vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-		int sz = accounts.size(), sz1, n = 0, i, j, root, root1;
-		unordered_map<string, int> A;
-		vector<int> B;
-		vector<string> C, D;
-		for (i = 0; i < sz; ++i) {
-			sz1 = accounts[i].size();
-			for (j = 1; j < sz1; ++j) {
-				if (!A.count(accounts[i][j])) {
-					A[accounts[i][j]] = n;
-					B.push_back(n);
-					C.push_back(accounts[i][0]);
-					++n;
-				}
-			}
-		}
-		vector<set<string>> E(n);
-		vector<vector<string>> result;
-		for (i = 0; i < sz; ++i) {
-			sz1 = accounts[i].size();
-			root = f1(A[accounts[i][1]], B);
-			for (j = 2; j < sz1; ++j) {
-				root1 = f1(A[accounts[i][j]], B);
-				if (root != root1) {
-					B[root1] = root;
-				}
-			}
-		}
-		for (i = 0; i < sz; ++i) {
-			sz1 = accounts[i].size();
-			for (j = 1; j < sz1; ++j) {
-				root = f1(A[accounts[i][j]], B);
-				E[root].insert(accounts[i][j]);
-			}
-		}
-		for (i = 0; i < n; ++i) {
-			if (!E[i].empty()) {
-				D.clear();
-				D.push_back(C[i]);
-				D.insert(end(D), begin(E[i]), end(E[i]));
-				result.push_back(D);
-			}
-		}
-		return result;
-	}
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        f1(accounts);
+        f2(accounts);
+        return f3(accounts);
+    }
 private:
-	int f1(int node, vector<int> & B) {
-		return node == B[node] ? node : B[node] = f1(B[node], B);
-	}
+    unordered_map<string, string> A, B;
+    void f1(vector<vector<string>>& accounts) {
+        for (auto & account : accounts) {
+            string & name = account[0];
+            for (int sz = account.size(), i = 1; i < sz; ++i) {
+                string & email = account[i];
+                A[email] = email;
+                B[email] = name;
+            }
+        }
+    }
+    void f2(vector<vector<string>>& accounts) {
+        for (auto & account : accounts) {
+            string & a = account[1];
+            for (int sz = account.size(), i = 2; i < sz; ++i) {
+                string & b = account[i];
+                unoin(b, a);
+            }
+        }
+    }
+    vector<vector<string>> f3(vector<vector<string>>& accounts) {
+        vector<vector<string>> result;
+        unordered_map<string, unordered_set<string>> C;
+        for (auto & account : accounts) {
+            for (int sz = account.size(), i = 1; i < sz; ++i) {
+                string & email = account[i];
+                C[find(email)].insert(email);
+            }
+        }
+        for (auto & i : C) {
+            vector<string> D;
+            D.push_back(B[i.first]);
+            for (auto & j : i.second) {
+                D.push_back(j);
+            }
+            sort(D.begin(), D.end());
+            result.push_back(D);
+        }
+        return result;
+    }
+    string find(string & node) {
+        return node == A[node] ? node : A[node] = find(A[node]);
+    }
+    void unoin(string & a, string & b) {
+        string x = find(a), y = find(b);
+        if (x == y) {
+            return;
+        }
+        A[x] = y;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<vector<string>> accounts, result;
-	
-	accounts = {{"John", "johnsmith@mail.com", "john00@mail.com"}, {"John", "johnnybravo@mail.com"}, {"John", "johnsmith@mail.com", "john_newyork@mail.com"}, {"Mary", "mary@mail.com"}};
-	result = solution.accountsMerge(accounts);
-	for (const auto & i : result) {
-		for (const auto & j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
+    Solution solution;
+    vector<vector<string>> accounts, result;
 
-	accounts = {{"Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"}, {"Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"}, {"Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"}, {"Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"}, {"Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"}};
-	result = solution.accountsMerge(accounts);
-	for (const auto & i : result) {
-		for (const auto & j : i) {
-			cout << j << '\t';
-		}
-		cout << '\n';
-	}
-	
-	return 0;
+    accounts = {{"John", "johnsmith@mail.com", "john00@mail.com"}, {"John", "johnnybravo@mail.com"}, {"John", "johnsmith@mail.com", "john_newyork@mail.com"}, {"Mary", "mary@mail.com"}};
+    result = solution.accountsMerge(accounts);
+    for (const auto & i : result) {
+        for (const auto & j : i) {
+            cout << j << '\t';
+        }
+        cout << '\n';
+    }
+
+    return 0;
 }
