c3509e7afac10b3015cf5aeaf01d20d633d05908
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-24 00:13:24 -0700

    Update number-of-longest-increasing-subsequence.cpp

--- Algorithms/number-of-longest-increasing-subsequence.cpp
+++ Algorithms/number-of-longest-increasing-subsequence.cpp
@@ -1,7 +1,6 @@
-// 673. Number of Longest Increasing Subsequence
-// https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+673. Number of Longest Increasing Subsequence
+https://leetcode.com/problems/number-of-longest-increasing-subsequence/
 
-/*
 Given an unsorted array of integers, find the number of longest increasing subsequence.
 
 Example 1:
@@ -13,10 +12,10 @@ Input: [2,2,2,2,2]
 Output: 5
 Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
 Note: Length of the given array will be not exceed 2000 and the answer is guaranteed to be fit in 32-bit signed int.
-*/
 
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -58,21 +57,21 @@ public:
 };
 
 int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int result;
-	
-	nums = {1, 3, 5, 4, 7};
-	result = solution.findNumberOfLIS(nums);
-	cout << result << '\n';
+    Solution solution;
+    vector<int> nums;
+    int result;
+    
+    nums = {1, 3, 5, 4, 7};
+    result = solution.findNumberOfLIS(nums);
+    cout << result << '\n';
 
-	nums = {2, 2, 2, 2, 2};
-	result = solution.findNumberOfLIS(nums);
-	cout << result << '\n';
+    nums = {2, 2, 2, 2, 2};
+    result = solution.findNumberOfLIS(nums);
+    cout << result << '\n';
 
-	nums = {1, 2, 4, 3, 5, 4, 7, 2};
-	result = solution.findNumberOfLIS(nums);
-	cout << result << '\n';
-	
-	return 0;
+    nums = {1, 2, 4, 3, 5, 4, 7, 2};
+    result = solution.findNumberOfLIS(nums);
+    cout << result << '\n';
+    
+    return 0;
 }
