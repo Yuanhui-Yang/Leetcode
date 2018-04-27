b6bea489b41116b3ecc0c9f342153156d57c13d6
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-27 11:56:06 -0700

    Update contiguous-array.cpp

--- Algorithms/contiguous-array.cpp
+++ Algorithms/contiguous-array.cpp
@@ -1,7 +1,6 @@
-// 525. Contiguous Array
-// https://leetcode.com/problems/contiguous-array/
+525. Contiguous Array
+https://leetcode.com/problems/contiguous-array/
 
-/*
 Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
 
 Example 1:
@@ -13,41 +12,49 @@ Input: [0,1,0]
 Output: 2
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
 Note: The length of the given binary array will not exceed 50,000.
-*/
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-	int findMaxLength(vector<int>& nums) {
-		int result = 0, sz = nums.size(), i = 0, cnt = 0;
-		vector<int> A(2 * sz + 1, -2);
-		A[sz] = -1;
-		for (i = 0; i < sz; ++i) {
-			cnt += nums[i] == 1 ? 1 : -1;
-			if (A[cnt + sz] >= -1) {
-				result = max(result, i - A[cnt + sz]);
-			}
-			else {
-				A[cnt + sz] = i;
-			}
-		}
-		return result;
-	}
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> A;
+        int result = 0;
+        A[0] = -1;
+        for (int sz = nums.size(), sum = 0, i = 0; i < sz; ++i) {
+            if (nums[i] == 0) {
+                --sum;
+            }
+            else {
+                ++sum;
+            }
+            if (A.count(sum)) {
+                result = max(result, i - A[sum]);
+            }
+            else {
+                A[sum] = i;
+            }
+        }
+        return result;
+    }
 };
 
 int main(void) {
-	Solution solution;
-	vector<int> nums;
-	int result;
+    Solution solution;
+    vector<int> nums;
+    int result;
 
-	nums = {0, 0, 1, 0, 0, 0, 1, 1};
-	result = solution.findMaxLength(nums);
-	cout << result << '\n';
-	
-	return 0;
+    nums = {0, 1};
+    result = solution.findMaxLength(nums);
+    cout << result << '\n';
+
+    nums = {0, 1, 0};
+    result = solution.findMaxLength(nums);
+    cout << result << '\n';
+
+    return 0;
 }
