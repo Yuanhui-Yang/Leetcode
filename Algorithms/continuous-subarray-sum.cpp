bcab910e04b4a63cc15e112f5e772509472fe047
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-27 17:01:00 -0700

    Update continuous-subarray-sum.cpp

--- Algorithms/continuous-subarray-sum.cpp
+++ Algorithms/continuous-subarray-sum.cpp
@@ -1,7 +1,6 @@
-// 523. Continuous Subarray Sum
-// https://leetcode.com/problems/continuous-subarray-sum/
+523. Continuous Subarray Sum
+https://leetcode.com/problems/continuous-subarray-sum/
 
-/*
 Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
 
 Example 1:
@@ -15,34 +14,49 @@ Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and su
 Note:
 The length of the array won't exceed 10,000.
 You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
-*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 class Solution {
 public:
-	bool checkSubarraySum(vector<int>& nums, int k) {
-		int sz = nums.size();
-		if (k == 0) {
-			for (int i = 1; i < sz; ++i) {
-				if (nums[i - 1] == 0 and nums[i] == 0) {
-					return true;
-				}
-			}
-			return false;
-		}
-		unordered_map<int, int> A;
-		A[0] = -1;
-		for (int sum = 0, i = 0; i < sz; ++i) {
-			sum += nums[i] % k;
-			sum %= k;
-			if (A.count(sum)) {
-				if (i - A[sum] > 1) {
-					return true;
-				}
-			}
-			else {
-				A[sum] = i;
-			}
-		}
-		return false;
-	}
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> A;
+        A[0] = -1;
+        for (int sz = nums.size(), sum = 0, i = 0; i < sz; ++i) {
+            sum += nums[i];
+            int val = k ? sum % k : sum;
+            if (A.count(val)) {
+                if (i - A[val] >= 2) {
+                    return true;
+                }
+            }
+            else {
+                A[val] = i;
+            }
+        }
+        return false;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<int> nums;
+    int k;
+    bool result;
+
+    nums = {23, 2, 4, 6, 7};
+    k = 6;
+    result = solution.checkSubarraySum(nums, k);
+    cout << boolalpha << result << '\n';
+
+    nums = {23, 2, 6, 4, 7};
+    k = 6;
+    result = solution.checkSubarraySum(nums, k);
+    cout << boolalpha << result << '\n';
+
+    return 0;
+}
