84db0b712417ba44941a606e90a02d82543b6312
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-28 00:05:50 -0700

    Update sum-of-left-leaves.cpp

--- Algorithms/sum-of-left-leaves.cpp
+++ Algorithms/sum-of-left-leaves.cpp
@@ -1,42 +1,68 @@
-// 404. Sum of Left Leaves
-// https://leetcode.com/problems/sum-of-left-leaves/
+404. Sum of Left Leaves
+https://leetcode.com/problems/sum-of-left-leaves/
 
-/*
 Find the sum of all left leaves in a given binary tree.
 
 Example:
 
-	3
+    3
    / \
   9  20
-	/  \
+    /  \
    15   7
 
 There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
-*/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode * left, * right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-	int sumOfLeftLeaves(TreeNode* root) {
-		return f(root, NULL);
-	}
+    int sumOfLeftLeaves(TreeNode* root) {
+        int result = 0;
+        stack<TreeNode*> stk;
+        while (!stk.empty() or root) {
+            if (root) {
+                stk.push(root);
+                root = root->left;
+            }
+            else {
+                TreeNode * top = stk.top();
+                stk.pop();
+                root = top->right;
+                if (f1(top)) {
+                    result += top->left->val;
+                }
+            }
+        }
+        return result;
+    }
 private:
-	int f(TreeNode * node, TreeNode* parent) {
-		if (!node) {
-			return 0;
-		}
-		if (!node->left and !node->right) {
-			return parent and parent->left == node ? node->val : 0;
-		}
-		return f(node->left, node) + f(node->right, node);
-	}
+    bool f1(TreeNode * node) {
+        return node and node->left and !node->left->left and !node->left->right;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    TreeNode * root;
+    int result;
+
+    root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+    result = solution.sumOfLeftLeaves(root);
+    cout << result << '\n';
+
+    return 0;
+}
