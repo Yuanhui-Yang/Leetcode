6cf31b0ba4ac0a5919e45d57174dcddfdb935e51
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-28 17:31:07 -0700

    Update insert-delete-getrandom-o1.cpp

--- Algorithms/insert-delete-getrandom-o1.cpp
+++ Algorithms/insert-delete-getrandom-o1.cpp
@@ -1,7 +1,6 @@
-// 380. Insert Delete GetRandom O(1)
-// https://leetcode.com/problems/insert-delete-getrandom-o1/
+380. Insert Delete GetRandom O(1)
+https://leetcode.com/problems/insert-delete-getrandom-o1/
 
-/*
 Design a data structure that supports all following operations in average O(1) time.
 
 insert(val): Inserts an item val to the set if not already present.
@@ -32,75 +31,53 @@ randomSet.insert(2);
 
 // Since 2 is the only number in the set, getRandom always return 2.
 randomSet.getRandom();
-*/
 
-#include <iostream> // std::cout; std::cin
-#include <fstream> // std::fstream::open; std::fstream::close; 
-#include <cstdlib> // rand
-#include <cassert> // assert
-#include <cctype> // isalnum; isalpha; isdigit; islower; isupper; isspace; tolower; toupper
-#include <cmath> // pow; sqrt; round; fabs; abs; log
-#include <climits> // INT_MIN; INT_MAX; LLONG_MIN; LLONG_MAX; ULLONG_MAX
-#include <cfloat> // DBL_EPSILON; LDBL_EPSILON
-#include <cstring> // std::memset
-#include <algorithm> // std::swap; std::max; std::min; std::min_element; std::max_element; std::minmax_element; std::next_permutation; std::prev_permutation; std::nth_element; std::sort; std::lower_bound; std::upper_bound; std::reverse
-#include <limits> // std::numeric_limits<int>::min; std::numeric_limits<int>::max; std::numeric_limits<double>::epsilon; std::numeric_limits<long double>::epsilon;
-#include <numeric> // std::accumulate; std::iota
-#include <string> // std::to_string; std::string::npos; std::stoul; std::stoull; std::stoi; std::stol; std::stoll; std::stof; std::stod; std::stold; 
-#include <list> // std::list::merge; std::list::splice; std::list::merge; std::list::unique; std::list::sort
-#include <bitset>
+#include <iostream>
 #include <vector>
-#include <deque>
-#include <stack> // std::stack::top; std::stack::pop; std::stack::push
-#include <queue> // std::queue::front; std::queue::back; std::queue::pop; std::queue::push
-#include <set> // std::set::count; std::set::find; std::set::equal_range; std::set::lower_bound; std::set::upper_bound
-#include <map> // std::map::count; std::map::find; std::map::equal_range; std::map::lower_bound; std::map::upper_bound
-#include <unordered_set>
+#include <algorithm>
 #include <unordered_map>
-#include <utility> // std::pair; std::make_pair
-#include <iterator>
-#include <functional> // std::less<int>; std::greater<int>
+
 using namespace std;
 
 class RandomizedSet {
 public:
-	/** Initialize your data structure here. */
-	RandomizedSet() {
-		this->v.clear();
-		this->h.clear();
-	}
-
-	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-	bool insert(int val) {
-		if (!h.empty() and h.count(val)) {
-			return false;
-		}
-		h[val] = v.size();
-		v.push_back(val);
-		return true;
-	}
-
-	/** Removes a value from the set. Returns true if the set contained the specified element. */
-	bool remove(int val) {
-		if (h.empty() or !h.count(val)) {
-			return false;
-		}
-		size_t i = h.at(val);
-		h.at(v.back()) = i;
-		v.at(i) = v.back();
-		h.erase(val);
-		v.pop_back();
-		return true;
-	}
-
-	/** Get a random element from the set. */
-	int getRandom() {
-		size_t i = rand() % v.size();
-		return v.at(i);
-	}
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        A.clear();
+        B.clear();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (B.count(val)) {
+            return false;
+        }
+        B[val] = A.size();
+        A.push_back(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (!B.count(val)) {
+            return false;
+        }
+        int sz = A.size(), a = B[val], b = sz - 1, back = A.back();
+        swap(A[a], A[b]);
+        A.pop_back();
+        B[back] = a;
+        B.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        int sz = A.size(), id = rand() % sz;
+        return A[id];
+    }
 private:
-	vector<int> v;
-	unordered_map<int, size_t> h;
+    vector<int> A;
+    unordered_map<int, int> B;
 };
 
 /**
@@ -111,15 +88,16 @@ private:
  * int param_3 = obj.getRandom();
  */
 
- int main(void) {
-	RandomizedSet randomSet;
-	assert(true == randomSet.insert(1));
-	assert(false == randomSet.remove(2));
-	assert(true == randomSet.insert(2));
-	randomSet.getRandom();
-	assert(true == randomSet.remove(1));
-	assert(false == randomSet.insert(2));
-	randomSet.getRandom();
-	cout <<  "\nPassed All\n";
-	return 0;
+int main(void) {
+    RandomizedSet randomSet;
+
+    cout << boolalpha << randomSet.insert(1) << '\n';
+    cout << boolalpha << randomSet.remove(2) << '\n';
+    cout << boolalpha << randomSet.insert(2) << '\n';
+    cout << randomSet.getRandom() << '\n';
+    cout << boolalpha << randomSet.remove(1) << '\n';
+    cout << boolalpha << randomSet.insert(2) << '\n';
+    cout << randomSet.getRandom() << '\n';
+
+    return 0;
 }
