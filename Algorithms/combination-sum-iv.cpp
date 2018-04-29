767b039e352c301a14f9511bc4441d85d10c5c62
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-28 18:19:46 -0700

    Update combination-sum-iv.cpp

--- Algorithms/combination-sum-iv.cpp
+++ Algorithms/combination-sum-iv.cpp
@@ -1,7 +1,6 @@
-// 377. Combination Sum IV
-// https://leetcode.com/problems/combination-sum-iv/
+377. Combination Sum IV
+https://leetcode.com/problems/combination-sum-iv/
 
-/*
 Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
 
 Example:
@@ -25,25 +24,37 @@ Follow up:
 What if negative numbers are allowed in the given array?
 How does it change the problem?
 What limitation we need to add to the question to allow negative numbers?
-*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-	int combinationSum4(vector<int>& nums, int target) {
-		if (nums.empty()) {
-			return 0;
-		}
-		sort(begin(nums), end(nums));
-		int M = max(target, nums.back());
-		vector<int> A(M + 1, 0);
-		A[0] = 1;
-		for (int i = 1; i <= M; ++i) {
-			vector<int>::iterator it = upper_bound(begin(nums), end(nums), i);
-			int N = distance(begin(nums), it);
-			for (int j = 0; j < N; ++j) {
-				A[i] += A[i - nums[j]];
-			}
-		}
-		return A[target];
-	}
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> A(target + 1, 0);
+        A[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (const auto & j : nums) {
+                if (j <= i) {
+                    A[i] += A[i - j];
+                }
+            }
+        }
+        return A[target];
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<int> nums;
+    int target, result;
+
+    nums = {1, 2, 3};
+    target = 4;
+    result = solution.combinationSum4(nums, target);
+    cout << result << '\n';
+
+    return 0;
+}
