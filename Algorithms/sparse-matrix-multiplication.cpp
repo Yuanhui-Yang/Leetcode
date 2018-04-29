496131e4841127363605af57049f611f9be829f1
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-28 20:40:03 -0700

    Update sparse-matrix-multiplication.cpp

--- Algorithms/sparse-matrix-multiplication.cpp
+++ Algorithms/sparse-matrix-multiplication.cpp
@@ -1,7 +1,6 @@
-// 311. Sparse Matrix Multiplication
-// https://leetcode.com/problems/sparse-matrix-multiplication/
+311. Sparse Matrix Multiplication
+https://leetcode.com/problems/sparse-matrix-multiplication/
 
-/*
 Given two sparse matrices A and B, return the result of AB.
 
 You may assume that A's column number is equal to B's row number.
@@ -20,76 +19,77 @@ B = [
 ]
 
 
-	 |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
 AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
-				  | 0 0 1 |
-*/
+                  | 0 0 1 |
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-	vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
-		int P = A.size(), Q = P == 0 ? 0 : A[0].size(), R = B.size(), S = R == 0 ? 0 : B[0].size();
-		if (Q != R) {
-			return {};
-		}
-		vector<list<array<int, 2>>> X(P), Y(R);
-		for (int i = 0; i < P; ++i) {
-			for (int j = 0; j < Q; ++j) {
-				int val = A[i][j];
-				if (val) {
-					X[i].push_back({j, val});
-				}
-			}
-		}
-		for (int i = 0; i < R; ++i) {
-			for (int j = 0; j < S; ++j) {
-				int val = B[i][j];
-				if (val) {
-					Y[i].push_back({j, val});
-				}
-			}
-		}
-		vector<vector<int>> result(P, vector<int>(S, 0));
-		for (int i = 0; i < P; ++i) {
-			for (const auto & j : X[i]) {
-				for (const auto & k : Y[j[0]]) {
-					result[i][k[0]] += j[1] * k[1];
-				}
-			}
-		}
-		return result;
-	}
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int X = A.size(), Y = X ? A[0].size() : 0, Z = Y ? B[0].size() : 0;
+        vector<vector<int>> C = f1(A), D = f1(B), result(X, vector<int>(Z, 0));
+        for (int i = 0; i < X; ++i) {
+            for (const auto & j : C[i]) {
+                for (const auto & k : D[j]) {
+                    result[i][k] += A[i][j] * B[j][k];
+                }
+            }
+        }
+        return result;
+    }
+private:
+    vector<vector<int>> f1(vector<vector<int>>& A) {
+        int X = A.size(), Y = X ? A[0].size() : 0;
+        vector<vector<int>> result(X);
+        for (int i = 0; i < X; ++i) {
+            for (int j = 0; j < Y; ++j) {
+                if (A[i][j]) {
+                    result[i].push_back(j);
+                }
+            }
+        }
+        return result;
+    }
 };
 
 class Solution {
 public:
-	vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
-		vector<array<int, 3>> P, Q;
-		for (int M = A.size(), i = 0; i < M; ++i) {
-			for (int N = A[i].size(), j = 0; j < N; ++j) {
-				int val = A[i][j];
-				if (val) {
-					P.push_back({i, j, val});
-				}
-			}
-		}
-		for (int M = B.size(), i = 0; i < M; ++i) {
-			for (int N = B[i].size(), j = 0; j < N; ++j) {
-				int val = B[i][j];
-				if (val) {
-					Q.push_back({i, j, val});
-				}
-			}
-		}
-		int M = A.size(), N = B.empty() ? 0 : B[0].size();
-		vector<vector<int>> result(M, vector<int>(N, 0));
-		for (const auto & x : P) {
-			for (const auto & y : Q) {
-				if (x[1] == y[0]) {
-					result[x[0]][y[1]] += x[2] * y[2];
-				}
-			}
-		}
-		return result;
-	}
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int X = A.size(), Y = X ? A[0].size() : 0, Z = Y ? B[0].size() : 0;
+        vector<vector<int>> result(X, vector<int>(Z, 0));
+        for (int i = 0; i < X; ++i) {
+            for (int j = 0; j < Y; ++j) {
+                if (A[i][j]) {
+                    for (int k = 0; k < Z; ++k) {
+                        if (B[j][k]) {
+                            result[i][k] += A[i][j] * B[j][k];   
+                        }
+                    }   
+                }
+            }
+        }
+        return result;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<vector<int>> A, B, result;
+
+    A = {{ 1, 0, 0}, {-1, 0, 3}};
+    B = {{ 7, 0, 0 }, { 0, 0, 0 }, { 0, 0, 1 }};
+    result = solution.multiply(A, B);
+    for (const auto & i : result) {
+        for (const auto & j : i) {
+            cout << j << '\t';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
