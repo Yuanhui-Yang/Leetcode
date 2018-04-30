d796eee75191c7f109c4e3499354734764b157a7
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-30 02:36:46 -0700

    Update h-index.cpp

--- Algorithms/h-index.cpp
+++ Algorithms/h-index.cpp
@@ -1,7 +1,6 @@
-// 274. H-Index
-// https://leetcode.com/problems/h-index/
+274. H-Index
+https://leetcode.com/problems/h-index/
 
-/*
 Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
 
 According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
@@ -9,21 +8,40 @@ According to the definition of h-index on Wikipedia: "A scientist has index h if
 For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
 
 Note: If there are several possible values for h, the maximum one is taken as the h-index.
-*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
 
 class Solution {
 public:
-	int hIndex(vector<int>& citations) {
-		sort(begin(citations), end(citations));
-		int sz = citations.size(), result = 0;
-		for (int i = sz - 1; i >= 0; --i) {
-			if (citations[i] >= (1 + result)) {
-				++result;
-			}
-			else {
-				break;
-			}
-		}
-		return result;
-	}
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+        int sz = citations.size(), i = 0, j = sz;
+        while (i < j) {
+            int k = i + (j - i) / 2;
+            if (citations[k] > k) {
+                i = k + 1;
+            }
+            else {
+                j = k;
+            }
+        }
+        return i;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<int> citations;
+    int result;
+
+    citations = {3, 0, 6, 1, 5};
+    result = solution.hIndex(citations);
+    cout << result << '\n';
+
+    return 0;
+}
