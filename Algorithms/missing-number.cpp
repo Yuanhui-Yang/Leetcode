02d628bf09947d51fe25b5dbe552a969e72c3acc
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-04-30 04:26:29 -0700

    Update missing-number.cpp

--- Algorithms/missing-number.cpp
+++ Algorithms/missing-number.cpp
@@ -1,19 +1,46 @@
-// 268. Missing Number
-// https://leetcode.com/problems/missing-number/
+268. Missing Number
+https://leetcode.com/problems/missing-number/
 
-/*
 Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
 
-For example,
-Given nums = [0, 1, 3] return 2.
+Example 1
+
+Input: [3,0,1]
+Output: 2
+Example 2
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
 
 Note:
 Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
-*/
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
 
 class Solution {
 public:
-	int missingNumber(vector<int>& nums) {
-		return nums.size() * (nums.size() + 1) / 2 - accumulate(begin(nums), end(nums), 0);
-	}
+    int missingNumber(vector<int>& nums) {
+        int sz = nums.size(), sum = accumulate(nums.begin(), nums.end(), 0);
+        return sz * (sz + 1) / 2 - sum;
+    }
 };
+
+int main(void) {
+    Solution solution;
+    vector<int> nums;
+    int result;
+
+    nums = {3, 0, 1};
+    result = solution.missingNumber(nums);
+    cout << result << '\n';
+
+    nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+    result = solution.missingNumber(nums);
+    cout << result << '\n';
+
+    return 0;
+}
