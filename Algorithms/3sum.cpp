d1662aaecd1f5253101460b9cf6e4f137a5ea6c5
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-05-02 02:47:21 -0700

    Update 3sum.cpp

--- Algorithms/3sum.cpp
+++ Algorithms/3sum.cpp
@@ -1,11 +1,15 @@
 15. 3Sum
 https://leetcode.com/problems/3sum/
 
-Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
-Note: The solution set must not contain duplicate triplets.
+Note:
 
-For example, given array S = [-1, 0, 1, 2, -1, -4],
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
 
 A solution set is:
 [
@@ -24,43 +28,31 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        for (int sz = nums.size(), i = 0; i < sz; ++i) {
+        for (int sz = nums.size(), i = 0; i + 2 < sz; ++i) {
             if (i == 0 or nums[i - 1] != nums[i]) {
-                vector<vector<int>> v = f1(nums, -nums[i], i);
-                result.insert(result.end(), v.begin(), v.end());
-            }
-        }
-        return result;
-    }
-private:
-    vector<vector<int>> f1(vector<int>& nums, int target, int begin) {
-        int end = nums.size();
-        vector<vector<int>> result;
-        int i = begin + 1, j = end - 1;
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-            if (sum == target) {
-                result.push_back({nums[begin], nums[i], nums[j]});
-                while (i + 1 < j and nums[i] == nums[i + 1]) {
-                    ++i;
+                int j = i + 1, k = sz - 1;
+                while (j < k) {
+                    if (j > i + 1 and nums[j - 1] == nums[j]) {
+                        ++j;
+                    }
+                    else if (k < sz - 1 and nums[k + 1] == nums[k]) {
+                        --k;
+                    }
+                    else {
+                        int sum = nums[i] + nums[j] + nums[k];
+                        if (sum == 0) {
+                            result.push_back({nums[i], nums[j], nums[k]});
+                            ++j;
+                            --k;
+                        }
+                        else if (sum < 0) {
+                            ++j;
+                        }
+                        else {
+                            --k;
+                        }
+                    }
                 }
-                ++i;
-                while (i < j - 1 and nums[j - 1] == nums[j]) {
-                    --j;
-                }
-                --j;
-            }
-            else if (sum < target) {
-                while (i + 1 < j and nums[i] == nums[i + 1]) {
-                    ++i;
-                }
-                ++i;
-            }
-            else {
-                while (i < j - 1 and nums[j - 1] == nums[j]) {
-                    --j;
-                }
-                --j;
             }
         }
         return result;
