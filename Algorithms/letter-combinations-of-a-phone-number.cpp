cf292a867a55389b8809fad88aec31306dbd6788
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-05-02 04:23:05 -0700

    Update letter-combinations-of-a-phone-number.cpp

--- Algorithms/letter-combinations-of-a-phone-number.cpp
+++ Algorithms/letter-combinations-of-a-phone-number.cpp
@@ -1,20 +1,24 @@
 17. Letter Combinations of a Phone Number
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
-Given a digit string, return all possible letter combinations that the number could represent.
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
-A mapping of digit to letters (just like on the telephone buttons) is given below.
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 
 
-Input:Digit string "23"
+Example:
+
+Input: "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 Note:
+
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
 
 using namespace std;
 
@@ -22,35 +26,39 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         vector<string> result;
-        if (digits.empty()) {
-            return result;
+        array<string, 10> A;
+        A[0] = "";
+        A[1] = "";
+        A[2] = "abc";
+        A[3] = "def";
+        A[4] = "ghi";
+        A[5] = "jkl";
+        A[6] = "mno";
+        A[7] = "pqrs";
+        A[8] = "tuv";
+        A[9] = "wxyz";
+        for (const auto & ch : digits) {
+            int id = ch - '0';
+            vector<string> next;
+            if (result.empty()) {
+                for (const auto & c : A[id]) {
+                    string s;
+                    s.push_back(c);
+                    next.push_back(s);
+                }
+            }
+            else {
+                for (auto & s : result) {
+                    for (const auto & c : A[id]) {
+                        s.push_back(c);
+                        next.push_back(s);
+                        s.pop_back();
+                    }
+                }
+            }
+            result = next;
         }
-        string path;
-        vector<string> A;
-        A.push_back("abc");
-        A.push_back("def");
-        A.push_back("ghi");
-        A.push_back("jkl");
-        A.push_back("mno");
-        A.push_back("pqrs");
-        A.push_back("tuv");
-        A.push_back("wxyz");
-        f1(result, path, A, digits, 0);
         return result;
-    }
-private:
-    void f1(vector<string> & result, string & path, vector<string> & A, string & digits, int begin) {
-        int end = digits.size();
-        if (begin == end) {
-            result.push_back(path);
-            return;
-        }
-        int id = digits[begin] - '2';
-        for (const auto & i : A[id]) {
-            path.push_back(i);
-            f1(result, path, A, digits, begin + 1);
-            path.pop_back();
-        }
     }
 };
 
