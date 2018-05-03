11a927cc5169d2e464e549f4e091a2d2f456e493
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-05-03 01:39:46 -0700

    Update integer-to-roman.cpp

--- Algorithms/integer-to-roman.cpp
+++ Algorithms/integer-to-roman.cpp
@@ -20,13 +20,13 @@ using namespace std;
 
 class Solution {
 public:
-	string intToRoman(int num) {
-		string M[] = {"", "M", "MM", "MMM"};
-		string C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-		string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-		string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-		return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
-	}
+    string intToRoman(int num) {
+        vector<string> A = {"", "M", "MM", "MMM"};
+        vector<string> B = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        vector<string> C = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        vector<string> D = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return A[num / 1000] + B[(num % 1000) / 100] + C[(num % 100) / 10] + D[num % 10];
+    }
 };
 
 int main(void) {
