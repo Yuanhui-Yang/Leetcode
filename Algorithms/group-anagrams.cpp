d0241fa2def157649b5eea3da70b832def8e70d1
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-05-07 16:52:25 -0700

    Update group-anagrams.cpp

--- Algorithms/group-anagrams.cpp
+++ Algorithms/group-anagrams.cpp
@@ -17,6 +17,45 @@ Note: All inputs will be in lower-case.
 
 class Solution {
 public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<size_t, vector<string>> A;
+        for (const auto & str : strs) {
+            A[f2(str)].push_back(str);
+        }
+        vector<vector<string>> result;
+        for (const auto & i : A) {
+            result.push_back(i.second);
+        }
+        return result;
+    }
+private:
+    string f1(const string & str) {
+        string result;
+        array<int, 26> A;
+        A.fill(26);
+        for (const auto & ch : str) {
+            int id = ch - 'a';
+            ++A[id];
+        }
+        for (int i = 0; i < 26; ++i) {
+            result.append(to_string(A[i]));
+            result.push_back('#');
+        }
+        return result;
+    }
+    size_t f2(const string & str) {
+        array<size_t, 26> A = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
+        size_t result = 1;
+        for (const auto & ch : str) {
+            int id = ch - 'a';
+            result *= A[id];
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
 	vector<vector<string>> groupAnagrams(vector<string>& strs) {
 		unordered_map<string, list<int>> A;
 		for (int sz = strs.size(), i = 0; i < sz; ++i) {
