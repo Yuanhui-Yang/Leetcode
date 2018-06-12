91830189b01f60df9ff7a6a678b87604e177e615
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-06-12 15:35:34 -0700

    Create two-sum.cs

---
+++ Algorithms/two-sum.cs
@@ -0,0 +1,32 @@
+1. Two Sum
+https://leetcode.com/problems/two-sum/
+
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public static int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> A = new Dictionary<int, int>();
+        for (int sz = nums.Length, i = 0; i < sz; ++i) {
+            int val = nums[i], complement = target - val;
+            if (A.ContainsKey(complement)) {
+                return new int[] {A[complement], i};
+            }
+            A[val] = i;
+        }
+        return null;
+    }
+    public static void Main() {
+        int[] nums, result;
+        int target;
+        
+        nums = new int[] {2, 7, 11, 15};
+        target = 9;
+        result = TwoSum(nums, target);
+        foreach (int i in result) {
+            Console.Write(i);
+            Console.Write(' ');
+        }
+        Console.WriteLine();
+    }
+}
