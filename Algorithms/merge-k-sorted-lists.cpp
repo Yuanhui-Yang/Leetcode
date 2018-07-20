55bfb87090638d5cd7e74993e062e0614c1bd517
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2018-07-20 16:35:25 -0700

    Update merge-k-sorted-lists.cpp

--- Algorithms/merge-k-sorted-lists.cpp
+++ Algorithms/merge-k-sorted-lists.cpp
@@ -12,6 +12,35 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
  * };
  */
 
+ class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode * dummy = new ListNode(-1), * head = dummy;
+        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>> pq;
+        for (int sz = lists.size(), i = 0; i < sz; ++i) {
+            ListNode * node = lists[i];
+            if (node) {
+                array<int, 2> element = {node->val, i};
+                pq.push(element);
+            }
+        }
+        while (!pq.empty()) {
+            array<int, 2> top = pq.top();
+            pq.pop();
+            int index = top[1];
+            ListNode * node = lists[index];
+            head->next = node;
+            head = head->next;
+            node = lists[index] = lists[index]->next;
+            if (node) {
+                array<int, 2> element = {node->val, index};
+                pq.push(element);
+            }
+        }
+        return dummy->next;
+    }
+};
+    
 struct Comp {
     bool operator() (const ListNode * a, const ListNode * b) {
         return a->val > b->val;
