ec23f4fe662ad961facc4ffb5e4e7dad16887506
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-09-05 23:45:30 -0700

    889. Construct Binary Tree from Preorder and Postorder Traversal

--- Algorithms/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ Algorithms/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,43 +1,23 @@
-887. Super Egg Drop
-https://leetcode.com/problems/super-egg-drop/
+889. Construct Binary Tree from Preorder and Postorder Traversal
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
 
-You are given K eggs, and you have access to a building with N floors from 1 to N. 
+Return any binary tree that matches the given preorder and postorder traversals.
 
-Each egg is identical in function, and if an egg breaks, you cannot drop it again.
-
-You know that there exists a floor F with 0 <= F <= N such that any egg dropped at a floor higher than F will break, and any egg dropped at or below floor F will not break.
-
-Each move, you may take an egg (if you have an unbroken one) and drop it from any floor X (with 1 <= X <= N). 
-
-Your goal is to know with certainty what the value of F is.
-
-What is the minimum number of moves that you need to know with certainty what F is, regardless of the initial value of F?
+Values in the traversals pre and post are distinct positive integers.
 
  
 
 Example 1:
 
-Input: K = 1, N = 2
-Output: 2
-Explanation: 
-Drop the egg from floor 1.  If it breaks, we know with certainty that F = 0.
-Otherwise, drop the egg from floor 2.  If it breaks, we know with certainty that F = 1.
-If it didn't break, then we know with certainty F = 2.
-Hence, we needed 2 moves in the worst case to know what F is with certainty.
-Example 2:
-
-Input: K = 2, N = 6
-Output: 3
-Example 3:
-
-Input: K = 3, N = 14
-Output: 4
+Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
  
 
 Note:
 
-1 <= K <= 100
-1 <= N <= 10000
+1 <= pre.length == post.length <= 30
+pre[] and post[] are both permutations of 1, 2, ..., pre.length.
+It is guaranteed an answer exists. If there exists multiple answers, you can return any of them.
 
 #include <iostream>
 #include <vector>
@@ -94,6 +74,9 @@ private:
 
 int main(void) {
     Solution solution;
-
+    vector<int> pre = {1, 2, 4, 5, 3, 6, 7};
+    vector<int> post = {4, 5, 2, 6, 7, 3, 1};
+    TreeNode * result = solution.constructFromPrePost(pre, post);
+    
     return 0;
 }
