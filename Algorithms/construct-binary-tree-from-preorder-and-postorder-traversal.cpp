874493cc75ef588fc6f487c85e34a7d5429db4a8
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2018-09-22 19:57:03 -0700

    889. Construct Binary Tree from Preorder and Postorder Traversal

--- Algorithms/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ Algorithms/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,23 +1,23 @@
-// 889. Construct Binary Tree from Preorder and Postorder Traversal
-// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+889. Construct Binary Tree from Preorder and Postorder Traversal
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
 
-// Return any binary tree that matches the given preorder and postorder traversals.
+Return any binary tree that matches the given preorder and postorder traversals.
 
-// Values in the traversals pre and post are distinct positive integers.
+Values in the traversals pre and post are distinct positive integers.
 
  
 
-// Example 1:
+Example 1:
 
-// Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
-// Output: [1,2,3,4,5,6,7]
+Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
  
 
-// Note:
+Note:
 
-// 1 <= pre.length == post.length <= 30
-// pre[] and post[] are both permutations of 1, 2, ..., pre.length.
-// It is guaranteed an answer exists. If there exists multiple answers, you can return any of them.
+1 <= pre.length == post.length <= 30
+pre[] and post[] are both permutations of 1, 2, ..., pre.length.
+It is guaranteed an answer exists. If there exists multiple answers, you can return any of them.
 
 #include <iostream>
 #include <vector>
