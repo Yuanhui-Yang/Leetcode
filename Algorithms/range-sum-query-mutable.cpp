ce022485a327c09c1e335a5539813af5f07b7cec
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2019-02-18 19:00:16 -0800

    Update range-sum-query-mutable.cpp

--- Algorithms/range-sum-query-mutable.cpp
+++ Algorithms/range-sum-query-mutable.cpp
@@ -6,54 +6,52 @@
 using namespace std;
 class NumArray {
 public:
-	NumArray(vector<int> &nums) {
-		this->tree.resize(nums.size() + 1, 0);
-		for (size_t i = 0; i < nums.size(); ++i)
-			this->update(i, nums[i]);
-	}
-
-	void update(int i, int val) {
-		this->add(i + 1, val - this->readSingle(i + 1));
-	}
-
-	int sumRange(int i, int j) {
-		return this->read(j + 1) - this->read(i);
-	}
+    NumArray(vector<int> nums) {
+        int sz = nums.size();
+        this->BIT = vector<int>(sz + 1, 0);
+        for (int i = 0; i < sz; ++i)
+        {
+            f2(i + 1, nums[i]);
+        }
+    }
+    
+    void update(int i, int val) {
+        int delta = val - f1(i + 1) + f1(i);
+        f2(i + 1, delta);
+    }
+    
+    int sumRange(int i, int j) {
+        return f1(j + 1) - f1(i);
+    }
 private:
-	vector<int> tree;
-	int readSingle(size_t i) {
-		int result = this->tree[i];
-		if (i == 0) return result;
-		size_t lowerBound = i - (i & (-i));
-		--i;
-		while (lowerBound != i) {
-			result -= this->tree[i];
-			i -= i & (-i);
-		}
-		return result;
-	}
-	int read(size_t i) {
-		int result = 0;
-		while (i != 0) {
-			result += this->tree[i];
-			i -= i & (-i);
-		}
-		return result;
-	}
-	void add(size_t i, int val) {
-		while (i < this->tree.size()) {
-			this->tree[i] += val;
-			i += i & (-i);
-		}
-	}
+    vector<int> BIT;
+    int f1(int bound)
+    {
+        int result = 0;
+        while (bound > 0)
+        {
+            result += BIT[bound];
+            bound -= bound & (-bound);
+        }
+        return result;
+    }
+    void f2(int i, int val)
+    {
+        int sz = BIT.size();
+        while (i < sz)
+        {
+            BIT[i] += val;
+            i += i & (-i);
+        }
+    }
 };
 
-
-// Your NumArray object will be instantiated and called as such:
-// NumArray numArray(nums);
-// numArray.sumRange(0, 1);
-// numArray.update(1, 10);
-// numArray.sumRange(1, 2);
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
 
 int main(void) {
 	vector<int> nums = {1, 3, 5};
