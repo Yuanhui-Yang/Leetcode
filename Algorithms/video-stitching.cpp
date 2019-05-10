036809fe655ec2da4f38d4aa59df22bececd5fb2
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-05-10 05:25:19 -0700

    1024. Video Stitching

---
+++ Algorithms/video-stitching.cpp
@@ -0,0 +1,93 @@
+1024. Video Stitching
+https://leetcode.com/problems/video-stitching/
+
+You are given a series of video clips from a sporting event that lasted T seconds.  These video clips can be overlapping with each other and have varied lengths.
+
+Each video clip clips[i] is an interval: it starts at time clips[i][0] and ends at time clips[i][1].  We can cut these clips into segments freely: for example, a clip [0, 7] can be cut into segments [0, 1] + [1, 3] + [3, 7].
+
+Return the minimum number of clips needed so that we can cut the clips into segments that cover the entire sporting event ([0, T]).  If the task is impossible, return -1.
+
+ 
+
+Example 1:
+
+Input: clips = [[0,2],[4,6],[8,10],[1,9],[1,5],[5,9]], T = 10
+Output: 3
+Explanation: 
+We take the clips [0,2], [8,10], [1,9]; a total of 3 clips.
+Then, we can reconstruct the sporting event as follows:
+We cut [1,9] into segments [1,2] + [2,8] + [8,9].
+Now we have segments [0,2] + [2,8] + [8,10] which cover the sporting event [0, 10].
+Example 2:
+
+Input: clips = [[0,1],[1,2]], T = 5
+Output: -1
+Explanation: 
+We can't cover [0,5] with only [0,1] and [0,2].
+Example 3:
+
+Input: clips = [[0,1],[6,8],[0,2],[5,6],[0,4],[0,3],[6,7],[1,3],[4,7],[1,4],[2,5],[2,6],[3,4],[4,5],[5,7],[6,9]], T = 9
+Output: 3
+Explanation: 
+We can take clips [0,4], [4,7], and [6,9].
+Example 4:
+
+Input: clips = [[0,4],[2,8]], T = 5
+Output: 2
+Explanation: 
+Notice you can have extra video after the event ends.
+ 
+
+Note:
+
+1 <= clips.length <= 100
+0 <= clips[i][0], clips[i][1] <= 100
+0 <= T <= 100
+
+struct Comp
+{
+    bool operator() (vector<int> & a, vector<int> & b)
+    {
+        return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];
+    }
+};
+
+class Solution {
+public:
+    int videoStitching(vector<vector<int>>& clips, int T) {
+        sort(clips.begin(), clips.end(), Comp());
+        int sz = clips.size(), result = 1, left = clips[0][0], right = clips[0][1], i = 0;
+        if (0 < left)
+        {
+            return -1;
+        }
+        if (T <= right)
+        {
+            return 1;
+        }
+        while (i < sz and right < T)
+        {
+            int x = clips[i][0], y = clips[i][1];
+            if (y <= right)
+            {
+                ++i;
+            }
+            else if (right < x)
+            {
+                return -1;
+            }
+            else 
+            {
+                ++result;
+                int next_right = right;
+                while (i < sz and next_right < T and clips[i][0] <= right)
+                {
+                    next_right = max(next_right, clips[i][1]);
+                    ++i;
+                }
+                right = next_right;
+            }
+        }
+        return left <= 0 and T <= right ? result : -1;
+    }
+};
