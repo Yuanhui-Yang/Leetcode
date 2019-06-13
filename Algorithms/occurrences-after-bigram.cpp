ce83b468db22cedfc5f703fcd6b69cafbfc0cd5f
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-06-13 08:46:53 -0700

    1078. Occurrences After Bigram

--- Algorithms/occurrences-after-bigram.cpp
+++ Algorithms/occurrences-after-bigram.cpp
@@ -1,4 +1,4 @@
-1083. Occurrences After Bigram
+1078. Occurrences After Bigram
 https://leetcode.com/problems/occurrences-after-bigram/
 
 Given words first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
