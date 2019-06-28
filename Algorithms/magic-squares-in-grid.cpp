6480484f4ab85594f3143756c3a070149b7bd288
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-06-28 05:10:42 -0700

    840. Magic Squares In Grid

--- Algorithms/magic-squares-in-grid.cpp
+++ Algorithms/magic-squares-in-grid.cpp
@@ -31,3 +31,45 @@ Note:
 1 <= grid[0].length <= 10
 0 <= grid[i][j] <= 15
 
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int X = grid.size(), Y = X ? grid[0].size() : 0, result = 0;
+        array<int, 3> rows, columns;
+        array<int, 2> diagonals;
+        for (int x = 0; x + 3 <= X; ++x)
+        {
+            for (int y = 0; y + 3 <= Y; ++y)
+            {
+                int mask = 0;
+                rows.fill(0);
+                columns.fill(0);
+                diagonals.fill(0);
+                for (int i = 0; i < 3; ++i)
+                {
+                    int val = 0;
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        val = grid[x + i][y + j];
+                        if (1 <= val and val <= 9)
+                        {
+                            mask |= (1 << (val - 1));
+                        }
+                        rows[i] += val;
+                        columns[j] += val;
+                    }
+                    val = grid[x + i][y + i];
+                    diagonals[0] += val;
+                    val = grid[x + i][y + 2 - i];
+                    diagonals[1] += val;
+                }
+                int sum = rows[0];
+                if (mask + 1 == (1 << 9) and sum == rows[1] and sum == rows[2] and sum == columns[0] and sum == columns[1] and sum == columns[2] and sum == diagonals[0] and sum == diagonals[1])
+                {
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+};
