6794a944da4c9616faf58fed79af0385c1def250
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-09-18 01:19:57 -0700

    243. Shortest Word Distance

--- Algorithms/shortest-word-distance.cpp
+++ Algorithms/shortest-word-distance.cpp
@@ -1,62 +1,69 @@
-// 243. Shortest Word Distance
-// https://leetcode.com/problems/shortest-word-distance/
-
 /*
-Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
-For example,
-Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
-Given word1 = “coding”, word2 = “practice”, return 3.
-Given word1 = "makes", word2 = "coding", return 1.
-
-Note:
-You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
+ * @lc app=leetcode id=243 lang=cpp
+ *
+ * [243] Shortest Word Distance
+ *
+ * https://leetcode.com/problems/shortest-word-distance/description/
+ *
+ * algorithms
+ * Easy (58.42%)
+ * Likes:    306
+ * Dislikes: 30
+ * Total Accepted:    75K
+ * Total Submissions: 128.1K
+ * Testcase Example:  '["practice", "makes", "perfect", "coding", "makes"]\n"coding"\n"practice"'
+ *
+ * Given a list of words and two words word1 and word2, return the shortest
+ * distance between these two words in the list.
+ * 
+ * Example:
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ * 
+ * 
+ * Input: word1 = “coding”, word2 = “practice”
+ * Output: 3
+ * 
+ * 
+ * 
+ * Input: word1 = "makes", word2 = "coding"
+ * Output: 1
+ * 
+ * 
+ * Note:
+ * You may assume that word1 does not equal to word2, and word1 and word2 are
+ * both in the list.
+ * 
+ */
 class Solution {
 public:
-	int shortestDistance(vector<string>& words, string word1, string word2) {
-		int n = words.size(), result = n;
-		for (int i = 0, x = -1, y = -1; i < n; ++i) {
-			const string& word = words[i];
-			if (word == word1) {
-				x = i;
-			}
-			if (word == word2) {
-				y = i;
-			}
-			if (x >= 0 and y >= 0) {
-				result = min(result, abs(x - y));
-			}
-		}
-		return result;
-	}
+    int shortestDistance(vector<string>& words, string word1, string word2) {
+        unordered_map<string, vector<int>> A = f1(words);
+        vector<int>& x = A[word1], &y = A[word2];
+        int sz1 = x.size(), sz2 = y.size(), i = 0, j = 0, result = words.size();
+        while (i < sz1 and j < sz2)
+        {
+            int a = x[i], b = y[j];
+            result = min(result, abs(a - b));
+            if (a < b)
+            {
+                ++i;
+            }
+            else
+            {
+                ++j;
+            }
+        }
+        return result;
+    }
+private:
+    unordered_map<string, vector<int>> f1(vector<string>& words)
+    {
+        unordered_map<string, vector<int>> result;
+        for (int sz = words.size(), i = 0; i < sz; ++i)
+        {
+            result[words[i]].push_back(i);
+        }
+        return result;
+    }
 };
 
-int main(void) {
-	Solution solution;
-	vector<string> words;
-	string word1, word2;
-	int answer, result;
-
-	words = {"practice", "makes", "perfect", "coding", "makes"};
-	word1 = "coding";
-	word2 = "practice";
-	answer = 3;
-	result = solution.shortestDistance(words, word1, word2);
-	assert(answer == result);
-
-	words = {"practice", "makes", "perfect", "coding", "makes"};
-	word1 = "makes";
-	word2 = "coding";
-	answer = 1;
-	result = solution.shortestDistance(words, word1, word2);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
