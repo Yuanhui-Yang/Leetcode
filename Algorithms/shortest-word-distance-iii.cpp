7bf66a6e807d89178642e6a1c59caafd40a2ebd8
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-09-18 02:23:32 -0700

    245. Shortest Word Distance III

--- Algorithms/shortest-word-distance-iii.cpp
+++ Algorithms/shortest-word-distance-iii.cpp
@@ -1,71 +1,86 @@
-// 245. Shortest Word Distance III
-// https://leetcode.com/problems/shortest-word-distance-iii/
-
 /*
-This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
-
-Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
-word1 and word2 may be the same and they represent two individual words in the list.
-
-For example,
-Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
-Given word1 = “makes”, word2 = “coding”, return 1.
-Given word1 = "makes", word2 = "makes", return 3.
-
-Note:
-You may assume word1 and word2 are both in the list.
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
+ * @lc app=leetcode id=245 lang=cpp
+ *
+ * [245] Shortest Word Distance III
+ *
+ * https://leetcode.com/problems/shortest-word-distance-iii/description/
+ *
+ * algorithms
+ * Medium (54.03%)
+ * Likes:    135
+ * Dislikes: 59
+ * Total Accepted:    41.9K
+ * Total Submissions: 77.4K
+ * Testcase Example:  '["practice", "makes", "perfect", "coding", "makes"]\n"makes"\n"coding"'
+ *
+ * Given a list of words and two words word1 and word2, return the shortest
+ * distance between these two words in the list.
+ * 
+ * word1 and word2 may be the same and they represent two individual words in
+ * the list.
+ * 
+ * Example:
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ * 
+ * 
+ * Input: word1 = “makes”, word2 = “coding”
+ * Output: 1
+ * 
+ * 
+ * 
+ * Input: word1 = "makes", word2 = "makes"
+ * Output: 3
+ * 
+ * 
+ * Note:
+ * You may assume word1 and word2 are both in the list.
+ * 
+ */
 class Solution {
 public:
-	int shortestWordDistance(vector<string>& words, string word1, string word2) {
-		int n = words.size(), result = n;
-		for (int i = 0, x = -1, y = -1; i < n; ++i) {
-			const string &word = words[i];
-			if (word == word1 and word == word2) {
-				x < y ? x = i : y = i;
-			}
-			else if (word == word1 or word == word2) {
-				if (word == word1) {
-					x = i;
-				}
-				if (word == word2) {
-					y = i;
-				}
-			}
-			if (x >= 0 and y >= 0) {
-				result = min(result, abs(x - y));
-			}
-		}
-		return result;
-	}
+    int shortestWordDistance(vector<string>& words, string word1, string word2) {
+        int sz = words.size(), result = sz;
+        for (int i = 0, a = -1, b = -1; i < sz; ++i)
+        {
+            string& word = words[i];
+            if (word1 == word2)
+            {
+                if (word == word1)
+                {
+                    if (a < 0)
+                    {
+                        a = i;
+                    }
+                    else if (b < 0)
+                    {
+                        b = i;
+                    }
+                    else if (a < b)
+                    {
+                        a = i;
+                    }
+                    else
+                    {
+                        b = i;
+                    }
+                }
+            }
+            else
+            {
+                if (word == word1)
+                {
+                    a = i;
+                }
+                else if (word == word2)
+                {
+                    b = i;
+                }
+            }
+            if (a >= 0 and b >= 0)
+            {
+                result = min(result, abs(a - b));
+            }
+        }
+        return result;
+    }
 };
-
-int main(void) {
-	Solution solution;
-	vector<string> words;
-	string word1, word2;
-	int answer, result;
-
-	words = {"practice", "makes", "perfect", "coding", "makes"};
-	word1 = "makes";
-	word2 = "coding";
-	answer = 1;
-	result = solution.shortestWordDistance(words, word1, word2);
-	assert(answer == result);
-
-	words = {"practice", "makes", "perfect", "coding", "makes"};
-	word1 = "makes";
-	word2 = "makes";
-	answer = 3;
-	result = solution.shortestWordDistance(words, word1, word2);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
