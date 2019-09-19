87d486eefae8c8a15ce220b7eb1e852f7ad29df5
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-09-19 09:00:46 -0700

    77. Combinations

--- Algorithms/combinations.cpp
+++ Algorithms/combinations.cpp
@@ -1,61 +1,60 @@
-// 77. Combinations
-// https://leetcode.com/problems/combinations/
-
 /*
-Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
-
-For example,
-If n = 4 and k = 2, a solution is:
-
-{
-  {2,4},
-  {3,4},
-  {2,3},
-  {1,2},
-  {1,3},
-  {1,4},
-}
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
+ * @lc app=leetcode id=77 lang=cpp
+ *
+ * [77] Combinations
+ *
+ * https://leetcode.com/problems/combinations/description/
+ *
+ * algorithms
+ * Medium (49.62%)
+ * Likes:    927
+ * Dislikes: 52
+ * Total Accepted:    227.2K
+ * Total Submissions: 456.2K
+ * Testcase Example:  '4\n2'
+ *
+ * Given two integers n and k, return all possible combinations of k numbers
+ * out of 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: n = 4, k = 2
+ * Output:
+ * [
+ * ⁠ [2,4],
+ * ⁠ [3,4],
+ * ⁠ [2,3],
+ * ⁠ [1,2],
+ * ⁠ [1,3],
+ * ⁠ [1,4],
+ * ]
+ * 
+ * 
+ */
 class Solution {
 public:
-	vector<vector<int>> combine(int n, int k) {
-		vector<int> v(n), w;
-		iota(begin(v), end(v), 1);
-		vector<vector<int>> result;
-		dfs(result, v, w, 0, n, k);
-		return result;
-	}
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> path;
+        f1(result, path, 1, n, k);
+        return result;
+    }
 private:
-	void dfs(vector<vector<int>>& result, const vector<int>& v, vector<int>& w, int i, const size_t n, const size_t k) {
-		if (w.size() == k) {
-			result.push_back(w);
-			return;
-		}
-		for (size_t j = i; j < n; j++) {
-			w.push_back(v[j]);
-			dfs(result, v, w, j + 1, n, k);
-			w.pop_back();
-		}
-	}
+    void f1(vector<vector<int>>& result, vector<int>& path, int lower, int upper, int k)
+    {
+        if (k == 0)
+        {
+            result.push_back(path);
+        }
+        else if (k > 0)
+        {
+            for (int i = lower; i <= upper; ++i)
+            {
+                path.push_back(i);
+                f1(result, path, i + 1, upper, k - 1);
+                path.pop_back();
+            }
+        }
+    }
 };
-
-int main(void) {
-	Solution solution;
-	int n, k;
-	vector<vector<int>> result, answer;
-
-	n = 4;
-	k = 2;
-	answer = {{2, 4}, {3, 4}, {2, 3}, {1, 2}, {1, 3}, {1, 4}};
-	sort(begin(answer), end(answer));
-	result = solution.combine(n, k);
-	sort(begin(result), end(result));
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
