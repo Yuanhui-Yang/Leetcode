68e9bd59a9c2b999a1e12e4dfb2d2649b663f861
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-09-19 02:24:39 -0700

    58. Length of Last Word

--- Algorithms/length-of-last-word.cpp
+++ Algorithms/length-of-last-word.cpp
@@ -1,49 +1,49 @@
-// 58. Length of Last Word
-// https://leetcode.com/problems/length-of-last-word/
-
 /*
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
-
-If the last word does not exist, return 0.
-
-Note: A word is defined as a character sequence consists of non-space characters only.
-
-For example, 
-Given s = "Hello World",
-return 5.
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
+ * @lc app=leetcode id=58 lang=cpp
+ *
+ * [58] Length of Last Word
+ *
+ * https://leetcode.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (32.28%)
+ * Likes:    440
+ * Dislikes: 1812
+ * Total Accepted:    297.3K
+ * Total Submissions: 920.8K
+ * Testcase Example:  '"Hello World"'
+ *
+ * Given a string s consists of upper/lower-case alphabets and empty space
+ * characters ' ', return the length of last word in the string.
+ * 
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a character sequence consists of non-space
+ * characters only.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "Hello World"
+ * Output: 5
+ * 
+ * 
+ * 
+ * 
+ */
 class Solution {
 public:
-	int lengthOfLastWord(string s) {
-		if (s.empty()) {
-			return 0;
-		}
-		int n = s.size(), i = n - 1;
-		while (i >= 0 and s[i] == ' ') {
-			i--;
-		}
-		int j = i;
-		while (i >= 0 and isalpha(s[i])) {
-			--i;
-		}
-		return j - i;
-	}
+    int lengthOfLastWord(string s) {
+        int sz = s.size(), i = sz - 1;
+        while (i >= 0 and s[i] == ' ')
+        {
+            --i;
+            --sz;
+        }
+        while (i >= 0 and s[i] != ' ')
+        {
+            --i;
+        }
+        return sz - i - 1;
+    }
 };
-
-int main(void) {
-	Solution solution;
-	string s;
-	int answer, result;
-
-	s = "Hello World";
-	answer = 5;
-	result = solution.lengthOfLastWord(s);
-	assert(answer == result);
-
-	cout << "\nPassed All\n";
-	return 0;
-}
