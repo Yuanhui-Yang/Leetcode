90abcdacc338858b080badf90019292d8a158b5f
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-09-20 05:56:14 -0700

    156. Binary Tree Upside Down

--- Algorithms/binary-tree-upside-down.cpp
+++ Algorithms/binary-tree-upside-down.cpp
@@ -1,39 +1,66 @@
-// 156. Binary Tree Upside Down
-// https://leetcode.com/problems/binary-tree-upside-down/
-
 /*
-Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
-
-For example:
-Given a binary tree {1,2,3,4,5},
-	1
-   / \
-  2   3
- / \
-4   5
-return the root of the binary tree [4,5,2,#,#,3,1].
-   4
-  / \
- 5   2
-	/ \
-   3   1  
-confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
-
-
-OJ's Binary Tree Serialization:
-The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
-
-Here's an example:
-   1
-  / \
- 2   3
-	/
-   4
-	\
-	 5
-The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
-*/
-
+ * @lc app=leetcode id=156 lang=cpp
+ *
+ * [156] Binary Tree Upside Down
+ *
+ * https://leetcode.com/problems/binary-tree-upside-down/description/
+ *
+ * algorithms
+ * Medium (52.11%)
+ * Likes:    201
+ * Dislikes: 667
+ * Total Accepted:    51.7K
+ * Total Submissions: 99K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a binary tree where all the right nodes are either leaf nodes with a
+ * sibling (a left node that shares the same parent node) or empty, flip it
+ * upside down and turn it into a tree where the original right nodes turned
+ * into left leaf nodes. Return the new root.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   3
+ * ⁠/ \
+ * 4   5
+ * 
+ * Output: return the root of the binary tree [4,5,2,#,#,3,1]
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠5   2
+ * ⁠   / \
+ * ⁠  3   1  
+ * 
+ * 
+ * Clarification:
+ * 
+ * Confused what [4,5,2,#,#,3,1] means? Read more below on how binary tree is
+ * serialized on OJ.
+ * 
+ * The serialization of a binary tree follows a level order traversal, where
+ * '#' signifies a path terminator where no node exists below.
+ * 
+ * Here's an example:
+ * 
+ * 
+ * ⁠  1
+ * ⁠ / \
+ * ⁠2   3
+ * ⁠   /
+ * ⁠  4
+ * ⁠   \
+ * ⁠    5
+ * 
+ * 
+ * The above binary tree is serialized as [1,2,3,#,#,4,#,#,5].
+ * 
+ */
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -45,23 +72,26 @@ The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
  */
 class Solution {
 public:
-	TreeNode* upsideDownBinaryTree(TreeNode* root) {
-		if (!root) {
-			return NULL;
-		}
-		if (!root->left) {
-			return root;
-		}
-		TreeNode * left = root->left, * right = root->right;
-		root->left = NULL;
-		root->right = NULL;
-		TreeNode * result = upsideDownBinaryTree(left);
-		TreeNode * node = result;
-		while (node->right) {
-			node = node->right;
-		}
-		node->left = right;
-		node->right = root;
-		return result;
-	}
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        if (!root)
+        {
+            return root;
+        }
+        TreeNode* left = root->left, *right = root->right;
+        root->left = NULL;
+        root->right = NULL;
+        if (!left and !right)
+        {
+            return root;
+        }
+        left = upsideDownBinaryTree(left);
+        TreeNode* node = left;
+        while (node->right)
+        {
+            node = node->right;
+        }
+        node->left = right;
+        node->right = root;
+        return left;
+    }
 };
