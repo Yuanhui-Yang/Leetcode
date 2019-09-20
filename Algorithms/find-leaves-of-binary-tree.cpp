cbd3039456be829e4ff8060729305ae89644dc88
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2019-09-20 08:49:00 -0700

    Update find-leaves-of-binary-tree.cpp

--- Algorithms/find-leaves-of-binary-tree.cpp
+++ Algorithms/find-leaves-of-binary-tree.cpp
@@ -1,33 +1,65 @@
-// 366. Find Leaves of Binary Tree
-// https://leetcode.com/problems/find-leaves-of-binary-tree/
-
 /*
-Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
-
-Example:
-Given binary tree 
-		  1
-		 / \
-		2   3
-	   / \     
-	  4   5    
-Returns [4, 5, 3], [2], [1].
-
-Explanation:
-1. Removing the leaves [4, 5, 3] would result in this tree:
-
-		  1
-		 / 
-		2          
-2. Now removing the leaf [2] would result in this tree:
-
-		  1          
-3. Now removing the leaf [1] would result in the empty tree:
-
-		  []         
-Returns [4, 5, 3], [2], [1].
-*/
-
+ * @lc app=leetcode id=366 lang=cpp
+ *
+ * [366] Find Leaves of Binary Tree
+ *
+ * https://leetcode.com/problems/find-leaves-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (67.39%)
+ * Likes:    691
+ * Dislikes: 10
+ * Total Accepted:    52K
+ * Total Submissions: 77.1K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a binary tree, collect a tree's nodes as if you were doing this:
+ * Collect and remove all leaves, repeat until the tree is empty.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * 
+ * 1
+ * ⁠        / \
+ * ⁠       2   3
+ * ⁠      / \     
+ * ⁠     4   5    
+ * 
+ * Output: [[4,5,3],[2],[1]]
+ * 
+ * 
+ * 
+ * 
+ * Explanation:
+ * 
+ * 1. Removing the leaves [4,5,3] would result in this tree:
+ * 
+ * 
+ * ⁠         1
+ * ⁠        / 
+ * ⁠       2          
+ * 
+ * 
+ * 
+ * 
+ * 2. Now removing the leaf [2] would result in this tree:
+ * 
+ * 
+ * ⁠         1          
+ * 
+ * 
+ * 
+ * 
+ * 3. Now removing the leaf [1] would result in the empty tree:
+ * 
+ * 
+ * ⁠         []         
+ * 
+ */
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -39,36 +71,26 @@ Returns [4, 5, 3], [2], [1].
  */
 class Solution {
 public:
-	vector<vector<int>> findLeaves(TreeNode* root) {
-		vector<vector<int>> result;
-		if (!root) {
-			return result;
-		}
-		for (vector<int> v = f(NULL, root); !v.empty(); v = f(NULL, root)) {
-			result.push_back(v);
-		}
-		return result;
-	}
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<vector<int>> result;
+        f1(result, root);
+        return result;
+    }
 private:
-	vector<int> f(TreeNode * parent, TreeNode * & current) {
-		vector<int> result;
-		if (!current) {
-			return result;
-		}
-		if (!current->left and !current->right) {
-			result.push_back(current->val);
-			if (parent and parent->left == current) {
-				parent->left = NULL;
-			}
-			if (parent and parent->right == current) {
-				parent->right = NULL;
-			}
-			current = NULL;
-			return result;
-		}
-		vector<int> a = f(current, current->left), b = f(current, current->right);
-		result.insert(end(result), begin(a), end(a));
-		result.insert(end(result), begin(b), end(b));
-		return result;
-	}
+    int f1(vector<vector<int>>& result, TreeNode* root)
+    {
+        if (!root)
+        {
+            return -1;
+        }
+        TreeNode* left = root->left, *right = root->right;
+        int height = max(f1(result, left), f1(result, right));
+        ++height;
+        if (result.size() <= height)
+        {
+            result.push_back(vector<int>());
+        }
+        result[height].push_back(root->val);
+        return height;
+    }
 };
