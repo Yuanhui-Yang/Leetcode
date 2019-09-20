05d8193d276af5fd1d43b502cfb3eafd0d96e29e
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2019-09-20 10:30:18 -0700

    Update second-minimum-node-in-a-binary-tree.cpp

--- Algorithms/second-minimum-node-in-a-binary-tree.cpp
+++ Algorithms/second-minimum-node-in-a-binary-tree.cpp
@@ -1,33 +1,61 @@
-// 671. Second Minimum Node In a Binary Tree
-// https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
-
 /*
-Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes.
-
-Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
-
-If no such second minimum value exists, output -1 instead.
-
-Example 1:
-Input: 
-	2
-   / \
-  2   5
-	 / \
-	5   7
-
-Output: 5
-Explanation: The smallest value is 2, the second smallest value is 5.
-Example 2:
-Input: 
-	2
-   / \
-  2   2
-
-Output: -1
-Explanation: The smallest value is 2, but there isn't any second smallest value.
-*/
-
+ * @lc app=leetcode id=671 lang=cpp
+ *
+ * [671] Second Minimum Node In a Binary Tree
+ *
+ * https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/description/
+ *
+ * algorithms
+ * Easy (43.12%)
+ * Likes:    425
+ * Dislikes: 641
+ * Total Accepted:    56K
+ * Total Submissions: 129.9K
+ * Testcase Example:  '[2,2,5,null,null,5,7]'
+ *
+ * Given a non-empty special binary tree consisting of nodes with the
+ * non-negative value, where each node in this tree has exactly two or zero
+ * sub-node. If the node has two sub-nodes, then this node's value is the
+ * smaller value among its two sub-nodes. More formally, the property root.val
+ * = min(root.left.val, root.right.val) always holds.
+ * 
+ * Given such a binary tree, you need to output the second minimum value in the
+ * set made of all the nodes' value in the whole tree.
+ * 
+ * If no such second minimum value exists, output -1 instead.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠    / \
+ * ⁠   5   7
+ * 
+ * Output: 5
+ * Explanation: The smallest value is 2, the second smallest value is 5.
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 2   2
+ * 
+ * Output: -1
+ * Explanation: The smallest value is 2, but there isn't any second smallest
+ * value.
+ * 
+ * 
+ * 
+ * 
+ */
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -39,29 +67,31 @@ Explanation: The smallest value is 2, but there isn't any second smallest value.
  */
 class Solution {
 public:
-	int findSecondMinimumValue(TreeNode* root) {
-		if (!root) {
-			return -1;
-		}
-		queue<TreeNode*> curr;
-		curr.push(root);
-		int a = root->val, b = INT_MAX;
-		while (!curr.empty()) {
-			TreeNode * front = curr.front(), * left = front->left, * right = front->right;
-			curr.pop();
-			if (left and left->val == a) {
-				curr.push(left);
-			}
-			else if (left and left->val < b) {
-				b = left->val;
-			}
-			if (right and right->val == a) {
-				curr.push(right);
-			}
-			else if (right and right->val < b) {
-				b = right->val;
-			}
-		}
-		return b == INT_MAX ? -1 : b;
-	}
+    int findSecondMinimumValue(TreeNode* root) {
+        long first = INT_MAX, second = INT_MAX;
+        ++first;
+        ++second;
+        f1(root, first, second);
+        return second <= INT_MAX ? second : -1;
+    }
+private:
+    void f1(TreeNode* root, long& first, long& second)
+    {
+        if (root)
+        {
+            long val = root->val;
+            TreeNode* left = root->left, * right = root->right;
+            if (val < first)
+            {
+                second = first;
+                first = val;
+            }
+            else if (first < val and val < second)
+            {
+                second = val;
+            }
+            f1(left, first, second);
+            f1(right, first, second);
+        }
+    }
 };
