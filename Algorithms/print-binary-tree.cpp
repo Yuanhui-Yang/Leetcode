2e118ed913e3dcc59550e27e69603dcb090c5ff3
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2019-10-03 05:03:57 -0700

    655. Print Binary Tree

--- Algorithms/print-binary-tree.cpp
+++ Algorithms/print-binary-tree.cpp
@@ -1,51 +1,90 @@
-// 655. Print Binary Tree
-// https://leetcode.com/problems/print-binary-tree/
-
 /*
-Print a binary tree in an m*n 2D string array following these rules:
+ * @lc app=leetcode id=655 lang=cpp
+ *
+ * [655] Print Binary Tree
+ *
+ * https://leetcode.com/problems/print-binary-tree/description/
+ *
+ * algorithms
+ * Medium (52.54%)
+ * Likes:    253
+ * Dislikes: 616
+ * Total Accepted:    24.2K
+ * Total Submissions: 46.1K
+ * Testcase Example:  '[1,2]'
+ *
+ * Print a binary tree in an m*n 2D string array following these rules: 
+ * 
+ * 
+ * The row number m should be equal to the height of the given binary tree.
+ * The column number n should always be an odd number.
+ * The root node's value (in string format) should be put in the exactly middle
+ * of the first row it can be put. The column and the row where the root node
+ * belongs will separate the rest space into two parts (left-bottom part and
+ * right-bottom part). You should print the left subtree in the left-bottom
+ * part and print the right subtree in the right-bottom part. The left-bottom
+ * part and the right-bottom part should have the same size. Even if one
+ * subtree is none while the other is not, you don't need to print anything for
+ * the none subtree but still need to leave the space as large as that for the
+ * other subtree. However, if two subtrees are none, then you don't need to
+ * leave space for both of them. 
+ * Each unused space should contain an empty string "".
+ * Print the subtrees following the same rules.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * ⁠    1
+ * ⁠   /
+ * ⁠  2
+ * Output:
+ * [["", "1", ""],
+ * ⁠["2", "", ""]]
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * ⁠    1
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \
+ * ⁠    4
+ * Output:
+ * [["", "", "", "1", "", "", ""],
+ * ⁠["", "2", "", "", "", "3", ""],
+ * ⁠["", "", "4", "", "", "", ""]]
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input:
+ * ⁠     1
+ * ⁠    / \
+ * ⁠   2   5
+ * ⁠  / 
+ * ⁠ 3 
+ * ⁠/ 
+ * 4 
+ * Output:
+ * 
+ * [["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
+ * ⁠["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
+ * ⁠["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
+ * ⁠["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
+ * 
+ * 
+ * 
+ * Note:
+ * The height of binary tree is in the range of [1, 10].
+ * 
+ */
 
-The row number m should be equal to the height of the given binary tree.
-The column number n should always be an odd number.
-The root node's value (in string format) should be put in the exactly middle of the first row it can be put. The column and the row where the root node belongs will separate the rest space into two parts (left-bottom part and right-bottom part). You should print the left subtree in the left-bottom part and print the right subtree in the right-bottom part. The left-bottom part and the right-bottom part should have the same size. Even if one subtree is none while the other is not, you don't need to print anything for the none subtree but still need to leave the space as large as that for the other subtree. However, if two subtrees are none, then you don't need to leave space for both of them.
-Each unused space should contain an empty string "".
-Print the subtrees following the same rules.
-Example 1:
-Input:
-	 1
-	/
-   2
-Output:
-[["", "1", ""],
- ["2", "", ""]]
-Example 2:
-Input:
-	 1
-	/ \
-   2   3
-	\
-	 4
-Output:
-[["", "", "", "1", "", "", ""],
- ["", "2", "", "", "", "3", ""],
- ["", "", "4", "", "", "", ""]]
-Example 3:
-Input:
-	  1
-	 / \
-	2   5
-   / 
-  3 
- / 
-4 
-Output:
-
-[["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
- ["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
- ["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
- ["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
-Note: The height of binary tree is in the range of [1, 10].
-*/
-
+// @lc code=start
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -57,29 +96,29 @@ Note: The height of binary tree is in the range of [1, 10].
  */
 class Solution {
 public:
-	vector<vector<string>> printTree(TreeNode* root) {
-		if (!root) {
-			return {};
-		}
-		int row = f(root), column = (1 << row) - 1;
-		vector<vector<string>> result(row, vector<string>(column));
-		g(root, result, 0, 0, column);
-		return result;
-	}
+    vector<vector<string>> printTree(TreeNode* root) {
+        if (!root)
+        {
+            return vector<vector<string>>();
+        }
+        int height = f1(root), width = (1 << height) - 1;
+        vector<vector<string>> result(height, vector<string>(width));
+        f1(result, root, 0, 0, width - 1);
+        return result;
+    }
 private:
-	int f(TreeNode * node) {
-		if (!node) {
-			return 0;
-		}
-		return 1 + max(f(node->left), f(node->right));
-	}
-	void g(TreeNode * node, vector<vector<string>> & result, int level, int begin, int end) {
-		if (!node) {
-			return;
-		}
-		int mid = begin + (end - begin) / 2;
-		result[level][mid] = to_string(node->val);
-		g(node->left, result, level + 1, begin, mid);
-		g(node->right, result, level + 1, mid + 1, end);
-	}
+    int f1(TreeNode* root)
+    {
+        return root ? 1 + max(f1(root->left), f1(root->right)) : 0;
+    }
+    void f1(vector<vector<string>>& result, TreeNode* root, int row, int left, int right)
+    {
+        if (root)
+        {
+            int mid = (left + right) >> 1;
+            result[row][mid] = to_string(root->val);
+            f1(result, root->left, row + 1, left, mid - 1);
+            f1(result, root->right, row + 1, mid + 1, right);
+        }
+    }
 };
