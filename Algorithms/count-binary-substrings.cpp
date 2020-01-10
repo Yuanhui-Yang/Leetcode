8f0d1e5e30237a2838ee501e1ca333c974614d91
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2020-01-10 06:02:00 -0800

    696. Count Binary Substrings

--- Algorithms/count-binary-substrings.cpp
+++ Algorithms/count-binary-substrings.cpp
@@ -1,64 +1,73 @@
-// 696. Count Binary Substrings
-// https://leetcode.com/problems/count-binary-substrings/
-
 /*
-Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+ * @lc app=leetcode id=696 lang=cpp
+ *
+ * [696] Count Binary Substrings
+ *
+ * https://leetcode.com/problems/count-binary-substrings/description/
+ *
+ * algorithms
+ * Easy (54.76%)
+ * Likes:    772
+ * Dislikes: 137
+ * Total Accepted:    38.4K
+ * Total Submissions: 70.1K
+ * Testcase Example:  '"00110"'
+ *
+ * Give a string s, count the number of non-empty (contiguous) substrings that
+ * have the same number of 0's and 1's, and all the 0's and all the 1's in
+ * these substrings are grouped consecutively. 
+ * 
+ * Substrings that occur multiple times are counted the number of times they
+ * occur.
+ * 
+ * Example 1:
+ * 
+ * Input: "00110011"
+ * Output: 6
+ * Explanation: There are 6 substrings that have equal number of consecutive
+ * 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+ * Notice that some of these substrings repeat and are counted the number of
+ * times they occur.
+ * Also, "00110011" is not a valid substring because all the 0's (and 1's) are
+ * not grouped together.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: "10101"
+ * Output: 4
+ * Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal
+ * number of consecutive 1's and 0's.
+ * 
+ * 
+ * 
+ * Note:
+ * s.length will be between 1 and 50,000.
+ * s will only consist of "0" or "1" characters.
+ * 
+ */
 
-Substrings that occur multiple times are counted the number of times they occur.
-
-Example 1:
-Input: "00110011"
-Output: 6
-Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
-
-Notice that some of these substrings repeat and are counted the number of times they occur.
-
-Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
-Example 2:
-Input: "10101"
-Output: 4
-Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
-Note:
-
-s.length will be between 1 and 50,000.
-s will only consist of "0" or "1" characters.
-*/
-
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
+// @lc code=start
 class Solution {
 public:
-	int countBinarySubstrings(string s) {
-		int result = 0, sz = s.size(), i, prev = 0, curr = 0;
-		for (i = 0; i < sz; ++i) {
-			curr = 1;
-			while (i + 1 < sz and s[i + 1] == s[i]) {
-				++i;
-				++curr;
-			}
-			result += min(prev, curr);
-			prev = curr;
-		}
-		return result;
-	}
+    int countBinarySubstrings(string s) {
+        int sz = s.size();
+        int i = 0;
+        int prev = 0;
+        int result = 0;
+        while (i < sz)
+        {
+            int curr = 0;
+            char ch = s[i];
+            while (i < sz and s[i] == ch)
+            {
+                ++i;
+                ++curr;
+            }
+            result += min(prev, curr);
+            prev = curr;
+        }
+        return result;
+    }
 };
-
-int main(void) {
-	Solution solution;
-	string s;
-	int result;
-	
-	s = "00110011";
-	result = solution.countBinarySubstrings(s);
-	cout << result << '\n';
-
-	s = "10101";
-	result = solution.countBinarySubstrings(s);
-	cout << result << '\n';
-
-	return 0;
-}
