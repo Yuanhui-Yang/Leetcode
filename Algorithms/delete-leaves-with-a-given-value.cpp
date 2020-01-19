5685a3fa46c9279cc4c4fc7d7d0ffae5c2004d1d
Yuanhui Yang <yuanhui.leslie.yang@gmail.com>
2020-01-18 22:03:47 -0800

    1325. Delete Leaves With a Given Value

---
+++ Algorithms/delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=1325 lang=cpp
+ *
+ * [1325] Delete Leaves With a Given Value
+ *
+ * https://leetcode.com/problems/delete-leaves-with-a-given-value/description/
+ *
+ * algorithms
+ * Medium (71.20%)
+ * Likes:    25
+ * Dislikes: 0
+ * Total Accepted:    3.4K
+ * Total Submissions: 4.8K
+ * Testcase Example:  '[1,2,3,2,null,2,4]\n2'
+ *
+ * Given a binary tree root and an integer target, delete all the leaf nodes
+ * with value target.
+ * 
+ * Note that once you delete a leaf node with value target, if it's parent node
+ * becomes a leaf node and has the value target, it should also be deleted (you
+ * need to continue doing that until you can't).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,2,3,2,null,2,4], target = 2
+ * Output: [1,null,3,null,4]
+ * Explanation: Leaf nodes in green with value (target = 2) are removed
+ * (Picture in left). 
+ * After removing, new nodes become leaf nodes with value (target = 2) (Picture
+ * in center).
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,3,3,3,2], target = 3
+ * Output: [1,3,null,null,2]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,2,null,2,null,2], target = 2
+ * Output: [1]
+ * Explanation: Leaf nodes in green with value (target = 2) are removed at each
+ * step.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [1,1,1], target = 1
+ * Output: []
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: root = [1,2,3], target = 1
+ * Output: [1,2,3]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= target <= 1000
+ * Each tree has at most 3000 nodes.
+ * Each node's value is between [1, 1000].
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root)
+        {
+            return root;
+        }
+        TreeNode* left = removeLeafNodes(root->left, target);
+        TreeNode* right = removeLeafNodes(root->right, target);
+        if (!left and !right and root->val == target)
+        {
+            return NULL;
+        }
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+};
+// @lc code=end
+
